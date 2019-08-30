b992bd225b39cfec52c59d29ec905d095d29689b
Piers Hollott <piers.hollott@gmail.com>
2019-08-29 19:39:29 -0700

    committed

--- prime-angular-frontend/src/app/core/services/primeapi.service.ts
+++ prime-angular-frontend/src/app/core/services/primeapi.service.ts
@@ -5,15 +5,16 @@ import { HttpClient } from "@angular/common/http";
   providedIn: "root"
 })
 export class PrimeAPIService {
-  constructor(private http: HttpClient) {}  
-  url = "http://api.optimizeprime.live/api/v1";
-  if (window.location.hostname.indexOf('localhost') > 0) {
-    url = "http://localhost:5000/api/v1";      
-  }
+  constructor(private http: HttpClient) {
+  }  
   getApplications() {
+    if (location.hostname.indexOf('localhost') > -1) {
+      this.url = "http://localhost:5000/api/v1";
+    }
     this.createApplication();
     return this.http.get(`${this.url}/application`);
   }
+  url = "http://api.optimizeprime.live/api/v1";
 
   createApplication() {
     console.log("POST!");
