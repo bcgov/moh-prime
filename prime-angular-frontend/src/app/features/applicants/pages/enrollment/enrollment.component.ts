e4e6311abce4abe2e0d68ac360d724d7c18148eb
Martin <mtpultz@gmail.com>
2019-08-30 11:42:04 -0700

    Removed margins from content, Removed login from toolbar

--- prime-angular-frontend/src/app/features/applicants/pages/enrollment/enrollment.component.ts
+++ prime-angular-frontend/src/app/features/applicants/pages/enrollment/enrollment.component.ts
@@ -1,39 +1,40 @@
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
-import { PrimeAPIService } from "src/app/core/services/primeapi.service";
-import { AuthTokenService } from "src/app/core/services/auth-token.service";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PrimeAPIService } from 'src/app/core/services/primeapi.service';
+import { AuthTokenService } from 'src/app/core/services/auth-token.service';
 
 @Component({
-  selector: "app-enrollment",
-  templateUrl: "./enrollment.component.html",
-  styleUrls: ["./enrollment.component.scss"]
+  selector: 'app-enrollment',
+  templateUrl: './enrollment.component.html',
+  styleUrls: ['./enrollment.component.scss']
 })
 export class EnrollmentComponent implements OnInit {
+  public pharmacist_prn: string;
+
   constructor(
     private router: Router,
     private primeAPIService: PrimeAPIService,
     private authTokenService: AuthTokenService
-  ) {}
-  pharmacist_prn: string;
+  ) { }
 
   submitApplication() {
     const decodedToken = this.authTokenService.decodeToken();
 
     this.primeAPIService
       .createApplication({
-        Content: "",
+        Content: '',
         ApplicantName: decodedToken.name,
         ApplicantId: decodedToken.at_hash,
         PharmacistRegistrationNumber: this.pharmacist_prn
       })
       .subscribe(res => {
         if (this.pharmacist_prn) {
-          this.router.navigate(["dashboard", "applicant", "complete"]);
+          this.router.navigate(['dashboard', 'applicant', 'complete']);
         } else {
-          this.router.navigate(["dashboard", "applicant", "inprogress"]);
+          this.router.navigate(['dashboard', 'applicant', 'inprogress']);
         }
       });
   }
 
-  ngOnInit() {}
+  ngOnInit() { }
 }
