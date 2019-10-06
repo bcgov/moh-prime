2de82a76c641991caa624aa252a48af7c4d077b7
Martin <mtpultz@gmail.com>
2019-10-06 16:22:58 -0700

    Added routing to review sub-headers

--- prime-angular-frontend/src/app/features/enrolment/pages/review/review.component.ts
+++ prime-angular-frontend/src/app/features/enrolment/pages/review/review.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-review',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
+
+  public route(route: string) {
+    this.router.navigate(['enrolment', route]);
+  }
 
   public ngOnInit() { }
 }
