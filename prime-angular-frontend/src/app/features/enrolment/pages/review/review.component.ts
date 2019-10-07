9fc4e0c68762e1046c7b7a6886b0c2812258c6ab
Martin <mtpultz@gmail.com>
2019-10-06 20:19:40 -0700

    Updated review to properly display enrolment

--- prime-angular-frontend/src/app/features/enrolment/pages/review/review.component.ts
+++ prime-angular-frontend/src/app/features/enrolment/pages/review/review.component.ts
@@ -1,20 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { LoggerService } from '@core/services/logger.service';
+import { EnrolmentStateService } from '../../shared/services/enrolment-state.service';
+
 @Component({
   selector: 'app-review',
   templateUrl: './review.component.html',
   styleUrls: ['./review.component.scss']
 })
 export class ReviewComponent implements OnInit {
+  // TODO: make a proper enrolment model
+  public enrolment: any;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private enrolmentStateService: EnrolmentStateService,
+    private logger: LoggerService
   ) { }
+
+  public declarationYesNo(declared: boolean) {
+    return (declared) ? 'Yes' : 'No';
+  }
 
   public route(route: string) {
     this.router.navigate(['enrolment', route]);
   }
 
-  public ngOnInit() { }
+  public ngOnInit() {
+    this.enrolment = this.enrolmentStateService.getEnrolment();
+
+    this.logger.info('ENROLMENT', this.enrolment);
+  }
 }
