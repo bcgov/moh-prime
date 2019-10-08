943115980e8a68c1ed839c49df9710b75f711fcf
Martin <mtpultz@gmail.com>
2019-10-08 16:36:06 -0700

    Updated enrolment resource and adapter

--- prime-angular-frontend/src/app/features/enrolment/shared/services/enrolment-resource.service.ts
+++ prime-angular-frontend/src/app/features/enrolment/shared/services/enrolment-resource.service.ts
@@ -4,9 +4,14 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+import * as moment from 'moment';
+
 import { APP_CONFIG, AppConfig } from 'app/app-config.module';
 import { LoggerService } from '@core/services/logger.service';
 import { Enrolment } from '../models/enrolment.model';
+import { Address } from '../models/address.model';
+import { EnrolmentStateService } from './enrolment-state.service';
+
 
 @Injectable({
   providedIn: 'root'
@@ -16,16 +21,21 @@ export class EnrolmentResourceService {
   constructor(
     @Inject(APP_CONFIG) private config: AppConfig,
     private http: HttpClient,
-    private logger: LoggerService
+    private logger: LoggerService,
+    // TODO: temporary enrolment data provided until JWT authentication is in place
+    private enrolmentStateService: EnrolmentStateService
   ) { }
 
   public enrolments(): Observable<Enrolment> {
-    // TODO: add endpoint /api/v1/Enrolment/1
     return this.http.get(`${this.config.apiEndpoint}/enrolments`)
       .pipe(
-        map((response: any) => {
-          const enrolment = this.enrolmentAdapterResponse(response);
-          this.logger.info('ENROLMENT', enrolment);
+        // TODO: temporary enrolment data provided until JWT authentication is in place
+        // map((enrolments: Enrolment[]) => [this.enrolmentStateService.getRawEnrolment()]),
+        map((enrolments: Enrolment[]) => {
+          this.logger.info('ENROLMENTS', enrolments);
+          const enrolment = (enrolments.length)
+            ? this.enrolmentAdapterResponse(enrolments.shift())
+            : null;
           return enrolment;
         })
       );
@@ -34,30 +44,33 @@ export class EnrolmentResourceService {
   public createEnrolment(payload: any): Observable<Enrolment> {
     return this.http.post(`${this.config.apiEndpoint}/enrolments`, this.enrolmentAdapterRequest(payload))
       .pipe(
-        map((response: any) => {
-          const enrolment = this.enrolmentAdapterResponse(response);
+        map((enrolment: Enrolment) => {
           this.logger.info('ENROLMENT', enrolment);
-          return enrolment;
+          return this.enrolmentAdapterResponse(this.enrolmentAdapterResponse(enrolment));
         })
       );
   }
 
-  public updateEnrolment(payload: any): Observable<Enrolment> {
-    return this.http.put(`${this.config.apiEndpoint}/enrolements`, this.enrolmentAdapterRequest(payload))
-      .pipe(
-        map((response: any) => {
-          const enrolment = this.enrolmentAdapterResponse(response);
-          this.logger.info('ENROLMENT', enrolment);
-          return enrolment;
-        })
-      );
+  // TODO: revisit response for an enrolment update if applicable
+  public updateEnrolment(enrolment: Enrolment): Observable<any> {
+    const { id } = enrolment;
+    return this.http.put(`${this.config.apiEndpoint}/enrolments/${id}`, this.enrolmentAdapterRequest(enrolment));
   }
 
-  private enrolmentAdapterResponse(enrolment: any): Enrolment {
+  private enrolmentAdapterResponse(enrolment: Enrolment): Enrolment {
+    if (!enrolment.enrollee.mailingAddress) {
+      enrolment.enrollee.mailingAddress = new Address();
+    }
+
     return enrolment;
   }
 
-  private enrolmentAdapterRequest(enrolment: any) {
+  private enrolmentAdapterRequest(enrolment: Enrolment): Enrolment {
+    // TODO: temporary placeholder for birthdate until JWT authorization in place
+    if (!enrolment.enrollee.dateOfBirth) {
+      enrolment.enrollee.dateOfBirth = moment().toISOString();
+    }
+
     return enrolment;
   }
 }
