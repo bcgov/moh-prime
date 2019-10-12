70735b35605d3e1030c0d923577c1c58211f1e2d
Paul Martin <poggdogg@gmail.com>
2019-10-12 10:50:57 -0700

    More tokens

--- player.sh
+++ player.sh
@@ -6,14 +6,12 @@ gitBranch=`git branch | sed -n '/\* /s///p'`
 branchName=`echo "$BRANCH_NAME" | awk '{print tolower($0)}'`
 
 
-echo "Tokens at play"
-echo "NAME=$1, VERSION/BUILD_NUMBER=$BUILD_NUMBER, branchName=$branchName, gitUrl=$gitUrl, SOURCE_CONTEXT_DIR=prime-$1, GIT_BRANCH=$GIT_BRANCH, BRANCH_NAME=$BRANCH_NAME, gitBranch=$gitBranch"
+function exposeTokens(){
+echo "NAME=$1, VERSION/BUILD_NUMBER=$BUILD_NUMBER, branchName=$branchName, gitUrl=$gitUrl, SOURCE_CONTEXT_DIR=prime-$1, GIT_BRANCH=$GIT_BRANCH, BRANCH_NAME=$BRANCH_NAME, gitBranch=$gitBranch GIT_LOCAL_BRANCH=$GIT_LOCAL_BRANCH"
+}
 
 function build(){
-    echo "Processing..."
-    echo "Tokens at play"
-    echo "NAME=$1, VERSION/BUILD_NUMBER=$BUILD_NUMBER, branchName=$branchName, gitUrl=$gitUrl, SOURCE_CONTEXT_DIR=prime-$1, GIT_BRANCH=$GIT_BRANCH, BRANCH_NAME=$BRANCH_NAME"
-    echo='oc process -f openshift/$1.bc.json -p NAME="$1" -p VERSION="$BUILD_NUMBER" -p SUFFIX="-$BRANCH_NAME" -p SOURCE_CONTEXT_DIR="prime-$1" -p SOURCE_REPOSITORY_URL="$gitUrl" -p SOURCE_REPOSITORY_REF="$gitBranch" | oc replace -f - --namespace=$licensePlate-dev'
+    exposeTokens
     oc process -f openshift/$1.bc.json \
     -p NAME="$1" \
     -p VERSION="$BUILD_NUMBER" \
@@ -25,9 +23,9 @@ function build(){
     echo "oc start-build $1-$branchName -n $licensePlate-dev"
     oc start-build $1-$branchName -n $licensePlate-dev --follow
 }
+
 function deploy(){
-    echo "Processing..."
-    echo='oc process -f openshift/$1.dc.json -p NAME="$1" -p VERSION="$BUILD_NUMBER" -p SUFFIX="$BRANCH_NAME" -p SOURCE_CONTEXT_DIR="prime-$1" -p SOURCE_REPOSITORY_URL="$gitUrl" -p SOURCE_REPOSITORY_REF="$gitBranch" | oc replace -f - --namespace=$licensePlate-dev'
+    exposeTokens
     oc process -f openshift/$1.dc.json \
     -p NAME="$1" \
     -p VERSION="$BUILD_NUMBER" \
