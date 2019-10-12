99130aa6b0fbb856f8772ebd19ef55bee7224edb
Paul Martin <poggdogg@gmail.com>
2019-10-12 11:08:46 -0700

    Found the ref in the environment

--- player.sh
+++ player.sh
@@ -2,37 +2,31 @@
 licensePlate='dqszvc'
 yamlLocation='openshift/compositions'
 gitUrl='https://github.com/bcgov/moh-prime.git'
-gitBranch=`git branch | sed -n '/\* /s///p'`
-branchName=`echo "$BRANCH_NAME" | awk '{print tolower($0)}'`
+branchName=`echo "$CHANGE_BRANCH" | awk '{print tolower($0)}'`
 
-
-function exposeTokens(){
-echo "NAME=$1, VERSION/BUILD_NUMBER=$BUILD_NUMBER, branchName=$branchName, gitUrl=$gitUrl, SOURCE_CONTEXT_DIR=prime-$1, GIT_BRANCH=$GIT_BRANCH, BRANCH_NAME=$BRANCH_NAME, gitBranch=$gitBranch GIT_LOCAL_BRANCH=$GIT_LOCAL_BRANCH"
-}
+alias exposeTokens='printenv'
 
 function build(){
-    exposeTokens
     oc process -f openshift/$1.bc.json \
     -p NAME="$1" \
     -p VERSION="$BUILD_NUMBER" \
     -p SUFFIX="$branchName" \
     -p SOURCE_CONTEXT_DIR="prime-$1" \
     -p SOURCE_REPOSITORY_URL="$gitUrl" \
-    -p SOURCE_REPOSITORY_REF="$gitBranch" | oc apply -f - --namespace=$licensePlate-dev
+    -p SOURCE_REPOSITORY_REF="$CHANGE_BRANCH" | oc apply -f - --namespace=$licensePlate-dev
     echo "Building..."
     echo "oc start-build $1-$branchName -n $licensePlate-dev"
     oc start-build $1-$branchName -n $licensePlate-dev --follow
 }
 
 function deploy(){
-    exposeTokens
     oc process -f openshift/$1.dc.json \
     -p NAME="$1" \
     -p VERSION="$BUILD_NUMBER" \
     -p SUFFIX="$branchName" \
     -p SOURCE_CONTEXT_DIR="prime-$1" \
     -p SOURCE_REPOSITORY_URL="$gitUrl" \
-    -p SOURCE_REPOSITORY_REF="$GIT_BRANCH" | oc apply -f - --namespace=$licensePlate-dev
+    -p SOURCE_REPOSITORY_REF="$CHANGE_BRANCH" | oc apply -f - --namespace=$licensePlate-dev
     echo "Building..."
     echo "oc rollout latest dc/$1-$branchName -n $licensePlate-dev"
     oc rollout latest dc/$1-$branchName -n $licensePlate-dev
