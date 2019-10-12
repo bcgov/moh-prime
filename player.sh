c701a7d1ba39af0cc929e6ebca3b3a91770995d9
Paul Martin <paul.b.martin@gov.bc.ca>
2019-10-11 17:41:42 -0700

    Trying to build in bash

--- player.sh
+++ player.sh
@@ -4,7 +4,6 @@
 export licensePlate="dqszvc"
 export yamlLocation="openshift/compositions"
 export gitUrl="https://github.com/bcgov/moh-prime.git"
-app=""
 
 function ocPush(){
     for file in `find $yamlLocation -type f -name *.yaml`
@@ -16,31 +15,12 @@ function dryRun(){
     do oc create -f $file --dry-run -o json
     done;
 }
-
 function build(){
-    oc process -f openshift/dotnet-webapi-bc.json \
+    oc process -f openshift/$1.bc.json \
     -p NAME="dotnet-webapi" \
-    -p VERSION=v1 \
+    -p VERSION={$BUILD_NUMBER} \
     -p SUFFIX="-PR-$pr" \
 	-p SOURCE_CONTEXT_DIR="prime-dotnet-webapi" \
     -p SOURCE_REPOSITORY_URL="${gitUrl}" \
-    -p SOURCE_REPOSITORY_REF="${BRANCH_NAME}" | oc apply -f - --namespace=dqszvc-dev
-
-
-oc process -f openshift/sonar-postgresql.bc.yaml \
-    -p NAME=sonarqube-postgresql \
-    -p VERSION=v1 | oc create -f -
- 
-oc process -f openshift/sonar-postgresql.dc.yaml \
-    -p NAME=sonarqube-postgresql \
-    -p DATABASE_SERVICE_NAME=sonarqube-postgresql \
-    -p IMAGE_STREAM_NAME=sonarqube-postgresql \
-    -p IMAGE_STREAM_VERSION=v1 \
-    -p POSTGRESQL_DATABASE=sonarqube \
-    -p VOLUME_CAPACITY=5Gi | oc create -f -
- 
-oc process -f openshift/sonarqube.dc.yaml \
-    -p NAME=sonarqube \
-    -p DATABASE_SERVICE_NAME=sonarqube-postgresql \
-    -p DATABASE_NAME=sonarqube \
-    -p VERSION=v1 | oc create -f -
+    -p SOURCE_REPOSITORY_REF="${BRANCH_NAME}" | oc apply -f - --namespace=$licensePlate-dev
+}
