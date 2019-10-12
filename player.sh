0744d742909da720ed976ae2498724d41a45ba6a
Paul Martin <paul.b.martin@gov.bc.ca>
2019-10-11 21:29:17 -0700

    trying to get branches distinct

--- player.sh
+++ player.sh
@@ -8,7 +8,7 @@ function build(){
     echo "Processing..."
     echo='oc process -f openshift/$1.bc.json -p NAME="$1" -p VERSION="$BUILD_NUMBER" -p SUFFIX="-$BRANCH_NAME" -p SOURCE_CONTEXT_DIR="prime-$1" -p SOURCE_REPOSITORY_URL="$gitUrl" -p SOURCE_REPOSITORY_REF="$gitBranch" | oc replace -f - --namespace=$licensePlate-dev'
     oc process -f openshift/$1.bc.json \
-    -p NAME="$1" \
+    -p NAME="$1-$BRANCH_NAME" \
     -p VERSION="$BUILD_NUMBER" \
     -p SUFFIX="-$BRANCH_NAME" \
     -p SOURCE_CONTEXT_DIR="prime-$1" \
