0f0feae9207f849f41e9c8d155e26b010102b066
Paul Martin <paul.b.martin@gov.bc.ca>
2019-10-11 19:14:39 -0700

    Null characters were breaking, builds not work

--- player.sh
+++ player.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
-
 licensePlate='dqszvc'
 yamlLocation='openshift/compositions'
 gitUrl='https://github.com/bcgov/moh-prime.git'
-app=''
 
 function build(){
-    oc process -f openshift/$app.bc.json \
-    -p NAME="$app" \
+    oc process -f openshift/$1.bc.json \
+    -p NAME="$1" \
     -p VERSION="$BUILD_NUMBER" \
     -p SUFFIX="-$BRANCH_NAME" \
-    -p SOURCE_CONTEXT_DIR="prime-$app" \
+    -p SOURCE_CONTEXT_DIR="prime-$1" \
     -p SOURCE_REPOSITORY_URL="$gitUrl" \
     -p SOURCE_REPOSITORY_REF="develop" | oc apply -f - --namespace=$licensePlate-dev
 }
 
 case "$1" in
     build)
-        build
+        build $2
         ;;
     deploy)
         deploy
@@ -30,6 +28,4 @@ case "$1" in
         ;;
     *)
     echo "Usage: $0 {build|deploy|sonar|zap|promote} <app> "
-
-
 esac
