90875c522d8704b9b47a5897a15dc2a1b29333fe
Paul Martin <poggdogg@gmail.com>
2019-10-12 08:01:37 -0700

    exposing token values

--- player.sh
+++ player.sh
@@ -5,8 +5,14 @@ gitUrl='https://github.com/bcgov/moh-prime.git'
 gitBranch=`git branch | sed -n '/\* /s///p'`
 branchName=`echo "$BRANCH_NAME" | awk '{print tolower($0)}'`
 
+
+echo "Tokens at play"
+echo "NAME=$1, VERSION/BUILD_NUMBER=$BUILD_NUMBER, branchName=$branchName, gitUrl=$gitUrl, SOURCE_CONTEXT_DIR=prime-$1, GIT_BRANCH=$GIT_BRANCH, BRANCH_NAME=$BRANCH_NAME"
+
 function build(){
     echo "Processing..."
+    echo "Tokens at play"
+    echo "NAME=$1, VERSION/BUILD_NUMBER=$BUILD_NUMBER, branchName=$branchName, gitUrl=$gitUrl, SOURCE_CONTEXT_DIR=prime-$1, GIT_BRANCH=$GIT_BRANCH, BRANCH_NAME=$BRANCH_NAME"
     echo='oc process -f openshift/$1.bc.json -p NAME="$1" -p VERSION="$BUILD_NUMBER" -p SUFFIX="-$BRANCH_NAME" -p SOURCE_CONTEXT_DIR="prime-$1" -p SOURCE_REPOSITORY_URL="$gitUrl" -p SOURCE_REPOSITORY_REF="$gitBranch" | oc replace -f - --namespace=$licensePlate-dev'
     oc process -f openshift/$1.bc.json \
     -p NAME="$1" \
