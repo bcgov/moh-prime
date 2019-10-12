251f52f606d71bc22251ce2ea295601c3d38ce83
Paul Martin <paul.b.martin@gov.bc.ca>
2019-10-11 20:52:11 -0700

    Bringing in deployment config

--- player.sh
+++ player.sh
@@ -3,7 +3,10 @@ licensePlate='dqszvc'
 yamlLocation='openshift/compositions'
 gitUrl='https://github.com/bcgov/moh-prime.git'
 gitBranch=`git rev-parse --abbrev-ref HEAD`
+
 function build(){
+    echo "Processing..."
+    echo='oc process -f openshift/$1.bc.json -p NAME="$1" -p VERSION="$BUILD_NUMBER" -p SUFFIX="-$BRANCH_NAME" -p SOURCE_CONTEXT_DIR="prime-$1" -p SOURCE_REPOSITORY_URL="$gitUrl" -p SOURCE_REPOSITORY_REF="$gitBranch" | oc replace -f - --namespace=$licensePlate-dev'
     oc process -f openshift/$1.bc.json \
     -p NAME="$1" \
     -p VERSION="$BUILD_NUMBER" \
@@ -11,6 +14,8 @@ function build(){
     -p SOURCE_CONTEXT_DIR="prime-$1" \
     -p SOURCE_REPOSITORY_URL="$gitUrl" \
     -p SOURCE_REPOSITORY_REF="$gitBranch" | oc replace -f - --namespace=$licensePlate-dev
+    echo "Building..."
+    echo "oc start-build $1 -n $licensePlate-dev"
     oc start-build $1 -n $licensePlate-dev
 }
 
