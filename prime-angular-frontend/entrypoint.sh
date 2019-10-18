7dbd5651bff3b8fa6484f727f2ea2c08bf094421
Paul Martin <paul.b.martin@gov.bc.ca>
2019-10-18 15:58:31 -0700

    gorram quotes

--- prime-angular-frontend/entrypoint.sh
+++ prime-angular-frontend/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-envsubst '$branchName' < /etc/nginx/nginx.template.conf > /etc/nginx/conf.d/default.conf; 
-"nginx -g daemon-off;"
+envsubst '$branchName' < /etc/nginx/nginx.template.conf > /etc/nginx/conf.d/default.conf && \
+nginx -g "daemon-off;"
