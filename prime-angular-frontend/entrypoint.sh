a9036cc827e056ad02085502d9395bf8413d1604
Paul Martin <paul.b.martin@gov.bc.ca>
2019-10-18 14:56:57 -0700

    different paths

--- prime-angular-frontend/entrypoint.sh
+++ prime-angular-frontend/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-envsubst '$branchName' < /etc/nginx/conf.d/nginx.template.conf > /etc/nginx/conf.d/default.conf; 
+envsubst '$branchName' < /etc/nginx/nginx.template.conf > /etc/nginx/conf.d/default.conf; 
 nginx -g daemon-off;
