6375fd048399540d3c658208ffdd317e0fc605aa
Paul Martin <poggdogg@gmail.com>
2019-10-23 16:27:36 -0700

    more debug

--- prime-angular-frontend/entrypoint.sh
+++ prime-angular-frontend/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 wall "SUFFIX=$SUFFIX"
+echo "SUFFIX=$SUFFIX" > /tmp/suffix.out
 envsubst '$SUFFIX' < /etc/nginx/nginx.template.conf > /etc/nginx/conf.d/default.conf 
 nginx
 tail -f /dev/null
