5f76f135c0f3ef8896520c6a7967ca62c6f79ae7
Paul Martin <poggdogg@gmail.com>
2019-10-23 13:10:10 -0700

    adding in token replacement for arg

--- player.sh
+++ player.sh
@@ -28,6 +28,7 @@ function cleanPR(){
 # This takes in Git, Jenkins and system variables to the template that will be processed.
 function ocApply() {
     echo "ocApply..."
+    export OC_APP="$3"
     if [ "$1" == "build" ];
     then 
     configType="bc"
@@ -48,7 +49,7 @@ function ocApply() {
     -p SOURCE_REPOSITORY_URL="$gitUrl" \
     -p SOURCE_REPOSITORY_REF="$CHANGE_BRANCH"  \
     -p OC_NAMESPACE="$licensePlate" \
-    -p OC_APP="$3" | oc apply -f - --namespace=$licensePlate-$3
+    -p OC_APP="$OC_APP" | oc apply -f - --namespace=$licensePlate-$OC_APP 
     $ocAction
 }
 
