b6f1281f8b876bd13a89db5ba94290de6df24469
Paul Martin <poggdogg@gmail.com>
2019-10-24 16:29:39 -0700

    sed, remarking envsubst

--- prime-angular-frontend/midpoint.sh
+++ prime-angular-frontend/midpoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-echo "REDIRECT_URL = $REDIRECT_URL"
-envsubst '$REDIRECT_URL' < /usr/src/app/src/environments/environment.prod.template.ts > //usr/src/app/src/environments/environment.prod.ts
+function replaceString() {
+sed s/$1/$2/g  /usr/src/app/src/environments/environment.prod.template.ts > /usr/src/app/src/environments/environment.prod.ts
+}
+replaceString '$REDIRECT_URL' "$REDIRECT_URL"
+# echo "REDIRECT_URL = $REDIRECT_URL"
+# echo "sed "
+# envsubst '$REDIRECT_URL' < /usr/src/app/src/environments/environment.prod.template.ts > /usr/src/app/src/environments/environment.prod.ts
 # Substitutes the environmental URL
