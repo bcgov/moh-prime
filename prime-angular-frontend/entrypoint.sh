ca4744fbee076de1a9e542802d39d32fbd090098
Paul Martin <poggdogg@gmail.com>
2019-10-23 17:12:36 -0700

    removing some debug, final commit

--- prime-angular-frontend/entrypoint.sh
+++ prime-angular-frontend/entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-wall "SUFFIX=$SUFFIX"
-echo "SUFFIX=$SUFFIX" > /tmp/suffix.out
 envsubst '$SUFFIX' < /etc/nginx/nginx.template.conf > /etc/nginx/conf.d/default.conf 
 nginx
 tail -f /dev/null
