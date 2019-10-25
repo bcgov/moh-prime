db4a2c916bf4fbbece8ed14e69f4680f795cf693
mardiel <6422508+mardiel@users.noreply.github.com>
2019-10-25 09:39:17 -0700

    adding environment value for login recirect url (#36)
    * adding environment value for login recirect url
    
    * Injecting production URL before build
    
    * cleanup, missing values
    
    * removing silence from npm install
    
    * probably the final subsititution
    
    * locating calling bash script
    
    * removed/fixed debugging value
    
    * Revert "removed/fixed debugging value"
    
    This reverts commit 72f384bd587bb9c10e13f288d2392f1f55d39447.
    
    * removed/fixed debugging value
    
    * Hate not using absolute paths
    
    * adding executable bit and path
    
    * removing pwd
    
    * using sed, removing some quotes
    
    * sed, remarking envsubst
    
    * removing envsubst
    
    * trying to get simpler still
    
    * adding some debug
    
    * just angular for now
    
    * debugging
    
    * trying full inline
    
    * trying different strategies
    
    * openshift assumed dockerfile wrong
    
    * more debug
    
    * more env

--- prime-angular-frontend/src/app/modules/auth/pages/info/info.component.ts
+++ prime-angular-frontend/src/app/modules/auth/pages/info/info.component.ts
@@ -1,6 +1,8 @@
+import { Injectable, Inject } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { KeycloakService } from 'keycloak-angular';
 import { Router } from '@angular/router';
+import { APP_CONFIG, AppConfig } from 'app/app-config.module';
 
 @Component({
   selector: 'app-info',
@@ -9,9 +11,12 @@ import { Router } from '@angular/router';
 })
 export class InfoComponent implements OnInit {
 
-  constructor(private keycloakService: KeycloakService, router: Router) {
+  constructor(
+    @Inject(APP_CONFIG) private config: AppConfig,
+    private keycloakService: KeycloakService,
+    router: Router) {
     if (keycloakService.getUserRoles().includes('prime_user')) {
-      router.navigate(['/enrolment/profile'])
+      router.navigate(['/enrolment/profile']);
     }
   }
 
@@ -21,7 +26,7 @@ export class InfoComponent implements OnInit {
   public bcscLogin() {
     this.keycloakService.login({
       idpHint: 'bcsc',
-      redirectUri: 'http://localhost:4200'
-    })
+      redirectUri: this.config.loginRedirectUrl
+    });
   }
 }
