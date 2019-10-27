8aae03cb48b6555598e48dc8074209b8a5d63367
Paul Martin <paul.b.martin@gov.bc.ca>
2019-10-26 18:18:04 -0700

    added population to vars

--- project.bash
+++ project.bash
@@ -1,3 +1,20 @@
 export PROJECT_PREFIX='dqszvc'
 export GIT_URL='https://github.com/bcgov/moh-prime.git'
 export BRANCH_LOWER=$(echo "$BRANCH_NAME" | tr '[:upper:]' '[:lower:]') 
+function variablePopulation() {
+    if [ "$1" == "build" ];
+    then 
+    configType="bc"
+    elif [ "$1" == "deploy" ];
+    then 
+    configType="dc"
+    fi
+    if [ "${BRANCH_LOWER}" == "develop" ] || [ "${BRANCH_LOWER}" == "master" ];
+    then 
+    SUFFIX=""
+    CHANGE_BRANCH="$BRANCH_NAME"
+    else 
+    SUFFIX="-${BRANCH_LOWER}";
+    fi
+}
+variablePopulation
