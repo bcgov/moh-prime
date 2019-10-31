002f2d70b3bbb211941d85b041bad565f9a1bfd2
Paul Martin <poggdogg@gmail.com>
2019-10-30 22:24:25 -0700

    more changes

--- sonarqube/sonarQube.cmd
+++ sonarqube/sonarQube.cmd
@@ -1,11 +1,13 @@
+dotnet build
+coverlet .\prime-dotnet-webapi-tests\bin\Debug\netcoreapp2.2\PrimeTests.dll --target "dotnet" --targetargs "test .\prime-dotnet-webapi-tests --no-build --logger "trx;LogFileName=TestResults.trx" --logger "xunit;LogFileName=TestResults.xml" --results-directory ./BuildReports/UnitTests" -f opencover -o ./BuildReports/Coverage/coverage
+dotnet build-server shutdown
+dotnet sonarscanner begin /k:"prime-dotnet-webapi" /d:sonar.host.url=http://sonarqube-dqszvc-tools.pathfinder.gov.bc.ca:9000 /d:sonar.cs.opencover.reportsPaths="./BuildReports/Coverage/coverage.opencover.xml" /d:sonar.coverage.exclusions="**Tests*.cs","**/Migrations/*","**/Program.cs" /d:sonar.cpd.exclusions="**/Migrations/*" /d:sonar.cs.vstest.reportsPaths="./BuildReports/UnitTests/TestResults.trx" /d:sonar.cs.nunit.reportsPaths="./BuildReports/UnitTests/TestResults.xml"
+dotnet build
+dotnet sonarscanner end
+
+
 rem https://docs.sonarqube.org/latest/analysis/scan/sonarscanner-for-msbuild/
 rem https://medium.com/agilix/collecting-test-coverage-using-coverlet-and-sonarqube-for-a-net-core-project-ef4a507d4b28
 rem docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube
 rem dotnet tool install --global coverlet.console --version 1.6.0
 rem dotnet tool install --global dotnet-sonarscanner --version 4.3.1
-dotnet build
-coverlet .\prime-dotnet-webapi-tests\bin\Debug\netcoreapp2.2\PrimeTests.dll --target "dotnet" --targetargs "test .\prime-dotnet-webapi-tests --no-build --logger "trx;LogFileName=TestResults.trx" --logger "xunit;LogFileName=TestResults.xml" --results-directory ./BuildReports/UnitTests" -f opencover -o ./BuildReports/Coverage/coverage
-dotnet build-server shutdown
-dotnet sonarscanner begin /k:"prime-dotnet-webapi" /d:sonar.host.url=http://localhost:9000 /d:sonar.cs.opencover.reportsPaths="./BuildReports/Coverage/coverage.opencover.xml" /d:sonar.coverage.exclusions="**Tests*.cs","**/Migrations/*","**/Program.cs" /d:sonar.cpd.exclusions="**/Migrations/*" /d:sonar.cs.vstest.reportsPaths="./BuildReports/UnitTests/TestResults.trx" /d:sonar.cs.nunit.reportsPaths="./BuildReports/UnitTests/TestResults.xml"
-dotnet build
-dotnet sonarscanner end
