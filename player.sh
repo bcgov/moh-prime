3fa4b821c42dabe730fa4a1db573b77c794e3979
Paul Martin <poggdogg@gmail.com>
2019-10-31 23:15:33 -0700

    bash needed

--- player.sh
+++ player.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-source project.conf
+#!/bin/bash
+source ./project.conf
 #export PROJECT_PREFIX="dqszvc"
 #export GIT_URL='https://github.com/bcgov/moh-prime.git'
 #export BRANCH_LOWER=`echo "${BRANCH_NAME}" | awk '{print tolower($0)}'`
