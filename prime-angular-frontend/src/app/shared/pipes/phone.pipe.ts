df5f8f3beb104f093a9c710586444c3321e6a7f7
James Hollinger <39168456+james-hollinger@users.noreply.github.com>
2019-11-01 15:43:17 -0700

    Added guard to phone pipe (#59)

--- prime-angular-frontend/src/app/shared/pipes/phone.pipe.ts
+++ prime-angular-frontend/src/app/shared/pipes/phone.pipe.ts
@@ -5,6 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class PhonePipe implements PipeTransform {
   transform(phoneNumber: string): any {
+    if (!phoneNumber) {
+      return phoneNumber;
+    }
+
     phoneNumber = phoneNumber.replace(/[\D]/g, '');
     const length = phoneNumber.length;
     const areaCode = phoneNumber.slice(length - 10, length - 7);
