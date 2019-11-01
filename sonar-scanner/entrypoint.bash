c12c0f8150f94c37d69dc6389d78d8f8d0b0cfb0
Paul Martin <poggdogg@gmail.com>
2019-10-31 22:46:13 -0700

    lots of cleanup

--- sonar-scanner/entrypoint.bash
+++ sonar-scanner/entrypoint.bash
@@ -2,4 +2,5 @@
 git clone git://github.com/bcgov/moh-prime.git
 cd moh-prime
 git checkout ${BRANCH_NAME}
-source sonar-scanner/.bash_profile
+sed -i 's/\r$//' sonar-scanner/sonar-runner.bash
+source sonar-scanner/sonar-runner.bash
