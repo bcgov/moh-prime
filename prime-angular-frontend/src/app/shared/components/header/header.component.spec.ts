769fbe305f77896d1238abaf1c45b9f00002509d
Martin <mtpultz@gmail.com>
2019-11-02 23:22:57 -0700

    Fixed header component spect

--- prime-angular-frontend/src/app/shared/components/header/header.component.spec.ts
+++ prime-angular-frontend/src/app/shared/components/header/header.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
 import { NgxMaterialModule } from '@shared/modules/ngx-material/ngx-material.module';
+import { NgxProgressModule } from '@shared/modules/ngx-progress/ngx-progress.module';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -11,7 +12,8 @@ describe('HeaderComponent', () => {
     TestBed.configureTestingModule(
       {
         imports: [
-          NgxMaterialModule
+          NgxMaterialModule,
+          NgxProgressModule
         ],
         declarations: [
           HeaderComponent
@@ -26,7 +28,7 @@ describe('HeaderComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create HeaderComponent', () => {
+  it('should create component', () => {
     expect(component).toBeTruthy();
   });
 });
