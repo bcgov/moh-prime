ae4c83fe455175d33d604e87994ab301cbba3678
Martin <mtpultz@gmail.com>
2019-11-02 23:31:32 -0700

    Fixed error handler interceptor spec

--- prime-angular-frontend/src/app/core/interceptors/error-handler.interceptor.spec.ts
+++ prime-angular-frontend/src/app/core/interceptors/error-handler.interceptor.spec.ts
@@ -1,9 +1,28 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { MockAuthService } from 'test/mocks/mock-auth.service';
 
 import { ErrorHandlerInterceptor } from './error-handler.interceptor';
+import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
+import { AuthService } from '@auth/shared/services/auth.service';
 
 describe('ErrorHandlerInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      RouterTestingModule
+    ],
+    providers: [
+      {
+        provide: APP_CONFIG,
+        useValue: APP_DI_CONFIG
+      },
+      {
+        provide: AuthService,
+        useValue: MockAuthService
+      }
+    ]
+  }));
 
   it('should be created', () => {
     const service: ErrorHandlerInterceptor = TestBed.get(ErrorHandlerInterceptor);
