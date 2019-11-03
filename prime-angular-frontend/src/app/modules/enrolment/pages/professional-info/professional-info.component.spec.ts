290e4c0a7bdf2304251ad3eb8cb3ae730f831606
Martin <mtpultz@gmail.com>
2019-11-02 21:46:41 -0700

    Fixed professional info component spec

--- prime-angular-frontend/src/app/modules/enrolment/pages/professional-info/professional-info.component.spec.ts
+++ prime-angular-frontend/src/app/modules/enrolment/pages/professional-info/professional-info.component.spec.ts
@@ -1,16 +1,47 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { MockConfigService } from 'test/mocks/mock-config.service';
 
 import { ProfessionalInfoComponent } from './professional-info.component';
+import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
+import { ConfigService } from '@config/config.service';
+import { NgxMaterialModule } from '@shared/modules/ngx-material/ngx-material.module';
+import { SubHeaderComponent } from '@shared/components/sub-header/sub-header.component';
+import { CollegeCertificationsComponent } from '@enrolment/shared/components/college-certifications/college-certifications.component';
 
 describe('ProfessionalInfoComponent', () => {
   let component: ProfessionalInfoComponent;
   let fixture: ComponentFixture<ProfessionalInfoComponent>;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ProfessionalInfoComponent ]
-    })
-    .compileComponents();
+    TestBed.configureTestingModule(
+      {
+        imports: [
+          HttpClientTestingModule,
+          NgxMaterialModule,
+          ReactiveFormsModule,
+          RouterTestingModule
+        ],
+        declarations: [
+          ProfessionalInfoComponent,
+          SubHeaderComponent,
+          CollegeCertificationsComponent
+        ],
+        providers: [
+          {
+            provide: APP_CONFIG,
+            useValue: APP_DI_CONFIG
+          },
+          {
+            provide: ConfigService,
+            useValue: MockConfigService
+          }
+        ]
+      }
+    ).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +50,7 @@ describe('ProfessionalInfoComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create ProfessionalInfoComponent', () => {
     expect(component).toBeTruthy();
   });
 });
