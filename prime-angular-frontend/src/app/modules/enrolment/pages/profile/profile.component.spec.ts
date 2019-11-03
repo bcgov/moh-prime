c8baf9b1910fad1d1b1ae65f811fc9ba07390c31
Martin <mtpultz@gmail.com>
2019-11-02 21:46:28 -0700

    Fixed profile component spec

--- prime-angular-frontend/src/app/modules/enrolment/pages/profile/profile.component.spec.ts
+++ prime-angular-frontend/src/app/modules/enrolment/pages/profile/profile.component.spec.ts
@@ -1,16 +1,54 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { KeycloakService } from 'keycloak-angular';
+
+import { MockConfigService } from 'test/mocks/mock-config.service';
+import { MockKeycloakService } from 'test/mocks/mock-keycloak.service';
 
 import { ProfileComponent } from './profile.component';
+import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
+import { ConfigService } from '@config/config.service';
+import { NgxMaterialModule } from '@shared/modules/ngx-material/ngx-material.module';
+import { SubHeaderComponent } from '@shared/components/sub-header/sub-header.component';
 
 describe('ProfileComponent', () => {
   let component: ProfileComponent;
   let fixture: ComponentFixture<ProfileComponent>;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ProfileComponent ]
-    })
-    .compileComponents();
+    TestBed.configureTestingModule(
+      {
+        imports: [
+          BrowserAnimationsModule,
+          HttpClientTestingModule,
+          NgxMaterialModule,
+          ReactiveFormsModule,
+          RouterTestingModule
+        ],
+        declarations: [
+          ProfileComponent,
+          SubHeaderComponent
+        ],
+        providers: [
+          {
+            provide: APP_CONFIG,
+            useValue: APP_DI_CONFIG
+          },
+          {
+            provide: ConfigService,
+            useValue: MockConfigService
+          },
+          {
+            provide: KeycloakService,
+            useValue: MockKeycloakService
+          }
+        ]
+      }
+    ).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +57,7 @@ describe('ProfileComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create ProfileComponent', () => {
     expect(component).toBeTruthy();
   });
 });
