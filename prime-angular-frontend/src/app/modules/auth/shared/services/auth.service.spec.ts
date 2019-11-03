3ce51aab910eb15e154f3c9af51aac4c94d1d228
Martin <mtpultz@gmail.com>
2019-11-02 23:32:40 -0700

    Fixed auth service spec

--- prime-angular-frontend/src/app/modules/auth/shared/services/auth.service.spec.ts
+++ prime-angular-frontend/src/app/modules/auth/shared/services/auth.service.spec.ts
@@ -1,9 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 
+import { KeycloakService } from 'keycloak-angular';
+
+import { MockKeycloakService } from 'test/mocks/mock-keycloak.service';
+
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      {
+        provide: KeycloakService,
+        useValue: MockKeycloakService
+      }
+    ]
+  }));
 
   it('should be created', () => {
     const service: AuthService = TestBed.get(AuthService);
