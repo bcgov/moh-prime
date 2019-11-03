35343887203fdbba3cb50842a86ae5f28e5e6307
Martin <mtpultz@gmail.com>
2019-11-02 21:46:15 -0700

    Fixed review component spec

--- prime-angular-frontend/src/app/modules/enrolment/pages/review/review.component.spec.ts
+++ prime-angular-frontend/src/app/modules/enrolment/pages/review/review.component.spec.ts
@@ -1,16 +1,48 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ReviewComponent } from './review.component';
+import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
+import { NgxMaterialModule } from '@shared/modules/ngx-material/ngx-material.module';
+import { SubHeaderComponent } from '@shared/components/sub-header/sub-header.component';
+import { ConfigCodePipe } from '@config/config-code.pipe';
+import { FormatDatePipe } from '@shared/pipes/format-date.pipe';
+import { PhonePipe } from '@shared/pipes/phone.pipe';
+import { PostalPipe } from '@shared/pipes/postal.pipe';
+import { EnrolmentPipe } from '@shared/pipes/enrolment.pipe';
 
 describe('ReviewComponent', () => {
   let component: ReviewComponent;
   let fixture: ComponentFixture<ReviewComponent>;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ReviewComponent ]
-    })
-    .compileComponents();
+    TestBed.configureTestingModule(
+      {
+        imports: [
+          HttpClientTestingModule,
+          NgxMaterialModule,
+          ReactiveFormsModule,
+          RouterTestingModule
+        ],
+        declarations: [
+          ReviewComponent,
+          SubHeaderComponent,
+          ConfigCodePipe,
+          EnrolmentPipe,
+          FormatDatePipe,
+          PhonePipe,
+          PostalPipe
+        ],
+        providers: [
+          {
+            provide: APP_CONFIG,
+            useValue: APP_DI_CONFIG
+          }
+        ]
+      }
+    ).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +51,7 @@ describe('ReviewComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create ReviewComponent', () => {
     expect(component).toBeTruthy();
   });
 });
