3e191ea3eb5c6f430d989f15c3715f0eebc4a063
Martin <mtpultz@gmail.com>
2019-11-02 20:03:36 -0700

    Fixed app component spec

--- prime-angular-frontend/src/app/app.component.spec.ts
+++ prime-angular-frontend/src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'angular-frontend'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-frontend');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-frontend app is running!');
-  });
 });
