0039d97fbf14c53d571701dd2862a8d9d03367d6
Martin <mtpultz@gmail.com>
2019-11-02 20:59:00 -0700

    Fixed contact component spec

--- prime-angular-frontend/src/app/modules/enrolment/pages/contact/contact.component.spec.ts
+++ prime-angular-frontend/src/app/modules/enrolment/pages/contact/contact.component.spec.ts
@@ -1,16 +1,43 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { NgxMaskModule } from 'ngx-mask';
 
 import { ContactComponent } from './contact.component';
+import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
+import { NgxMaterialModule } from '@shared/modules/ngx-material/ngx-material.module';
+import { SubHeaderComponent } from '@shared/components/sub-header/sub-header.component';
 
 describe('ContactComponent', () => {
   let component: ContactComponent;
   let fixture: ComponentFixture<ContactComponent>;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ContactComponent ]
-    })
-    .compileComponents();
+    TestBed.configureTestingModule(
+      {
+        imports: [
+          BrowserAnimationsModule,
+          HttpClientTestingModule,
+          NgxMaskModule.forRoot(),
+          NgxMaterialModule,
+          ReactiveFormsModule,
+          RouterTestingModule
+        ],
+        declarations: [
+          ContactComponent,
+          SubHeaderComponent
+        ],
+        providers: [
+          {
+            provide: APP_CONFIG,
+            useValue: APP_DI_CONFIG
+          }
+        ]
+      }
+    ).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +46,7 @@ describe('ContactComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create ContactComponent', () => {
     expect(component).toBeTruthy();
   });
 });
