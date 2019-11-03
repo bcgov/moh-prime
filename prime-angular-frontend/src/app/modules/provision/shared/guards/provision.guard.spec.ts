8df5e23e996bac7d11406bcc3c8d3fc9ed4f12c7
Martin <mtpultz@gmail.com>
2019-11-02 23:24:23 -0700

    Fixed provision guard spec

--- prime-angular-frontend/src/app/modules/provision/shared/guards/provision.guard.spec.ts
+++ prime-angular-frontend/src/app/modules/provision/shared/guards/provision.guard.spec.ts
@@ -1,15 +1,34 @@
 import { TestBed, async, inject } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { KeycloakService } from 'keycloak-angular';
+
+import { MockKeycloakService } from 'test/mocks/mock-keycloak.service';
 
 import { ProvisionGuard } from './provision.guard';
+import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
 
 describe('ProvisionGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ProvisionGuard]
+      imports: [
+        RouterTestingModule
+      ],
+      providers: [
+        ProvisionGuard,
+        {
+          provide: APP_CONFIG,
+          useValue: APP_DI_CONFIG
+        },
+        {
+          provide: KeycloakService,
+          useValue: MockKeycloakService
+        }
+      ]
     });
   });
 
-  it('should ...', inject([ProvisionGuard], (guard: ProvisionGuard) => {
+  it('should be injected', inject([ProvisionGuard], (guard: ProvisionGuard) => {
     expect(guard).toBeTruthy();
   }));
 });
