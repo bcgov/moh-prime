62b93f802a9a2d53bc7a6ac4c7b149621f36920b
Martin <mtpultz@gmail.com>
2019-11-02 23:31:46 -0700

    Fixed authentication guard spec

--- prime-angular-frontend/src/app/modules/auth/shared/guards/authentication.guard.spec.ts
+++ prime-angular-frontend/src/app/modules/auth/shared/guards/authentication.guard.spec.ts
@@ -1,15 +1,34 @@
 import { TestBed, async, inject } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { KeycloakService } from 'keycloak-angular';
+
+import { MockKeycloakService } from 'test/mocks/mock-keycloak.service';
 
 import { AuthenticationGuard } from './authentication.guard';
+import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
 
 describe('AuthenticationGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AuthenticationGuard]
+      imports: [
+        RouterTestingModule
+      ],
+      providers: [
+        AuthenticationGuard,
+        {
+          provide: APP_CONFIG,
+          useValue: APP_DI_CONFIG
+        },
+        {
+          provide: KeycloakService,
+          useValue: MockKeycloakService
+        }
+      ]
     });
   });
 
-  it('should ...', inject([AuthenticationGuard], (guard: AuthenticationGuard) => {
+  it('should be injected', inject([AuthenticationGuard], (guard: AuthenticationGuard) => {
     expect(guard).toBeTruthy();
   }));
 });
