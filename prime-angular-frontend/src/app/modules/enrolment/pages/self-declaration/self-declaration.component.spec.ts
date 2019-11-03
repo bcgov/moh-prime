54ee58ac1e9a1e8cfb41fc8e200a6934de14d710
Martin <mtpultz@gmail.com>
2019-11-02 20:58:48 -0700

    Fixed self declaration component spec

--- prime-angular-frontend/src/app/modules/enrolment/pages/self-declaration/self-declaration.component.spec.ts
+++ prime-angular-frontend/src/app/modules/enrolment/pages/self-declaration/self-declaration.component.spec.ts
@@ -1,16 +1,38 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SelfDeclarationComponent } from './self-declaration.component';
+import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
+import { NgxMaterialModule } from '@shared/modules/ngx-material/ngx-material.module';
+import { SubHeaderComponent } from '@shared/components/sub-header/sub-header.component';
 
 describe('SelfDeclarationComponent', () => {
   let component: SelfDeclarationComponent;
   let fixture: ComponentFixture<SelfDeclarationComponent>;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SelfDeclarationComponent ]
-    })
-    .compileComponents();
+    TestBed.configureTestingModule(
+      {
+        imports: [
+          HttpClientTestingModule,
+          NgxMaterialModule,
+          ReactiveFormsModule,
+          RouterTestingModule
+        ],
+        declarations: [
+          SelfDeclarationComponent,
+          SubHeaderComponent
+        ],
+        providers: [
+          {
+            provide: APP_CONFIG,
+            useValue: APP_DI_CONFIG
+          }
+        ]
+      }
+    ).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +41,7 @@ describe('SelfDeclarationComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create SelfDeclarationComponent', () => {
     expect(component).toBeTruthy();
   });
 });
