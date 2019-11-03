f8173d0422e74b05ba21ba3987c2f2a7c64620c3
Martin <mtpultz@gmail.com>
2019-11-02 23:32:31 -0700

    Fixed authorization redirect guard spec

--- prime-angular-frontend/src/app/modules/auth/shared/guards/authorization-redirect.guard.spec.ts
+++ prime-angular-frontend/src/app/modules/auth/shared/guards/authorization-redirect.guard.spec.ts
@@ -1,15 +1,34 @@
 import { TestBed, async, inject } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { KeycloakService } from 'keycloak-angular';
+
+import { MockKeycloakService } from 'test/mocks/mock-keycloak.service';
 
 import { AuthorizationRedirectGuard } from './authorization-redirect.guard';
+import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
 
 describe('AuthorizationRedirectGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AuthorizationRedirectGuard]
+      imports: [
+        RouterTestingModule
+      ],
+      providers: [
+        AuthorizationRedirectGuard,
+        {
+          provide: APP_CONFIG,
+          useValue: APP_DI_CONFIG
+        },
+        {
+          provide: KeycloakService,
+          useValue: MockKeycloakService
+        }
+      ]
     });
   });
 
-  it('should ...', inject([AuthorizationRedirectGuard], (guard: AuthorizationRedirectGuard) => {
+  it('should be injected', inject([AuthorizationRedirectGuard], (guard: AuthorizationRedirectGuard) => {
     expect(guard).toBeTruthy();
   }));
 });
