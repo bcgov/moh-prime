445f1cbf7d2140dbaa8806abe9b91b1e0c7249f6
Paul Martin <poggdogg@gmail.com>
2019-11-04 14:59:32 -0800

    going to base image

--- sonar-scanner/sonar-runner.bash
+++ sonar-scanner/sonar-runner.bash
@@ -5,17 +5,9 @@ function dotnetTests()
     dotnet build 
     coverlet "./prime-dotnet-webapi-tests/bin/Debug/netcoreapp2.2/PrimeTests.dll" --target "dotnet" --targetargs "test . --no-build --logger "trx;LogFileName=TestResults.trx" --logger "xunit;LogFileName=TestResults.xml" --results-directory ./BuildReports/UnitTests" -f opencover -o ./BuildReports/Coverage/coverage 
     dotnet build-server shutdown 
-    dotnet sonarscanner begin \ 
-        /k:"prime-web-api" \ 
-        /d:sonar.host.url=http://sonar-backend-dqszvc-tools.pathfinder.gov.bc.ca \ 
-        /d:sonar.cs.opencover.reportsPaths="./BuildReports/Coverage/coverage.opencover.xml" \
-        /d:sonar.exclusions="**/Migrations/*" \ 
-        /d:sonar.coverage.exclusions="**Tests*.cs","**/Migrations/*","**/Program.cs" \
-        /d:sonar.cpd.exclusions="**/Migrations/*" \ 
-        /d:sonar.cs.vstest.reportsPaths="./BuildReports/UnitTests/TestResults.trx" \ 
-        /d:sonar.cs.nunit.reportsPaths="./BuildReports/UnitTests/TestResults.xml"
+    $HOME/.dotnet/tools/dotnet-sonarscanner begin /k:"prime-dotnet-webapi" /d:sonar.host.url=http://sonarqube:9000 /d:sonar.cs.opencover.reportsPaths="./BuildReports/Coverage/coverage.opencover.xml" /d:sonar.exclusions="**/Migrations/*" /d:sonar.coverage.exclusions="**Tests*.cs","**/Migrations/*","**/Program.cs" /d:sonar.cpd.exclusions="**/Migrations/*" /d:sonar.cs.vstest.reportsPaths="./BuildReports/UnitTests/TestResults.trx" /d:sonar.cs.nunit.reportsPaths="./BuildReports/UnitTests/TestResults.xml"
     dotnet build
-    dotnet sonarscanner end
+    $HOME/.dotnet/tools/dotnet-sonarscanner end
 } 
 function angularTests()
 { 
