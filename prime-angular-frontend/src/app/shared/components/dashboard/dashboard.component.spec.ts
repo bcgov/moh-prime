4dc409c449c2e3ca526d2665b6a9d770be8b4ef6
Martin <mtpultz@gmail.com>
2019-11-05 12:41:58 -0800

    Removed unused imports

--- prime-angular-frontend/src/app/shared/components/dashboard/dashboard.component.spec.ts
+++ prime-angular-frontend/src/app/shared/components/dashboard/dashboard.component.spec.ts
@@ -4,15 +4,12 @@ import { RouterTestingModule } from '@angular/router/testing';
 
 import { MockAuthService } from 'test/mocks/mock-auth.service';
 
-import { KeycloakOptions } from 'keycloak-angular';
-
 import { DashboardComponent } from './dashboard.component';
 import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
 import { NgxMaterialModule } from '@shared/modules/ngx-material/ngx-material.module';
 import { NgxProgressModule } from '@shared/modules/ngx-progress/ngx-progress.module';
 import { HeaderComponent } from '@shared/components/header/header.component';
 import { AuthService } from '@auth/shared/services/auth.service';
-import { environment } from '@env/environment';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
