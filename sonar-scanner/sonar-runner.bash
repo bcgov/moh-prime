84b0cac78e76f73237a4fa32f67de3de9ab2cf93
Paul Martin <50643076+poggdogg@users.noreply.github.com>
2019-11-05 14:47:51 -0800

    Sonar Runner Phase 2 (#64)
    * keepalive
    
    * pruning drive contents
    
    * removed null chars
    
    * improving context
    
    * beginning enslavement to Jenkins
    
    * closing in on sonar
    
    * duplicate functionname
    
    * wrong arg
    
    * building
    
    * removing entrypoint
    
    * adding jenkins work directory
    
    * begin the slavery
    
    * fixing namespace for image
    
    * executable entrypoint
    
    * closing in on slavery
    
    * order of operations
    
    * more pathing
    
    * almost there
    
    * more polishing
    
    * call tests
    
    * adding jenkins-prod user
    
    * exec bit
    
    * fixing entrypoint
    
    * slaving on startup
    
    * adding to player
    
    * moving to explicit user
    
    * fixing jenkins link
    
    * trying default user
    
    * adding default group
    
    * handling user cases
    
    * fixing dangling :
    
    * allowing auto group creation
    
    * going to official node image
    
    * node should be there already
    
    * added jenkins user ack
    
    * cleanup
    
    * fixed hanging command
    
    * handling nullspace
    
    * mirror local to openshift
    
    * Changing entrypoint to match home/jenkins
    
    * Fixing null chars
    
    * adding ownership of home directory
    
    * more default changes
    
    * going to base image
    
    * absolute for coverlet
    
    * local ref for sonarqube
    
    * updated .local
    
    * adding scans, fixing calls
    
    * added sonarscanner to path
    
    * adding agent
    
    * local ref
    
    * add sources for angular
    
    * cleanup
    
    * correected source image name
    
    * adding java home
    
    * added environment to help jenkins
    
    * more environment
    
    * absolute paths
    
    * full cmd on launch
    
    * back to entrypoint, fixed path
    
    * more permissions
    
    * more env
    
    * jenkins home
    
    * more home
    
    * changing order of file switches.
    
    * Final changes
    
    * final jenkins
    
    * deleted extra files
    
    * removing RSA

--- sonar-scanner/sonar-runner.bash
+++ sonar-scanner/sonar-runner.bash
@@ -3,28 +3,21 @@ function dotnetTests()
 {   
     echo "Starting tests..." 
     dotnet build 
-    coverlet "./prime-dotnet-webapi-tests/bin/Debug/netcoreapp2.2/PrimeTests.dll" \ 
-        --target "dotnet" \ 
-        --targetargs "test . --no-build --logger "trx;LogFileName=TestResults.trx" --logger "xunit;LogFileName=TestResults.xml" --results-directory ./BuildReports/UnitTests" \ 
-        -f opencover -o ./BuildReports/Coverage/coverage 
+    echo "Beginning .NET code coverage scan..."
+    $HOME/.dotnet/tools/coverlet "./prime-dotnet-webapi-tests/bin/Debug/netcoreapp2.2/PrimeTests.dll" -f opencover -o ./BuildReports/Coverage/coverage --target "dotnet" --targetargs "test . --no-build --logger "trx;LogFileName=TestResults.trx" --logger "xunit;LogFileName=TestResults.xml" --results-directory ./BuildReports/UnitTests" 2>/dev/null ;
     dotnet build-server shutdown 
-    dotnet sonarscanner begin \ 
-        /k:"prime-web-api" \ 
-        /d:sonar.host.url=http://sonar-backend-dqszvc-tools.pathfinder.gov.bc.ca \ 
-        /d:sonar.cs.opencover.reportsPaths="./BuildReports/Coverage/coverage.opencover.xml" \
-        /d:sonar.exclusions="**/Migrations/*" \ 
-        /d:sonar.coverage.exclusions="**Tests*.cs","**/Migrations/*","**/Program.cs" \
-        /d:sonar.cpd.exclusions="**/Migrations/*" \ 
-        /d:sonar.cs.vstest.reportsPaths="./BuildReports/UnitTests/TestResults.trx" \ 
-        /d:sonar.cs.nunit.reportsPaths="./BuildReports/UnitTests/TestResults.xml"
+    echo "Beginning .NET sonar scan..."
+    $HOME/.dotnet/tools/dotnet-sonarscanner begin /k:"prime-dotnet-webapi" /d:sonar.host.url=http://sonarqube:9000 /d:sonar.cs.opencover.reportsPaths="./BuildReports/Coverage/coverage.opencover.xml" /d:sonar.exclusions="**/Migrations/*" /d:sonar.coverage.exclusions="**Tests*.cs","**/Migrations/*","**/Program.cs" /d:sonar.cpd.exclusions="**/Migrations/*" /d:sonar.cs.vstest.reportsPaths="./BuildReports/UnitTests/TestResults.trx" /d:sonar.cs.nunit.reportsPaths="./BuildReports/UnitTests/TestResults.xml"
     dotnet build
-    dotnet sonarscanner end
-} 
+    $HOME/.dotnet/tools/dotnet-sonarscanner end
+}
+
 function angularTests()
 { 
-    cd prime-angular-frontend
-    sonar-scanner -Dsonar.host.url=http://sonar-backend-dqszvc-tools.pathfinder.gov.bc.ca
+    echo "Beginning angular/javascript sonar scan..."
+    sonar-scanner -Dsonar.host.url=http://sonarqube:9000 -Dsonar.projectKey=angular-frontend -Dsonar.sources=prime-angular-frontend
 }
+
 echo "Beginning tests in container..."
 dotnetTests
 angularTests
