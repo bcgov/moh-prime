583ce37da8b898e6efb7053f1dae6bf46fb8eae2
Paul Martin <poggdogg@gmail.com>
2019-11-04 17:07:36 -0800

    local ref

--- sonar-scanner/entrypoint.bash
+++ sonar-scanner/entrypoint.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget https://jenkins-prod-dqszvc-tools.pathfinder.gov.bc.ca/jnlpJars/agent.jar
+wget http://jenkins-prod/jnlpJars/agent.jar
 java -jar agent.jar -jnlpUrl http://jenkins-prod/computer/code-tests/slave-agent.jnlp -secret c598ca95983a9f6df4d06cc7f770b0d1ea404b806851f1a7f1066d89515c2c12 -workDir "$HOME"
 tail -f /dev/null
