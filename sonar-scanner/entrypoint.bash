132367c7d9347b6f5abccc4d235c1a46ff145bd9
Paul Martin <poggdogg@gmail.com>
2019-11-04 18:01:52 -0800

    adding java home

--- sonar-scanner/entrypoint.bash
+++ sonar-scanner/entrypoint.bash
@@ -1,4 +1,3 @@
 #!/bin/bash
-wget http://jenkins-prod/jnlpJars/agent.jar
 java -jar agent.jar -jnlpUrl http://jenkins-prod/computer/code-tests/slave-agent.jnlp -secret c598ca95983a9f6df4d06cc7f770b0d1ea404b806851f1a7f1066d89515c2c12 -workDir "$HOME"
 tail -f /dev/null
