9ad3cc0abcb6e43e8b1f0f1cf3ca93b958c3d365
Paul Martin <poggdogg@gmail.com>
2019-11-05 06:23:14 -0800

    changing order of file switches.

--- sonar-scanner/sonar-runner.bash
+++ sonar-scanner/sonar-runner.bash
@@ -3,7 +3,7 @@ function dotnetTests()
 {   
     echo "Starting tests..." 
     dotnet build 
-    $HOME/.dotnet/tools/coverlet "./prime-dotnet-webapi-tests/bin/Debug/netcoreapp2.2/PrimeTests.dll" --target "dotnet" --targetargs "test . --no-build --logger "trx;LogFileName=TestResults.trx" --logger "xunit;LogFileName=TestResults.xml" --results-directory ./BuildReports/UnitTests" -f opencover -o ./BuildReports/Coverage/coverage 
+    $HOME/.dotnet/tools/coverlet "./prime-dotnet-webapi-tests/bin/Debug/netcoreapp2.2/PrimeTests.dll" -f opencover -o ./BuildReports/Coverage/coverage --target "dotnet" --targetargs "test . --no-build --logger "trx;LogFileName=TestResults.trx" --logger "xunit;LogFileName=TestResults.xml" --results-directory ./BuildReports/UnitTests" 
     dotnet build-server shutdown 
     $HOME/.dotnet/tools/dotnet-sonarscanner begin /k:"prime-dotnet-webapi" /d:sonar.host.url=http://sonarqube:9000 /d:sonar.cs.opencover.reportsPaths="./BuildReports/Coverage/coverage.opencover.xml" /d:sonar.exclusions="**/Migrations/*" /d:sonar.coverage.exclusions="**Tests*.cs","**/Migrations/*","**/Program.cs" /d:sonar.cpd.exclusions="**/Migrations/*" /d:sonar.cs.vstest.reportsPaths="./BuildReports/UnitTests/TestResults.trx" /d:sonar.cs.nunit.reportsPaths="./BuildReports/UnitTests/TestResults.xml"
     dotnet build
