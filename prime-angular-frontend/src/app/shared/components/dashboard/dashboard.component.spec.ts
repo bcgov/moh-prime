17aec8439a854909a920ceb5e00cb8da3e032993
Martin <mtpultz@gmail.com>
2019-11-05 12:43:14 -0800

    Removed unused import

--- prime-angular-frontend/src/app/shared/components/dashboard/dashboard.component.spec.ts
+++ prime-angular-frontend/src/app/shared/components/dashboard/dashboard.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 
