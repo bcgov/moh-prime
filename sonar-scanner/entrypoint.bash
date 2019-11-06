190f65b8dc924656bf189da677d8188912ac799e
Paul Martin <poggdogg@gmail.com>
2019-11-06 07:47:27 -0800

    wait for jenkins to be ready for connect

--- sonar-scanner/entrypoint.bash
+++ sonar-scanner/entrypoint.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
+sleep 60
 java -jar agent.jar -jnlpUrl http://jenkins-prod/computer/code-tests/slave-agent.jnlp -secret c598ca95983a9f6df4d06cc7f770b0d1ea404b806851f1a7f1066d89515c2c12 -workDir "$HOME"
 tail -f /dev/null
