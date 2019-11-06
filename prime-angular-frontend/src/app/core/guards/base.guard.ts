ce95c2c59c423aef5bc240eaaa7e8c76e898e3d7
Martin <mtpultz@gmail.com>
2019-11-05 21:01:00 -0800

    Updated base guard

--- prime-angular-frontend/src/app/core/guards/base.guard.ts
+++ prime-angular-frontend/src/app/core/guards/base.guard.ts
@@ -1,33 +1,68 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, CanActivateChild, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  CanActivate, CanActivateChild, CanLoad, Route, UrlSegment,
+  ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree
+} from '@angular/router';
+
 import { Observable } from 'rxjs';
+
+import { LoggerService } from '@core/services/logger.service';
+import { AuthService } from '@auth/shared/services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class BaseGuard implements CanActivate, CanActivateChild, CanLoad {
+export abstract class BaseGuard implements CanActivate, CanActivateChild, CanLoad {
   private authenticated: boolean;
-  private roles: string[];
+
+  constructor(
+    protected authService: AuthService,
+    protected logger: LoggerService
+  ) { }
+
+  public get isAuthenticated() {
+    return this.authenticated;
+  }
 
   public canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    return this.checkAccess();
   }
 
   public canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.checkAccess();
   }
 
   public canActivateChild(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.checkAccess();
   }
 
-  public canAccess() {
+  /**
+   * @description
+   * Hook for customizing access, which defaults to validating
+   * access based on authentication.
+   */
+  protected canAccess(authenticated: boolean, roles: string[]): Promise<boolean> {
+    return new Promise((resolve, reject) => (authenticated) ? resolve(true) : reject(false));
+  }
 
+  private checkAccess(): Promise<boolean> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        this.authenticated = await this.authService.isLoggedIn();
+        const roles = this.authService.getUserRoles(true);
+        const result = await this.canAccess(this.authenticated, roles);
+        resolve(result);
+      } catch (error) {
+        const message = 'Error has occurred during access validation';
+        this.logger.error(message, error);
+        reject(`${message}. Details: ${error}`);
+      }
+    });
   }
 }
