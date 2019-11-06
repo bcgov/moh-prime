e239f9ad0a53e73fbbb0322449bd6a1679279f24
Paul Martin <poggdogg@gmail.com>
2019-11-05 16:49:02 -0800

    Fixing pathing

--- player.sh
+++ player.sh
@@ -216,12 +216,13 @@ function dotnetTests()
     echo "Starting tests..." 
     dotnet build 
     echo "Beginning .NET code coverage scan..."
-    $HOME/.dotnet/tools/coverlet "./prime-dotnet-webapi-tests/bin/Debug/netcoreapp2.2/PrimeTests.dll" -f opencover -o ./BuildReports/Coverage/coverage --target "dotnet" --targetargs "test . --no-build --logger "trx;LogFileName=TestResults.trx" --logger "xunit;LogFileName=TestResults.xml" --results-directory ./BuildReports/UnitTests" 2&>/dev/null
+    coverlet "./prime-dotnet-webapi-tests/bin/Debug/netcoreapp2.2/PrimeTests.dll" -f opencover -o ./BuildReports/Coverage/coverage --target "dotnet" --targetargs "test . --no-build --logger "trx;LogFileName=TestResults.trx" --logger "xunit;LogFileName=TestResults.xml" --results-directory ./BuildReports/UnitTests" 
     dotnet build-server shutdown 
     echo "Beginning .NET sonar scan..."
-    $HOME/.dotnet/tools/dotnet-sonarscanner begin /k:"prime-dotnet-webapi" /d:sonar.host.url=http://sonarqube:9000 /d:sonar.cs.opencover.reportsPaths="./BuildReports/Coverage/coverage.opencover.xml" /d:sonar.exclusions="**/Migrations/*" /d:sonar.coverage.exclusions="**Tests*.cs","**/Migrations/*","**/Program.cs" /d:sonar.cpd.exclusions="**/Migrations/*" /d:sonar.cs.vstest.reportsPaths="./BuildReports/UnitTests/TestResults.trx" /d:sonar.cs.nunit.reportsPaths="./BuildReports/UnitTests/TestResults.xml" 2&>/dev/null
+    dotnet-sonarscanner begin /k:"prime-dotnet-webapi" /d:sonar.host.url=http://sonarqube:9000 /d:sonar.cs.opencover.reportsPaths="./BuildReports/Coverage/coverage.opencover.xml" /d:sonar.exclusions="**/Migrations/*" /d:sonar.coverage.exclusions="**Tests*.cs","**/Migrations/*","**/Program.cs" /d:sonar.cpd.exclusions="**/Migrations/*" /d:sonar.cs.vstest.reportsPaths="./BuildReports/UnitTests/TestResults.trx" /d:sonar.cs.nunit.reportsPaths="./BuildReports/UnitTests/TestResults.xml"
     dotnet build
-    $HOME/.dotnet/tools/dotnet-sonarscanner end
+    dotnet-sonarscanner end
+#Since errors are expected, force 0 to not break Jenkins
     exit 0
 }
 
