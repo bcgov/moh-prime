1e106b73b0a6102f373ea6fad0d76cb7c4d0f21d
James <jameshollinger.ee@gmail.com>
2019-11-07 17:49:51 -0800

    Basic SMTP emails using local server

---
+++ prime-dotnet-webapi/Services/DefaultEmailService.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Http;
+using System.Net.Mail;
+
+namespace Prime.Services
+{
+    public class DefaultEmailService : BaseService, IEmailService
+    {
+        public DefaultEmailService(
+            ApiDbContext context, IHttpContextAccessor httpContext)
+            : base(context, httpContext)
+        { }
+
+        public void Send(string from, string to, string subject, string body)
+        {
+            var fromAddress = new MailAddress(from);
+            var toAddress = new MailAddress(to);
+
+            MailMessage mail = new MailMessage(from: fromAddress, to: toAddress)
+            {
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true,
+            };
+
+            SmtpClient smtp = new SmtpClient("127.0.0.1", 1025);
+            smtp.Send(mail);
+        }
+    }
+}
