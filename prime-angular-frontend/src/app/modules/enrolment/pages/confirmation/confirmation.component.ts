12a7f77013e90371b80602d9d31b30a2173c0a0b
Martin <mtpultz@gmail.com>
2019-11-07 20:37:35 -0800

    Updated lorem ipsum on the confirmation page for manual and automatic approval processes

--- prime-angular-frontend/src/app/modules/enrolment/pages/confirmation/confirmation.component.ts
+++ prime-angular-frontend/src/app/modules/enrolment/pages/confirmation/confirmation.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { EnrolmentService } from '@enrolment/shared/services/enrolment.service';
+import { Enrolment } from '@shared/models/enrolment.model';
 
-// TODO: temporary placeholder for submission of an enrolment
 @Component({
   selector: 'app-confirmation',
   templateUrl: './confirmation.component.html',
   styleUrls: ['./confirmation.component.scss']
 })
 export class ConfirmationComponent implements OnInit {
+  public isAutomatic: boolean;
 
-  constructor() { }
+  constructor(
+    private enrolmentService: EnrolmentService
+  ) { }
 
-  public ngOnInit() { }
+  public ngOnInit() {
+    this.enrolmentService.enrolment$
+      .subscribe((enrolment: Enrolment) => {
+        this.isAutomatic = (!enrolment.currentStatus.enrolmentStatusReasons.length) ? true : false;
+      });
+  }
 }
