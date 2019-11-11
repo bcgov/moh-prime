4fc365684fa828f4528362c99b9bcc203659d5eb
Martin Pultz <mtpultz@gmail.com>
2019-11-11 13:39:04 -0800

    Updated app root with route constants

--- prime-angular-frontend/src/app/app-routing.module.ts
+++ prime-angular-frontend/src/app/app-routing.module.ts
@@ -4,24 +4,25 @@ import { Routes, RouterModule } from '@angular/router';
 import { AccessDeniedComponent } from '@core/components/access-denied/access-denied.component';
 import { MaintenanceComponent } from '@core/components/maintenance/maintenance.component';
 import { PageNotFoundComponent } from '@core/components/page-not-found/page-not-found.component';
+import { AppRoutes } from './app.routes';
 
 const routes: Routes = [
   {
-    path: 'denied',
+    path: AppRoutes.DENIED,
     component: AccessDeniedComponent,
     data: {
       title: 'Access Denied'
     }
   },
   {
-    path: 'maintenance',
+    path: AppRoutes.MAINTENANCE,
     component: MaintenanceComponent,
     data: {
       title: 'Under Scheduled Maintenace'
     }
   },
   {
-    path: '**',
+    path: AppRoutes.PAGE_NOT_FOUND,
     component: PageNotFoundComponent,
     data: {
       title: 'Page Not Found'
