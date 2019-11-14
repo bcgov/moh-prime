ea9a467347938aa7e59df12d8eb06a76f5e6049e
Martin Pultz <mtpultz@users.noreply.github.com>
2019-11-14 15:29:20 -0800

    Date Format (#106)
    * Updated global date format date pickers
    
    * Updated format date pipe with date format changes

--- prime-angular-frontend/src/app/shared/pipes/format-date.pipe.ts
+++ prime-angular-frontend/src/app/shared/pipes/format-date.pipe.ts
@@ -1,11 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import * as moment from 'moment';
 
+import { APP_DATE_FORMAT } from '@shared/modules/ngx-material/ngx-material.module';
+
 @Pipe({
   name: 'formatDate'
 })
 export class FormatDatePipe implements PipeTransform {
-  transform(date: string, format: string = 'MMM D, Y'): string {
+  transform(date: string, format: string = APP_DATE_FORMAT): string {
     if (date) {
       date = moment(date).format(format);
     }
