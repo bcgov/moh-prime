67ce096c371fcbcf792957214a8d6a075c56b5bb
Martin Pultz <mtpultz@gmail.com>
2019-11-15 14:12:31 -0800

    Updates to route constants for prod build

--- prime-angular-frontend/src/app/modules/adjudication/adjudication.routes.ts
+++ prime-angular-frontend/src/app/modules/adjudication/adjudication.routes.ts
@@ -1,12 +1,8 @@
 import { BaseRoute } from '@core/models/base-route';
 
 export class AdjudicationRoutes extends BaseRoute {
-  public static get MODULE_PATH(): string {
-    return AdjudicationRoutes.ADJUDICATION;
-  }
-
-  // Module
   public static ADJUDICATION = 'adjudication';
-  // Adjudication
   public static ENROLMENTS = 'enrolments';
+
+  public static MODULE_PATH = AdjudicationRoutes.ADJUDICATION;
 }
--- prime-angular-frontend/src/app/modules/auth/auth.routes.ts
+++ prime-angular-frontend/src/app/modules/auth/auth.routes.ts
@@ -1,13 +1,9 @@
 import { BaseRoute } from '@core/models/base-route';
 
 export class AuthRoutes extends BaseRoute {
-  public static get MODULE_PATH(): string {
-    return AuthRoutes.AUTH;
-  }
-
-  // Module
   public static AUTH = '';
-  // Authentication
   public static INFO = 'info';
   public static ADMIN = 'admin';
+
+  public static MODULE_PATH = AuthRoutes.AUTH;
 }
