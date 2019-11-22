0d713491494690f96328a3e427f3f951464b7f71
Martin Pultz <mtpultz@gmail.com>
2019-11-22 15:51:10 -0800

    Fixed page ref spec

--- prime-angular-frontend/src/app/modules/enrolment/pages/access-agreement/page-ref.directive.spec.ts
+++ prime-angular-frontend/src/app/modules/enrolment/pages/access-agreement/page-ref.directive.spec.ts
@@ -1,8 +1,11 @@
+import { inject } from '@angular/core/testing';
+
 import { PageRefDirective } from './page-ref.directive';
+import { TemplateRef } from '@angular/core';
 
 describe('PageRefDirective', () => {
-  it('should create an instance', () => {
-    const directive = new PageRefDirective();
+  it('should create an instance', inject([TemplateRef], (templateRef: TemplateRef<any>) => {
+    const directive = new PageRefDirective(templateRef);
     expect(directive).toBeTruthy();
-  });
+  }));
 });
