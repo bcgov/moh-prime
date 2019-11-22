b40d93152bbb0d88418d3dc254fdf1715d9035da
Martin Pultz <mtpultz@gmail.com>
2019-11-22 15:57:07 -0800

    Fix to completely remove headless Chrome

--- prime-angular-frontend/karma.conf.js
+++ prime-angular-frontend/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadlessNoSandbox', 'ChromeHeadless'],
+    browsers: ['Chrome'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
