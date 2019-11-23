2a2ee91229d479ae600ae050b18b91caac2a87f5
Martin Pultz <mtpultz@gmail.com>
2019-11-22 17:56:20 -0800

    Fixes for tests

--- prime-angular-frontend/src/app/modules/enrolment/pages/access-agreement/page-ref.directive.spec.ts
+++ prime-angular-frontend/src/app/modules/enrolment/pages/access-agreement/page-ref.directive.spec.ts
@@ -1,9 +1,27 @@
-import { inject } from '@angular/core/testing';
+import { async, TestBed, inject } from '@angular/core/testing';
 
 import { PageRefDirective } from './page-ref.directive';
-import { TemplateRef } from '@angular/core';
+import { TemplateRef, ElementRef, EmbeddedViewRef } from '@angular/core';
+
+export class MockTemplateRef extends TemplateRef<any> {
+  elementRef: ElementRef<any>;
+  createEmbeddedView(context: any): EmbeddedViewRef<any> {
+    return null;
+  }
+}
 
 describe('PageRefDirective', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: TemplateRef,
+          useClass: MockTemplateRef
+        }
+      ]
+    }).compileComponents();
+  }));
+
   it('should create an instance', inject([TemplateRef], (templateRef: TemplateRef<any>) => {
     const directive = new PageRefDirective(templateRef);
     expect(directive).toBeTruthy();
--- prime-angular-frontend/src/app/shared/components/form-icon-group/form-icon-group.component.spec.ts
+++ prime-angular-frontend/src/app/shared/components/form-icon-group/form-icon-group.component.spec.ts
@@ -1,16 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormIconGroupComponent } from './form-icon-group.component';
+import { NgxContextualHelpModule } from '@shared/modules/ngx-contextual-help/ngx-contextual-help.module';
+import { NgxMaterialModule } from '@shared/modules/ngx-material/ngx-material.module';
 
 describe('FormIconGroupComponent', () => {
   let component: FormIconGroupComponent;
   let fixture: ComponentFixture<FormIconGroupComponent>;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ FormIconGroupComponent ]
-    })
-    .compileComponents();
+    TestBed.configureTestingModule(
+      {
+        imports: [
+          NgxContextualHelpModule,
+          NgxMaterialModule
+        ],
+        declarations: [
+          FormIconGroupComponent
+        ]
+      }
+    ).compileComponents();
   }));
 
   beforeEach(() => {
