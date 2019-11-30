3115b4308c70bc5dddbb49b9af447f1b614d5d75
Paul Martin <50643076+poggdogg@users.noreply.github.com>
2019-11-30 09:25:28 -0800

    Hotfix/remove cleanup (#146)
    * Removing cleanup
    
    * Removing cleanup
    
    * re-adding changes
    
    * Adding cleanup to tools
    
    * extra quotes from "helpful" IDE
    
    * renamed a function
    
    * More 'helpful' spacing
    
    * Added arrays to the logic
    
    * need to wrap executable
    
    * Cleanup was WAY too thorough
    
    * For loops honed with arrays
    
    * further turning requirerd
    
    * more testing
    
    * Looks good
    
    * Need to declare namespace

--- functions.bash
+++ functions.bash
@@ -1,10 +1,10 @@
 export BRANCH_LOWER=`echo "${BRANCH_NAME}" | awk '{print tolower($0)}'`
 function variablePopulation() {
     if [ "${BRANCH_LOWER}" == "develop" ] || [ "${BRANCH_LOWER}" == "master" ];
-    then 
+    then
         export SUFFIX=""
         export CHANGE_BRANCH="$BRANCH_NAME"
-    else 
+    else
         export SUFFIX="-${BRANCH_LOWER}";
     fi
 }
@@ -15,14 +15,14 @@ function build() {
     echo "Building $1 (${APP_NAME}) to $PROJECT_PREFIX-$2..."
     buildPresent=$(oc get bc/"$APP_NAME-$BRANCH_LOWER" --ignore-not-found=true)
     if [ -z "${buildPresent}" ];
-    then 
+    then
         MODE="apply"
-    else 
+    else
         MODE="create"
     fi;
     if [ "${BRANCH_LOWER}" == "develop" ] || [ "${BRANCH_LOWER}" == "master" ];
-    then 
-        echo "oc process -f ./${TEMPLATE_DIRECTORY}/${BUILD_CONFIG_TEMPLATE} -p NAME=${APP_NAME} -p VERSION=${BUILD_NUMBER} -p SOURCE_CONTEXT_DIR=${SOURCE_CONTEXT_DIR} -p SOURCE_REPOSITORY_URL=${GIT_URL} -p SOURCE_REPOSITORY_REF=${BRANCH_NAME} -p OC_NAMESPACE=${PROJECT_PREFIX} -p OC_APP=$2 | oc ${MODE} -f - --namespace=${PROJECT_PREFIX}-$2"  
+    then
+        echo "oc process -f ./${TEMPLATE_DIRECTORY}/${BUILD_CONFIG_TEMPLATE} -p NAME=${APP_NAME} -p VERSION=${BUILD_NUMBER} -p SOURCE_CONTEXT_DIR=${SOURCE_CONTEXT_DIR} -p SOURCE_REPOSITORY_URL=${GIT_URL} -p SOURCE_REPOSITORY_REF=${BRANCH_NAME} -p OC_NAMESPACE=${PROJECT_PREFIX} -p OC_APP=$2 | oc ${MODE} -f - --namespace=${PROJECT_PREFIX}-$2"
         oc process -f ./"${TEMPLATE_DIRECTORY}/${BUILD_CONFIG_TEMPLATE}" \
         -p NAME="${APP_NAME}" \
         -p VERSION="${BUILD_NUMBER}" \
@@ -30,9 +30,9 @@ function build() {
         -p SOURCE_REPOSITORY_URL="${GIT_URL}" \
         -p SOURCE_REPOSITORY_REF="${BRANCH_NAME}" \
         -p OC_NAMESPACE="${PROJECT_PREFIX}" \
-        -p OC_APP="$2" | oc "${MODE}" -f - --namespace="${PROJECT_PREFIX}-$2"  
-    else 
-        echo "oc process -f ./${TEMPLATE_DIRECTORY}/${BUILD_CONFIG_TEMPLATE} -p NAME=${APP_NAME} -p VERSION=${BUILD_NUMBER} -p SUFFIX=-${BRANCH_LOWER} -p SOURCE_CONTEXT_DIR=${SOURCE_CONTEXT_DIR} -p SOURCE_REPOSITORY_URL=${GIT_URL} -p SOURCE_REPOSITORY_REF=${BRANCH_NAME} -p OC_NAMESPACE=${PROJECT_PREFIX} -p OC_APP=$2 | oc ${MODE} -f - --namespace=${PROJECT_PREFIX}-$2"  
+        -p OC_APP="$2" | oc "${MODE}" -f - --namespace="${PROJECT_PREFIX}-$2"
+    else
+        echo "oc process -f ./${TEMPLATE_DIRECTORY}/${BUILD_CONFIG_TEMPLATE} -p NAME=${APP_NAME} -p VERSION=${BUILD_NUMBER} -p SUFFIX=-${BRANCH_LOWER} -p SOURCE_CONTEXT_DIR=${SOURCE_CONTEXT_DIR} -p SOURCE_REPOSITORY_URL=${GIT_URL} -p SOURCE_REPOSITORY_REF=${BRANCH_NAME} -p OC_NAMESPACE=${PROJECT_PREFIX} -p OC_APP=$2 | oc ${MODE} -f - --namespace=${PROJECT_PREFIX}-$2"
         oc process -f ./"${TEMPLATE_DIRECTORY}/${BUILD_CONFIG_TEMPLATE}" \
         -p NAME="${APP_NAME}" \
         -p VERSION="${BUILD_NUMBER}" \
@@ -46,7 +46,7 @@ function build() {
     if [ "$BUILD_REQUIRED" == true ];
     then
         echo "Building oc start-build $APP_NAME$SUFFIX -n $PROJECT_PREFIX-$2 --wait --follow ..."
-        oc start-build "$APP_NAME$SUFFIX" -n "$PROJECT_PREFIX-$2" --wait --follow 
+        oc start-build "$APP_NAME$SUFFIX" -n "$PROJECT_PREFIX-$2" --wait --follow
     else
         echo "Deployment should be automatic..."
     fi
@@ -57,13 +57,13 @@ function deploy() {
     echo "Deploying $1 (${APP_NAME}) to $2 ..."
     deployPresent=$(oc get dc/"${APP_NAME}-${BRANCH_LOWER}" --ignore-not-found=true)
     if [ -z "${deployPresent}" ];
-    then 
+    then
         MODE="apply"
-    else 
+    else
         MODE="create"
     fi;
     if [ "${BRANCH_LOWER}" == "develop" ] || [ "${BRANCH_LOWER}" == "master" ];
-    then 
+    then
         oc process -f ./"${TEMPLATE_DIRECTORY}/${DEPLOY_CONFIG_TEMPLATE}" \
         -p NAME="${APP_NAME}" \
         -p VERSION="${BUILD_NUMBER}" \
@@ -71,8 +71,8 @@ function deploy() {
         -p SOURCE_REPOSITORY_URL="${GIT_URL}" \
         -p SOURCE_REPOSITORY_REF="${BRANCH_NAME}" \
         -p OC_NAMESPACE="${PROJECT_PREFIX}" \
-        -p OC_APP="$2" | oc "${MODE}" -f - --namespace="${PROJECT_PREFIX}-$2"  
-    else 
+        -p OC_APP="$2" | oc "${MODE}" -f - --namespace="${PROJECT_PREFIX}-$2"
+    else
         oc process -f ./"${TEMPLATE_DIRECTORY}/${DEPLOY_CONFIG_TEMPLATE}" \
         -p NAME="${APP_NAME}" \
         -p VERSION="${BUILD_NUMBER}" \
@@ -104,7 +104,7 @@ function toolbelt() {
     if [ "$BUILD_REQUIRED" == true ];
     then
         echo "Building oc start-build $APP_NAME -n ${PROJECT_PREFIX}-${OC_APP} --wait --follow ..."
-        oc start-build $APP_NAME -n ${PROJECT_PREFIX}-$2 --wait --follow 
+        oc start-build $APP_NAME -n ${PROJECT_PREFIX}-$2 --wait --follow
     else
         echo "Deployment should be automatic..."
     fi
@@ -118,36 +118,24 @@ function determineMode() {
     fi;
 }
 
-# Scrubs all PR assets from the environment
+function occleanup() {
+    OPEN_PR_ARRAY=()
+    LIVE_BRANCH_ARRAY=()
+    ORPPHANS=()
+    curl -o openPRs.txt "https://api.github.com/repos/${PROJECT_OWNER}/${PROJECT_NAME}/pulls?status=open&sort=number"
+    declare -p OPEN_PR_ARRAY=( $(grep '"number"' openPRs.txt | column -t | sed 's|[:,]||g' | awk '{print $2}') )
+    declare -p LIVE_BRANCH_ARRAY=( $(oc get route -n ${PROJECT_PREFIX}-dev | awk '{print $1}' | grep -P "(\-pr\-\d+)" | sed 's/[^0-9]*//g' | sort -un) )
+    ORPHANS=$(echo ${OPEN_PR_ARRAY[@]} ${LIVE_BRANCH_ARRAY[@]} | tr ' ' '\n' | sort | uniq -u)
+    for i in $ORPHANS
+    do
+        cleanOcArtifacts $i
+    done
+}
+
 function cleanOcArtifacts() {
-    artifactItems=$(oc get all,secrets,pvc -n ${PROJECT_PREFIX}-dev | grep -i "\-${BRANCH_NAME}"  | column -t | awk '{print $1}' | sort)
-    echo "${artifactItems}"
-    for i in ${artifactItems};
+    declare -p ALL_BRANCH_ARTIFACTS=( $(oc get all,pvc,secrets,route -n ${PROJECT_PREFIX}-dev | grep -i "\-$1" | awk '{print $1}' | grep -P "(\-pr\-\d+)") )
+    for a in "${ALL_BRANCH_ARTIFACTS[@]}"
     do
-        oc delete -n ${PROJECT_PREFIX}-dev $i
+    oc delete -n ${PROJECT_PREFIX}-dev $a
     done
-}
-
-function cleanup() {
-    artifactItems=$(oc get all,pvc,secrets -n ${PROJECT_PREFIX}-dev | grep -i "\-$1"  | column -t | awk '{print $1}' | sort)
-    echo "${artifactItems}"
-    for i in ${artifactItems};
-    do
-        oc delete -n ${PROJECT_PREFIX}-dev $i
-    done
-}
-
-function gitPromote() {
-    # Update branch with latest changes from branch
-    rm -fr ${PROJECT_NAME}
-    git clone https://${GIT_REPO}
-    cd ${PROJECT_NAME}
-    git checkout ${CHANGE_BRANCH} 
-    git fetch
-    git merge --squash -s ours -m "Merging $1 to ${CHANGE_BRANCH}" $1 ${CHANGE_BRANCH}
-    git commit -a -m "Merge branch $1 into ${CHANGE_BRANCH}"
-    git push https://${GIT_USERNAME}:${GIT_PASSWORD}@${GIT_REPO} ${CHANGE_BRANCH}
-    git checkout $1
-    git merge -s ours -m "Updating branch with $1" ${CHANGE_BRANCH} origin/$1
-    git push https://${GIT_USERNAME}:${GIT_PASSWORD}@${GIT_REPO} origin/$1
 }
