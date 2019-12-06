4254ff7d00c27020e055469e440e3ddefb53efd3
Martin Pultz <mtpultz@gmail.com>
2019-12-06 09:17:11 -0800

    Updated header to show first and last, not first first name (for Jason's sanity)

--- prime-angular-frontend/src/app/shared/components/dashboard/dashboard.component.ts
+++ prime-angular-frontend/src/app/shared/components/dashboard/dashboard.component.ts
@@ -90,7 +90,7 @@ export class DashboardComponent implements OnInit {
       .subscribe((device: string) => this.setSideNavProps(device));
 
     const user = await this.authService.getUser();
-    this.username = `${user.firstName} ${user.firstName}`;
+    this.username = `${user.firstName} ${user.lastName}`;
   }
 
   private getSideNavSections() {
