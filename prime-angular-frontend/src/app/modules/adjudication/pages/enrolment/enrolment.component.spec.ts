6a6b180df625bbf60354fb726d14415c2b35c1a4
Martin Pultz <mtpultz@gmail.com>
2019-12-08 01:19:07 -0800

    Removed unused import

--- prime-angular-frontend/src/app/modules/adjudication/pages/enrolment/enrolment.component.spec.ts
+++ prime-angular-frontend/src/app/modules/adjudication/pages/enrolment/enrolment.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
