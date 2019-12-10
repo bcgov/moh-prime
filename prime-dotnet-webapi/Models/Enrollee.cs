3ecbf58305196bf85643709bcee4e0338df7d120
Martin Pultz <mtpultz@users.noreply.github.com>
2019-12-10 13:21:22 -0800

    Fixes for Enrollees/Enrollee (#170)
    * Fix for enrolments current status check
    
    * Fix for having multiple applied and approved dates

--- prime-dotnet-webapi/Models/Enrollee.cs
+++ prime-dotnet-webapi/Models/Enrollee.cs
@@ -83,7 +83,10 @@ namespace Prime.Models
         [NotMapped]
         public EnrolmentStatus CurrentStatus
         {
-            get => this.EnrolmentStatuses?.SingleOrDefault(es => es.StatusDate == this.EnrolmentStatuses.Max(x => x.StatusDate));
+            get => this.EnrolmentStatuses?
+                .OrderByDescending(es => es.StatusDate)
+                .ThenByDescending(es => es.Id)
+                .FirstOrDefault();
         }
 
         [NotMapped]
@@ -98,10 +101,22 @@ namespace Prime.Models
         public ICollection<Status> AvailableStatuses { get; set; }
 
         [NotMapped]
-        public DateTime? AppliedDate { get => this.EnrolmentStatuses?.SingleOrDefault(es => es.StatusCode == Status.SUBMITTED_CODE)?.StatusDate; }
+        public DateTime? AppliedDate
+        {
+            get => this.EnrolmentStatuses?
+                .OrderByDescending(en => en.StatusDate)
+                .FirstOrDefault(es => es.StatusCode == Status.SUBMITTED_CODE)?
+                .StatusDate;
+        }
 
         [NotMapped]
-        public DateTime? ApprovedDate { get => this.EnrolmentStatuses?.SingleOrDefault(es => es.StatusCode == Status.APPROVED_CODE)?.StatusDate; }
+        public DateTime? ApprovedDate
+        {
+            get => this.EnrolmentStatuses?
+                .OrderByDescending(en => en.StatusDate)
+                .FirstOrDefault(es => es.StatusCode == Status.APPROVED_CODE)?
+                .StatusDate;
+        }
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
