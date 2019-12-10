5b0b6cda1a34f927f431f29d6647fff28d4211c1
Martin Pultz <mtpultz@gmail.com>
2019-12-09 20:28:09 -0800

    Added initial declined components

---
+++ prime-angular-frontend/src/app/modules/enrolment/pages/declined/declined.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-declined',
+  templateUrl: './declined.component.html',
+  styleUrls: ['./declined.component.scss']
+})
+export class DeclinedComponent implements OnInit {
+  constructor() { }
+
+  public ngOnInit() { }
+}
