024ac5ade36e67c86a4f619bda52895537b67e12
Martin Pultz <mtpultz@gmail.com>
2019-12-18 15:48:50 -0800

    Added not mapped

--- prime-dotnet-webapi/Models/AccessClauses/SantaClaus.cs
+++ prime-dotnet-webapi/Models/AccessClauses/SantaClaus.cs
@@ -1,9 +1,9 @@
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Prime.Models
 {
+    [NotMapped]
     public class SantaClaus : BaseAuditable
     {
         [Key]
