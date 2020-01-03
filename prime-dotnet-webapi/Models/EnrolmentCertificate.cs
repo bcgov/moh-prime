1159c31e22070cc5eb67694f7a19c695d801d133
Anais <anais.hebert@nttdata.com>
2020-01-03 10:03:52 -0800

    Remove unnecessary imports

--- prime-dotnet-webapi/Models/EnrolmentCertificate.cs
+++ prime-dotnet-webapi/Models/EnrolmentCertificate.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Newtonsoft.Json;
 
 namespace Prime.Models
 {
