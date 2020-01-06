05bd13c9a6395ec0117f94a8cd5a47389e409411
Anais <anais.hebert@nttdata.com>
2020-01-06 15:53:16 -0800

    tests!!

--- prime-angular-frontend/src/app/modules/enrolment/shared/components/collection-notice-alert/collection-notice-alert.component.spec.ts
+++ prime-angular-frontend/src/app/modules/enrolment/shared/components/collection-notice-alert/collection-notice-alert.component.spec.ts
@@ -1,6 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { NgxMaterialModule } from '@shared/modules/ngx-material/ngx-material.module';
+
+import { MockConfigService } from 'test/mocks/mock-config.service';
+import { MockAuthService } from 'test/mocks/mock-auth.service';
+import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
+import { ConfigService } from '@config/config.service';
+import { AuthService } from '@auth/shared/services/auth.service';
+
 import { CollectionNoticeAlertComponent } from './collection-notice-alert.component';
+import { AlertComponent } from '@shared/components/alert/alert.component';
+import { PageHeaderComponent } from '@shared/components/page-header/page-header.component';
+import { EnrolmentService } from '@enrolment/shared/services/enrolment.service';
+import { MockEnrolmentService } from 'test/mocks/mock-enrolment.service';
+import { RouterTestingModule } from '@angular/router/testing';
+
+
+
+
+
 
 describe('CollectionNoticeAlertComponent', () => {
   let component: CollectionNoticeAlertComponent;
@@ -8,7 +26,33 @@ describe('CollectionNoticeAlertComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [CollectionNoticeAlertComponent]
+      imports: [
+        NgxMaterialModule,
+        RouterTestingModule
+      ],
+      declarations: [
+        AlertComponent,
+        CollectionNoticeAlertComponent,
+        PageHeaderComponent
+      ],
+      providers: [
+        {
+          provide: APP_CONFIG,
+          useValue: APP_DI_CONFIG
+        },
+        {
+          provide: ConfigService,
+          useClass: MockConfigService
+        },
+        {
+          provide: EnrolmentService,
+          useClass: MockEnrolmentService
+        },
+        {
+          provide: AuthService,
+          useClass: MockAuthService
+        }
+      ]
     })
       .compileComponents();
   }));
