d34773d61725f762eb6b2cab2607f0a24d11af05
Martin Pultz <mtpultz@gmail.com>
2020-01-06 16:32:14 -0700

    Added global clause seed

---
+++ prime-dotnet-webapi/Configuration/GlobalClauseConfiguration.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Prime.Models;
+
+namespace Prime.Configuration
+{
+    public class GlobalClauseConfiguration
+    {
+        private readonly Guid SYSTEM_USER = Guid.Empty;
+
+        private readonly DateTime SEEDING_DATE = DateTime.Now;
+
+        public void Configure(EntityTypeBuilder<StatusReason> builder)
+        {
+            #region GlobalClauseSeed
+            builder.HasData(
+                new GlobalClause { Clause = "Global clause", EffectiveDate = DateTime.Now, CreatedUserId = SYSTEM_USER, CreatedTimeStamp = SEEDING_DATE, UpdatedUserId = SYSTEM_USER, UpdatedTimeStamp = SEEDING_DATE }
+            );
+            #endregion
+        }
+    }
+}
