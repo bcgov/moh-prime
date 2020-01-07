ac18c12941c718c418ca6653f6c2b98fb842ba32
Paul Martin <poggdogg@gmail.com>
2020-01-07 15:05:04 -0800

    Tokenized secrets for runner

--- sonar-scanner/entrypoint.bash
+++ sonar-scanner/entrypoint.bash
@@ -2,6 +2,8 @@
 sleep 30
 npm install @angular/core
 /zap/zap.sh -daemon -host 0.0.0.0 -port 8080 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true -config spider.maxDuration=1 -addonupdate -addoninstall pscanrulesBeta > /dev/null 2>&1 
-/usr/local/openjdk-8/bin/java -jar /usr/share/jenkins/agent.jar -jnlpUrl http://jenkins-prod/computer/code-tests/slave-agent.jnlp -secret c598ca95983a9f6df4d06cc7f770b0d1ea404b806851f1a7f1066d89515c2c12 -workDir $HOME
+wget https://jenkins-prod-dqszvc-tools.pathfinder.gov.bc.ca/computer/code-tests/slave-agent.jnlp
+wget https://jenkins-prod-dqszvc-tools.pathfinder.gov.bc.ca/jnlpJars/agent.jar
+/usr/local/openjdk-8/bin/java -jar /usr/share/jenkins/agent.jar -jnlpUrl http://jenkins-prod/computer/code-tests/slave-agent.jnlp -secret $NODE_SECRET -workDir $HOME
 tail -f /dev/null
 #Jenkins sometimes takes a while to drop connections. This order mitigates the issue.
