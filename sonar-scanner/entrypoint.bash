c4a64985b3eb8393b4f373a85ccf97b80ba2b747
Paul Martin <poggdogg@gmail.com>
2020-01-07 18:42:45 -0800

    fixing entrypoint

--- sonar-scanner/entrypoint.bash
+++ sonar-scanner/entrypoint.bash
@@ -2,8 +2,8 @@
 sleep 30
 npm install @angular/core
 /zap/zap.sh -daemon -host 0.0.0.0 -port 8080 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true -config spider.maxDuration=1 -addonupdate -addoninstall pscanrulesBeta > /dev/null 2>&1 
-wget https://jenkins-prod-dqszvc-tools.pathfinder.gov.bc.ca/computer/code-tests/slave-agent.jnlp
+
 wget https://jenkins-prod-dqszvc-tools.pathfinder.gov.bc.ca/jnlpJars/agent.jar
-/usr/local/openjdk-8/bin/java -jar /usr/share/jenkins/agent.jar -jnlpUrl http://jenkins-prod/computer/code-tests/slave-agent.jnlp -secret $NODE_SECRET -workDir $HOME
+/usr/local/openjdk-8/bin/java -jar agent.jar -jnlpUrl http://jenkins-prod/computer/code-tests/slave-agent.jnlp -secret $NODE_SECRET -workDir $HOME
 tail -f /dev/null
 #Jenkins sometimes takes a while to drop connections. This order mitigates the issue.
