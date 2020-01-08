8bd2efe6ce40704a5b4722a5e4a678a3e0555a15
Martin Pultz <mtpultz@gmail.com>
2020-01-08 00:00:04 -0700

    Removed Google API source

--- prime-dotnet-webapi/Models/Enrollee.cs
+++ prime-dotnet-webapi/Models/Enrollee.cs
@@ -165,9 +165,12 @@ namespace Prime.Models
             get
             {
                 ICollection<EnrolmentStatusReason> enrolmentStatusReasons = this.CurrentStatus?.EnrolmentStatusReasons;
+
                 if (enrolmentStatusReasons != null && enrolmentStatusReasons.Count > 0)
                 {
-                    return enrolmentStatusReasons.Any(r => r.StatusReason?.Code == 1) ? PrimeConstants.PRIME_MOA : PrimeConstants.PRIME_RU;
+                    return enrolmentStatusReasons.Any(r => r.StatusReason?.Code == 1)
+                        ? PrimeConstants.PRIME_MOA
+                        : PrimeConstants.PRIME_RU;
                 }
 
                 return null;
