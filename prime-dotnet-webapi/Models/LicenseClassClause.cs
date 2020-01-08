f81fa60ec052b5ea0fa05be93ec5f4cd10bb7188
Martin Pultz <mtpultz@gmail.com>
2020-01-08 03:56:44 -0700

    Added JsonIgnore to clauses to fix circular reference

--- prime-dotnet-webapi/Models/LicenseClassClause.cs
+++ prime-dotnet-webapi/Models/LicenseClassClause.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
 using Prime.Models.AccessAgreement;
 
 namespace Prime.Models
@@ -24,6 +25,7 @@ namespace Prime.Models
         [Required]
         public DateTime EffectiveDate { get; set; }
 
+        [JsonIgnore]
         public List<TermsOfAccessLicenseClassClause> TermsOfAccessLicenseClassClauses { get; set; }
     }
 }
