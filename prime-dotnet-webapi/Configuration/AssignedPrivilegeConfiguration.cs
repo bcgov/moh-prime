2f85a75e6e6d858b415fa5c49d869f9080282ce7
James <jameshollinger.ee@gmail.com>
2020-01-08 16:12:35 -0800

    update db configuration

--- prime-dotnet-webapi/Configuration/AssignedPrivilegeConfiguration.cs
+++ prime-dotnet-webapi/Configuration/AssignedPrivilegeConfiguration.cs
@@ -6,6 +6,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Prime.Models;
+
 namespace Prime.Configuration
 {
     public class AssignedPrivilegeConfiguration : IEntityTypeConfiguration<AssignedPrivilege>
