e77920d6f171f66d5b26244843c34fab90d6b46e
James <jameshollinger.ee@gmail.com>
2020-01-09 10:52:31 -0800

    Fixing tests

---
+++ prime-dotnet-webapi-tests/Mocks/TermsOfAccessMockService.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Prime.Models;
+using Prime.Services;
+
+namespace PrimeTests.Mocks
+{
+    public class TermsOfAccessServiceMock : BaseMockService, ITermsOfAccessService
+    {
+        public TermsOfAccessServiceMock() : base()
+        { }
+
+        public Task SetEnrolleeTermsOfAccessAsync(Enrollee enrollee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TermsOfAccess> GetEnrolleeTermsOfAccessAsync(int enrolleeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SeedData()
+        {
+            // no data to seed, as it is done in the base class
+        }
+    }
+}
