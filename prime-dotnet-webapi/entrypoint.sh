ce151701cd40512b8017d7e05319bba16db72a1a
Paul Martin <poggdogg@gmail.com>
2020-01-10 15:39:49 -0800

    adding more to entrypoint

--- prime-dotnet-webapi/entrypoint.sh
+++ prime-dotnet-webapi/entrypoint.sh
@@ -2,9 +2,10 @@
 #export MAIL_SERVER_URL=`route -n|grep "UG"|grep -v "UGH"|cut -f 10 -d " "`
 #export MAIL_SERVER_PORT=1025
 #dotnet prime.dll
-echo "Running .NET..."
+
 export DB_CONNECTION_STRING="host=${DB_HOST};port=5432;database=${POSTGRESQL_DATABASE};username=${POSTGRESQL_USER};password=${POSTGRESQL_ADMIN_PASSWORD}"
-/opt/rh/rh-dotnet22/root/usr/lib64/dotnet/dotnet prime.dll
+echo "Running .NET..."
+/opt/rh/rh-dotnet22/root/usr/lib64/dotnet/dotnet prime.dll &dissown 
 until [ "$response" -eq "401" ]
     echo "Waiting for the host ..."
     sleep 1
