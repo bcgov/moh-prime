357b4f2c84d8ed4a464af7c67092849fe75e023c
James Hollinger <39168456+james-hollinger@users.noreply.github.com>
2020-01-13 10:29:21 -0800

    Set Single Seeding Date (#221)
    * WIP
    
    * rest of config
    
    * Update migrations
    
    * remove using

--- prime-dotnet-webapi/Configuration/LimitsAndConditionsClauseConfiguration.cs
+++ prime-dotnet-webapi/Configuration/LimitsAndConditionsClauseConfiguration.cs
@@ -1,4 +1,3 @@
-using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Prime.Models;
@@ -7,14 +6,11 @@ namespace Prime.Configuration
 {
     public class LimitsAndConditionsClauseConfiguration : IEntityTypeConfiguration<LimitsAndConditionsClause>
     {
-        private readonly Guid SYSTEM_USER = Guid.Empty;
-        private readonly DateTime SEEDING_DATE = DateTime.Now;
-
         public void Configure(EntityTypeBuilder<LimitsAndConditionsClause> builder)
         {
             builder.HasData(
-                new LimitsAndConditionsClause { Id = 1, Clause = "Limit and condition 1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sit, rerum assumenda sed facere quam vel soluta suscipit esse neque quod.", EffectiveDate = DateTime.Now, CreatedUserId = SYSTEM_USER, CreatedTimeStamp = SEEDING_DATE, UpdatedUserId = SYSTEM_USER, UpdatedTimeStamp = SEEDING_DATE },
-                new LimitsAndConditionsClause { Id = 2, Clause = "Limit and condition 2 Adipisicing elit. Doloremque sit, rerum assumenda sed facere quam vel soluta suscipit esse neque quod.", EffectiveDate = DateTime.Now, CreatedUserId = SYSTEM_USER, CreatedTimeStamp = SEEDING_DATE, UpdatedUserId = SYSTEM_USER, UpdatedTimeStamp = SEEDING_DATE }
+                new LimitsAndConditionsClause { Id = 1, Clause = "Limit and condition 1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sit, rerum assumenda sed facere quam vel soluta suscipit esse neque quod.", EffectiveDate = SeedConstants.SEEDING_DATE, CreatedTimeStamp = SeedConstants.SEEDING_DATE, UpdatedTimeStamp = SeedConstants.SEEDING_DATE },
+                new LimitsAndConditionsClause { Id = 2, Clause = "Limit and condition 2 Adipisicing elit. Doloremque sit, rerum assumenda sed facere quam vel soluta suscipit esse neque quod.", EffectiveDate = SeedConstants.SEEDING_DATE, CreatedTimeStamp = SeedConstants.SEEDING_DATE, UpdatedTimeStamp = SeedConstants.SEEDING_DATE }
             );
         }
     }
---
+++ prime-dotnet-webapi/Configuration/SeedConstants.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Prime.Configuration
+{
+    public static class SeedConstants
+    {
+        public static readonly DateTime SEEDING_DATE = new DateTime(2019, 9, 16);
+    }
+}
