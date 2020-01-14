0e8c9d72d20032ec83b5aeb123f10820587fe78a
James <jameshollinger.ee@gmail.com>
2020-01-14 10:57:21 -0800

    wip

--- prime-dotnet-webapi/TEST.cs
+++ prime-dotnet-webapi/TEST.cs
@@ -10,13 +10,23 @@ namespace Prime
     {
         public static void DO_STUFF()
         {
-            var plant = new BasePlant();//.WithBlueprintsFromAssemblyOf<PersonBlueprint>();
-            plant.DefinePropertiesOf<FakeEnrollee>(new { FirstName = "Barbara", Addresses = new List<FakeXref>() });
+            // var plant = new BasePlant().WithBlueprintsFromAssemblyOf<PersonBlueprint>();
+            var plant = new BasePlant();
+            plant.DefinePropertiesOf<FakeEnrollee>(new
+            {
+                FirstName = "Barbara",
+                Addresses = new LazyProperty<ICollection<FakeXref>>(() =>
+                {
+
+                    return new List<FakeXref> { plant.Create<FakeXref>() };
+                })
+            });
             plant.DefinePropertiesOf<FakeAddress>(new
             {
                 Id = new Sequence<int>((sequenceValue) => sequenceValue),
-                Street = new Sequence<string>((sequenceValue) => "A street" + sequenceValue),
+                Street = new Sequence<string>((sequenceValue) => "street" + sequenceValue),
             });
+            plant.DefinePropertiesOf<FakeXref>(new { Occupant = new FakeEnrollee() });
             // var address = plant.Create<FakeAddress>();
             var enrollee = plant.Create<FakeEnrollee>();
             var t = 1;
