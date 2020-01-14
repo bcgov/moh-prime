8f95f83c812a24d43b2a4583c519fe0e9139c994
Martin Pultz <mtpultz@gmail.com>
2020-01-13 17:53:39 -0800

    Fix merge conflict

--- prime-angular-frontend/src/app/modules/enrolment/pages/declined-access-agreement/declined-access-agreement.component.spec.ts
+++ prime-angular-frontend/src/app/modules/enrolment/pages/declined-access-agreement/declined-access-agreement.component.spec.ts
@@ -1,10 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DeclinedAccessAgreementComponent } from './declined-access-agreement.component';
+import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
 import { PageHeaderComponent } from '@shared/components/page-header/page-header.component';
 import { AlertComponent } from '@shared/components/alert/alert.component';
 import { NgxMaterialModule } from '@shared/modules/ngx-material/ngx-material.module';
 import { ProgressIndicatorComponent } from '@shared/components/progress-indicator/progress-indicator.component';
+import { PrimeContactComponent } from '@shared/components/prime-contact/prime-contact.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { EnrolmentPipe } from '@shared/pipes/enrolment.pipe';
 
 describe('DeclinedAccessAgreementComponent', () => {
   let component: DeclinedAccessAgreementComponent;
@@ -14,13 +18,22 @@ describe('DeclinedAccessAgreementComponent', () => {
     TestBed.configureTestingModule(
       {
         imports: [
+          RouterTestingModule,
           NgxMaterialModule
         ],
         declarations: [
           DeclinedAccessAgreementComponent,
           PageHeaderComponent,
           ProgressIndicatorComponent,
-          AlertComponent
+          PrimeContactComponent,
+          AlertComponent,
+          EnrolmentPipe
+        ],
+        providers: [
+          {
+            provide: APP_CONFIG,
+            useValue: APP_DI_CONFIG
+          }
         ]
       }
     ).compileComponents();
