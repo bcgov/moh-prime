01f3c7807418b1e6ce76017070947f4c2683d54f
Martin Pultz <mtpultz@gmail.com>
2020-01-15 10:17:54 -0800

    Added mode binding to page component

--- prime-angular-frontend/src/app/shared/components/page/page.component.ts
+++ prime-angular-frontend/src/app/shared/components/page/page.component.ts
@@ -1,23 +1,19 @@
-import { Component, OnInit, ViewChild, AfterViewInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { Subscription } from 'rxjs';
-
-import { PageHeaderComponent } from '@shared/components/page-header/page-header.component';
 
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
   styleUrls: ['./page.component.scss']
 })
-export class PageComponent implements OnInit, AfterViewInit {
+export class PageComponent implements OnInit {
   @Input() public busy: Subscription;
-  @ViewChild(PageHeaderComponent, { static: false }) public pageHeader: PageHeaderComponent;
+  @Input() public mode: 'default' | 'full';
 
-  constructor() { }
+  constructor() {
+    this.mode = 'default';
+  }
 
   public ngOnInit() { }
-
-  public ngAfterViewInit() {
-    console.log('COUNT', this.pageHeader);
-  }
 }
