7c54f22b0d790fa1dea7ca2583957f7202cef62a
James <jameshollinger.ee@gmail.com>
2020-01-16 14:37:13 -0800

    test a thing?

--- prime-dotnet-webapi/Configuration/CollegeConfiguration.cs
+++ prime-dotnet-webapi/Configuration/CollegeConfiguration.cs
@@ -6,10 +6,7 @@ namespace Prime.Configuration
     public class CollegeConfiguration : SeededTable<College>
     {
         public override ICollection<College> SeedData
-        {
-            get
-            {
-                return new[] {
+        {            get            {                return new[] {
                 new College { Code = 1, Name = "College of Physicians and Surgeons of BC (CPSBC)", Prefix = "91", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
                 new College { Code = 2, Name = "College of Pharmacists of BC (CPBC)", Prefix = "P1", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
                 new College { Code = 3, Name = "BC College of Nursing Professionals (BCCNP)", Prefix = "96", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE }
