76708ab5fddce4e6e99c4409257681f4c49a4104
James <jameshollinger.ee@gmail.com>
2020-01-16 14:50:46 -0800

    change formatting for github diffs

--- prime-dotnet-webapi/Configuration/OrganizationTypeConfiguration.cs
+++ prime-dotnet-webapi/Configuration/OrganizationTypeConfiguration.cs
@@ -10,11 +10,11 @@ namespace Prime.Configuration
             get
             {
                 return new[] {
-                    new OrganizationType { Code = 1, Name = "Community Health Practice Access to PharmaNet (ComPAP)", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new OrganizationType { Code = 2, Name = "Health Authority", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new OrganizationType { Code = 3, Name = "Community Practice", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new OrganizationType { Code = 4, Name = "Community Pharmacy", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new OrganizationType { Code = 5, Name = "Primary Care Network", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE }
+                new OrganizationType { Code = 1, Name = "Community Health Practice Access to PharmaNet (ComPAP)", CreatedTimeStamp = SeedConstants.SEEDING_DATE, UpdatedTimeStamp = SeedConstants.SEEDING_DATE },
+                new OrganizationType { Code = 2, Name = "Health Authority", CreatedTimeStamp = SeedConstants.SEEDING_DATE, UpdatedTimeStamp = SeedConstants.SEEDING_DATE },
+                new OrganizationType { Code = 3, Name = "Community Practice", CreatedTimeStamp = SeedConstants.SEEDING_DATE, UpdatedTimeStamp = SeedConstants.SEEDING_DATE },
+                new OrganizationType { Code = 4, Name = "Community Pharmacy", CreatedTimeStamp = SeedConstants.SEEDING_DATE, UpdatedTimeStamp = SeedConstants.SEEDING_DATE },
+                new OrganizationType { Code = 5, Name = "Primary Care Network", CreatedTimeStamp = SeedConstants.SEEDING_DATE, UpdatedTimeStamp = SeedConstants.SEEDING_DATE }
                 };
             }
         }
--- prime-dotnet-webapi/Configuration/StatusConfiguration.cs
+++ prime-dotnet-webapi/Configuration/StatusConfiguration.cs
@@ -10,12 +10,12 @@ namespace Prime.Configuration
             get
             {
                 return new[] {
-                    new Status { Code = 1, Name = "In Progress", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new Status { Code = 2, Name = "Submitted", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new Status { Code = 3, Name = "Adjudicated/Approved", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new Status { Code = 4, Name = "Declined", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new Status { Code = 5, Name = "Accepted Access Agreement", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new Status { Code = 6, Name = "Declined Access Agreement", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE }
+                new Status { Code = 1, Name = "In Progress", CreatedTimeStamp = SeedConstants.SEEDING_DATE, UpdatedTimeStamp = SeedConstants.SEEDING_DATE },
+                new Status { Code = 2, Name = "Submitted", CreatedTimeStamp = SeedConstants.SEEDING_DATE, UpdatedTimeStamp = SeedConstants.SEEDING_DATE },
+                new Status { Code = 3, Name = "Adjudicated/Approved", CreatedTimeStamp = SeedConstants.SEEDING_DATE, UpdatedTimeStamp = SeedConstants.SEEDING_DATE },
+                new Status { Code = 4, Name = "Declined", CreatedTimeStamp = SeedConstants.SEEDING_DATE, UpdatedTimeStamp = SeedConstants.SEEDING_DATE },
+                new Status { Code = 5, Name = "Accepted Access Agreement", CreatedTimeStamp = SeedConstants.SEEDING_DATE, UpdatedTimeStamp = SeedConstants.SEEDING_DATE },
+                new Status { Code = 6, Name = "Declined Access Agreement", CreatedTimeStamp = SeedConstants.SEEDING_DATE, UpdatedTimeStamp = SeedConstants.SEEDING_DATE }
                 };
             }
         }
