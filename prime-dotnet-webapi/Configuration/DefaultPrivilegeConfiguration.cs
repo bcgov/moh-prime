d07ab7f62b3c944a60bfc273b3be5da7caafd932
James Hollinger <39168456+james-hollinger@users.noreply.github.com>
2020-01-16 16:14:32 -0800

    undo

--- prime-dotnet-webapi/Configuration/DefaultPrivilegeConfiguration.cs
+++ prime-dotnet-webapi/Configuration/DefaultPrivilegeConfiguration.cs
@@ -5,6 +5,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Prime.Models;
+
 namespace Prime.Configuration
 {
     public class DefaultPrivilegeConfiguration : SeededTable<DefaultPrivilege>
