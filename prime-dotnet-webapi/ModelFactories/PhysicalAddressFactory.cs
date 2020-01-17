011208d172cb9dd379bc79daf0ec4a2ad1aff2c2
James <jameshollinger.ee@gmail.com>
2020-01-17 11:27:09 -0800

    physical address

--- prime-dotnet-webapi/ModelFactories/PhysicalAddressFactory.cs
+++ prime-dotnet-webapi/ModelFactories/PhysicalAddressFactory.cs
@@ -1,10 +1,9 @@
 using Bogus;
 using Prime.Models;
-using Prime.Configuration;
 
 namespace Prime.ModelFactories
 {
-    public class PhysicalAddressFactory : Faker<Address>
+    public class PhysicalAddressFactory : Faker<PhysicalAddress>
     {
         private static int IdCounter = 1;
         public PhysicalAddressFactory(Enrollee owner)
@@ -13,15 +12,18 @@ namespace Prime.ModelFactories
             RuleFor(x => x.Id, () => IdCounter++);
             RuleFor(x => x.Enrollee, () => owner);
             RuleFor(x => x.EnrolleeId, () => owner.Id);
-            RuleFor(x => x.CountryCode, f => f.PickRandom(countries));
-            RuleFor(x => x.Country, f => f.PickRandom(CountryConfiguration));
-            RuleFor(x => x.ProvinceCode, TestUtils.RandomProvinceCode());
+            RuleFor(x => x.Country, (f, x) => x.Province.Country);
+            RuleFor(x => x.CountryCode, (f, x) => x.Country.Code);
+            RuleFor(x => x.Province, () => ProvinceLookup.BC);
+            RuleSet("notBC", (set) =>
+            {
+                set.RuleFor(x => x.Province, f => f.PickRandom(ProvinceLookup.NotBC));
+            });
+            RuleFor(x => x.ProvinceCode, (f, x) => x.Province.Code);
             RuleFor(x => x.Street, f => f.Address.StreetAddress());
+            RuleFor(x => x.Street2, f => f.Address.StreetAddress());
             RuleFor(x => x.City, f => f.Address.City());
             RuleFor(x => x.Postal, f => f.Address.ZipCode("?#?#?#"));
-
-
-
         }
     }
 }
