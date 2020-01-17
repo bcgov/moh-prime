ea32c56a803d675ee32d99a1e3297abe51c9c8c3
James <jameshollinger.ee@gmail.com>
2020-01-16 16:42:50 -0800

    fix merge

--- prime-dotnet-webapi/Configuration/OrganizationTypeConfiguration.cs
+++ prime-dotnet-webapi/Configuration/OrganizationTypeConfiguration.cs
@@ -9,11 +9,12 @@ namespace Prime.Configuration
         {
             get
             {
-                return new [] {
-                new OrganizationType { Code = 1, Name = "Health Authority", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                new OrganizationType { Code = 2, Name = "Community Practice", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                new OrganizationType { Code = 3, Name = "Community Pharmacy", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                new OrganizationType { Code = 4, Name = "Primary Care Network", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE }
+                return new[] {
+                    new OrganizationType { Code = 1, Name = "Health Authority", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new OrganizationType { Code = 2, Name = "Community Practice", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new OrganizationType { Code = 3, Name = "Community Pharmacy", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new OrganizationType { Code = 4, Name = "Primary Care Network", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE }
+                };
             }
         }
     }
