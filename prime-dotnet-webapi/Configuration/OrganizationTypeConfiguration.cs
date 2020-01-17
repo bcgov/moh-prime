fef8069709108d7bba7d9688e755a372285d7258
AnaisHebert <53796823+AnaisHebert@users.noreply.github.com>
2020-01-16 17:09:51 -0800

    Remove comPap, Disable pharmacists again (#246)
    * Remove comPap, Disable pharmacists again
    
    * fix merge
    
    * fix merge
    
    * merge
    
    * fix merge
    
    * Remake migrations
    
    Co-authored-by: James Hollinger <39168456+james-hollinger@users.noreply.github.com>

--- prime-dotnet-webapi/Configuration/OrganizationTypeConfiguration.cs
+++ prime-dotnet-webapi/Configuration/OrganizationTypeConfiguration.cs
@@ -10,11 +10,10 @@ namespace Prime.Configuration
             get
             {
                 return new[] {
-                    new OrganizationType { Code = 1, Name = "Community Health Practice Access to PharmaNet (ComPAP)", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new OrganizationType { Code = 2, Name = "Health Authority", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new OrganizationType { Code = 3, Name = "Community Practice", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new OrganizationType { Code = 4, Name = "Community Pharmacy", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new OrganizationType { Code = 5, Name = "Primary Care Network", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE }
+                    new OrganizationType { Code = 1, Name = "Health Authority", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new OrganizationType { Code = 2, Name = "Community Practice", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new OrganizationType { Code = 3, Name = "Community Pharmacy", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new OrganizationType { Code = 4, Name = "Primary Care Network", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE }
                 };
             }
         }
