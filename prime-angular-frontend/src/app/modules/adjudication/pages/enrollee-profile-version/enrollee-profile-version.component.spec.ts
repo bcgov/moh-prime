d0f8dd0a968128123ec1e70fbac44a7cd5fae46e
Martin Pultz <mtpultz@gmail.com>
2020-01-16 17:55:32 -0800

    Fix profile version test

--- prime-angular-frontend/src/app/modules/adjudication/pages/enrollee-profile-version/enrollee-profile-version.component.spec.ts
+++ prime-angular-frontend/src/app/modules/adjudication/pages/enrollee-profile-version/enrollee-profile-version.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { EnrolleeProfileVersionComponent } from './enrollee-profile-version.component';
 import { PageComponent } from '@shared/components/page/page.component';
 import { PageHeaderComponent } from '@shared/components/page-header/page-header.component';
+import { EnrolleeReviewComponent } from '@shared/components/enrollee/enrollee-review/enrollee-review.component';
 
 describe('EnrolleeProfileVersionComponent', () => {
   let component: EnrolleeProfileVersionComponent;
@@ -14,7 +15,8 @@ describe('EnrolleeProfileVersionComponent', () => {
         declarations: [
           EnrolleeProfileVersionComponent,
           PageComponent,
-          PageHeaderComponent
+          PageHeaderComponent,
+          EnrolleeReviewComponent
         ]
       }
     ).compileComponents();
