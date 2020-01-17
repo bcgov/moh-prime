6f951e593a14f53cf594314074539c281fb10d89
James <jameshollinger.ee@gmail.com>
2020-01-17 14:05:23 -0800

    token

---
+++ prime-dotnet-webapi/ModelFactories/EnrolmentCertificateAccessTokenFactory.cs
@@ -0,0 +1,29 @@
+using System;
+using Bogus;
+using Prime.Models;
+
+namespace Prime.ModelFactories
+{
+    public class EnrolmentCertificateAccessTokenFactory : Faker<EnrolmentCertificateAccessToken>
+    {
+        public EnrolmentCertificateAccessTokenFactory(Enrollee owner)
+        {
+            StrictMode(true);
+            RuleFor(x => x.Id, f => Guid.NewGuid());
+            RuleFor(x => x.Enrollee, f => owner);
+            RuleFor(x => x.EnrolleeId, f => owner.Id);
+            RuleFor(x => x.Expires, f => f.Date.Future());
+            RuleFor(x => x.ViewCount, 0);
+            RuleFor(x => x.Active, true);
+
+            RuleSet("invalid", (set) =>
+            {
+                set.RuleFor(x => x.Active, false);
+            });
+            RuleSet("expired", (set) =>
+            {
+                set.RuleFor(x => x.Expires, f => f.Date.Past());
+            });
+        }
+    }
+}
