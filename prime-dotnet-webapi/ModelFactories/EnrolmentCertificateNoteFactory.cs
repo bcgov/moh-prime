edf88f8ab0d279fb6503f221b5889e5ce9110b1a
James <jameshollinger.ee@gmail.com>
2020-01-17 17:46:23 -0800

    enrol note fac

---
+++ prime-dotnet-webapi/ModelFactories/EnrolmentCertificateNoteFactory.cs
@@ -0,0 +1,22 @@
+using System;
+using Bogus;
+using Prime.Models;
+
+namespace Prime.ModelFactories
+{
+    public class EnrolmentCertificateNoteFactory : Faker<EnrolmentCertificateNote>
+    {
+        private static int IdCounter = 1;
+
+        public EnrolmentCertificateNoteFactory(Enrollee owner)
+        {
+            this.SetBaseRules();
+
+            RuleFor(x => x.Id, f => IdCounter++);
+            RuleFor(x => x.Enrollee, f => owner);
+            RuleFor(x => x.EnrolleeId, f => owner.Id);
+            RuleFor(x => x.Note, f => f.Lorem.Paragraph(1));
+            RuleFor(x => x.NoteDate, f => f.Date.Past());
+        }
+    }
+}
