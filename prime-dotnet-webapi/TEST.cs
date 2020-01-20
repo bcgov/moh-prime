c57ef65669555f127cb24f719752b32f812fc6e8
James <jameshollinger.ee@gmail.com>
2020-01-20 14:21:31 -0800

    TEST

--- prime-dotnet-webapi/TEST.cs
+++ prime-dotnet-webapi/TEST.cs
@@ -16,8 +16,10 @@ namespace Prime
         {
             var owner = new Enrollee { Id = 1 };
 
-            var a1 = new PhysicalAddressFactory(owner).Generate(2);
-            var a2 = new MailingAddressFactory(owner).Generate(2);
+            var a1 = new AdjudicatorNoteFactory(owner).Generate(2);
+            var a2 = new AccessAgreementNoteFactory(owner).Generate(2);
+            var a3 = new EnrolmentCertificateNoteFactory(owner).Generate(2);
+            var add = new PhysicalAddressFactory(owner).Generate();
             var t = 1;
         }
     }
