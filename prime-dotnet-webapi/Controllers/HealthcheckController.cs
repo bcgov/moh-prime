3cfcd509a44b4c18da7264041ee97cb9766164d6
Anais <anais.hebert@nttdata.com>
2020-01-20 12:41:01 -0800

    healthcheck controller

---
+++ prime-dotnet-webapi/Controllers/HealthcheckController.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Prime.Models;
+using Prime.Services;
+
+namespace Prime.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class HealthcheckController : ControllerBase
+    {
+        private readonly IEnrolleeService _enrolleeService;
+
+        public HealthcheckController(IEnrolleeService enrolleeService)
+        {
+            _enrolleeService = enrolleeService;
+        }
+
+        // GET: api/Healthcheck
+        /// <summary>
+        /// Does a healthcheck that queries the enrollee table to wake up the database
+        /// </summary>
+        [HttpGet]
+        [ProducesResponseType(typeof(ApiOkResponse<Enrollee>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<Enrollee>> GetHealthcheck()
+        {
+            var enrollees = await _enrolleeService.GetEnrolleesAsync();
+            var empty = new Enrollee();
+            return Ok(new ApiOkResponse<Enrollee>(empty));
+        }
+    }
+}
