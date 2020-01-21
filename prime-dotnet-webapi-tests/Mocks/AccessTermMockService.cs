b6f31820b08888bc301abe8260f59842ac81415f
James Hollinger <39168456+james-hollinger@users.noreply.github.com>
2020-01-20 18:37:31 -0800

    Remove Availible Statuses (#272)
    * Removed available status in web client
    
    * remove avilible status from model
    
    * remove warnings
    
    Co-authored-by: Martin Pultz <mtpultz@users.noreply.github.com>

--- prime-dotnet-webapi-tests/Mocks/AccessTermMockService.cs
+++ prime-dotnet-webapi-tests/Mocks/AccessTermMockService.cs
@@ -13,24 +13,24 @@ namespace PrimeTests.Mocks
         public override void SeedData()
         { }
 
-        public async Task<AccessTerm> GetAccessTermAsync(Enrollee enrollee)
+        public Task<AccessTerm> GetAccessTermAsync(Enrollee enrollee)
         {
             throw new NotImplementedException();
         }
 
-        public async Task<AccessTerm> GetEnrolleeAccessTermsAsync(int enrolleeId)
+        public Task<AccessTerm> GetEnrolleeAccessTermsAsync(int enrolleeId)
         {
             throw new NotImplementedException();
         }
 
-        public async Task CreateEnrolleeAccessTermAsync(Enrollee enrollee)
+        public Task CreateEnrolleeAccessTermAsync(Enrollee enrollee)
         {
             throw new NotImplementedException();
         }
 
-        public async Task SetAcceptedDateForAccessTermAsync(Enrollee enrollee)
+        public Task SetAcceptedDateForAccessTermAsync(Enrollee enrollee)
         {
-            // throw new NotImplementedException();
+            return Task.CompletedTask;
         }
     }
 }
