247f093b3f340613247dfd76c8c1ba9b7887716c
James <jameshollinger.ee@gmail.com>
2020-01-21 12:48:24 -0800

    enrolementStatusFactory as enumerator

--- prime-dotnet-webapi/ModelFactories/EnrolmentStatusFactory.cs
+++ prime-dotnet-webapi/ModelFactories/EnrolmentStatusFactory.cs
@@ -1,4 +1,5 @@
-using System;
+using System.Linq;
+using System.Collections.Generic;
 using Bogus;
 using Prime.Models;
 
@@ -8,18 +9,36 @@ namespace Prime.ModelFactories
     {
         private static int IdCounter = 1;
 
-        public EnrolmentStatusFactory(Enrollee owner)
+        private IEnumerator<Status> _statusEnumerator;
+
+        public EnrolmentStatusFactory(Enrollee owner) : this(owner, Enumerable.Empty<Status>()) { }
+
+        public EnrolmentStatusFactory(Enrollee owner, IEnumerable<Status> statuses)
         {
+            _statusEnumerator = statuses.GetEnumerator();
+
             this.SetBaseRules();
 
             RuleFor(x => x.Id, f => IdCounter++);
             RuleFor(x => x.Enrollee, f => owner);
             RuleFor(x => x.EnrolleeId, f => owner.Id);
-            RuleFor(x => x.Status, f => f.PickRandom(StatusLookup.All));
+            RuleFor(x => x.Status, f => GetNextStatus() ?? f.PickRandom(StatusLookup.All));
             RuleFor(x => x.StatusCode, (f, x) => x.Status.Code);
             RuleFor(x => x.StatusDate, f => f.Date.Past());
             RuleFor(x => x.PharmaNetStatus, false);
             RuleFor(x => x.EnrolmentStatusReasons, f => null);
+
+            RuleSet("inProgress", (set) =>
+            {
+                RuleFor(x => x.Status, f => StatusLookup.InProgress);
+                RuleFor(x => x.StatusCode, (f, x) => x.Status.Code);
+
+            });
+        }
+
+        private Status GetNextStatus()
+        {
+            return _statusEnumerator.MoveNext() ? _statusEnumerator.Current : null;
         }
     }
 }
