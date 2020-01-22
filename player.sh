336ff1743286290790f9b3a315f0ab5399bcd52c
Paul Martin <poggdogg@gmail.com>
2020-01-22 09:24:00 -0800

    handling more args

--- player.sh
+++ player.sh
@@ -5,30 +5,30 @@ source tests.bash
 
 case "$1" in
     build)
-        build $2 $3 $4
-        echo "Extra params= $4"
+        build $2 $3 $@
+        echo "Extra params= $@"
         #> /dev/null 2>&1
         ;;
     deploy)
-        deploy $2 $3 $4
-        echo "Extra params= $4"
+        deploy $2 $3 $@
+        echo "Extra params= $@"
         #> /dev/null 2>&1
         ;;
     toolbelt)
-        toolbelt $2 $3 $4
-        echo "Extra params= $4"
+        toolbelt $2 $3 $@
+        echo "Extra params= $@"
         ;;
     scan)
         scan
         ;;
     zap)
-        zap $2 $3 $4
+        zap $2 $3 $@
         ;;
     occleanup)
         occleanup
         ;;
     nukenpave)
-        nukenpave $2 $3 $4
+        nukenpave $2 $3 $@
         ;;
     pipeline_args)
         pipeline_args $*
