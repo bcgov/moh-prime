53c79a760c2e4917b347f56068b651325bc289c3
Martin Pultz <mtpultz@gmail.com>
2020-01-21 20:16:12 -0800

    Fix broken front-end test

--- prime-angular-frontend/src/app/shared/components/enrollee/enrollee-profile/enrollee-profile.component.spec.ts
+++ prime-angular-frontend/src/app/shared/components/enrollee/enrollee-profile/enrollee-profile.component.spec.ts
@@ -1,13 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { EnrolleeProfileComponent } from './enrollee-profile.component';
+import { EnrolleePropertyErrorComponent } from '../enrollee-property-error/enrollee-property-error.component';
 import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
 import { DefaultPipe } from '@shared/pipes/default.pipe';
 import { FormatDatePipe } from '@shared/pipes/format-date.pipe';
 import { EnrolleePipe } from '@shared/pipes/enrollee.pipe';
 import { ConfigCodePipe } from '@config/config-code.pipe';
 import { PostalPipe } from '@shared/pipes/postal.pipe';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('EnrolleeProfileComponent', () => {
   let component: EnrolleeProfileComponent;
@@ -21,6 +22,7 @@ describe('EnrolleeProfileComponent', () => {
         ],
         declarations: [
           EnrolleeProfileComponent,
+          EnrolleePropertyErrorComponent,
           ConfigCodePipe,
           DefaultPipe,
           FormatDatePipe,
