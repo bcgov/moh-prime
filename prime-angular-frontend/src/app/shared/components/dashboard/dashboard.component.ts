596d83833e776d8bb3359410e3ad1acfc595276f
Martin Pultz <mtpultz@users.noreply.github.com>
2020-01-30 15:44:27 -0800

    Update dashboard.component.ts

--- prime-angular-frontend/src/app/shared/components/dashboard/dashboard.component.ts
+++ prime-angular-frontend/src/app/shared/components/dashboard/dashboard.component.ts
@@ -217,30 +217,7 @@ export class DashboardComponent implements OnInit {
               ].includes(enrolmentStatus)
             ),
             deemphasize: (!enrolment || (enrolment && !enrolment.profileCompleted)) ? true : false
-          },
-          // TODO hidden for now and possibly removed later
-          // {
-          //   name: 'Enrolment Log History',
-          //   icon: (
-          //     progressStatus !== ProgressStatus.FINISHED ||
-          //     [
-          //       EnrolmentStatus.DECLINED,
-          //       EnrolmentStatus.DECLINED_TOS
-          //     ].includes(enrolmentStatus)
-          //   )
-          //     ? 'lock'
-          //     : 'history',
-          //   route: EnrolmentRoutes.ENROLMENT_LOG_HISTORY,
-          //   showItem: true,
-          //   disabled: (
-          //     progressStatus !== ProgressStatus.FINISHED ||
-          //     [
-          //       EnrolmentStatus.DECLINED,
-          //       EnrolmentStatus.DECLINED_TOS
-          //     ].includes(enrolmentStatus)
-          //   ),
-          //   deemphasize: (!enrolment || (enrolment && !enrolment.profileCompleted)) ? true : false
-          // }
+          }
         ]
       }
     ];
