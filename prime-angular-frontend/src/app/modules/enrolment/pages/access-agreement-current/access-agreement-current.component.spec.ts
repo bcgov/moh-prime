ca1eff19b4e82711c1f078b74212946cd449d2ac
Nathan Hogman <nhogman@gmail.com>
2020-02-03 13:23:35 -0800

    update some teting

--- prime-angular-frontend/src/app/modules/enrolment/pages/access-agreement-current/access-agreement-current.component.spec.ts
+++ prime-angular-frontend/src/app/modules/enrolment/pages/access-agreement-current/access-agreement-current.component.spec.ts
@@ -1,12 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AccessAgreementCurrentComponent } from './access-agreement-current.component';
-import { AccessTermComponent } from '../access-agreement/components/access-term/access-term.component';
-import { PageHeaderComponent } from '@shared/components/page-header/page-header.component';
-import { PageSubheaderComponent } from '@shared/components/page-subheader/page-subheader.component';
-import { PageComponent } from '@shared/components/page/page.component';
-import { FormatDatePipe } from '@shared/pipes/format-date.pipe';
-import { SafeHtmlPipe } from '@shared/pipes/safe-html.pipe';
 import { NgxMaterialModule } from '@shared/modules/ngx-material/ngx-material.module';
 import { NgxContextualHelpModule } from '@shared/modules/ngx-contextual-help/ngx-contextual-help.module';
 import { NgxBusyModule } from '@shared/modules/ngx-busy/ngx-busy.module';
@@ -15,6 +8,8 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MockEnrolmentService } from 'test/mocks/mock-enrolment.service';
 import { EnrolmentService } from '@enrolment/shared/services/enrolment.service';
+import { EnrolmentModule } from '@enrolment/enrolment.module';
+import { SharedModule } from '@shared/shared.module';
 
 describe('AccessAgreementCurrentComponent', () => {
   let component: AccessAgreementCurrentComponent;
@@ -27,16 +22,9 @@ describe('AccessAgreementCurrentComponent', () => {
         NgxMaterialModule,
         NgxContextualHelpModule,
         HttpClientTestingModule,
-        RouterTestingModule
-      ],
-      declarations: [
-        AccessAgreementCurrentComponent,
-        AccessTermComponent,
-        PageHeaderComponent,
-        PageSubheaderComponent,
-        PageComponent,
-        FormatDatePipe,
-        SafeHtmlPipe
+        RouterTestingModule,
+        EnrolmentModule,
+        SharedModule
       ],
       providers: [
         {
--- prime-angular-frontend/src/app/modules/enrolment/pages/access-agreement/access-agreement.component.spec.ts
+++ prime-angular-frontend/src/app/modules/enrolment/pages/access-agreement/access-agreement.component.spec.ts
@@ -2,40 +2,21 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { MockEnrolmentService } from 'test/mocks/mock-enrolment.service';
-
 import { AccessAgreementComponent } from './access-agreement.component';
 import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
 import { KeycloakService } from 'keycloak-angular';
 import { NgxBusyModule } from '@shared/modules/ngx-busy/ngx-busy.module';
 import { NgxContextualHelpModule } from '@shared/modules/ngx-contextual-help/ngx-contextual-help.module';
 import { NgxMaterialModule } from '@shared/modules/ngx-material/ngx-material.module';
-import { AlertComponent } from '@shared/components/alert/alert.component';
-import { PageComponent } from '@shared/components/page/page.component';
-import { PageHeaderComponent } from '@shared/components/page-header/page-header.component';
-import { PageSubheaderComponent } from '@shared/components/page-subheader/page-subheader.component';
-import { ProgressIndicatorComponent } from '@shared/components/progress-indicator/progress-indicator.component';
-import { PrimeContactComponent } from '@shared/components/prime-contact/prime-contact.component';
-import { PrimePhoneComponent } from '@shared/components/prime-phone/prime-phone.component';
-import { PrimeEmailComponent } from '@shared/components/prime-email/prime-email.component';
-import { SafeHtmlPipe } from '@shared/pipes/safe-html.pipe';
 import { EnrolmentService } from '@enrolment/shared/services/enrolment.service';
-import { CollectionNoticeAlertComponent } from '@enrolment/shared/components/collection-notice-alert/collection-notice-alert.component';
-import { GlobalClauseComponent } from './components/global-clause/global-clause.component';
-import { UserClauseComponent } from './components/user-clause/user-clause.component';
-import { LicenceClassClauseComponent } from './components/licence-class-clause/licence-class-clause.component';
-import { LimitsAndConditionsClauseComponent } from './components/limits-and-conditions-clause/limits-and-conditions-clause.component';
-import { AccessTermsPagerComponent } from './components/access-terms-pager/access-terms-pager.component';
-import { RuAccessTermsComponent } from './components/ru-access-terms/ru-access-terms.component';
-import { OboAccessTermsComponent } from './components/obo-access-terms/obo-access-terms.component';
-import { AccessTermComponent } from './components/access-term/access-term.component';
-import { formatDate } from '@angular/common';
-import { FormatDatePipe } from '@shared/pipes/format-date.pipe';
+import { EnrolmentModule } from '@enrolment/enrolment.module';
+import { SharedModule } from '@shared/shared.module';
 
 describe('AccessAgreementComponent', () => {
   let component: AccessAgreementComponent;
   let fixture: ComponentFixture<AccessAgreementComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule(
       {
@@ -45,29 +26,9 @@ describe('AccessAgreementComponent', () => {
           NgxContextualHelpModule,
           NgxMaterialModule,
           ReactiveFormsModule,
-          RouterTestingModule
-        ],
-        declarations: [
-          AlertComponent,
-          AccessAgreementComponent,
-          PageComponent,
-          PageHeaderComponent,
-          PageSubheaderComponent,
-          ProgressIndicatorComponent,
-          PrimeContactComponent,
-          PrimePhoneComponent,
-          PrimeEmailComponent,
-          GlobalClauseComponent,
-          UserClauseComponent,
-          LicenceClassClauseComponent,
-          LimitsAndConditionsClauseComponent,
-          AccessTermsPagerComponent,
-          CollectionNoticeAlertComponent,
-          RuAccessTermsComponent,
-          OboAccessTermsComponent,
-          AccessTermComponent,
-          SafeHtmlPipe,
-          FormatDatePipe
+          RouterTestingModule,
+          EnrolmentModule,
+          SharedModule
         ],
         providers: [
           {
