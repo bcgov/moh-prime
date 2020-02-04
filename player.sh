f14c374d98126b82c53ad16eda4c1c1f4be7bea5
Paul Martin <poggdogg@gmail.com>
2020-02-04 11:47:10 -0800

    supressing stdout, keeping stderr

--- player.sh
+++ player.sh
@@ -5,11 +5,13 @@ source tests.bash
 
 case "$1" in
     build)
-        build $@ #> /dev/null 2>&1
+        build $@ > /dev/null
+        #> /dev/null 2>&1
         echo "Extra params= ${@:4}"
         ;;
     deploy)
-        deploy $@ #>  /dev/null 2>&1
+        deploy $@ > /dev/null
+        #>  /dev/null 2>&1
         echo "Extra params= ${@:4}"
         ;;
     toolbelt)
