f3e0f5d6ad977fedbcea4d80e3bfc90b3fc70927
Paul Martin <poggdogg@gmail.com>
2020-02-05 00:19:39 -0800

    locking down the postgres version, changing pid location for nginx

--- prime-angular-frontend/entrypoint.sh
+++ prime-angular-frontend/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 envsubst '$SUFFIX' < /etc/nginx/nginx.template.conf > /etc/nginx/conf.d/default.conf 
-nginx 
+nginx -g "pid /tmp/nginx.pid; worker_processes 2; daemon-off;"
 tail -f /dev/null
+
 #nginx -g "daemon-off;"
