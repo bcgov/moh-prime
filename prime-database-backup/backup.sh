dd31bb4c7159195e07dd9e7fa710a1e8b4d0f634
Paul Martin <poggdogg@gmail.com>
2020-02-05 02:58:15 -0800

    more stuff

--- prime-database-backup/backup.sh
+++ prime-database-backup/backup.sh
@@ -2,14 +2,14 @@
 logfile="/opt/backup/pgsql.log"
 backup_dir="/opt/backup"
 touch $logfile
-databases=`psql -h localhost -U postgres -q -c "\l" | sed -n 4,/\eof/p | grep -v rows\) | grep -v template0 | grep -v template1 | awk {'print $1'}`
+databases=`psql -h ${PG_HOST} -U ${PG_USER} -q -c "\l" | sed -n 4,/\eof/p | grep -v rows\) | grep -v template0 | grep -v template1 | awk {'print $1'}`
 
 echo "Starting backup of databases " >> $logfile
 for i in $databases; do
         dateinfo=`date '+%Y-%m-%d %H:%M:%S'`
         timeslot=`date '+%Y%m%d%H%M'`
-        /usr/bin/vacuumdb -z -h localhost -U postgres $i >/dev/null 2>&1
-        /usr/bin/pg_dump -U postgres -i -F c -b $i -h 127.0.0.1 -f $backup_dir/$i-database-$timeslot.backup
+        /usr/bin/vacuumdb -z -h ${PG_HOST} -U ${PG_USER} $i >/dev/null 2>&1
+        /usr/bin/pg_dump -U ${PG_USER} -i -F c -b $i -h 127.0.0.1 -f $backup_dir/$i-database-$timeslot.backup
         echo "Backup and Vacuum complete on $dateinfo for database: $i " >> $logfile
 done
 echo "Done backup of databases " >> $logfile
--- prime-database-backup/openshift.dockerfile
+++ prime-database-backup/openshift.dockerfile
@@ -7,8 +7,11 @@ ENV PG_PASSWORD "${POSTGRESQL_ADMIN_PASSWORD}"
 ENV PG_USERNAME "${POSTGRESQL_USER}"
 
 COPY . /opt/backup
-RUN chmod -R 777 /opt/backup && \
+RUN apt-get update -yqq && \ 
+    apt-get install -yqq inetutils-ping && \ 
+    chmod -R 777 /opt/backup && \
     chmod +x /opt/backup/backup.sh && \
     chmod +x /opt/backup/entrypoint.sh && \
     cp /opt/backup/backup.sh /etc/cron.d/
+
 CMD /opt/backup/entrypoint.sh
