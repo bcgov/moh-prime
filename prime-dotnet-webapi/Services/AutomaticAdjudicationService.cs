59081b254954fcc644647cb205ae59af7e1a0834
Anais <anais.hebert@nttdata.com>
2020-02-07 11:07:30 -0800

    Remove device provider rule check

--- prime-dotnet-webapi/Services/AutomaticAdjudicationService.cs
+++ prime-dotnet-webapi/Services/AutomaticAdjudicationService.cs
@@ -10,220 +10,221 @@ using Prime.Models;
 
 namespace Prime.Services
 {
-    public class AutomaticAdjudicationService : BaseService, IAutomaticAdjudicationService
-    {
-        private readonly List<IAutomaticAdjudicationRule> _rules;
+  public class AutomaticAdjudicationService : BaseService, IAutomaticAdjudicationService
+  {
+    private readonly List<IAutomaticAdjudicationRule> _rules;
 
-        public AutomaticAdjudicationService(
-            ApiDbContext context, IHttpContextAccessor httpContext, IPharmanetApiService pharmanetApiService)
-            : base(context, httpContext)
-        {
-            _rules = new List<IAutomaticAdjudicationRule>
+    public AutomaticAdjudicationService(
+        ApiDbContext context, IHttpContextAccessor httpContext, IPharmanetApiService pharmanetApiService)
+        : base(context, httpContext)
+    {
+      _rules = new List<IAutomaticAdjudicationRule>
             {
                 new SelfDeclarationRule(),
                 new AddressRule(),
                 new PharmanetValidationRule(pharmanetApiService),
-                new DeviceProviderRule(),
+                // TODO removed until after Community Practice
+                // new DeviceProviderRule(),
                 new LicenceClassRule(),
                 new AlwaysManualRule()
             };
-        }
-
-        public async Task<bool> QualifiesForAutomaticAdjudication(Enrollee enrollee)
-        {
-            // All rules must pass for this enrollee to qualify to be automatically adjudicated.
-            // Failing rules will add Status Reasons to the current status.
-            bool passed = true;
-            foreach (var rule in _rules)
-            {
-                passed &= await rule.ProcessRule(enrollee);
-            }
-
-            return passed;
-        }
-
-        public interface IAutomaticAdjudicationRule
-        {
-            Task<bool> ProcessRule(Enrollee enrollee);
-        }
-
-        public abstract class BaseAutomaticAdjudicationRule : IAutomaticAdjudicationRule
-        {
-            public async Task<bool> ProcessRule(Enrollee enrollee)
-            {
-                if (enrollee == null)
-                {
-                    throw new ArgumentNullException(nameof(enrollee));
-                }
-
-                return await ProcessRuleInternal(enrollee);
-            }
-
-            protected void AddReason(Enrollee enrollee, short statusReasonCode, string statusReasonNote = null)
-            {
-                var currentStatus = enrollee.CurrentStatus;
-                if (currentStatus == null)
-                {
-                    throw new InvalidOperationException($"Could not add Status Reason for Enrollee with UserId \"{enrollee.UserId}\", Current Status is invalid.");
-                }
-
-                currentStatus.AddStatusReason(statusReasonCode, statusReasonNote);
-            }
-
-            protected abstract Task<bool> ProcessRuleInternal(Enrollee enrollee);
-        }
-
-        // check to see if any of the self-declaration rules were answered as 'Yes'
-        public class SelfDeclarationRule : BaseAutomaticAdjudicationRule
-        {
-            protected override Task<bool> ProcessRuleInternal(Enrollee enrollee)
-            {
-                // check to see if any of the self-declaration rules were answered as 'Yes'
-                // note: if for some reason the question was not answered, we will assume 'Yes'
-                if (enrollee.HasConviction.GetValueOrDefault(true)
-                    || enrollee.HasDisciplinaryAction.GetValueOrDefault(true)
-                    || enrollee.HasPharmaNetSuspended.GetValueOrDefault(true)
-                    || enrollee.HasRegistrationSuspended.GetValueOrDefault(true))
-                {
-                    AddReason(enrollee, StatusReason.SELF_DECLARATION_CODE);
-                    return Task.FromResult(false);
-                }
-
-                return Task.FromResult(true);
-            }
-        }
-
-        // check to see if any of the addresses are outside of BC
-        public class AddressRule : BaseAutomaticAdjudicationRule
-        {
-            protected override Task<bool> ProcessRuleInternal(Enrollee enrollee)
-            {
-                // check to see if any of the addresses are outside of BC
-                var provinceCodes = new[] { enrollee.PhysicalAddress?.ProvinceCode, enrollee.MailingAddress?.ProvinceCode };
-                if (provinceCodes.Any(p => p != null
-                    && !p.Equals(Province.BRITISH_COLUMBIA_CODE, StringComparison.OrdinalIgnoreCase)))
-                {
-                    AddReason(enrollee, StatusReason.ADDRESS_CODE);
-                    return Task.FromResult(false);
-                }
-
-                return Task.FromResult(true);
-            }
-        }
-
-        // If enrollee has credentials, check to see if the college license is active in PharmaNet and matches the enrollee
-        public class PharmanetValidationRule : BaseAutomaticAdjudicationRule
-        {
-            private readonly IPharmanetApiService _pharmanetApiService;
-
-            public PharmanetValidationRule(IPharmanetApiService pharmanetApiService)
-            {
-                _pharmanetApiService = pharmanetApiService;
-            }
-
-            protected override async Task<bool> ProcessRuleInternal(Enrollee enrollee)
-            {
-                if (enrollee.Certifications?.Any() != true)
-                {
-                    // No certs to verify
-                    return true;
-                }
-
-                bool passed = true;
-
-                foreach (var cert in enrollee.Certifications)
-                {
-                    PharmanetCollegeRecord record = null;
-                    try
-                    {
-                        record = await _pharmanetApiService.GetCollegeRecordAsync(cert);
-                    }
-                    catch (PharmanetApiService.PharmanetCollegeApiException)
-                    {
-                        AddReason(enrollee, StatusReason.PHARMANET_ERROR_CODE, $"{cert.FullLicenseNumber}");
-                        passed = false;
-                        continue;
-                    }
-                    if (record == null)
-                    {
-                        AddReason(enrollee, StatusReason.NOT_IN_PHARMANET_CODE, $"{cert.FullLicenseNumber}");
-                        passed = false;
-                        continue;
-                    }
-
-                    if (!record.MatchesEnrolleeByName(enrollee))
-                    {
-                        AddReason(enrollee, StatusReason.NAME_DISCREPANCY_CODE, $"{cert.FullLicenseNumber} returned \"{record.firstName} {record.lastName}\".");
-                        passed = false;
-                    }
-                    if (record.dateofBirth.Date != enrollee.DateOfBirth.Date)
-                    {
-                        AddReason(enrollee, StatusReason.BIRTHDATE_DISCREPANCY_CODE, $"{cert.FullLicenseNumber} returned {record.dateofBirth.ToString("d MMM yyyy")}");
-                        passed = false;
-                    }
-                    if (record.status != "P")
-                    {
-                        AddReason(enrollee, StatusReason.PRACTICING_CODE, $"{cert.FullLicenseNumber}");
-                        passed = false;
-                    }
-                }
-
-                return passed;
-            }
-        }
-
-        // Check to see if the enrollee is a device or pump provider
-        public class DeviceProviderRule : BaseAutomaticAdjudicationRule
-        {
-            protected override Task<bool> ProcessRuleInternal(Enrollee enrollee)
-            {
-                if (!string.IsNullOrWhiteSpace(enrollee.DeviceProviderNumber)
-                    || enrollee.IsInsulinPumpProvider.GetValueOrDefault(true))
-                {
-                    AddReason(enrollee, StatusReason.PUMP_PROVIDER_CODE);
-                    return Task.FromResult(false);
-                }
-
-                return Task.FromResult(true);
-            }
-        }
-
-        // If the enrollee has licence classes, validate them
-        public class LicenceClassRule : BaseAutomaticAdjudicationRule
-        {
-            protected override Task<bool> ProcessRuleInternal(Enrollee enrollee)
-            {
-                var passed = true;
-                if (enrollee.Certifications?.Any() == true)
-                {
-                    foreach (var item in enrollee.Certifications)
-                    {
-                        if (item.License.Manual)
-                        {
-                            AddReason(enrollee, StatusReason.LICENCE_CLASS_CODE);
-                            passed = false;
-                            break;
-                        }
-                    }
-                }
-
-                return Task.FromResult(passed);
-            }
-        }
-
-        // Check if the enrollee has always manual flag set
-        public class AlwaysManualRule : BaseAutomaticAdjudicationRule
-        {
-
-            protected override Task<bool> ProcessRuleInternal(Enrollee enrollee)
-            {
-                if (enrollee.AlwaysManual)
-                {
-                    AddReason(enrollee, StatusReason.ALWAYS_MANUAL_CODE);
-                }
-
-                return Task.FromResult(!enrollee.AlwaysManual);
-            }
-        }
     }
+
+    public async Task<bool> QualifiesForAutomaticAdjudication(Enrollee enrollee)
+    {
+      // All rules must pass for this enrollee to qualify to be automatically adjudicated.
+      // Failing rules will add Status Reasons to the current status.
+      bool passed = true;
+      foreach (var rule in _rules)
+      {
+        passed &= await rule.ProcessRule(enrollee);
+      }
+
+      return passed;
+    }
+
+    public interface IAutomaticAdjudicationRule
+    {
+      Task<bool> ProcessRule(Enrollee enrollee);
+    }
+
+    public abstract class BaseAutomaticAdjudicationRule : IAutomaticAdjudicationRule
+    {
+      public async Task<bool> ProcessRule(Enrollee enrollee)
+      {
+        if (enrollee == null)
+        {
+          throw new ArgumentNullException(nameof(enrollee));
+        }
+
+        return await ProcessRuleInternal(enrollee);
+      }
+
+      protected void AddReason(Enrollee enrollee, short statusReasonCode, string statusReasonNote = null)
+      {
+        var currentStatus = enrollee.CurrentStatus;
+        if (currentStatus == null)
+        {
+          throw new InvalidOperationException($"Could not add Status Reason for Enrollee with UserId \"{enrollee.UserId}\", Current Status is invalid.");
+        }
+
+        currentStatus.AddStatusReason(statusReasonCode, statusReasonNote);
+      }
+
+      protected abstract Task<bool> ProcessRuleInternal(Enrollee enrollee);
+    }
+
+    // check to see if any of the self-declaration rules were answered as 'Yes'
+    public class SelfDeclarationRule : BaseAutomaticAdjudicationRule
+    {
+      protected override Task<bool> ProcessRuleInternal(Enrollee enrollee)
+      {
+        // check to see if any of the self-declaration rules were answered as 'Yes'
+        // note: if for some reason the question was not answered, we will assume 'Yes'
+        if (enrollee.HasConviction.GetValueOrDefault(true)
+            || enrollee.HasDisciplinaryAction.GetValueOrDefault(true)
+            || enrollee.HasPharmaNetSuspended.GetValueOrDefault(true)
+            || enrollee.HasRegistrationSuspended.GetValueOrDefault(true))
+        {
+          AddReason(enrollee, StatusReason.SELF_DECLARATION_CODE);
+          return Task.FromResult(false);
+        }
+
+        return Task.FromResult(true);
+      }
+    }
+
+    // check to see if any of the addresses are outside of BC
+    public class AddressRule : BaseAutomaticAdjudicationRule
+    {
+      protected override Task<bool> ProcessRuleInternal(Enrollee enrollee)
+      {
+        // check to see if any of the addresses are outside of BC
+        var provinceCodes = new[] { enrollee.PhysicalAddress?.ProvinceCode, enrollee.MailingAddress?.ProvinceCode };
+        if (provinceCodes.Any(p => p != null
+            && !p.Equals(Province.BRITISH_COLUMBIA_CODE, StringComparison.OrdinalIgnoreCase)))
+        {
+          AddReason(enrollee, StatusReason.ADDRESS_CODE);
+          return Task.FromResult(false);
+        }
+
+        return Task.FromResult(true);
+      }
+    }
+
+    // If enrollee has credentials, check to see if the college license is active in PharmaNet and matches the enrollee
+    public class PharmanetValidationRule : BaseAutomaticAdjudicationRule
+    {
+      private readonly IPharmanetApiService _pharmanetApiService;
+
+      public PharmanetValidationRule(IPharmanetApiService pharmanetApiService)
+      {
+        _pharmanetApiService = pharmanetApiService;
+      }
+
+      protected override async Task<bool> ProcessRuleInternal(Enrollee enrollee)
+      {
+        if (enrollee.Certifications?.Any() != true)
+        {
+          // No certs to verify
+          return true;
+        }
+
+        bool passed = true;
+
+        foreach (var cert in enrollee.Certifications)
+        {
+          PharmanetCollegeRecord record = null;
+          try
+          {
+            record = await _pharmanetApiService.GetCollegeRecordAsync(cert);
+          }
+          catch (PharmanetApiService.PharmanetCollegeApiException)
+          {
+            AddReason(enrollee, StatusReason.PHARMANET_ERROR_CODE, $"{cert.FullLicenseNumber}");
+            passed = false;
+            continue;
+          }
+          if (record == null)
+          {
+            AddReason(enrollee, StatusReason.NOT_IN_PHARMANET_CODE, $"{cert.FullLicenseNumber}");
+            passed = false;
+            continue;
+          }
+
+          if (!record.MatchesEnrolleeByName(enrollee))
+          {
+            AddReason(enrollee, StatusReason.NAME_DISCREPANCY_CODE, $"{cert.FullLicenseNumber} returned \"{record.firstName} {record.lastName}\".");
+            passed = false;
+          }
+          if (record.dateofBirth.Date != enrollee.DateOfBirth.Date)
+          {
+            AddReason(enrollee, StatusReason.BIRTHDATE_DISCREPANCY_CODE, $"{cert.FullLicenseNumber} returned {record.dateofBirth.ToString("d MMM yyyy")}");
+            passed = false;
+          }
+          if (record.status != "P")
+          {
+            AddReason(enrollee, StatusReason.PRACTICING_CODE, $"{cert.FullLicenseNumber}");
+            passed = false;
+          }
+        }
+
+        return passed;
+      }
+    }
+
+    // Check to see if the enrollee is a device or pump provider
+    public class DeviceProviderRule : BaseAutomaticAdjudicationRule
+    {
+      protected override Task<bool> ProcessRuleInternal(Enrollee enrollee)
+      {
+        if (!string.IsNullOrWhiteSpace(enrollee.DeviceProviderNumber)
+            || enrollee.IsInsulinPumpProvider.GetValueOrDefault(true))
+        {
+          AddReason(enrollee, StatusReason.PUMP_PROVIDER_CODE);
+          return Task.FromResult(false);
+        }
+
+        return Task.FromResult(true);
+      }
+    }
+
+    // If the enrollee has licence classes, validate them
+    public class LicenceClassRule : BaseAutomaticAdjudicationRule
+    {
+      protected override Task<bool> ProcessRuleInternal(Enrollee enrollee)
+      {
+        var passed = true;
+        if (enrollee.Certifications?.Any() == true)
+        {
+          foreach (var item in enrollee.Certifications)
+          {
+            if (item.License.Manual)
+            {
+              AddReason(enrollee, StatusReason.LICENCE_CLASS_CODE);
+              passed = false;
+              break;
+            }
+          }
+        }
+
+        return Task.FromResult(passed);
+      }
+    }
+
+    // Check if the enrollee has always manual flag set
+    public class AlwaysManualRule : BaseAutomaticAdjudicationRule
+    {
+
+      protected override Task<bool> ProcessRuleInternal(Enrollee enrollee)
+      {
+        if (enrollee.AlwaysManual)
+        {
+          AddReason(enrollee, StatusReason.ALWAYS_MANUAL_CODE);
+        }
+
+        return Task.FromResult(!enrollee.AlwaysManual);
+      }
+    }
+  }
 }
