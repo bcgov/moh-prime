d0aaca40637bd3095c4ddfea895453a99e53dbc0
Martin Pultz <mtpultz@gmail.com>
2020-02-13 15:37:28 -0800

    Updated info

--- prime-angular-frontend/src/app/shared/shared.module.ts
+++ prime-angular-frontend/src/app/shared/shared.module.ts
@@ -46,15 +46,13 @@ import { EnrolleeProfileComponent } from '@shared/components/enrollee/enrollee-p
 import { EnrolleeAddressComponent } from '@shared/components/enrollee/enrollee-address/enrollee-address.component';
 import { EnrolleePrivilegesComponent } from '@shared/components/enrollee/enrollee-privileges/enrollee-privileges.component';
 import { EnrolleeOrganizationsComponent } from '@shared/components/enrollee/enrollee-organizations/enrollee-organizations.component';
-import { PrimeEmailComponent } from './components/prime-email/prime-email.component';
-import { PrimePhoneComponent } from './components/prime-phone/prime-phone.component';
 import { PrimeEmailComponent } from '@shared/components/prime-email/prime-email.component';
 import { PrimePhoneComponent } from '@shared/components/prime-phone/prime-phone.component';
 import { PrimeLogoComponent } from '@shared/components/prime-logo/prime-logo.component';
 import { ApproveEnrolmentComponent } from '@shared/components/dialogs/content/approve-enrolment/approve-enrolment.component';
 import { SafeHtmlPipe } from '@shared/pipes/safe-html.pipe';
 import { MarkdownComponent } from '@shared/components/dialogs/content/markdown/markdown.component';
-import { PrimeSupportEmailComponent } from './components/prime-support-email/prime-support-email.component';
+import { PrimeSupportEmailComponent } from '@shared/components/prime-support-email/prime-support-email.component';
 
 @NgModule({
   declarations: [
@@ -156,7 +154,7 @@ import { PrimeSupportEmailComponent } from './components/prime-support-email/pri
     PrimeEmailComponent,
     PrimePhoneComponent,
     PrimeLogoComponent,
-    SafeHtmlPipe
+    SafeHtmlPipe,
     PrimeSupportEmailComponent
   ],
   entryComponents: [
