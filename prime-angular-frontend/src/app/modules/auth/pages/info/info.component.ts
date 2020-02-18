fc8a40265d77c1cc5b0f855b97931e56ce2d1bb0
Martin Pultz <mtpultz@users.noreply.github.com>
2020-02-18 11:09:25 -0800

    Updates for Demo (#391)
    * Update prime transaction history to prime history
    
    * Remove lorem
    
    * Remove lorem
    
    * Update certificate expired text
    
    * Filter community practice to top
    
    * Updated mobile card link
    
    * Scroll to pill
    
    * Updated support email on collection page
    
    * Updated text

--- prime-angular-frontend/src/app/modules/auth/pages/info/info.component.ts
+++ prime-angular-frontend/src/app/modules/auth/pages/info/info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Inject } from '@angular/core';
 
 import { APP_CONFIG, AppConfig } from 'app/app-config.module';
@@ -13,6 +13,7 @@ import { UtilsService } from '@core/services/utils.service';
   styleUrls: ['./info.component.scss']
 })
 export class InfoComponent implements OnInit {
+  @ViewChild('learnmore', { static: true }) public learnMore: ElementRef;
   public isIE: boolean;
 
   constructor(
@@ -31,6 +32,10 @@ export class InfoComponent implements OnInit {
       idpHint: AuthProvider.BCSC,
       redirectUri
     });
+  }
+
+  public scrollTo() {
+    this.utilsService.scrollTo(this.learnMore.nativeElement);
   }
 
   public ngOnInit() { }
