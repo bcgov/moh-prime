88582cd6236859353a74e451f33750ed4ebefa17
James <jameshollinger.ee@gmail.com>
2020-02-18 16:34:06 -0800

    Update file loading

--- prime-dotnet-webapi/Configuration/UserClauseConfiguration.cs
+++ prime-dotnet-webapi/Configuration/UserClauseConfiguration.cs
@@ -1,33 +1,53 @@
-using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Collections.Generic;
 using Prime.Models;
 
 namespace Prime.Configuration
 {
     public class UserClauseConfiguration : SeededTable<UserClause>
     {
-        public string getClause(string file)
-        {
-            string clauseFile = Path.GetFullPath("./Configuration/assets/" + file);
+        private string ruClause;
+        private string oboClause;
 
-            if (File.Exists(clauseFile))
-            {
-                return System.IO.File.ReadAllText(clauseFile);
-            }
-            else if (clauseFile.Contains("prime-dotnet-webapi-tests"))
-            {
-                return "";
-            }
-            throw new FileNotFoundException("Could not find file");
-        }
         public override ICollection<UserClause> SeedData
         {
             get
             {
+                if (string.IsNullOrWhiteSpace(ruClause))
+                {
+                    LoadResources();
+                }
+
                 return new[] {
-                    new UserClause { Id = 1, Clause = this.getClause("obo-access-terms.html"), EnrolleeClassification = PrimeConstants.PRIME_OBO, EffectiveDate = SEEDING_DATE, CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new UserClause { Id = 2, Clause = this.getClause("ru-access-terms.html"), EnrolleeClassification = PrimeConstants.PRIME_RU, EffectiveDate = SEEDING_DATE, CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE }
+                    new UserClause { Id = 1, Clause = ruClause, EnrolleeClassification = PrimeConstants.PRIME_OBO, EffectiveDate = SEEDING_DATE, CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new UserClause { Id = 2, Clause = oboClause, EnrolleeClassification = PrimeConstants.PRIME_RU, EffectiveDate = SEEDING_DATE, CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE }
                 };
+            }
+        }
+
+        private void LoadResources()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceNames = assembly.GetManifestResourceNames();
+
+            string ruResource = resourceNames
+              .Single(str => str.EndsWith("ru-access-terms.html"));
+
+            using (Stream stream = assembly.GetManifestResourceStream(ruResource))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                ruClause = reader.ReadToEnd();
+            }
+
+            string oboResource = resourceNames
+              .Single(str => str.EndsWith("obo-access-terms.html"));
+
+            using (Stream stream = assembly.GetManifestResourceStream(oboResource))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                oboClause = reader.ReadToEnd();
             }
         }
     }
