5b625b19f9d730faa8bbeda7c13e7b226b968b4e
James <jameshollinger.ee@gmail.com>
2020-02-18 18:19:45 -0800

    refactor

--- prime-dotnet-webapi/Configuration/UserClauseConfiguration.cs
+++ prime-dotnet-webapi/Configuration/UserClauseConfiguration.cs
@@ -1,8 +1,6 @@
-using System.IO;
-using System.Linq;
-using System.Reflection;
 using System.Collections.Generic;
 using Prime.Models;
+using Prime.Configuration.Resources;
 
 namespace Prime.Configuration
 {
@@ -15,39 +13,16 @@ namespace Prime.Configuration
         {
             get
             {
-                if (string.IsNullOrWhiteSpace(ruClause))
+                if (string.IsNullOrWhiteSpace(ruClause) || string.IsNullOrWhiteSpace(oboClause))
                 {
-                    LoadResources();
+                    ruClause = ResourceLoader.Load("ru-access-terms.html");
+                    oboClause = ResourceLoader.Load("obo-access-terms.html");
                 }
 
                 return new[] {
                     new UserClause { Id = 1, Clause = oboClause, EnrolleeClassification = PrimeConstants.PRIME_OBO, EffectiveDate = SEEDING_DATE, CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
                     new UserClause { Id = 2, Clause = ruClause, EnrolleeClassification = PrimeConstants.PRIME_RU, EffectiveDate = SEEDING_DATE, CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE }
                 };
-            }
-        }
-
-        private void LoadResources()
-        {
-            var assembly = Assembly.GetExecutingAssembly();
-            var resourceNames = assembly.GetManifestResourceNames();
-
-            string ruResource = resourceNames
-              .Single(str => str.EndsWith("ru-access-terms.html"));
-
-            using (Stream stream = assembly.GetManifestResourceStream(ruResource))
-            using (StreamReader reader = new StreamReader(stream))
-            {
-                ruClause = reader.ReadToEnd();
-            }
-
-            string oboResource = resourceNames
-              .Single(str => str.EndsWith("obo-access-terms.html"));
-
-            using (Stream stream = assembly.GetManifestResourceStream(oboResource))
-            using (StreamReader reader = new StreamReader(stream))
-            {
-                oboClause = reader.ReadToEnd();
             }
         }
     }
