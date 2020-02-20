597d51b4b2b2931682f0b8df0ba3ef484ce7def9
Anais <anais.hebert@nttdata.com>
2020-02-20 13:22:33 -0800

    Admin Controller

--- prime-dotnet-webapi/Controllers/AdminsController.cs
+++ prime-dotnet-webapi/Controllers/AdminsController.cs
@@ -1,5 +1,9 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Prime.Models;
+using Prime.Models.Api;
 using Prime.Services;
 
 namespace Prime.Controllers
@@ -16,6 +20,61 @@ namespace Prime.Controllers
         public AdminsController(IAdminService adminService)
         {
             _adminService = adminService;
+        }
+
+        // POST: api/Admins
+        /// <summary>
+        /// Creates a new Admin.
+        /// </summary>
+        [HttpPost(Name = nameof(CreateAdmin))]
+        [ProducesResponseType(typeof(ApiBadRequestResponse), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(ApiCreatedResponse<Admin>), StatusCodes.Status201Created)]
+        public async Task<ActionResult<Admin>> CreateAdmin(Admin admin)
+        {
+            if (admin == null)
+            {
+                this.ModelState.AddModelError("Admin", "Could not create an admin, the passed in Admin cannot be null.");
+                return BadRequest(new ApiBadRequestResponse(this.ModelState));
+            }
+
+            // Check to see if this userId is already an admin, if so, reject creating another
+            if (await _adminService.AdminUserIdExistsAsync(admin.UserId))
+            {
+                this.ModelState.AddModelError("Admin.UserId", "An admin already exists for this User Id, only one admin is allowed per User Id.");
+                return BadRequest(new ApiBadRequestResponse(this.ModelState));
+            }
+
+            var createdAdminId = await _adminService.CreateAdminAsync(admin);
+
+            return CreatedAtAction(
+                nameof(GetAdminById),
+                new { adminId = createdAdminId },
+                new ApiCreatedResponse<Admin>(admin)
+            );
+        }
+
+
+        // GET: api/Admins/5
+        /// <summary>
+        /// Gets a specific Admin.
+        /// </summary>
+        /// <param name="adminId"></param>
+        [HttpGet("{adminId}", Name = nameof(GetAdminById))]
+        [ProducesResponseType(typeof(ApiBadRequestResponse), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ApiOkResponse<Admin>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<Admin>> GetAdminById(int adminId)
+        {
+            var admin = await _adminService.GetAdminAsync(adminId);
+
+            if (admin == null)
+            {
+                return NotFound(new ApiResponse(404, $"Admin not found with id {adminId}"));
+            }
+
+            return Ok(new ApiOkResponse<Admin>(admin));
         }
     }
 }
