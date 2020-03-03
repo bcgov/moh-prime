58757262d7dc7ef7843c3258ed4adedf992dd919
James <jameshollinger.ee@gmail.com>
2020-03-02 17:15:59 -0800

    *Testing errors, revert this

--- prime-dotnet-webapi/Services/PharmanetAPIService.cs
+++ prime-dotnet-webapi/Services/PharmanetAPIService.cs
@@ -164,7 +164,7 @@ namespace Prime.Services
             {
                 applicationUUID = Guid.NewGuid().ToString();
                 programArea = "PRIME";
-                this.licenceNumber = licenceNumber;
+                //this.licenceNumber = licenceNumber;
                 this.collegeReferenceId = collegeReferenceId;
             }
         }
