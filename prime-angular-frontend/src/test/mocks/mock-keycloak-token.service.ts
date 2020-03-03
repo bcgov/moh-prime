d2a04f4bc891b69c3bb0af04427b89ed11a22334
Nathan Hogman <nhogman@gmail.com>
2020-03-03 10:14:05 -0800

    update visibility

--- prime-angular-frontend/src/test/mocks/mock-keycloak-token.service.ts
+++ prime-angular-frontend/src/test/mocks/mock-keycloak-token.service.ts
@@ -1,37 +1,97 @@
 import { Token } from '@auth/shared/services/token.interface';
+import { User } from '@auth/shared/models/user.model';
+import { Admin } from '@auth/shared/models/admin.model';
+import { Role } from '@auth/shared/enum/role.enum';
+import * as faker from 'faker';
+import { KeycloakTokenParsed, KeycloakLoginOptions } from 'keycloak-js';
 
 export class MockKeycloakTokenService implements Token {
-  decodeToken() {
-    throw new Error("Method not implemented.");
-  } clearToken(): void {
-    throw new Error("Method not implemented.");
+  // tslint:disable-next-line: variable-name
+  private _role: Role;
+  // tslint:disable-next-line: variable-name
+  private _loggedIn: boolean;
+
+  public hasJustLoggedIn: boolean;
+
+  constructor(
+  ) {
+    this._loggedIn = false;
   }
-  isTokenExpired(): boolean {
-    throw new Error("Method not implemented.");
+
+  public set role(role: Role) {
+    this._role = role;
   }
-  login(): Promise<void> {
-    throw new Error("Method not implemented.");
+
+  public set loggedIn(loggedIn: boolean) {
+    this._loggedIn = loggedIn;
   }
-  logout(redirectUri: string): Promise<void> {
-    throw new Error("Method not implemented.");
+
+  public getUserId(): Promise<string> {
+    throw new Error('Method not implemented.');
   }
-  isLoggedIn(): Promise<boolean> {
-    throw new Error("Method not implemented.");
+
+  public getUser(forceReload?: boolean): Promise<User> {
+    return new Promise((resolve, reject) => resolve({
+      userId: `${faker.random.uuid()}`,
+      hpdid: `${faker.random.uuid()}`,
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      dateOfBirth: faker.date.past().toISOString(),
+      physicalAddress: {
+        countryCode: faker.address.countryCode(),
+        provinceCode: faker.address.stateAbbr(),
+        street: faker.address.streetAddress(),
+        city: faker.address.city(),
+        postal: faker.address.zipCode()
+      },
+      contactEmail: faker.internet.email()
+    }));
   }
-  getUserId(): Promise<string> {
-    throw new Error("Method not implemented.");
+
+  public getAdmin(forceReload?: boolean): Promise<Admin> {
+    return new Promise((resolve, reject) => resolve({
+      userId: `${faker.random.uuid()}`,
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      email: faker.internet.email(),
+      idir: `${faker.random.uuid()}`,
+    }));
   }
-  getUser(forceReload?: boolean): Promise<import("../../app/modules/auth/shared/models/user.model").User> {
-    throw new Error("Method not implemented.");
+
+  public getUserRoles(): string[] {
+    return [this._role];
   }
-  getAdmin(forceReload?: boolean): Promise<import("../../app/modules/auth/shared/models/admin.model").Admin> {
-    throw new Error("Method not implemented.");
+
+  public isUserInRole(role: string): boolean {
+    return this.getUserRoles().includes(role);
   }
-  getUserRoles(): string[] {
-    throw new Error("Method not implemented.");
+
+  public decodeToken(): Promise<KeycloakTokenParsed> {
+    throw new Error('Method not implemented.');
   }
-  isUserInRole(role: string): boolean {
-    throw new Error("Method not implemented.");
+
+  public login(options?: KeycloakLoginOptions): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+
+  public isLoggedIn(): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      this._loggedIn
+        ? resolve(true)
+        : reject(false);
+    });
+  }
+
+  public logout(redirectUri: string): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+
+  public isTokenExpired(): boolean {
+    throw new Error('Method not implemented.');
+  }
+
+  public clearToken(): void {
+    throw new Error('Method not implemented.');
   }
 
 }
