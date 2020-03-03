f1ca484745262fc4ad39a411e56e9f0d3a694ae4
Martin Pultz <mtpultz@gmail.com>
2020-03-02 17:40:48 -0800

    Added pagination model

---
+++ prime-angular-frontend/src/app/core/resources/models/api-resource-pagination.model.ts
@@ -0,0 +1,11 @@
+export class ApiResourcePagination {
+  public page?: number;
+  public perPage?: number;
+  public total?: number;
+
+  constructor({ page, perPage: perPage }: Partial<ApiResourcePagination> = { page: 1, perPage: 20 }) {
+    this.page = page || 1;
+    this.perPage = perPage || 20;
+    this.total = 0;
+  }
+}
