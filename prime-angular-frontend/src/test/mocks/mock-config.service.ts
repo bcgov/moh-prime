f33ec9d95db0542996a7792291a68f66e611f918
Martin Pultz <mtpultz@gmail.com>
2020-03-03 10:50:31 -0800

    Fix tests

--- prime-angular-frontend/src/test/mocks/mock-config.service.ts
+++ prime-angular-frontend/src/test/mocks/mock-config.service.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Inject } from '@angular/core';
 
 import { Observable } from 'rxjs';
@@ -6,13 +5,14 @@ import { Observable } from 'rxjs';
 import { APP_CONFIG, AppConfig } from 'app/app-config.module';
 import { Configuration } from '@config/config.model';
 import { IConfigService, ConfigService } from '@config/config.service';
+import { ApiResource } from '@core/resources/api-resource.service';
 
 export class MockConfigService extends ConfigService implements IConfigService {
   constructor(
     @Inject(APP_CONFIG) protected config: AppConfig,
-    protected http: HttpClient
+    protected apiResource: ApiResource
   ) {
-    super(config, http);
+    super(config, apiResource);
 
     // Load the runtime configuration
     this.load().subscribe();
