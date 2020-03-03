c2f6a47bec095fe442a237fe4cb29f87cce83b28
Martin Pultz <mtpultz@gmail.com>
2020-03-03 11:06:05 -0800

    Fix for api resource test

--- prime-angular-frontend/src/app/core/resources/api-resource.service.spec.ts
+++ prime-angular-frontend/src/app/core/resources/api-resource.service.spec.ts
@@ -1,11 +1,18 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { ApiResource } from './api-resource.service';
+import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
 
 describe('ApiResource', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ApiResource]
+      providers: [
+        ApiResource,
+        {
+          provide: APP_CONFIG,
+          useValue: APP_DI_CONFIG
+        }
+      ]
     });
   });
 
