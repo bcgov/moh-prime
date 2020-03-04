12898a9659e44538b36acfcd629b0a027edc58d3
James <jameshollinger.ee@gmail.com>
2020-03-03 18:17:11 -0800

    extras

--- prime-dotnet-webapi/Services/interfaces/ILookupService.cs
+++ prime-dotnet-webapi/Services/interfaces/ILookupService.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Prime.Models;
+using Prime.Models.Api;
 
 namespace Prime.Services
 {
     public interface ILookupService
-    {            
+    {
         Task<LookupEntity> GetLookupsAsync();
     }
 }
