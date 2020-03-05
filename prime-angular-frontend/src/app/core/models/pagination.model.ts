cff913277c140b0c86122358d277dba7df79ec55
Martin Pultz <mtpultz@gmail.com>
2020-03-04 16:20:59 -0800

    Moved pagination model

--- prime-angular-frontend/src/app/core/models/pagination.model.ts
+++ prime-angular-frontend/src/app/core/models/pagination.model.ts
@@ -1,9 +1,9 @@
-export class ApiResourcePagination {
+export class Pagination {
   public page?: number;
   public perPage?: number;
   public total?: number;
 
-  constructor({ page, perPage: perPage }: Partial<ApiResourcePagination> = { page: 1, perPage: 20 }) {
+  constructor({ page, perPage: perPage }: Partial<Pagination> = { page: 1, perPage: 20 }) {
     this.page = page || 1;
     this.perPage = perPage || 20;
     this.total = 0;
