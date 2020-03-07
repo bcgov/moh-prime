e6b42b3c7d53ceedd8054d022ab6c5e4d949cdf1
Paul Martin <poggdogg@gmail.com>
2020-03-06 17:21:24 -0800

    Tweaking tests

--- prime-dotnet-webapi/entrypoint.sh
+++ prime-dotnet-webapi/entrypoint.sh
@@ -28,7 +28,6 @@ fi
 psql -h $DB_HOST -U ${POSTGRESQL_USER} -d ${POSTGRESQL_DATABASE} -a -f databaseMigrations.sql
 
 echo "Resting 5 seconds to let things settle down..."
-
 echo "Running .NET..."
 dotnet prime.dll -v 2>&1 | ts > $logfile & 
 echo "Launched, waiting for connection to API internally..."
@@ -44,21 +43,27 @@ echo "$1 responded $2"
 }
 
 function pharmanetCheck() {
+    UUID=$(cat /proc/sys/kernel/random/uuid)
+    AUTH=$(printf $PHARMANET_API_USERNAME:$PHARMANET_API_PASSWORD|base64)
+    printf {\"applicationUUID\":\"${UUID}\"\,\"programArea\":\"PRIME\"\,\"licenceNumber\":\"20361\"\,\"collegeReferenceId\":\"P1\"} > /tmp/data.out
     openssl pkcs12 -in $PHARMANET_SSL_CERT_FILENAME -out /tmp/pharmanet-api-cert.pem -nodes -passin pass:$PHARMANET_SSL_CERT_PASSWORD
     curl -s -o /dev/null -w "%{http_code}" --cert /tmp/pharmanet-api-cert.pem \
     -H "Authorization: Basic $AUTH" \
     -H "Content-Type: application/json" $PHARMANET_API_URL \
     -H "Accept: application/json" \
-    --data '{"applicationUUID":"db645527-8765-4811-bf63-2b00af1887d5","programArea":"PRIME","licenceNumber":"20361","collegeReferenceId":"P1"}' 
+    --data "@/tmp/data.out"
 }
 
 function pharmanetVerboseCheck() {
+    UUID=$(cat /proc/sys/kernel/random/uuid)
+    AUTH=$(printf $PHARMANET_API_USERNAME:$PHARMANET_API_PASSWORD|base64)
+    printf {\"applicationUUID\":\"${UUID}\"\,\"programArea\":\"PRIME\"\,\"licenceNumber\":\"20361\"\,\"collegeReferenceId\":\"P1\"} > /tmp/data.out
     openssl pkcs12 -in $PHARMANET_SSL_CERT_FILENAME -out /tmp/pharmanet-api-cert.pem -nodes -passin pass:$PHARMANET_SSL_CERT_PASSWORD
     curl -v -s -o /dev/null --cert /tmp/pharmanet-api-cert.pem \
     -H "Authorization: Basic $AUTH" \
     -H "Content-Type: application/json" $PHARMANET_API_URL \
     -H "Accept: application/json" \
-    --data '{"applicationUUID":"db645527-8765-4811-bf63-2b00af1887d5","programArea":"PRIME","licenceNumber":"20361","collegeReferenceId":"P1"}'
+    --data "@/tmp/data.out"
 }
 
 waitForIt localhost:${API_PORT}/api/enrollees 401 2>&1 | logger & 
