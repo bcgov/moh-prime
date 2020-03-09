088601979ef68d335c5a6d74b1145efb5e3d3cad
Paul Martin <poggdogg@gmail.com>
2020-03-09 11:56:18 -0700

    adding 2 tails

--- prime-database-backup/entrypoint.sh
+++ prime-database-backup/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 backup_dir="/opt/backup"
 logfile="${backup_dir}/backup.log"
+touch ${logfile}
 echo "Unauthorized access prohibited."
 /opt/backup.sh
-tail -f ${logfile}
+tail -F ${logfile}
+tail -F /dev/null
