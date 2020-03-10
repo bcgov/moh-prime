71d11ac0064a2ea2f643fcf3c5921904cb9b1f24
Martin Pultz <mtpultz@gmail.com>
2020-03-10 13:06:24 -0700

    Remove unused dependency

--- prime-dotnet-webapi-tests/Mocks/LookupServiceMock.cs
+++ prime-dotnet-webapi-tests/Mocks/LookupServiceMock.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
-using Prime.Models.Api;
 using Prime.Services;
 
 namespace PrimeTests.Mocks
