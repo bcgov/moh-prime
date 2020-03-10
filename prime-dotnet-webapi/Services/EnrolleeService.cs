cf66d3dd9e85080d4377ec21ebebbce05ddd0473
James <jameshollinger.ee@gmail.com>
2020-03-10 16:49:09 -0700

    remove useing

--- prime-dotnet-webapi/Services/EnrolleeService.cs
+++ prime-dotnet-webapi/Services/EnrolleeService.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
-using SimpleBase;
 using Prime.Models;
 using Prime.ViewModels;
 using Prime.Models.Api;
