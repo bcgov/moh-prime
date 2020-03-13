6b86787ad29e2a6ca422ff1c02834706fa03dc59
Martin Pultz <mtpultz@gmail.com>
2020-03-13 01:00:45 -0700

    Removed unused deps

--- prime-angular-frontend/src/app/modules/adjudication/pages/enrollee-events/enrollee-events.component.ts
+++ prime-angular-frontend/src/app/modules/adjudication/pages/enrollee-events/enrollee-events.component.ts
@@ -1,19 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-
-import { Subscription, EMPTY, noop, of } from 'rxjs';
-
-import { HttpEnrollee } from '@shared/models/enrolment.model';
-import { AbstractComponent } from '@shared/classes/abstract-component';
-import { AdjudicationResource } from '@adjudication/shared/services/adjudication-resource.service';
-import { AdjudicationRoutes } from '@adjudication/adjudication.routes';
-import { exhaustMap } from 'rxjs/operators';
-import { DialogOptions } from '@shared/components/dialogs/dialog-options.model';
-import { ConfirmDialogComponent } from '@shared/components/dialogs/confirm-dialog/confirm-dialog.component';
-import { ApproveEnrolmentComponent } from '@shared/components/dialogs/content/approve-enrolment/approve-enrolment.component';
-import { MatDialog } from '@angular/material';
-import { EnrolmentStatus } from '@shared/enums/enrolment-status.enum';
-import { AuthService } from '@auth/shared/services/auth.service';
 
 @Component({
   selector: 'app-enrollee-events',
