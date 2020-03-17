22e9a699d64523694af03491cb551589669318c4
Martin Pultz <mtpultz@gmail.com>
2020-03-17 12:57:00 -0700

    Updated enrollee TOA history to use cards and filter

--- prime-angular-frontend/src/app/modules/adjudication/pages/enrollee-access-terms/enrollee-access-terms.component.ts
+++ prime-angular-frontend/src/app/modules/adjudication/pages/enrollee-access-terms/enrollee-access-terms.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { MatTableDataSource } from '@angular/material';
 
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+import moment from 'moment';
 
 import { AccessTerm } from '@shared/models/access-term.model';
 
 import { AdjudicationRoutes } from '@adjudication/adjudication.routes';
 import { AdjudicationResource } from '@adjudication/shared/services/adjudication-resource.service';
+import { MatSelectChange, yearsPerRow } from '@angular/material';
 
 @Component({
   selector: 'app-enrollee-access-terms',
@@ -16,24 +19,40 @@ import { AdjudicationResource } from '@adjudication/shared/services/adjudication
 })
 export class EnrolleeAccessTermsComponent implements OnInit {
   public busy: Subscription;
-  public dataSource: MatTableDataSource<AccessTerm>;
+  public accessTerms: AccessTerm[];
+  public years: number[];
+  public currentYear: number;
 
   public AdjudicationRoutes = AdjudicationRoutes;
 
   constructor(
     private route: ActivatedRoute,
     private adjudicationResource: AdjudicationResource
-  ) { }
-
-  public ngOnInit() {
-    this.getAccessTerms();
+  ) {
+    this.getYears();
   }
 
-  private getAccessTerms() {
+  public onChange({ value: year }: MatSelectChange) {
+    this.getAccessTerms(year);
+  }
+
+  public ngOnInit() {
+    this.getAccessTerms(this.currentYear);
+  }
+
+  private getAccessTerms(year: number = null) {
     const enrolleeId = this.route.snapshot.params.id;
-    this.busy = this.adjudicationResource.getAccessTerms(enrolleeId)
-      .subscribe((accessTerms: AccessTerm[]) =>
-        this.dataSource = new MatTableDataSource<AccessTerm>(accessTerms)
-      );
+    this.busy = this.adjudicationResource.getAccessTerms(enrolleeId, year)
+      .subscribe((accessTerms: AccessTerm[]) => this.accessTerms = accessTerms);
+  }
+
+  private getYears() {
+    const initialYear = 2018;
+    this.currentYear = +moment().format('YYYY');
+    this.years = [];
+
+    for (let i = this.currentYear; i >= initialYear; i--) {
+      this.years.push(i);
+    }
   }
 }
