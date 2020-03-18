514bf7e0e07be252266a2d82578d0bf482a5aa08
Martin Pultz <mtpultz@gmail.com>
2020-03-17 19:58:24 -0700

    Fix error and test

--- prime-angular-frontend/src/app/shared/components/enrollee/enrollee-property-error/enrollee-property-error.component.spec.ts
+++ prime-angular-frontend/src/app/shared/components/enrollee/enrollee-property-error/enrollee-property-error.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EnrolleePropertyErrorComponent } from './enrollee-property-error.component';
+import { SharedModule } from '@shared/shared.module';
 
 describe('EnrolleePropertyErrorComponent', () => {
   let component: EnrolleePropertyErrorComponent;
@@ -8,9 +9,12 @@ describe('EnrolleePropertyErrorComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EnrolleePropertyErrorComponent ]
+      imports: [
+        SharedModule
+      ],
+      declarations: []
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
