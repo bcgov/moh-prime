ff6034eb4b4d1f1d617a8bc4eab6800b969e4c8d
Nathan <Hogman>
2020-03-19 12:23:02 -0700

    PRIME-527-541-537-579 Text changes

---
+++ prime-dotnet-webapi/Migrations/20200319191742_advancePracticeTextUpdate.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Prime.Migrations
+{
+    public partial class advancePracticeTextUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "PracticeLookup",
+                keyColumn: "Code",
+                keyValue: 2,
+                column: "Name",
+                value: "Reproductive Health - Sexually Transmitted Infections");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "PracticeLookup",
+                keyColumn: "Code",
+                keyValue: 2,
+                column: "Name",
+                value: "Reproductive Health - STI");
+        }
+    }
+}
