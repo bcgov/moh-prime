9d89b4bb0b9cb65a2ea57e13bbaf8b3fcc39c708
Anais <anais.hebert@nttdata.com>
2020-03-23 15:35:47 -0700

    add enrollee pages toa renewal dates

--- prime-angular-frontend/src/app/modules/enrolment/pages/access-agreement-history-enrolment/access-agreement-history-enrolment.component.ts
+++ prime-angular-frontend/src/app/modules/enrolment/pages/access-agreement-history-enrolment/access-agreement-history-enrolment.component.ts
@@ -8,6 +8,7 @@ import { LoggerService } from '@core/services/logger.service';
 import { EnrolmentService } from '@enrolment/shared/services/enrolment.service';
 import { EnrolmentRoutes } from '@enrolment/enrolment.routes';
 import { BaseEnrolmentPage } from '@enrolment/shared/classes/BaseEnrolmentPage';
+import moment from 'moment';
 
 @Component({
   selector: 'app-access-agreement-history-enrolment',
@@ -17,6 +18,7 @@ import { BaseEnrolmentPage } from '@enrolment/shared/classes/BaseEnrolmentPage';
 export class AccessAgreementHistoryEnrolmentComponent extends BaseEnrolmentPage implements OnInit {
   public busy: Subscription;
   public enrolmentProfileHistory: EnrolmentProfileVersion;
+  public expiryDate: string;
 
   constructor(
     protected route: ActivatedRoute,
@@ -36,6 +38,14 @@ export class AccessAgreementHistoryEnrolmentComponent extends BaseEnrolmentPage 
   public ngOnInit() {
     const enrolleeId = this.enrolmentService.enrolment.id;
     const accessTermId = this.route.snapshot.params.id;
+    const enrolment = this.enrolmentService.enrolment;
+
+    if (enrolment.expiryDate) {
+      const expiryMoment = moment(enrolment.expiryDate);
+      this.expiryDate = expiryMoment.isAfter(moment.now())
+        ? expiryMoment.format('MMMM Do, YYYY') : null;
+    }
+
     this.busy = this.enrolmentResource
       .getEnrolmentProfileForAccessTerm(enrolleeId, accessTermId)
       .subscribe(
