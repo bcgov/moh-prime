c32f24485f3734fe4506fa692571fa84138fa3bf
Martin Pultz <mtpultz@users.noreply.github.com>
2020-03-25 10:53:58 -0700

    PRIME-565/592 - Send Provisioner Link / Office Manager Multiple Emails (#531)
    * Updated to allow multiple emails in view and validations
    
    * Added AreValidEmails
    
    * Icon update for Pillar
    
    * Updated request, validators, and message
    
    * Refactored send provisioner link
    
    * Rename var
    
    * Updated tests
    
    * Fix linting
    
    * Updated to consolidate logic
    
    * Updated business event message
    
    * Fix other provisioner not sending
    
    * Continue to fix other
    
    * Alignment of help on subtitle
    
    * Update chained method
    
    * Repositioned validation exception

--- prime-dotnet-webapi/Services/interfaces/IEmailService.cs
+++ prime-dotnet-webapi/Services/interfaces/IEmailService.cs
@@ -7,6 +7,6 @@ namespace Prime.Services
     {
         Task SendReminderEmailAsync(Enrollee enrollee);
 
-        Task SendProvisionerLinkAsync(string provisionerName, string provisionerEmail, EnrolmentCertificateAccessToken token, string ccEmail);
+        Task SendProvisionerLinkAsync(string[] recipients, EnrolmentCertificateAccessToken token, string provisionerName = null);
     }
 }
