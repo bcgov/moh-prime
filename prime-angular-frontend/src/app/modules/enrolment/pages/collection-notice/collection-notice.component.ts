aeced1f00dca0cf3b1d6da76ca40dea0f0e1021e
Martin Pultz <mtpultz@gmail.com>
2020-03-24 22:46:26 -0700

    Updated collection notice to redirect based on enrolment status

--- prime-angular-frontend/src/app/modules/enrolment/pages/collection-notice/collection-notice.component.ts
+++ prime-angular-frontend/src/app/modules/enrolment/pages/collection-notice/collection-notice.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
-import { AuthService } from '@auth/shared/services/auth.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { EnrolmentService } from '@enrolment/shared/services/enrolment.service';
+
+import { EnrolmentStatus } from '@shared/enums/enrolment-status.enum';
+import { AuthService } from '@auth/shared/services/auth.service';
 import { EnrolmentRoutes } from '@enrolment/enrolment.routes';
+import { EnrolmentService } from '@enrolment/shared/services/enrolment.service';
 
 @Component({
   selector: 'app-collection-notice',
@@ -21,8 +22,20 @@ export class CollectionNoticeComponent implements OnInit {
   public ngOnInit() {
     this.authService.hasJustLoggedIn = true;
 
-    if (this.enrolmentService.isProfileComplete) {
-      this.router.navigate([EnrolmentRoutes.OVERVIEW], { relativeTo: this.route.parent });
+    // Collection notice is the initial route after login, and used as a hub
+    // for redirection to an appropriate view based on the enrolment
+    switch (this.enrolmentService.enrolment.currentStatus.statusCode) {
+      case EnrolmentStatus.UNDER_REVIEW:
+        this.router.navigate([EnrolmentRoutes.SUBMISSION_CONFIRMATION], { relativeTo: this.route.parent });
+        break;
+      case EnrolmentStatus.REQUIRES_TOA:
+        this.router.navigate([EnrolmentRoutes.PENDING_ACCESS_TERM], { relativeTo: this.route.parent });
+        break;
+      default: {
+        if (this.enrolmentService.isProfileComplete) {
+          this.router.navigate([EnrolmentRoutes.OVERVIEW], { relativeTo: this.route.parent });
+        }
+      }
     }
   }
 }
