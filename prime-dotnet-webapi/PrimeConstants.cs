fddad24c2801994d4ae9b9b137fb5f7cda7f8022
James Hollinger <39168456+james-hollinger@users.noreply.github.com>
2020-03-25 18:13:18 -0700

    PRIME-510 - HPDONE for careconnect (#538)
    * new endpoint
    
    * dev keycloak update
    
    * typo
    
    * smaller  request
    
    * update keycloak

--- prime-dotnet-webapi/PrimeConstants.cs
+++ prime-dotnet-webapi/PrimeConstants.cs
@@ -21,6 +21,7 @@ namespace Prime
         public const string PRIME_ADMIN_ROLE = "prime_admin";
         public const string PRIME_ENROLLEE_ROLE = "prime_user";
         public const string PRIME_READONLY_ADMIN = "prime_readonly_admin";
+        public const string EXTERNAL_HPDID_ACCESS_ROLE = "external_hpdid_access";
         public const string ASSURANCE_LEVEL_CLAIM_TYPE = "identity_assurance_level";
         public const string KEYCLOAK_REALM_ACCESS_KEY = "realm_access";
         public const string KEYCLOAK_RESOURCE_ACCESS_KEY = "resource_access";
@@ -29,7 +30,8 @@ namespace Prime
         public const string READONLY_ADMIN_POLICY = "readonly-admin-policy";
         public const string ADMIN_POLICY = "admin-policy";
         public const string USER_POLICY = "user-policy";
-        public const string VIEW_POLICY = "user-policy,readonly-admin-policy";
+        public const string EXTERNAL_HPDID_ACCESS_POLICY = "external-hpdid-access-policy";
+
 
         // Enrollee Classifications
         public const string PRIME_OBO = "OBO";
