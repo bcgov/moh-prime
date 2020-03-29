54a10af362af47301059573926b68636551c52bc
Martin Pultz <mtpultz@gmail.com>
2020-03-28 17:45:05 -0700

    Moved privileges model to shared

--- prime-angular-frontend/src/app/modules/provisioner-access/shared/models/enrolment-certificate.model.ts
+++ prime-angular-frontend/src/app/modules/provisioner-access/shared/models/enrolment-certificate.model.ts
@@ -1,6 +1,4 @@
 import { Config } from '@config/config.model';
-import { Privilege } from '@enrolment/shared/models/privilege.model';
-import { AdjudicationNote } from '@adjudication/shared/models/adjudication-note.model';
 
 export interface EnrolmentCertificate {
   // TODO duplicate of EnrolleeProfile
