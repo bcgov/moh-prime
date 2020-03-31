de120e3666664719fe12327ed2c406509c17a042
Anais <anais.hebert@nttdata.com>
2020-03-31 14:21:18 -0700

    mongoservice interface

--- prime-dotnet-webapi/Services/interfaces/IMongoService.cs
+++ prime-dotnet-webapi/Services/interfaces/IMongoService.cs
@@ -1,9 +1,15 @@
-using System.Threading.Tasks;
+using System.Collections.Generic;
+using Prime.Models;
 
 namespace Prime.Services
 {
     public interface IMongoService
     {
-
+        List<Locations> Get();
+        Locations Get(string id);
+        Locations Create(Locations location);
+        void Update(string id, Locations locationIn);
+        void Remove(Locations locationIn);
+        void Remove(string id);
     }
 }
