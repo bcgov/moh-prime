154c3ceaf85abd7edae2f095404e6ce4eec0f61a
Martin Pultz <mtpultz@users.noreply.github.com>
2020-04-06 19:30:19 -0700

    PRIME-506 - Logging (#555)
    * Added logging dependencies
    
    * Removed logging config from appsettings
    
    * Added initial pass at logging setup
    
    * Added middleware for request logging
    
    * Added gitignore for local logs folder
    
    * Added async sink
    
    * Updated log folder path constant
    
    * Updated .vscode formatting
    
    * Updated app settings for logging
    
    * Added enricher for environment to show machine name
    
    * Updated to logger information
    
    * Removed logger settings
    
    * Added user id to properties
    
    * Clean up
    
    * Clean up

--- prime-dotnet-webapi/Program.cs
+++ prime-dotnet-webapi/Program.cs
@@ -1,26 +1,104 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+
+using Serilog;
+using Serilog.Formatting.Json;
+using Serilog.Sinks.SystemConsole.Themes;
 
 namespace Prime
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                CreateHostBuilder(args)
+                    .Build()
+                    .Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                // Ensure buffered logs are written to their target sink
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+                .UseSerilog();
+
+        private static void CreateLogger()
+        {
+            string path = PrimeConstants.LOG_FILE_PATH;
+
+            try
+            {
+                if (isDevelopment())
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    Directory.CreateDirectory(path);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Creating the logging directory failed: {0}", e.ToString());
+            }
+
+            var environment = (isDevelopment()) ? ".Development" : "";
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"appsettings{environment}.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var name = Assembly.GetExecutingAssembly().GetName();
+            var outputTemplate = "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}";
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
+                .Enrich.WithProperty("Assembly", $"{name.Name}")
+                .Enrich.WithProperty("Version", $"{name.Version}")
+                .WriteTo.Console(
+                    outputTemplate: outputTemplate,
+                    theme: AnsiConsoleTheme.Code)
+                .WriteTo.Async(a => a.File(
+                    $@"{path}/prime.log",
+                    outputTemplate: outputTemplate,
+                    rollingInterval: RollingInterval.Day,
+                    // TODO can be configured to rollover on file size limit
+                    // fileSizeLimitBytes: 1_000_000,
+                    // rollOnFileSizeLimit: true,
+                    shared: true))
+                .WriteTo.Async(a => a.File(
+                    new JsonFormatter(),
+                    // TODO performance boost (in μs) using compact, but less readable keys
+                    // new RenderedCompactJsonFormatter(),
+                    $@"{path}/prime.json",
+                    rollingInterval: RollingInterval.Day))
+                // TODO add ElasticSearch sink for Kibana
+                // @see https://github.com/serilog/serilog-sinks-elasticsearch
+                // TODO add sink for Splunk
+                // @see https://github.com/serilog/serilog-sinks-splunk
+                .CreateLogger();
+        }
+
+        private static bool isDevelopment() =>
+            Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == Environments.Development;
     }
 }
