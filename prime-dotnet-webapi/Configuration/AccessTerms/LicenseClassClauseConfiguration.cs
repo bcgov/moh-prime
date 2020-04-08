7858385c6b9d23de55b34165c77a07e69990cb8e
Anais <anais.hebert@nttdata.com>
2020-04-07 17:01:10 -0700

    remove new license class clause from testing

--- prime-dotnet-webapi/Configuration/AccessTerms/LicenseClassClauseConfiguration.cs
+++ prime-dotnet-webapi/Configuration/AccessTerms/LicenseClassClauseConfiguration.cs
@@ -11,8 +11,7 @@ namespace Prime.Configuration
             {
                 return new[] {
                     new LicenseClassClause { Id = 1, Clause = "", Type = "Dispense", EffectiveDate = SEEDING_DATE, CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new LicenseClassClause { Id = 2, Clause = "", Type = "Prescribe", EffectiveDate = SEEDING_DATE, CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new LicenseClassClause { Id = 3, Clause = "", Type = "Dispense", EffectiveDate = System.DateTimeOffset.Parse("2020-04-07 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new LicenseClassClause { Id = 2, Clause = "", Type = "Prescribe", EffectiveDate = SEEDING_DATE, CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE }
                 };
             }
         }
