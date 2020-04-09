c3bd1d69e96e090426cc52e0a7c019dc46051793
Paul Martin <50643076+poggdogg@users.noreply.github.com>
2020-04-09 16:10:45 -0700

    PRIME #609 Adding database template to pipeline (#557)
    * Adding datatabase template to pipeline
    
    * adding logs to .NET, got client installed on backup
    
    * fixed secret reference
    
    * replaced token
    
    * working template
    
    * missing some templates
    
    * initial kick at mongodb backups
    
    * fixing template
    
    * more linting
    
    * adding deps to openshift dockerfile
    
    * set mongo host and DB
    
    * matching nomenclature
    
    * Better variables

--- prime-database-backup/backup.sh
+++ prime-database-backup/backup.sh
@@ -32,27 +32,30 @@ function databaseBackup() {
         tar czf ${logfile}.${timestamp}.tgz ${logfile}
         touch ${logfile}
     fi
+    # Backup PostGRES
     PGPASSWORD=${POSTGRES_PASSWORD}
     /usr/bin/vacuumdb -z -h ${PGHOST} -U ${PGUSERNAME} ${PGDATABASE} >/dev/null 2>&1
-    /usr/bin/pg_dump -U ${PGUSERNAME} -F c -b ${PGDATABASE} -h ${PGHOST} -f ${backup_dir}/${PGDATABASE}-database-${timestamp}.backup
-    echo "${dateinfo} - Backup and Vacuum complete on ${dateinfo} for database: ${PGDATABASE} " >> ${logfile}
-    tar czf ${backup_dir}/${PGDATABASE}-database-${timestamp}.backup.tgz ${backup_dir}/${PGDATABASE}-database-${timestamp}.backup 
-    rm -f ${backup_dir}/${PGDATABASE}-database-${timestamp}.backup
-    echo "${dateinfo} - Backup compressed for database: ${PGDATABASE} " >> ${logfile}
+    /usr/bin/pg_dump -U ${PGUSERNAME} -F c -b ${PGDATABASE} -h ${PGHOST} -f ${backup_dir}/postgresdump-${PGDATABASE}-database-${timestamp}.backup
+    echo "${dateinfo} - Backup and Vacuum complete on ${dateinfo} for postgres database: ${PGDATABASE} " >> ${logfile}
+    tar czf ${backup_dir}/postgresdump-${PGDATABASE}-database-${timestamp}.backup.tgz ${backup_dir}/postgresdump-${PGDATABASE}-database-${timestamp}.backup
+    rm -f ${backup_dir}/postgresdump-${PGDATABASE}-database-${timestamp}.backup
+    echo "${dateinfo} - Backup compressed for postgres database: ${PGDATABASE} " >> ${logfile}
     echo "Starting backup of metabase" >> ${logfile}
-    file_size=`du -b ${logfile} | tr -s '\t' ' ' | cut -d' ' -f1`
-    if [ "$file_size" -gt "$MaxFileSize" ]
-    then   
-        tar czf ${logfile}.${timestamp}.tgz ${logfile}
-        touch ${logfile}
-    fi
+    # Backup Mongo
+    mongodump --host=${MONGO_HOST} --port=27017 --username=${PGUSERNAME} --password="${POSTGRES_PASSWORD}" --out=${backup_dir}/mongodump-${MONGO_DATABASE}-${timestamp}.backup
+    echo "${dateinfo} - Backup and Vacuum complete on ${dateinfo} for mongo database: ${PGDATABASE} " >> ${logfile}
+    tar czf ${backup_dir}/mongodump-${MONGO_DATABASE}-${timestamp}.backup.tgz ${backup_dir}/mongodump-${MONGO_DATABASE}-${timestamp}.backup 
+    rm -f ${backup_dir}/mongodump-${MONGO_DATABASE}-${timestamp}.backup
+    echo "${dateinfo} - Backup compressed for mongo database: ${PGDATABASE} " >> ${logfile}
+    # Backup Metabase
     PGPASSWORD=${METABASE_PASSWORD}
     /usr/bin/vacuumdb -z -h ${METABASE_HOST} -U ${METABASE_USERNAME} ${METABASE_DATABASE} >/dev/null 2>&1
     /usr/bin/pg_dump -U ${METABASE_USERNAME} -F c -b ${METABASE_DATABASE} -h ${METABASE_HOST} -f ${backup_dir}/${METABASE_DATABASE}-database-${timestamp}.backup
-    echo "${dateinfo} - Backup and Vacuum complete on ${dateinfo} for database: ${METABASE_DATABASE} " >> ${logfile}
+    echo "${dateinfo} - Backup and Vacuum complete on ${dateinfo} for metabase database: ${METABASE_DATABASE} " >> ${logfile}
     tar czf ${backup_dir}/${METABASE_DATABASE}-database-${timestamp}.backup.tgz ${backup_dir}/${METABASE_DATABASE}-database-${timestamp}.backup 
     rm -f ${backup_dir}/${METABASE_DATABASE}-database-${timestamp}.backup
-    echo "${dateinfo} - Backup compressed for database: ${METABASE_DATABASE} " >> ${logfile}
+    echo "${dateinfo} - Backup compressed for metabase database: ${METABASE_DATABASE} " >> ${logfile}
+    # All backups finished
     echo "${dateinfo} - Done backup of databases " >> ${logfile}
     echo "${dateinfo} Done backup of databases"
 }
