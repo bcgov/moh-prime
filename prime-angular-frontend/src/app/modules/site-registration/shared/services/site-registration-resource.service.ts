9bbc487766f02884d44bd903cc8c9c0f26ac0c7f
Martin Pultz <mtpultz@gmail.com>
2020-04-20 15:32:23 -0700

    Added resource service

--- prime-angular-frontend/src/app/modules/site-registration/shared/services/site-registration-resource.service.ts
+++ prime-angular-frontend/src/app/modules/site-registration/shared/services/site-registration-resource.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
 
+import { Observable } from 'rxjs';
+import { map, catchError, tap } from 'rxjs/operators';
+
 import { ApiResource } from '@core/resources/api-resource.service';
 import { ApiResourceUtilsService } from '@core/resources/api-resource-utils.service';
 import { LoggerService } from '@core/services/logger.service';
+import { ApiHttpResponse } from '@core/models/api-http-response.model';
+import { ToastService } from '@core/services/toast.service';
+import { NoContent } from '@core/resources/abstract-resource';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +17,88 @@ export class SiteRegistrationResource {
   constructor(
     private apiResource: ApiResource,
     private apiResourceUtilsService: ApiResourceUtilsService,
+    private toastService: ToastService,
     private logger: LoggerService
   ) { }
 
+  public getSites() {
+    return this.apiResource.get<Site[]>('sites')
+      .pipe(
+        map((response: ApiHttpResponse<Site[]>) => response.result),
+        tap((sites: Site[]) => this.logger.info('SITES', sites)),
+        catchError((error: any) => {
+          this.toastService.openErrorToast('Sites could not be retrieved');
+          this.logger.error('[SiteRegistration] SiteRegistrationResource::getSites error has occurred: ', error);
+          throw error;
+        })
+      );
+  }
 
+  public getSiteById(siteId: number) {
+    return this.apiResource.get<Site>(`sites/${siteId}`)
+      .pipe(
+        map((response: ApiHttpResponse<Site>) => response.result),
+        tap((site: Site) => this.logger.info('SITE', site)),
+        catchError((error: any) => {
+          this.toastService.openErrorToast('Site could not be retrieved');
+          this.logger.error('[SiteRegistration] SiteRegistrationResource::getSiteById error has occurred: ', error);
+          throw error;
+        })
+      );
+  }
+
+  public createSite(site: Site) {
+    return this.apiResource.post<Site>('sites', site)
+      .pipe(
+        map((response: ApiHttpResponse<Site>) => response.result),
+        tap((newSite: Site) => this.logger.info('NEW_SITE', newSite)),
+        catchError((error: any) => {
+          this.logger.error('[SiteRegistration] SiteRegistrationResource::createSite error has occurred: ', error);
+          throw error;
+        })
+      );
+  }
+
+  public updateSite(site: Site, isComplete: boolean): NoContent {
+    const { id } = site;
+    const params = this.apiResourceUtilsService.makeHttpParams({ isComplete });
+    return this.apiResource.put<NoContent>(`sites/${id}`, site, params)
+      // TODO remove pipe when ApiResource handles NoContent
+      .pipe(
+        map(() => { }),
+        catchError((error: any) => {
+          this.toastService.openErrorToast('Site could not be updated');
+          this.logger.error('[SiteRegistration] SiteRegistrationResource::updateSite error has occurred: ', error);
+          throw error;
+        })
+      );
+  }
+
+  public deleteSite(siteId: number) {
+    return this.apiResource.delete<Site>(`sites/${siteId}`)
+      .pipe(
+        map((response: ApiHttpResponse<Site>) => response.result),
+        tap((site: Site) => {
+          this.toastService.openSuccessToast('Site has been deleted');
+          this.logger.info('DELETED_SITE', site);
+        }),
+        catchError((error: any) => {
+          this.toastService.openErrorToast('Site could not be deleted');
+          this.logger.error('[SiteRegistration] SiteRegistrationResource::deleteSite error has occurred: ', error);
+          throw error;
+        })
+      );
+  }
+
+  public getOrganizationAgreement(siteId: number): Observable<string> {
+    return this.apiResource.get<string>(`sites/${siteId}/organization-agreement`)
+      .pipe(
+        map((response: ApiHttpResponse<string>) => response.result),
+        catchError((error: any) => {
+          this.toastService.openErrorToast('Organization agreement could not be retrieved');
+          this.logger.error('[SiteRegistration] SiteRegistrationResource::getOrganizationAgreement error has occurred: ', error);
+          throw error;
+        })
+      );
+  }
 }
