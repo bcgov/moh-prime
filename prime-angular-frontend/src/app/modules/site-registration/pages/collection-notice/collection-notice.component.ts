8932fae318c0ca0e7fbb454672792d8ee151645a
Martin Pultz <mtpultz@gmail.com>
2020-04-22 09:35:07 -0700

    Added redirect to collection notice if completed on login

--- prime-angular-frontend/src/app/modules/site-registration/pages/collection-notice/collection-notice.component.ts
+++ prime-angular-frontend/src/app/modules/site-registration/pages/collection-notice/collection-notice.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 import { AuthService } from '@auth/shared/services/auth.service';
 import { SiteRoutes } from '@registration/site-registration.routes';
+import { SiteRegistrationService } from '@registration/shared/services/site-registration.service';
+import { RouteUtils } from '@registration/shared/classes/route-utils.class';
 
 @Component({
   selector: 'app-collection-notice',
@@ -11,21 +13,30 @@ import { SiteRoutes } from '@registration/site-registration.routes';
 })
 export class CollectionNoticeComponent implements OnInit {
   public isFull: boolean;
+  public routeUtils: RouteUtils;
+  public isCompleted: boolean;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private siteRegistrationService: SiteRegistrationService
   ) {
     this.isFull = true;
+    this.routeUtils = new RouteUtils(route, router, SiteRoutes.MODULE_PATH);
   }
 
   public onAccept() {
     this.authService.hasJustLoggedIn = false;
-    this.router.navigate([SiteRoutes.MULTIPLE_SITES], { relativeTo: this.route.parent });
+    this.routeUtils.routeRelativeTo(SiteRoutes.MULTIPLE_SITES);
   }
 
   public ngOnInit() {
     this.authService.hasJustLoggedIn = true;
+    this.isCompleted = this.siteRegistrationService.site.complete;
+
+    if (this.isCompleted) {
+      this.routeUtils.routeRelativeTo(SiteRoutes.SITE_REVIEW);
+    }
   }
 }
