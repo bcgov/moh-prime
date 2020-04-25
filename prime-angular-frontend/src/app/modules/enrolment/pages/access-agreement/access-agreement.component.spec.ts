88d0728b85f0415f489838b6192b7073c413beee
Martin Pultz <mtpultz@gmail.com>
2020-04-25 14:25:41 -0700

    Fix tests

--- prime-angular-frontend/src/app/modules/enrolment/pages/access-agreement/access-agreement.component.spec.ts
+++ prime-angular-frontend/src/app/modules/enrolment/pages/access-agreement/access-agreement.component.spec.ts
@@ -1,16 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+
+import { KeycloakService } from 'keycloak-angular';
+
 import { MockEnrolmentService } from 'test/mocks/mock-enrolment.service';
+
 import { AccessAgreementComponent } from './access-agreement.component';
 import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
-import { KeycloakService } from 'keycloak-angular';
-import { NgxBusyModule } from '@shared/modules/ngx-busy/ngx-busy.module';
-import { NgxContextualHelpModule } from '@shared/modules/ngx-contextual-help/ngx-contextual-help.module';
-import { NgxMaterialModule } from '@shared/modules/ngx-material/ngx-material.module';
 import { EnrolmentService } from '@enrolment/shared/services/enrolment.service';
 import { EnrolmentModule } from '@enrolment/enrolment.module';
+import { SharedModule } from '@shared/shared.module';
 
 describe('AccessAgreementComponent', () => {
   let component: AccessAgreementComponent;
@@ -21,10 +21,6 @@ describe('AccessAgreementComponent', () => {
       {
         imports: [
           HttpClientTestingModule,
-          NgxBusyModule,
-          NgxContextualHelpModule,
-          NgxMaterialModule,
-          ReactiveFormsModule,
           RouterTestingModule,
           EnrolmentModule
         ],
