5ee112542c2b2bf5ebd38ac25a6106846a573b79
James <jameshollinger.ee@gmail.com>
2020-05-01 14:04:30 -0700

    stubs

--- prime-dotnet-webapi/Services/HealthbookService.cs
+++ prime-dotnet-webapi/Services/HealthbookService.cs
@@ -1,7 +1,18 @@
+using System.Threading.Tasks;
+using Prime.Models;
+
 namespace Prime.Services
 {
     public class HealthbookService : IHealthbookService
     {
+        public async Task PushBcscInfoAsync(Enrollee enrollee)
+        {
+            throw new System.NotImplementedException();
+        }
 
+        public async Task PushGpidAsync(Enrollee enrollee)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
