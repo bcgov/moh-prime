e7669c20a04d2bd5759592e2508b01588caf4c02
Martin Pultz <mtpultz@gmail.com>
2020-05-07 16:50:37 -0700

    Updated user clause seed ids

--- prime-dotnet-webapi/Configuration/AccessTerms/UserClauseConfiguration.cs
+++ prime-dotnet-webapi/Configuration/AccessTerms/UserClauseConfiguration.cs
@@ -40,8 +40,8 @@ namespace Prime.Configuration
                     new UserClause { Id = 3, Clause = oboClause2, EnrolleeClassification = PrimeConstants.PRIME_OBO, EffectiveDate = System.DateTimeOffset.Parse("2020-03-05 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
                     new UserClause { Id = 4, Clause = ruClause2, EnrolleeClassification = PrimeConstants.PRIME_RU, EffectiveDate = System.DateTimeOffset.Parse("2020-03-05 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
                     new UserClause { Id = 5, Clause = oboClause3, EnrolleeClassification = PrimeConstants.PRIME_OBO, EffectiveDate = System.DateTimeOffset.Parse("2020-03-10 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new UserClause { Id = 4, Clause = ruClause3, EnrolleeClassification = PrimeConstants.PRIME_RU, EffectiveDate = System.DateTimeOffset.Parse("2020-05-07 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new UserClause { Id = 5, Clause = oboClause4, EnrolleeClassification = PrimeConstants.PRIME_OBO, EffectiveDate = System.DateTimeOffset.Parse("2020-05-07 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE }
+                    new UserClause { Id = 6, Clause = ruClause3, EnrolleeClassification = PrimeConstants.PRIME_RU, EffectiveDate = System.DateTimeOffset.Parse("2020-05-07 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new UserClause { Id = 7, Clause = oboClause4, EnrolleeClassification = PrimeConstants.PRIME_OBO, EffectiveDate = System.DateTimeOffset.Parse("2020-05-07 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE }
                 };
             }
         }
