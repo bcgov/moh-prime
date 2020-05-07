b50f2495f9797d19bed52b730a1ae71636b8ba9f
Martin Pultz <mtpultz@gmail.com>
2020-05-07 16:26:37 -0700

    Added new version to project resources, and Updated configuration

--- prime-dotnet-webapi/Configuration/AccessTerms/UserClauseConfiguration.cs
+++ prime-dotnet-webapi/Configuration/AccessTerms/UserClauseConfiguration.cs
@@ -8,9 +8,11 @@ namespace Prime.Configuration
     {
         private string ruClause1;
         private string ruClause2;
+        private string ruClause3;
         private string oboClause1;
         private string oboClause2;
         private string oboClause3;
+        private string oboClause4;
 
         public override ICollection<UserClause> SeedData
         {
@@ -25,9 +27,11 @@ namespace Prime.Configuration
                 {
                     ruClause1 = ResourceLoader.Load("ru-access-terms-v1.html");
                     ruClause2 = ResourceLoader.Load("ru-access-terms-v2.html");
+                    ruClause3 = ResourceLoader.Load("ru-access-terms-v3.html");
                     oboClause1 = ResourceLoader.Load("obo-access-terms-v1.html");
                     oboClause2 = ResourceLoader.Load("obo-access-terms-v2.html");
                     oboClause3 = ResourceLoader.Load("obo-access-terms-v3.html");
+                    oboClause4 = ResourceLoader.Load("obo-access-terms-v4.html");
                 }
 
                 return new[] {
@@ -35,7 +39,9 @@ namespace Prime.Configuration
                     new UserClause { Id = 2, Clause = ruClause1, EnrolleeClassification = PrimeConstants.PRIME_RU, EffectiveDate = SEEDING_DATE, CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
                     new UserClause { Id = 3, Clause = oboClause2, EnrolleeClassification = PrimeConstants.PRIME_OBO, EffectiveDate = System.DateTimeOffset.Parse("2020-03-05 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
                     new UserClause { Id = 4, Clause = ruClause2, EnrolleeClassification = PrimeConstants.PRIME_RU, EffectiveDate = System.DateTimeOffset.Parse("2020-03-05 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new UserClause { Id = 5, Clause = oboClause3, EnrolleeClassification = PrimeConstants.PRIME_OBO, EffectiveDate = System.DateTimeOffset.Parse("2020-03-10 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE }
+                    new UserClause { Id = 5, Clause = oboClause3, EnrolleeClassification = PrimeConstants.PRIME_OBO, EffectiveDate = System.DateTimeOffset.Parse("2020-03-10 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new UserClause { Id = 4, Clause = ruClause3, EnrolleeClassification = PrimeConstants.PRIME_RU, EffectiveDate = System.DateTimeOffset.Parse("2020-05-07 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new UserClause { Id = 5, Clause = oboClause4, EnrolleeClassification = PrimeConstants.PRIME_OBO, EffectiveDate = System.DateTimeOffset.Parse("2020-05-07 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE }
                 };
             }
         }
