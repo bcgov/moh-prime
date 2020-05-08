0ec1893a4e067fc786a02870634652a94874af83
Martin Pultz <mtpultz@users.noreply.github.com>
2020-05-08 11:25:11 -0700

    PRIME-718 - Feedback Update (#605)
    * Updated feedback controller, service, and model
    
    * Updated dependencies
    
    * Updated feedback and notification confirmation components
    
    * Added migration
    
    * Update confirm-dialog.component.scss
    
    * Update FeedbackController.cs

--- prime-dotnet-webapi/Services/interfaces/IFeedbackService.cs
+++ prime-dotnet-webapi/Services/interfaces/IFeedbackService.cs
@@ -1,13 +1,11 @@
-using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
+
 using Prime.Models;
 
 namespace Prime.Services
 {
     public interface IFeedbackService
     {
-
         Task<int> CreateFeedbackAsync(Feedback feedback);
     }
 }
