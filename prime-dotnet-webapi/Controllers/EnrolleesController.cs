0499c1fdd50eba766eb6852e64f6aae016ec6cb3
Martin Pultz <mtpultz@gmail.com>
2020-05-08 11:29:38 -0700

    Autoformatting update

--- prime-dotnet-webapi/Controllers/EnrolleesController.cs
+++ prime-dotnet-webapi/Controllers/EnrolleesController.cs
@@ -50,7 +50,7 @@ namespace Prime.Controllers
         [ProducesResponseType(typeof(ApiBadRequestResponse), StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(typeof(ApiResultResponse<IEnumerable<Enrollee>>), StatusCodes.Status200OK)]
-        public async Task<ActionResult<IEnumerable<Enrollee>>> GetEnrollees([FromQuery]EnrolleeSearchOptions searchOptions)
+        public async Task<ActionResult<IEnumerable<Enrollee>>> GetEnrollees([FromQuery] EnrolleeSearchOptions searchOptions)
         {
             IEnumerable<Enrollee> enrollees = null;
 
@@ -148,7 +148,7 @@ namespace Prime.Controllers
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(typeof(ApiMessageResponse), StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
-        public async Task<IActionResult> UpdateEnrollee(int enrolleeId, EnrolleeProfileViewModel enrolleeProfile, [FromQuery]bool beenThroughTheWizard)
+        public async Task<IActionResult> UpdateEnrollee(int enrolleeId, EnrolleeProfileViewModel enrolleeProfile, [FromQuery] bool beenThroughTheWizard)
         {
             var enrollee = await _enrolleeService.GetEnrolleeNoTrackingAsync(enrolleeId);
             if (enrollee == null)
