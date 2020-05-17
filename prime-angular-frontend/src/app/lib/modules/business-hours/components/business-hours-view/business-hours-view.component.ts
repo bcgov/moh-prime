7ceca653802ad24786d1e91f9455a803c635516b
Martin Pultz <mtpultz@gmail.com>
2020-05-17 02:11:49 -0700

    Added business hours view component

---
+++ prime-angular-frontend/src/app/lib/modules/business-hours/components/business-hours-view/business-hours-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-business-hours-view',
+  templateUrl: './business-hours-view.component.html',
+  styleUrls: ['./business-hours-view.component.scss']
+})
+export class BusinessHoursViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
