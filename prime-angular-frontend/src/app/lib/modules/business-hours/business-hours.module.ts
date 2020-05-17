8a46ad5cd019eed3b4af9124dda9e293f907dda3
Martin Pultz <mtpultz@gmail.com>
2020-05-17 02:22:15 -0700

    Added business hours module

---
+++ prime-angular-frontend/src/app/lib/modules/business-hours/business-hours.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    // BusinessHoursComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    // BusinessHoursComponent
+  ]
+})
+export class BusinessHoursModule { }
