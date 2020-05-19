72aa1cd322c0e330c2579a8d503c28658f8f7c58
Martin Pultz <mtpultz@gmail.com>
2020-05-19 00:40:36 -0700

    Fix tests

--- prime-angular-frontend/src/app/modules/site-registration/shared/components/registrant-profile-form/registrant-profile-form.component.spec.ts
+++ prime-angular-frontend/src/app/modules/site-registration/shared/components/registrant-profile-form/registrant-profile-form.component.spec.ts
@@ -1,10 +1,17 @@
 import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FormGroup } from '@angular/forms';
 
+import { MockConfigService } from 'test/mocks/mock-config.service';
+import { MockSiteRegistrationService } from 'test/mocks/mock-site-registration.service';
+
 import { RegistrantProfileFormComponent } from './registrant-profile-form.component';
+import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
+import { ConfigService } from '@config/config.service';
 import { SiteRegistrationModule } from '@registration/site-registration.module';
+import { SiteRegistrationService } from '@registration/shared/services/site-registration.service';
 import { SiteRegistrationStateService } from '@registration/shared/services/site-registration-state.service';
 
 describe('RegistrantProfileFormComponent', () => {
@@ -14,11 +21,24 @@ describe('RegistrantProfileFormComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
+        HttpClientTestingModule,
         SiteRegistrationModule,
         BrowserAnimationsModule,
         RouterTestingModule
       ],
       providers: [
+        {
+          provide: APP_CONFIG,
+          useValue: APP_DI_CONFIG
+        },
+        {
+          provide: ConfigService,
+          useClass: MockConfigService
+        },
+        {
+          provide: SiteRegistrationService,
+          useClass: MockSiteRegistrationService
+        },
         SiteRegistrationStateService
       ]
     }).compileComponents();
@@ -30,13 +50,17 @@ describe('RegistrantProfileFormComponent', () => {
     fixture.detectChanges();
   });
 
-  beforeEach(inject([SiteRegistrationStateService], (siteRegistrationStateService: SiteRegistrationStateService) => {
-    fixture = TestBed.createComponent(RegistrantProfileFormComponent);
-    component = fixture.componentInstance;
-    // Add the bound FormGroup to the component
-    component.form = siteRegistrationStateService.signingAuthorityForm as FormGroup;
-    fixture.detectChanges();
-  }));
+  beforeEach(inject(
+    [SiteRegistrationService, SiteRegistrationStateService],
+    (siteRegistrationService: SiteRegistrationService, siteRegistrationStateService: SiteRegistrationStateService
+    ) => {
+      fixture = TestBed.createComponent(RegistrantProfileFormComponent);
+      component = fixture.componentInstance;
+      siteRegistrationStateService.setSite(siteRegistrationService.site);
+      // Add the bound FormGroup to the component
+      component.form = siteRegistrationStateService.administratorPharmaNetForm as FormGroup;
+      fixture.detectChanges();
+    }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
