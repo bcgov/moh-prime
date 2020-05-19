2756fea7a06d0230f67473c4e41d58851ac6bd2b
Martin Pultz <mtpultz@gmail.com>
2020-05-18 22:03:40 -0700

    Remove form group dependency from hours view

--- prime-angular-frontend/src/app/lib/modules/business-hours/components/business-hours-view/business-hours-view.component.ts
+++ prime-angular-frontend/src/app/lib/modules/business-hours/components/business-hours-view/business-hours-view.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { FormArray, FormGroup } from '@angular/forms';
-// TODO businessDay array instead of form group
-// import { BusinessDay } from '@registration/shared/models/business-day.model';
+
+import { BusinessDay } from '../../models/business-day.model';
 
 @Component({
   selector: 'app-business-hours-view',
@@ -9,17 +8,11 @@ import { FormArray, FormGroup } from '@angular/forms';
   styleUrls: ['./business-hours-view.component.scss']
 })
 export class BusinessHoursViewComponent implements OnInit {
-  @Input() public form: FormGroup;
-  // TODO businessDay array instead of form group
-  // @Input() public businessDays: BusinessDay[];
+  @Input() public businessDays: BusinessDay[];
   @Output() public remove: EventEmitter<number>;
 
   constructor() {
     this.remove = new EventEmitter<number>();
-  }
-
-  public get businessDays(): FormArray {
-    return this.form.get('businessDays') as FormArray;
   }
 
   public onRemove(index: number) {
