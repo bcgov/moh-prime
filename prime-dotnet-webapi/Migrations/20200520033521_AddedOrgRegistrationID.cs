5558b6e6a72ba107b7f70babb423daac1bf415a6
Martin Pultz <mtpultz@gmail.com>
2020-05-19 22:27:41 -0700

    Added migration for addition of org ID

---
+++ prime-dotnet-webapi/Migrations/20200520033521_AddedOrgRegistrationID.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Prime.Migrations
+{
+    public partial class AddedOrgRegistrationID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RegistrationId",
+                table: "Organization",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RegistrationId",
+                table: "Organization");
+        }
+    }
+}
