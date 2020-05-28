b9a07bf6f6a258a20d3ca7a415c5988be76fdbaa
Martin Pultz <mtpultz@gmail.com>
2020-05-28 13:44:27 -0700

    Added organization overview

---
+++ prime-angular-frontend/src/app/modules/site-registration/pages/organization-overview/organization-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-organization-overview',
+  templateUrl: './organization-overview.component.html',
+  styleUrls: ['./organization-overview.component.scss']
+})
+export class OrganizationOverviewComponent implements OnInit {
+  constructor() { }
+
+  public ngOnInit(): void { }
+}
