25625a398840bcdfa45deec655fe65a5931e27b6
Martin Pultz <mtpultz@gmail.com>
2020-06-01 17:23:44 -0700

    Fix from merging remote users

--- prime-dotnet-webapi/Models/SiteRegistration/Site.cs
+++ prime-dotnet-webapi/Models/SiteRegistration/Site.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace Prime.Models
 {
