3739e4361a395ac36b24df3f76588bdf445ee4c5
Martin Pultz <mtpultz@gmail.com>
2020-06-03 16:23:39 -0700

    Update remote users

--- prime-dotnet-webapi/Services/SiteService.cs
+++ prime-dotnet-webapi/Services/SiteService.cs
@@ -90,22 +90,22 @@ namespace Prime.Services
             UpdateLocation(currentSite.Location, updatedSite.Location);
 
             // Wholesale replace the remote users
-            // TODO definitely won't work and I'm too #&$@ tired to care!!!
-            // if (currentSite?.RemoteUsers != null && currentSite?.RemoteUsers.Count() != 0)
-            // {
-            //     foreach (var remoteUser in currentSite.RemoteUsers)
-            //     {
-            //         _context.RemoteUsers.Remove(remoteUser);
-            //     }
-            // }
+            if (currentSite?.RemoteUsers != null && currentSite?.RemoteUsers.Count() != 0)
+            {
+                foreach (var remoteUser in currentSite.RemoteUsers)
+                {
+                    _context.RemoteUsers.Remove(remoteUser);
+                }
+            }
 
-            // if (updatedSite?.RemoteUsers != null && updatedSite?.RemoteUsers.Count() != 0)
-            // {
-            //     foreach (var remoteUser in updatedSite.RemoteUsers)
-            //     {
-            //         _context.RemoteUsers.Add(remoteUser);
-            //     }
-            // }
+            if (updatedSite?.RemoteUsers != null && updatedSite?.RemoteUsers.Count() != 0)
+            {
+                foreach (var remoteUser in updatedSite.RemoteUsers)
+                {
+                    remoteUser.SiteId = currentSite.Id;
+                    _context.RemoteUsers.Add(remoteUser);
+                }
+            }
 
             // Update foreign key only if not null
             currentSite.VendorId = (updatedSite.VendorId != 0)
