3ef9f5dfdd4753efc686d6a9c45754bbd2e2cd6a
Anais <anais.hebert@nttdata.com>
2020-06-05 14:12:37 -0700

    remove remote user hack, and return unused location delete for later

--- prime-angular-frontend/src/app/modules/site-registration/shared/services/site-form-state-service.service.ts
+++ prime-angular-frontend/src/app/modules/site-registration/shared/services/site-form-state-service.service.ts
@@ -102,10 +102,6 @@ export class SiteFormStateService {
       return party;
     });
 
-    // remoteUsers.map((user) => {
-    //   user.remoteUserLocations.forEach(location => location.physicalAddress.provinceCode = 'BC');
-    // });
-
     // Includes site and location related keys to uphold relationships, and
     // allow for updates to a site. Keys not for update have been omitted
     // and the type enforced
