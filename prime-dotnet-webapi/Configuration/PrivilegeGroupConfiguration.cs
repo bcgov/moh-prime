4510233b1fda193413e565a9d54ac525954648cb
James <jameshollinger.ee@gmail.com>
2020-06-08 17:30:21 -0700

    remove RU privilege

--- prime-dotnet-webapi/Configuration/PrivilegeGroupConfiguration.cs
+++ prime-dotnet-webapi/Configuration/PrivilegeGroupConfiguration.cs
@@ -18,7 +18,6 @@ namespace Prime.Configuration
                     new PrivilegeGroup { Code = 1, PrivilegeTypeCode = 2, Name = "Submit and Access Claims", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
                     new PrivilegeGroup { Code = 2, PrivilegeTypeCode = 2, Name = "Record Medical History", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
                     new PrivilegeGroup { Code = 3, PrivilegeTypeCode = 2, Name = "Access Medical History", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new PrivilegeGroup { Code = 4, PrivilegeTypeCode = 1, Name = "Role", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
                     new PrivilegeGroup { Code = 5, PrivilegeTypeCode = 1, Name = "RU That Can Have OBOs", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE }
                 };
             }
