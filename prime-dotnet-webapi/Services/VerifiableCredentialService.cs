025454ef8a75cbed1ada071ada43415a99210eab
Martin Pultz <mtpultz@users.noreply.github.com>
2020-06-24 14:37:06 -0700

    Update VerifiableCredentialService.cs

--- prime-dotnet-webapi/Services/VerifiableCredentialService.cs
+++ prime-dotnet-webapi/Services/VerifiableCredentialService.cs
@@ -26,6 +26,7 @@ namespace Prime.Services
     public class VerifiableCredentialService : BaseService, IVerifiableCredentialService
     {
         private readonly IVerifiableCredentialClient _verifiableCredentialClient;
+        
         public VerifiableCredentialService(
             ApiDbContext context,
             IHttpContextAccessor httpContext,
@@ -60,14 +61,10 @@ namespace Prime.Services
 
         public async Task<JObject> SendCredential(String gpid)
         {
-
             var sendResponse = await _verifiableCredentialClient.SendCredential(gpid);
             return sendResponse;
         }
 
-
-        // TODO temporary data object provided, and return type
-        // @see https://github.com/esune/issuer-kit/blob/api-refactor/api/src/services/webhooks/webhooks.class.ts#L30
         public async Task<bool> create(JObject data, WebhookTopic topic)
         {
             switch (topic)
@@ -77,20 +74,17 @@ namespace Prime.Services
                 case WebhookTopic.IssueCredential:
                     return await handleIssueCredential(data);
                 default:
-                    // TODO log a message $"Webhook {topic} is not supported";
+                    // TODO log a message using serilog $"Webhook {topic} is not supported";
                     return false;
             };
         }
 
-        // TODO temporary data object provided, and return type
-        // @see https://github.com/esune/issuer-kit/blob/api-refactor/api/src/services/webhooks/webhooks.class.ts#L44
         private async Task<bool> handleConnection(JObject data)
         {
             var connection_id = data.GetValue("connection_id").ToString();
-            // TODO implement connection webhook logic
             var response = await _verifiableCredentialClient.AcceptRequest(connection_id);
 
-            // connection successful, issue credential
+            // Connection successful, issue credential
             if (response != null)
             {
                 var credResponse = await SendCredential(connection_id);
@@ -99,28 +93,25 @@ namespace Prime.Services
             return await Task.FromResult(true);
         }
 
-        // TODO temporary data object provided, and return type
-        // @see https://github.com/esune/issuer-kit/blob/api-refactor/api/src/services/webhooks/webhooks.class.ts#L48
         private async Task<bool> handleIssueCredential(JObject data)
         {
             var state = data.GetValue("state").ToString();
             string credential_exchange_id;
-            // TODO switch statement based on the credential exchange state
             switch (state)
             {
                 case "request_sent":
-                    // call aries agent to create credential
+                    // Call aries agent to create credential
                     credential_exchange_id = data.GetValue("credential_exchange_id").ToString();
                     var attributes = data.GetValue("attributes") as JArray;
                     await _verifiableCredentialClient.IssueCredential(credential_exchange_id, attributes);
                     return await Task.FromResult(true);
                 case "credential_received":
-                    // store a received credential
+                    // Store a received credential
                     credential_exchange_id = data.GetValue("credential_exchange_id").ToString();
                     await _verifiableCredentialClient.StoreCredential(credential_exchange_id);
                     return await Task.FromResult(true);
                 default:
-                    // TODO log a message $"Received unexpected state {state} for CredentialExchangeState ${state.id}"
+                    // TODO log a message using serilog
                     Console.WriteLine($"Received unexpected state {state} for CredentialExchangeState ${state}");
                     return await Task.FromResult(false);
             }
