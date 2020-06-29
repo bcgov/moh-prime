1bedc03f01371fdc4be19a61e58559d26bb973c7
Martin Pultz <mtpultz@gmail.com>
2020-06-28 17:30:32 -0700

    Remove test endpoint

--- prime-dotnet-webapi/Controllers/VerifiableCredentialController.cs
+++ prime-dotnet-webapi/Controllers/VerifiableCredentialController.cs
@@ -9,7 +9,6 @@ using Prime.Models;
 using Prime.Models.Api;
 using Prime.Services;
 
-// TODO how are we securing the endpoint?
 namespace Prime.Controllers
 {
     [Produces("application/json")]
@@ -26,22 +25,6 @@ namespace Prime.Controllers
             _verifiableCredentialsService = verifiableCredentialService;
         }
 
-        // POST: api/connections/create-invitation
-        /// <summary>
-        ///
-        /// </summary>
-        [HttpPost("/api/connections/create-connection", Name = nameof(CreateConnection))]
-        [ProducesResponseType(typeof(ApiBadRequestResponse), StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        public async Task<ActionResult<JObject>> CreateConnection()
-        {
-            // TODO how should this respond and what occurs on their side regarding retries?
-            var response = await _verifiableCredentialsService.CreateConnection();
-            return Ok(ApiResponse.Result(response));
-        }
-
-        // TODO add security to the endpoint by adding a param after /topic before param
         // POST: api/topic/:topic
         /// <summary>
         /// Handle webhook events sent from the issuing agent.
@@ -49,7 +32,8 @@ namespace Prime.Controllers
         /// <param name="topic"></param>
         /// <param name="data"></param>
         // Webhooks explained "In Soviet Russia API call you!" - Jason Aitchison 2020
-        // Reverse third-party API, which instead of pulling data from the third-party it pushes data
+        // Reverse third-party API, which instead of pulling data from the third-party they pushes data
+        // TODO add security to the webhook receiver endpoint
         [HttpPost("/api/webhooks/topic/{topic}", Name = nameof(Webhook))]
         [ProducesResponseType(typeof(ApiBadRequestResponse), StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
@@ -57,7 +41,7 @@ namespace Prime.Controllers
         public async Task<ActionResult> Webhook(string topic, [FromBody] JObject data)
         {
             // TODO how should this respond and what occurs on their side regarding retries?
-            await _verifiableCredentialsService.Webhook(data, topic);
+            await _verifiableCredentialsService.WebhookAsync(data, topic);
             return NoContent();
         }
     }
