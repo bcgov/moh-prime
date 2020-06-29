7c13b9851325955e6cfecb705eab24349d8dc9c0
Martin Pultz <mtpultz@gmail.com>
2020-06-29 14:19:37 -0700

    Added logging to API calls for DID and CredDefIds

--- prime-dotnet-webapi/Services/Clients/VerifiableCredential/VerifiableCredentialClient.cs
+++ prime-dotnet-webapi/Services/Clients/VerifiableCredential/VerifiableCredentialClient.cs
@@ -95,6 +95,10 @@ namespace Prime.Services.Clients
             }
 
             JObject body = JObject.Parse(await response.Content.ReadAsStringAsync());
+
+            System.Console.WriteLine("GET Issuer DID");
+            System.Console.WriteLine(JsonConvert.SerializeObject(body));
+
             return body.Value<string>("did");
         }
 
@@ -117,8 +121,12 @@ namespace Prime.Services.Clients
                 throw new VerifiableCredentialApiException($"Error code {response.StatusCode} was provided when calling VerifiableCredentialClient::GetCredentialDefinitionAsync");
             }
 
-            JObject credentialDefinition = JObject.Parse(await response.Content.ReadAsStringAsync());
-            return (string)credentialDefinition["credential_definition_ids"][0];
+            JObject body = JObject.Parse(await response.Content.ReadAsStringAsync());
+
+            System.Console.WriteLine("GET Credential Definition IDs");
+            System.Console.WriteLine(JsonConvert.SerializeObject(body));
+
+            return (string)body["credential_definition_ids"][0];
         }
 
         private async Task LogError(HttpResponseMessage response, Exception exception = null)
