29b3a483a784fe985f78c0aef1a7ffe1b4ba08b6
Anais <anais.hebert@nttdata.com>
2020-07-02 10:51:28 -0700

    move http client setup in startup

--- prime-dotnet-webapi/Startup.cs
+++ prime-dotnet-webapi/Startup.cs
@@ -68,12 +68,6 @@ namespace Prime
 
             ConfigureClients(services);
 
-            services.AddHttpClient<IVerifiableCredentialClient, VerifiableCredentialClient>(client =>
-            {
-                client.BaseAddress = new Uri(PrimeConstants.VERIFIABLE_CREDENTIAL_API_URL);
-                client.DefaultRequestHeaders.Add("x-api-key", PrimeConstants.VERIFIABLE_CREDENTIAL_API_KEY);
-            });
-
             services.AddControllers()
                 .AddNewtonsoftJson(options =>
                 {
@@ -148,6 +142,12 @@ namespace Prime
                 Address = PrimeConstants.KEYCLOAK_TOKEN_URL,
                 ClientId = PrimeConstants.DOCUMENT_MANAGER_CLIENT_ID,
                 ClientSecret = PrimeConstants.DOCUMENT_MANAGER_CLIENT_SECRET,
+            });
+
+            services.AddHttpClient<IVerifiableCredentialClient, VerifiableCredentialClient>(client =>
+            {
+                client.BaseAddress = new Uri(PrimeConstants.VERIFIABLE_CREDENTIAL_API_URL);
+                client.DefaultRequestHeaders.Add("x-api-key", PrimeConstants.VERIFIABLE_CREDENTIAL_API_KEY);
             });
         }
 
