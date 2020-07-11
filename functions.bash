ff0049ee0ab5f9d3af218b45f40bfa5eb6e60a27
Martin Pultz <mtpultz@gmail.com>
2020-07-10 19:34:50 -0600

    Does a GET work?

--- functions.bash
+++ functions.bash
@@ -129,6 +129,10 @@ function preventMerge() {
     -H "Accept: application/vnd.github.v3+json" \
     "https://api.github.com/repos/${PROJECT_OWNER}/${PROJECT_NAME}/statuses/$GIT_COMMIT" \
     -d "{\"state\": \"failure\",\"context\": \"continuous-integration/jenkins\", \"description\": \"Jenkins\", \"target_url\": \"https://jenkins-prod-dqszvc-tools.pathfinder.gov.bc.ca/job/${PROJECT_OWNER}/$BUILD_NUMBER/console\"}"
+
+  curl \
+    -X GET \
+    "https://api.github.com/repos/${PROJECT_OWNER}/${PROJECT_NAME}/pulls?status=open&sort=number"
 }
 
 function getOldPr () {
