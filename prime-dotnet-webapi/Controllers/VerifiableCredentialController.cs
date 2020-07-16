6fb62653fb5d648e39f9d81677b44b6a00cc4f01
Anais <anais.hebert@nttdata.com>
2020-07-16 15:04:43 -0700

    Add guid to webhook url

--- prime-dotnet-webapi/Controllers/VerifiableCredentialController.cs
+++ prime-dotnet-webapi/Controllers/VerifiableCredentialController.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Linq;
 
 using Prime.Models.Api;
@@ -14,29 +15,39 @@ namespace Prime.Controllers
     public class VerifiableCredentialController : ControllerBase
     {
         private readonly IVerifiableCredentialService _verifiableCredentialsService;
+        private readonly ILogger _logger;
 
         public VerifiableCredentialController(
-            IVerifiableCredentialService verifiableCredentialService
+            IVerifiableCredentialService verifiableCredentialService,
+            ILogger<VerifiableCredentialController> logger
         )
         {
             _verifiableCredentialsService = verifiableCredentialService;
+            _logger = logger;
         }
 
-        // POST: api/topic/:topic
+        // POST: api/topic/:topic/{guid}
         /// <summary>
         /// Handle webhook events sent from the issuing agent.
         /// </summary>
+        /// <param name="apiKey"></param>
         /// <param name="topic"></param>
         /// <param name="data"></param>
         // Webhooks explained "In Soviet Russia API call you!" - Jason Aitchison 2020
         // TODO add security to the webhook receiver endpoint
-        [HttpPost("/api/webhooks/topic/{topic}", Name = nameof(Webhook))]
+        [HttpPost("/api/webhooks/{apiKey}/topic/{topic}/", Name = nameof(Webhook))]
         [ProducesResponseType(typeof(ApiBadRequestResponse), StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         // TODO update to response code 202 when queue has been added for webhooks
         [ProducesResponseType(StatusCodes.Status204NoContent)]
-        public async Task<ActionResult> Webhook(string topic, [FromBody] JObject data)
+        public async Task<ActionResult> Webhook(string apiKey, string topic, [FromBody] JObject data)
         {
+            _logger.LogInformation($"API-KEY: [{apiKey}], VERIFIABLE_CREDENTIAL_WEBHOOK_KEY: [{PrimeConstants.VERIFIABLE_CREDENTIAL_WEBHOOK_KEY}]");
+
+            if (apiKey != PrimeConstants.VERIFIABLE_CREDENTIAL_WEBHOOK_KEY)
+            {
+                return Forbid();
+            }
             await _verifiableCredentialsService.WebhookAsync(data, topic);
             return NoContent();
         }
