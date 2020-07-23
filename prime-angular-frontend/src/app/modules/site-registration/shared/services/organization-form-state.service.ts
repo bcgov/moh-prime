7c7409f8b344bd0adcde47696171d855f79ac536
Martin Pultz <mtpultz@gmail.com>
2020-07-22 18:50:07 -0600

    Added abstract class for org form state

--- prime-angular-frontend/src/app/modules/site-registration/shared/services/organization-form-state.service.ts
+++ prime-angular-frontend/src/app/modules/site-registration/shared/services/organization-form-state.service.ts
@@ -1,65 +1,33 @@
 import { Injectable } from '@angular/core';
-import { FormBuilder, Validators, FormGroup, AbstractControl } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 import { FormControlValidators } from '@lib/validators/form-control.validators';
-import { FormUtilsService } from '@core/services/form-utils.service';
-import { Province } from '@shared/enums/province.enum';
-import { Country } from '@shared/enums/country.enum';
 
 import { Party } from '@registration/shared/models/party.model';
 import { Organization } from '@registration/shared/models/organization.model';
+import { AbstractFormState } from '@registration/shared/classes/abstract-form-state.class';
 
-// TODO default is null and on reset it would be great if no special 0 id was required
-// TODO add a form state service interface/abstract class for form state services
-// TODO should the forms built be stored in a different file or service
 @Injectable({
   providedIn: 'root'
 })
-export class OrganizationFormStateService {
+export class OrganizationFormStateService extends AbstractFormState<Organization> {
   public signingAuthorityForm: FormGroup;
-  public organizationInformationForm: FormGroup;
-  public organizationTypeForm: FormGroup;
-
-  private patched: boolean;
+  public organizationNameForm: FormGroup;
 
   constructor(
-    private fb: FormBuilder,
-    private formUtilsService: FormUtilsService,
+    protected fb: FormBuilder
   ) {
-    // Initial state of the form is unpatched and ready for
-    // enrolment information
-    this.patched = false;
-
-    this.init();
-  }
-
-  /**
-   * @description
-   * Convert JSON into reactive form abstract controls, which can
-   * only be set more than once when explicitly forced.
-   */
-  public setForm(organization: Organization, forcePatch: boolean = false) {
-    if (this.patched && !forcePatch) {
-      return;
-    }
-
-    // Indicate that the form is patched, and may contain unsaved information
-    this.patched = true;
-
-    this.patchForm(organization);
+    super(fb);
   }
 
   /**
    * @description
    * Convert reactive form abstract controls into JSON.
    */
-  // TODO method constructs the JSON, and attempts to adapt, should
-  // adapt in only one place and separately in method
-  public get organization(): Organization {
-    const organizationInformation = this.organizationInformationForm.getRawValue();
-    const { organizationTypeCode } = this.organizationTypeForm.getRawValue();
+  public get json(): Organization {
+    const organizationName = this.organizationNameForm.getRawValue();
 
-    // TODO duplicated until services are completely split apart
+    // TODO create a helper to reconstruct the party into JSON format
     const [
       signingAuthority
     ] = [
@@ -83,51 +51,21 @@ export class OrganizationFormStateService {
       : 0;
 
     return {
-      // OrganizationInformation is the only form
-      // that contains the organization ID
-      ...organizationInformation,
-      organizationTypeCode,
+      // OrganizationName is the only form that contains the organization ID
+      ...organizationName,
       signingAuthorityId: signingAuthority?.id,
       signingAuthority
     };
   }
 
-  public get isValid() {
-    return this.forms
-      .reduce((valid: boolean, form: AbstractControl) => valid && form.valid, true);
-  }
-
-  public get isDirty(): boolean {
-    return this.forms
-      .reduce((dirty: boolean, form: AbstractControl) => dirty || form.dirty, false);
-  }
-
-  public markAsPristine(): void {
-    this.forms
-      .forEach((form: AbstractControl) => form.markAsPristine());
-  }
-
   /**
    * @description
    * Initialize and configure the forms for patching, which is also used
-   * clear previous form data from the service.
+   * to clear previous form data from the service.
    */
   public init() {
     this.signingAuthorityForm = this.buildSigningAuthorityForm();
-    this.organizationInformationForm = this.buildOrganizationInformationForm();
-    this.organizationTypeForm = this.buildOrganizationTypeForm();
-  }
-
-  /**
-   * @description
-   * Helper for getting a list of organization forms.
-   */
-  private get forms(): AbstractControl[] {
-    return [
-      this.signingAuthorityForm,
-      this.organizationInformationForm,
-      this.organizationTypeForm
-    ];
+    this.organizationNameForm = this.buildOrganizationNameForm();
   }
 
   /**
@@ -135,13 +73,12 @@ export class OrganizationFormStateService {
    * Manage the conversion of JSON to reactive forms.
    */
   // TODO refactor into separate adapters
-  private patchForm(organization: Organization): Organization {
+  protected patchForm(organization: Organization): Organization {
     if (!organization) {
       return null;
     }
 
-    this.organizationInformationForm.patchValue(organization);
-    this.organizationTypeForm.patchValue(organization);
+    this.organizationNameForm.patchValue(organization);
 
     // TODO duplicated until services are completely split apart
     [
@@ -272,9 +209,9 @@ export class OrganizationFormStateService {
     });
   }
 
-  private buildOrganizationInformationForm(): FormGroup {
+  private buildOrganizationNameForm(): FormGroup {
     return this.fb.group({
-      // OrganizationInformation is the only form
+      // OrganizationName is the only form
       // that contains the organization ID
       id: [
         0,
@@ -293,160 +230,5 @@ export class OrganizationFormStateService {
         []
       ]
     });
-  }
-
-  private buildOrganizationTypeForm(code: number = null): FormGroup {
-    return this.fb.group({
-      organizationTypeCode: [code, [Validators.required]]
-    });
-  }
-
-  /**
-   * @deprecated no longer used in organizations
-   */
-  private partyFormGroup(disabled: boolean = false): FormGroup {
-    return this.fb.group({
-      id: [
-        0,
-        []
-      ],
-      firstName: [
-        { value: null, disabled },
-        [Validators.required]
-      ],
-      lastName: [
-        { value: null, disabled },
-        [Validators.required]
-      ],
-      jobRoleTitle: [
-        null,
-        [Validators.required]
-      ],
-      phone: [
-        null,
-        [
-          Validators.required,
-          FormControlValidators.phone
-        ]
-      ],
-      fax: [
-        null,
-        [
-          Validators.required,
-          FormControlValidators.phone
-        ]
-      ],
-      smsPhone: [
-        null,
-        [
-          Validators.required,
-          FormControlValidators.phone
-        ]
-      ],
-      email: [
-        null,
-        [
-          Validators.required,
-          FormControlValidators.email
-        ]
-      ],
-      // TODO duplication split out into reuseable address model
-      physicalAddress: this.fb.group({
-        id: [
-          0,
-          []
-        ],
-        street: [
-          { value: null, disabled: false },
-          []
-        ],
-        street2: [
-          { value: null, disabled: false },
-          []
-        ],
-        city: [
-          { value: null, disabled: false },
-          []
-        ],
-        provinceCode: [
-          { value: null, disabled: false },
-          []
-        ],
-        countryCode: [
-          { value: null, disabled: false },
-          []
-        ],
-        postal: [
-          { value: null, disabled: false },
-          []
-        ]
-      })
-    });
-  }
-
-  /**
-   * @description
-   * Provide an address form group.
-   *
-   * @param options available for manipulating the form group
-   *  areRequired control names
-   *  areDisabled control names
-   *  useDefaults for province and country, otherwise empty
-   */
-  // TODO when everything is working then start sliding this into place
-  private buildAddressForm(options: {
-    areRequired: string[],
-    areDisabled: string[],
-    useDefaults: boolean
-  }): FormGroup {
-    const controlsConfig = {
-      id: [
-        0,
-        []
-      ],
-      street: [
-        { value: null, disabled: false },
-        []
-      ],
-      street2: [
-        { value: null, disabled: false },
-        []
-      ],
-      city: [
-        { value: null, disabled: false },
-        []
-      ],
-      provinceCode: [
-        { value: null, disabled: false },
-        []
-      ],
-      countryCode: [
-        { value: null, disabled: false },
-        []
-      ],
-      postal: [
-        { value: null, disabled: false },
-        []
-      ]
-    };
-
-    Object.keys(controlsConfig).map((key: string, index: number) => {
-      const control = controlsConfig[key];
-      if (options.areDisabled.includes(key)) {
-        control[0].disabled = true;
-      }
-      if (options.useDefaults) {
-        if (key === 'provinceCode') {
-          control[0].value = Province.BRITISH_COLUMBIA;
-        } else if (key === 'countryCode') {
-          control[0].value = Country.CANADA;
-        }
-      }
-      if (options.areRequired.includes(key)) {
-        control[1].push(Validators.required);
-      }
-    });
-
-    return this.fb.group(controlsConfig);
   }
 }
