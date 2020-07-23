ca099ec138238681a90a64b9a156e99b7cf2c48b
Martin Pultz <mtpultz@gmail.com>
2020-07-22 18:49:55 -0600

    Updated abstract form state class

---
+++ prime-angular-frontend/src/app/modules/site-registration/shared/classes/abstract-form-state.class.ts
@@ -0,0 +1,155 @@
+import { AbstractControl, FormBuilder, Validators, FormGroup } from '@angular/forms';
+
+import { Province } from '@shared/enums/province.enum';
+import { Country } from '@shared/enums/country.enum';
+
+export abstract class AbstractFormState<T> {
+  protected patched: boolean;
+
+  constructor(
+    protected fb: FormBuilder
+  ) {
+    // Initial state of the form is unpatched and ready for
+    // enrolment information
+    this.patched = false;
+
+    this.init();
+  }
+
+  /**
+   * @description
+   * Convert JSON into reactive form abstract controls, which can
+   * only be set more than once when explicitly forced.
+   */
+  public setForm(model: T, forcePatch: boolean = false) {
+    if (this.patched && !forcePatch) {
+      return;
+    }
+
+    // Indicate that the form is patched, and may contain unsaved information
+    this.patched = true;
+
+    this.patchForm(model);
+  }
+
+  /**
+   * @description
+   * Convert reactive form abstract controls into JSON.
+   */
+  public abstract get json(): T;
+
+  /**
+   * @description
+   * List of constituent model forms, which is used at minimum to
+   * drive internal form helper methods.
+   */
+  public get forms(): AbstractControl[] {
+    return [];
+  }
+
+  /**
+   * @description
+   * Check that all constituent forms are valid.
+   */
+  public get isValid(): boolean {
+    return this.forms
+      .reduce((valid: boolean, form: AbstractControl) => valid && form.valid, true);
+  }
+
+  /**
+   * @description
+   * Check that at least one constituent form is dirty.
+   */
+  public get isDirty(): boolean {
+    return this.forms
+      .reduce((dirty: boolean, form: AbstractControl) => dirty || form.dirty, false);
+  }
+
+  /**
+   * @description
+   * Mark all constituent forms as pristine.
+   */
+  public markAsPristine(): void {
+    this.forms
+      .forEach((form: AbstractControl) => form.markAsPristine());
+  }
+
+  /**
+   * @description
+   * Initialize and configure the forms for patching, which is also used
+   * to clear previous form data from the service.
+   */
+  public abstract init(): void;
+
+  /**
+   * @description
+   * Manage the conversion of JSON to reactive forms.
+   */
+  protected abstract patchForm(model: T): T;
+
+  /**
+   * @description
+   * Provide an address form group.
+   *
+   * @param options available for manipulating the form group
+   *  areRequired control names
+   *  areDisabled control names
+   *  useDefaults for province and country, otherwise empty
+   */
+  // TODO start sliding this into form builders
+  protected buildAddressForm(options: {
+    areRequired: string[],
+    areDisabled: string[],
+    useDefaults: boolean
+  }): FormGroup {
+    const controlsConfig = {
+      id: [
+        0,
+        []
+      ],
+      street: [
+        { value: null, disabled: false },
+        []
+      ],
+      street2: [
+        { value: null, disabled: false },
+        []
+      ],
+      city: [
+        { value: null, disabled: false },
+        []
+      ],
+      provinceCode: [
+        { value: null, disabled: false },
+        []
+      ],
+      countryCode: [
+        { value: null, disabled: false },
+        []
+      ],
+      postal: [
+        { value: null, disabled: false },
+        []
+      ]
+    };
+
+    Object.keys(controlsConfig).map((key: string, index: number) => {
+      const control = controlsConfig[key];
+      if (options.areDisabled.includes(key)) {
+        control[0].disabled = true;
+      }
+      if (options.useDefaults) {
+        if (key === 'provinceCode') {
+          control[0].value = Province.BRITISH_COLUMBIA;
+        } else if (key === 'countryCode') {
+          control[0].value = Country.CANADA;
+        }
+      }
+      if (options.areRequired.includes(key)) {
+        control[1].push(Validators.required);
+      }
+    });
+
+    return this.fb.group(controlsConfig);
+  }
+}
