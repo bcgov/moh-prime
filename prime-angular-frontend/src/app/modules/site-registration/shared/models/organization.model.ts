68278567e59c88c86181098d19502c70fbea3a5c
Martin Pultz <mtpultz@gmail.com>
2020-07-23 13:29:53 -0600

    Drop location model, Update site and organization models

--- prime-angular-frontend/src/app/modules/site-registration/shared/models/organization.model.ts
+++ prime-angular-frontend/src/app/modules/site-registration/shared/models/organization.model.ts
@@ -1,18 +1,17 @@
 import { Party } from './party.model';
-import { Location } from './location.model';
 import { SignedAgreementDocument } from './signed-agreement-document.model';
 
 export interface Organization {
   id?: number;
+
   signingAuthorityId?: number;
   signingAuthority: Party;
   name: string;
   registrationId: string;
   doingBusinessAs?: string;
-  organizationTypeCode: number;
-  acceptedAgreementDate: string;
-  locations: Location[];
-  submittedDate: string;
+
   completed: boolean;
+  acceptedAgreementDate: string;
   signedAgreementDocuments: SignedAgreementDocument[];
+  submittedDate: string;
 }
