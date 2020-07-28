d4b29d9ceecf1e51876235b89714b245bcbb3761
Anais <anais.hebert@nttdata.com>
2020-07-27 17:32:21 -0700

    update name of in controller

--- prime-dotnet-webapi/Controllers/SitesController.cs
+++ prime-dotnet-webapi/Controllers/SitesController.cs
@@ -179,7 +179,7 @@ namespace Prime.Controllers
         /// Updates a sites state
         /// </summary>
         /// <param name="siteId"></param>
-        [HttpPut("{siteId}/completed", Name = nameof(UpdateSite))]
+        [HttpPut("{siteId}/completed", Name = nameof(UpdateSiteCompleted))]
         [ProducesResponseType(typeof(ApiBadRequestResponse), StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
