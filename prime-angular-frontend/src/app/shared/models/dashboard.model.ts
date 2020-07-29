97e0b61ac619fde43f19cb808d56698d750830b3
Martin Pultz <mtpultz@gmail.com>
2020-07-29 11:59:43 -0600

    Updated dashboard model

--- prime-angular-frontend/src/app/shared/models/dashboard.model.ts
+++ prime-angular-frontend/src/app/shared/models/dashboard.model.ts
@@ -6,9 +6,9 @@ export interface DashboardNavSection {
 
 export interface DashboardNavSectionItem {
   name: string;
-  icon: string;
-  route: string;
-  showItem: boolean;
+  route: string | (string | number)[];
+  icon?: string;
+  showItem?: boolean;
   disabled?: boolean;
   deemphasize?: boolean; // Reduce opacity
   forceActive?: boolean;
@@ -32,14 +32,27 @@ export class DashboardNavProps {
   fixedInViewport: boolean;
   /**
    * @description
-   * Whether to show the icon, or the icon and label.
+   * Whether to show the dashboard navigation item icon.
+   */
+  showNavItemIcons: boolean;
+  /**
+   * @description
+   * Whether to show the dashboard navigation item label.
    */
   showNavItemLabels: boolean;
 
-  constructor(mode: MatDrawerMode, opened: boolean, fixedInViewport: boolean, showLabel: boolean) {
+  constructor(
+    mode: MatDrawerMode,
+    opened: boolean,
+    fixedInViewport: boolean,
+    showNavItemIcons: boolean,
+    showNavItemLabels: boolean
+  ) {
     this.mode = mode;
     this.opened = opened;
     this.fixedInViewport = fixedInViewport;
-    this.showNavItemLabels = showLabel;
+    this.showNavItemIcons = showNavItemIcons;
+    // Force labels to be shown when icons are turned off
+    this.showNavItemLabels = (!showNavItemIcons) ? true : showNavItemLabels;
   }
 }
