f17e9207cb3cd1cac10cfc8a1a4ec175f743ea01
Martin Pultz <mtpultz@gmail.com>
2020-07-28 22:59:01 -0600

    Updated dashboard models

--- prime-angular-frontend/src/app/shared/models/dashboard.model.ts
+++ prime-angular-frontend/src/app/shared/models/dashboard.model.ts
@@ -1,6 +1,6 @@
+import { MatDrawerMode } from '@angular/material/sidenav';
+
 export interface DashboardNavSection {
-  header?: string;
-  showHeader?: boolean;
   items: DashboardNavSectionItem[];
 }
 
@@ -10,6 +10,36 @@ export interface DashboardNavSectionItem {
   route: string;
   showItem: boolean;
   disabled?: boolean;
-  deemphasize?: boolean;
+  deemphasize?: boolean; // Reduce opacity
   forceActive?: boolean;
+}
+
+export class DashboardNavProps {
+  /**
+   * @description
+   * Mode of the drawer; one of 'over', 'push' or 'side'.
+   */
+  mode: MatDrawerMode;
+  /**
+   * @description
+   * Whether the drawer is opened.
+   */
+  opened: boolean;
+  /**
+   * @description
+   * Whether the sidenav is fixed in the viewport.
+   */
+  fixedInViewport: boolean;
+  /**
+   * @description
+   * Whether to show the icon, or the icon and label.
+   */
+  showNavItemLabels: boolean;
+
+  constructor(mode: MatDrawerMode, opened: boolean, fixedInViewport: boolean, showLabel: boolean) {
+    this.mode = mode;
+    this.opened = opened;
+    this.fixedInViewport = fixedInViewport;
+    this.showNavItemLabels = showLabel;
+  }
 }
