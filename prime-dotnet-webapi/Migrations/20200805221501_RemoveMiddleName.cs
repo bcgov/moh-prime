0665acd09588562be5a93400b0f4e16df5cf24bb
Nathan <Hogman>
2020-08-06 15:01:25 -0700

    PRIME-933 condense migrations

--- prime-dotnet-webapi/Migrations/20200805221501_RemoveMiddleName.cs
+++ prime-dotnet-webapi/Migrations/20200805221501_RemoveMiddleName.cs
@@ -6,6 +6,12 @@ namespace Prime.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.AddColumn<string>(
+                name: "GivenNames",
+                table: "Enrollee",
+                nullable: false,
+                defaultValue: "");
+
             migrationBuilder.DropColumn(
                 name: "MiddleName",
                 table: "Enrollee");
@@ -18,6 +24,10 @@ namespace Prime.Migrations
                 table: "Enrollee",
                 type: "text",
                 nullable: true);
+
+            migrationBuilder.DropColumn(
+                name: "GivenNames",
+                table: "Enrollee");
         }
     }
 }
