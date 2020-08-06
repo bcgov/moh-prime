c72854438e1b93e1d0d92948005a741fb55e082d
Martin Pultz <mtpultz@gmail.com>
2020-08-06 15:53:24 -0700

    Fix collection notice test

--- prime-angular-frontend/src/app/modules/site-registration/pages/collection-notice/collection-notice.component.spec.ts
+++ prime-angular-frontend/src/app/modules/site-registration/pages/collection-notice/collection-notice.component.spec.ts
@@ -3,9 +3,12 @@ import { RouterTestingModule } from '@angular/router/testing';
 
 import { KeycloakService } from 'keycloak-angular';
 
+import { MockOrganizationService } from 'test/mocks/mock-organization.service';
+
 import { CollectionNoticeComponent } from './collection-notice.component';
 import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
 import { SiteRegistrationModule } from '@registration/site-registration.module';
+import { OrganizationService } from '@registration/shared/services/organization.service';
 
 describe('CollectionNoticeComponent', () => {
   let component: CollectionNoticeComponent;
@@ -21,6 +24,10 @@ describe('CollectionNoticeComponent', () => {
         {
           provide: APP_CONFIG,
           useValue: APP_DI_CONFIG
+        },
+        {
+          provide: OrganizationService,
+          useClass: MockOrganizationService
         },
         KeycloakService
       ]
