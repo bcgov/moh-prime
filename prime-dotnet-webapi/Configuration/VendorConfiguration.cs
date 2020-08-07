5a87de10c3d8173dd05928aacf4fc1b4a4c38895
James <jameshollinger.ee@gmail.com>
2020-08-07 15:20:06 -0700

    remove orgtype keys

--- prime-dotnet-webapi/Configuration/VendorConfiguration.cs
+++ prime-dotnet-webapi/Configuration/VendorConfiguration.cs
@@ -10,16 +10,16 @@ namespace Prime.Configuration
             get
             {
                 return new[] {
-                    new Vendor { Code = 1, OrganizationTypeCode = OrganizationType.CommunityPractice, Name = "CareConnect", Email = "CareConnect@phsa.ca", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new Vendor { Code = 2, OrganizationTypeCode = OrganizationType.CommunityPractice, Name = "Excelleris", Email = "support@excelleris.com", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new Vendor { Code = 3, OrganizationTypeCode = OrganizationType.CommunityPractice, Name = "iClinic", Email = "help@iclinicemr.com", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new Vendor { Code = 4, OrganizationTypeCode = OrganizationType.CommunityPractice, Name = "Medinet", Email = "prime@medinet.ca", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new Vendor { Code = 5, OrganizationTypeCode = OrganizationType.CommunityPractice, Name = "Plexia", Email = "service@plexia.ca", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new Vendor { Code = 6, OrganizationTypeCode = OrganizationType.CommunityPharmacy, Name = "Telus Health", Email = "", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new Vendor { Code = 7, OrganizationTypeCode = OrganizationType.CommunityPharmacy, Name = "Shoppers Drug Mart", Email = "", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new Vendor { Code = 8, OrganizationTypeCode = OrganizationType.CommunityPharmacy, Name = "Applied Robotics", Email = "", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new Vendor { Code = 9, OrganizationTypeCode = OrganizationType.CommunityPharmacy, Name = "McKesson", Email = "", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new Vendor { Code = 10, OrganizationTypeCode = OrganizationType.CommunityPharmacy, Name = "Commander Group", Email = "", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new Vendor { Code = 1,  OrganizationTypeCode = (int)CareSettingType.CommunityPractice, Name = "CareConnect", Email = "CareConnect@phsa.ca", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new Vendor { Code = 2,  OrganizationTypeCode = (int)CareSettingType.CommunityPractice, Name = "Excelleris", Email = "support@excelleris.com", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new Vendor { Code = 3,  OrganizationTypeCode = (int)CareSettingType.CommunityPractice, Name = "iClinic", Email = "help@iclinicemr.com", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new Vendor { Code = 4,  OrganizationTypeCode = (int)CareSettingType.CommunityPractice, Name = "Medinet", Email = "prime@medinet.ca", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new Vendor { Code = 5,  OrganizationTypeCode = (int)CareSettingType.CommunityPractice, Name = "Plexia", Email = "service@plexia.ca", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new Vendor { Code = 6,  OrganizationTypeCode = (int)CareSettingType.CommunityPharmacy, Name = "Telus Health", Email = "", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new Vendor { Code = 7,  OrganizationTypeCode = (int)CareSettingType.CommunityPharmacy, Name = "Shoppers Drug Mart", Email = "", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new Vendor { Code = 8,  OrganizationTypeCode = (int)CareSettingType.CommunityPharmacy, Name = "Applied Robotics", Email = "", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new Vendor { Code = 9,  OrganizationTypeCode = (int)CareSettingType.CommunityPharmacy, Name = "McKesson", Email = "", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new Vendor { Code = 10, OrganizationTypeCode = (int)CareSettingType.CommunityPharmacy, Name = "Commander Group", Email = "", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
                 };
             }
         }
