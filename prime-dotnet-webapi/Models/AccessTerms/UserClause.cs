a8b7086c10ffeb8026f04c2b79505f48c41cc31d
James <jameshollinger.ee@gmail.com>
2020-08-06 20:55:23 -0700

    user clause seed data stuff

--- prime-dotnet-webapi/Models/AccessTerms/UserClause.cs
+++ prime-dotnet-webapi/Models/AccessTerms/UserClause.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Prime.Configuration.Agreements;
 
 namespace Prime.Models
 {
@@ -13,6 +16,17 @@ namespace Prime.Models
         public string Text { get; set; }
 
         public DateTimeOffset EffectiveDate { get; set; }
+
+        /// <summary>
+        /// Returns a list containing the IDs of the newest version of each type of Agreement.
+        /// </summary>
+        public static IEnumerable<int> NewestAgreementIds()
+        {
+            return AgreementConfiguration.SeedData
+                .GroupBy(a => a.GetType())
+                .Select(g => g.OrderByDescending(a => a.EffectiveDate).First())
+                .Select(a => a.Id);
+        }
     }
 
     public class CommunityPharmacistAgreement : UserClause
