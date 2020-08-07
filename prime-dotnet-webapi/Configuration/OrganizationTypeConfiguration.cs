99a92406f13708a1f7af70da5e45e04bf5fe02a1
Martin Pultz <mtpultz@users.noreply.github.com>
2020-08-07 17:05:27 -0600

    Updated device provider lookup code (#769)

--- prime-dotnet-webapi/Configuration/OrganizationTypeConfiguration.cs
+++ prime-dotnet-webapi/Configuration/OrganizationTypeConfiguration.cs
@@ -13,7 +13,7 @@ namespace Prime.Configuration
                     new OrganizationType { Code = 1, Name = "Health Authority", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
                     new OrganizationType { Code = 2, Name = "Private Community Health Practice", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
                     new OrganizationType { Code = 3, Name = "Community Pharmacy", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new OrganizationType { Code = 5, Name = "Device Provider", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE }
+                    new OrganizationType { Code = 4, Name = "Device Provider", CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE }
                 };
             }
         }
