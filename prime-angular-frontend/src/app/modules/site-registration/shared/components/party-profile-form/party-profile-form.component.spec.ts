a1914dbeacdb651ca23bf2f7980f94c2a057b0c3
Martin Pultz <mtpultz@gmail.com>
2020-08-06 19:50:05 -0700

    Fix dashboard2 test

--- prime-angular-frontend/src/app/modules/site-registration/shared/components/party-profile-form/party-profile-form.component.spec.ts
+++ prime-angular-frontend/src/app/modules/site-registration/shared/components/party-profile-form/party-profile-form.component.spec.ts
@@ -1,8 +1,9 @@
 import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { FormGroup } from '@angular/forms';
+import { ReactiveFormsModule, FormGroup } from '@angular/forms';
 
 import { MockConfigService } from 'test/mocks/mock-config.service';
 import { MockSiteService } from 'test/mocks/mock-site.service';
@@ -10,7 +11,6 @@ import { MockSiteService } from 'test/mocks/mock-site.service';
 import { PartyProfileFormComponent } from './party-profile-form.component';
 import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
 import { ConfigService } from '@config/config.service';
-import { SiteRegistrationModule } from '@registration/site-registration.module';
 import { SiteService } from '@registration/shared/services/site.service';
 import { SiteFormStateService } from '@registration/shared/services/site-form-state.service';
 
@@ -20,11 +20,14 @@ describe('PartyProfileFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      declarations: [
+        PartyProfileFormComponent
+      ],
       imports: [
-        HttpClientTestingModule,
-        SiteRegistrationModule,
         BrowserAnimationsModule,
-        RouterTestingModule
+        HttpClientTestingModule,
+        RouterTestingModule,
+        ReactiveFormsModule
       ],
       providers: [
         {
@@ -40,18 +43,22 @@ describe('PartyProfileFormComponent', () => {
           useClass: MockSiteService
         },
         SiteFormStateService
-      ]
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
 
-  beforeEach(inject([SiteService, SiteFormStateService], (siteService: SiteService, siteFormStateService: SiteFormStateService) => {
-    fixture = TestBed.createComponent(PartyProfileFormComponent);
-    component = fixture.componentInstance;
-    siteFormStateService.setForm(siteService.site);
-    // Add the bound FormGroup to the component
-    component.form = siteFormStateService.administratorPharmaNetForm as FormGroup;
-    fixture.detectChanges();
-  }));
+  beforeEach(inject(
+    [SiteService, SiteFormStateService],
+    (siteService: SiteService, siteFormStateService: SiteFormStateService) => {
+      fixture = TestBed.createComponent(PartyProfileFormComponent);
+      component = fixture.componentInstance;
+      siteFormStateService.setForm(siteService.site);
+      // Add the bound FormGroup to the component
+      component.form = siteFormStateService.administratorPharmaNetForm as FormGroup;
+      fixture.detectChanges();
+    })
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
--- prime-angular-frontend/src/app/shared/components/dashboard2/dashboard2.component.spec.ts
+++ prime-angular-frontend/src/app/shared/components/dashboard2/dashboard2.component.spec.ts
@@ -2,8 +2,11 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { KeycloakService } from 'keycloak-angular';
 
+import { MockConfigService } from 'test/mocks/mock-config.service';
+
 import { Dashboard2Component } from './dashboard2.component';
 import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
+import { ConfigService } from '@config/config.service';
 
 describe('Dashboard2Component', () => {
   let component: Dashboard2Component;
@@ -16,6 +19,10 @@ describe('Dashboard2Component', () => {
         {
           provide: APP_CONFIG,
           useValue: APP_DI_CONFIG
+        },
+        {
+          provide: ConfigService,
+          useClass: MockConfigService
         },
         KeycloakService
       ]
