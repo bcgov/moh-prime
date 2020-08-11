eac16922dac76df78f31395cec98a179c7ec84f5
Martin Pultz <mtpultz@gmail.com>
2020-08-11 14:09:07 -0700

    Updated document access controller endpoints

--- prime-dotnet-webapi/Controllers/DocumentAccessController.cs
+++ prime-dotnet-webapi/Controllers/DocumentAccessController.cs
@@ -4,10 +4,10 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Prime.Auth;
-using Prime.Auth;
 using Prime.Models;
 using Prime.Models.Api;
 using Prime.Services;
+using Prime.Services.Clients;
 
 namespace Prime.Controllers
 {
@@ -18,14 +18,17 @@ namespace Prime.Controllers
     {
         private readonly IDocumentService _documentService;
         private readonly IDocumentAccessTokenService _documentAccessTokenService;
+        private readonly IDocumentManagerClient _documentManagerClient;
 
 
         public DocumentAccessController(
             IDocumentService documentService,
-            IDocumentAccessTokenService documentAccessTokenService)
+            IDocumentAccessTokenService documentAccessTokenService,
+            IDocumentManagerClient documentManagerClient)
         {
             _documentService = documentService;
             _documentAccessTokenService = documentAccessTokenService;
+            _documentManagerClient = documentManagerClient;
         }
 
         // GET: api/document-access/file-download/{accessTokenId}
@@ -33,69 +36,56 @@ namespace Prime.Controllers
         /// Gets the Document for downloading based on the supplied Document Access Token GUID.
         /// </summary>
         /// <remarks>This endpoint is not authenticated.</remarks>
-        [HttpGet("file-download/{accessTokenId}", Name = nameof(GetDownloadDocument))]
+        [HttpGet("file-download/{accessTokenId}", Name = nameof(GetDocumentByAccessToken))]
         [AllowAnonymous]
         [ProducesResponseType(typeof(ApiMessageResponse), StatusCodes.Status404NotFound)]
-        [ProducesResponseType(typeof(ApiResultResponse<EnrolmentCertificate>), StatusCodes.Status200OK)]
-        public async Task<ActionResult<EnrolmentCertificate>> GetDownloadDocument(Guid accessTokenId)
+        [ProducesResponseType(typeof(ApiResultResponse<bool>), StatusCodes.Status200OK)]
+        public async Task<ActionResult> GetDocumentByAccessToken(Guid accessTokenId)
         {
-            var document = await _documentService.GetDocumentByAccessToken(accessTokenId);
+            var documentAccessToken = await _documentAccessTokenService.GetDocumentAccessAsync(accessTokenId);
 
-            // TODO does this return null if not found?
-            if (document == null)
+            if (documentAccessToken == null)
             {
                 return NotFound(ApiResponse.Message($"No valid Document Access Token was found with id {accessTokenId}"));
             }
 
-            // TODO download the document
+            var document = await _documentManagerClient.GetFileAsync(documentAccessToken.DocumentGuid);
 
-            return Ok(ApiResponse.Result(document));
+            // TODO download the document
+            // var net = new System.Net.WebClient();
+            // var data = net.DownloadData(link);
+            // var content = new System.IO.MemoryStream(data);
+            // var contentType = "application/octet-stream";
+            // var fileName = "something.bin";
+            // return File(content, contentType, fileName);
+
+            return Ok(true);
         }
 
-        // DELETE: api/document-access/file-download/{accessTokenId}
+        // DELETE: api/document-access/{accessTokenId}
         /// <summary>
-        /// Delete the Document for downloading based on the supplied Document Access Token GUID.
+        /// Delete the Document Access Token for downloading a Document.
         /// </summary>
-        /// <remarks>This endpoint is not authenticated.</remarks>
-        [HttpDelete("file-download/{accessTokenId}", Name = nameof(DeleteDownloadDocument))]
+        [HttpDelete("{accessTokenId}", Name = nameof(DeleteDocumentAccessToken))]
         [Authorize(Policy = AuthConstants.SUPER_ADMIN_POLICY)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(typeof(ApiMessageResponse), StatusCodes.Status404NotFound)]
-        [ProducesResponseType(typeof(ApiResultResponse<EnrolmentCertificate>), StatusCodes.Status200OK)]
-        public async Task<ActionResult<EnrolmentCertificate>> DeleteDownloadDocument(Guid accessTokenId)
+        // [ProducesResponseType(typeof(ApiResultResponse<Enrollee>), StatusCodes.Status200OK)]
+        // TODO delete tends to provide the resource it deletes, but that exposes a DocumentGuid so NoContent?
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<ActionResult<EnrolmentCertificate>> DeleteDocumentAccessToken(Guid accessTokenId)
         {
-            if (accessTokenId == null)
-            {
+            var documentAccessToken = _documentAccessTokenService.GetDocumentAccessAsync(accessTokenId);
 
-            }
-
-            await _documentAccessTokenService.DeleteDocumentAccessToken(accessTokenId);
-
-            // TODO does this return null if not found?
-            if (document == null)
+            if (documentAccessToken == null)
             {
                 return NotFound(ApiResponse.Message($"No valid Document Access Token was found with id {accessTokenId}"));
             }
 
-            // TODO download the document
+            await _documentAccessTokenService.DeleteDocumentAccessTokenAsync(accessTokenId);
 
-            return Ok(ApiResponse.Result(document));
+            // return Ok(ApiResponse.Result(documentAccessToken));
+            return NoContent();
         }
-
-        //         var enrollee = await _enrolleeService.GetEnrolleeAsync(enrolleeId);
-
-        //             if (enrollee == null)
-        //             {
-        //                 return NotFound(ApiResponse.Message($"Enrollee not found with id {enrolleeId}"));
-        //             }
-
-        //             if (!User.CanEdit(enrollee))
-        //             {
-        //                 return Forbid();
-        // }
-
-        // await _enrolleeService.DeleteEnrolleeAsync(enrolleeId);
-
-        // return Ok(ApiResponse.Result(enrollee));
     }
 }
