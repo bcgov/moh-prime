f9da1bc9a54dd46fde2cdab3415b22a9b6dae63f
James <jameshollinger.ee@gmail.com>
2020-08-18 15:36:50 -0700

    updates

--- prime-angular-frontend/src/app/shared/components/document-upload/document-upload/document-upload.component.ts
+++ prime-angular-frontend/src/app/shared/components/document-upload/document-upload/document-upload.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit, ViewChild, Input, Output } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
 import { EventEmitter } from '@angular/core';
 
-import { FilePondOptions, FilePondServerConfigProps } from 'filepond';
+import { FilePondOptions, ProcessServerConfigFunction } from 'filepond';
 import { FilePondPluginFileValidateTypeProps } from 'filepond-plugin-file-validate-type';
 import { FilePondPluginFileValidateSizeProps } from 'filepond-plugin-file-validate-size';
 import { FilePondComponent } from 'ngx-filepond/filepond.component';
@@ -10,14 +9,8 @@ import tus from 'tus-js-client';
 
 import { environment } from '@env/environment';
 
-import { ToastService } from '@core/services/toast.service';
 import { LoggerService } from '@core/services/logger.service';
-import { FormUtilsService } from '@core/services/form-utils.service';
-import { SiteResource } from '@core/resources/site-resource.service';
-
 import { KeycloakTokenService } from '@auth/shared/services/keycloak-token.service';
-import { SiteService } from '@registration/shared/services/site.service';
-import { from } from 'rxjs';
 
 export class BaseDocument {
   id: number;
@@ -43,27 +36,29 @@ export class DocumentUploadComponent implements OnInit {
   @ViewChild('filePond') public filePondComponent: FilePondComponent;
   public filePondOptions: FilePondOptions & FilePondPluginFileValidateSizeProps & FilePondPluginFileValidateTypeProps;
   public filePondFiles = [];
-  public apiSuffix = '/document';
+
+  private apiSuffix = 'document';
+  private jwt: string;
 
   constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-    private siteService: SiteService,
-    private siteResource: SiteResource,
     private keycloakTokenService: KeycloakTokenService,
-    private toastService: ToastService,
     private logger: LoggerService,
-    private formUtilsService: FormUtilsService
   ) { }
 
   public ngOnInit(): void {
+    const fileValidateTypeLabelExpectedTypesMap = {
+      'image/jpeg': '.jpeg, .jpg',
+      'image/png': '.png'
+    };
+
     this.filePondOptions = {
       className: `prime-filepond-${this.componentName}`,
       labelIdle: 'Click to Browse or Drop files here',
-      acceptedFileTypes: ['image/jpeg', 'image/png'],
+      fileValidateTypeLabelExpectedTypesMap,
+      acceptedFileTypes: Object.keys(fileValidateTypeLabelExpectedTypesMap),
       allowFileTypeValidation: true,
       allowMultiple: !!this.multiple,
-      maxFileSize: null,// '3MB',
+      maxFileSize: '3MB',
       maxTotalFileSize: null,
       server: this.constructServer()
     };
@@ -76,56 +71,50 @@ export class DocumentUploadComponent implements OnInit {
     this.logger.info('FilePond has initialized', this.filePondComponent);
   }
 
-  private constructServer() {
-    return {
-      process: (fieldName, file, metadata, load, error, progress, abort) => {
-        from(this.keycloakTokenService.token())
-          .subscribe(token => {
-
-            const { name: filename, type: filetype } = file;
-
-            const upload = new tus.Upload(file, {
-              endpoint: `${environment.apiEndpoint}${this.apiSuffix}`,
-              retryDelays: [100, 3000],
-              metadata: { filename, filetype },
-              chunkSize: 1048576, // 1 MB
-              headers: this.createHeaders(),
-              onError: (err: Error) => {
-                this.logger.error('DocumentUpload::onFilePondAddFile', err);
-                error(err);
-                // TODO throws an error intermittently so commented out for release
-                // this.toastService.openErrorToast(err.message),
-              },
-              onProgress: (bytesUploaded: number, bytesTotal: number) => progress(true, bytesUploaded, bytesTotal),
-              onSuccess: () => {
-                const documentGuid = upload.url.split('/').pop();
-                load(documentGuid);
-                //this.completed.emit(new BaseDocument(filename, documentGuid));
-                this.toastService.openSuccessToast('File(s) have been uploaded');
-              }
-            });
-
-            upload.start();
-            return {
-              abort: () => {
-                upload.abort();
-                abort();
-              },
-            };
-          });
-      },
-
-      // No-Op for revert to prevent the default DELETE request that FilePond does when removing a file.
-      revert: (seource: any, load: () => void, error: (errorText: string) => void) => load(),
-    };
+  public async onFilePondAddFile() {
+    // Can't get token synchronously inside server.process(), so refresh token on file add.
+    this.jwt = await this.keycloakTokenService.token();
   }
 
-  private createHeaders() {
-    const token = ""; //await this.keycloakTokenService.token();
+  private constructServer() {
+    const process: ProcessServerConfigFunction = (fieldName, file, metadata, load, error, progress, abort) => {
+      const { name: filename, type: filetype } = file;
+
+      const upload = new tus.Upload(file, {
+        endpoint: `${environment.apiEndpoint}/${this.apiSuffix}`,
+        metadata: { filename, filetype },
+        chunkSize: 1048576, // 1 MB
+        removeFingerprintOnSuccess: true,
+        retryDelays: [100, 3000],
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          Authorization: `Bearer ${this.jwt}`,
+        },
+        onError: (err: Error) => {
+          this.logger.error('DocumentUpload::onFilePondAddFile', err);
+          error(err.message);
+        },
+        onProgress: (bytesUploaded: number, bytesTotal: number) => progress(true, bytesUploaded, bytesTotal),
+        onSuccess: () => {
+          const documentGuid = upload.url.split('/').pop();
+          load(documentGuid);
+          this.completed.emit(new BaseDocument(filename, documentGuid));
+        }
+      });
+
+      upload.start();
+      return {
+        abort: () => {
+          upload.abort();
+          abort();
+        },
+      };
+    };
 
     return {
-      'Access-Control-Allow-Origin': '*',
-      Authorization: `Bearer ${token}`,
+      process,
+      // No-Op for revert to prevent the default DELETE request that FilePond does when removing a file.
+      revert: (source: any, load: () => void, error: (errorText: string) => void) => load(),
     };
   }
 }
