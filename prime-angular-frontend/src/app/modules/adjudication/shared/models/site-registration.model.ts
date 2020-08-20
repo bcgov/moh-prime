2a4fe77474de776fb64471879fe142a72d3498c8
Martin Pultz <mtpultz@gmail.com>
2020-08-19 17:54:38 -0700

    Updated default for organization information in the table

--- prime-angular-frontend/src/app/modules/adjudication/shared/models/site-registration.model.ts
+++ prime-angular-frontend/src/app/modules/adjudication/shared/models/site-registration.model.ts
@@ -1,7 +1,14 @@
 import { SiteViewModel } from '@registration/shared/models/site.model';
 import { OrganizationViewModel } from '@registration/shared/models/organization.model';
 
-// TODO add a comment on why this exists
+/**
+ * View model specifically created to combine multiple models to allow use
+ * of the Angular Material table, which requires the model hierarchy to be
+ * flattened as it manages iterating over the datasource internally.
+ *
+ * NOTE: should only be used within the SiteRegistrationContainer,
+ * SiteRegistrationTable, and SiteRegistrationActions
+ */
 export interface SiteRegistrationViewModel extends Omit<OrganizationViewModel, 'id'>, Omit<SiteViewModel, 'id'> {
   organizationId: number;
   siteId: number;
