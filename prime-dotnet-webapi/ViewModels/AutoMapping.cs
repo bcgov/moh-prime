1ccaf21befb69a5b4da5ef93e3ed838c47e087bb
James <jameshollinger.ee@gmail.com>
2020-08-21 18:58:52 -0700

    AUTOMAPPER

---
+++ prime-dotnet-webapi/ViewModels/AutoMapping.cs
@@ -0,0 +1,17 @@
+using AutoMapper;
+
+using Prime.Models;
+using Prime.ViewModels;
+
+/**
+ * Automapper Documentation
+ * @see https://docs.automapper.org/en/stable
+ */
+public class AutoMapping : Profile
+{
+    public AutoMapping()
+    {
+        // CreateMap<Organization, OrganizationViewModel>();
+        // CreateMap<Site, SiteViewModel>();
+    }
+}
