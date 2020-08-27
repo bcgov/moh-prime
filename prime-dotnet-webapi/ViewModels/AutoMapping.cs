457683195ec8a84e4e7bda950b48c40196ca07c6
James <jameshollinger.ee@gmail.com>
2020-08-27 04:30:13 -0700

    rename

--- prime-dotnet-webapi/ViewModels/AutoMapping.cs
+++ prime-dotnet-webapi/ViewModels/AutoMapping.cs
@@ -14,13 +14,13 @@ public class AutoMapping : Profile
     public AutoMapping()
     {
         CreateMap<Organization, OrganizationListViewModel>()
-            .ForMember(vm => vm.SignedAgreementDocumentCount, dest => dest.MapFrom(src => src.SignedAgreementDocuments.Count));
+            .ForMember(dest => dest.SignedAgreementDocumentCount, opt => opt.MapFrom(src => src.SignedAgreementDocuments.Count));
         CreateMap<Site, SiteListViewModel>();
 
         IQueryable<NewestAgreement> newestAgreements = null;
         CreateMap<Enrollee, EnrolleeListViewModel>()
-            .ForMember(dest => dest.CurrentStatusCode, dest => dest.MapFrom(src => src.CurrentStatus.StatusCode))
-            .ForMember(dest => dest.AdjudicatorIdir, dest => dest.MapFrom(src => src.Adjudicator.IDIR))
-            .ForMember(dest => dest.HasNewestAgreement, dest => dest.MapFrom(src => newestAgreements.Any(n => n.Id == src.CurrentAgreementId)));
+            .ForMember(dest => dest.CurrentStatusCode, opt => opt.MapFrom(src => src.CurrentStatus.StatusCode))
+            .ForMember(dest => dest.AdjudicatorIdir, opt => opt.MapFrom(src => src.Adjudicator.IDIR))
+            .ForMember(dest => dest.HasNewestAgreement, opt => opt.MapFrom(src => newestAgreements.Any(n => n.Id == src.CurrentAgreementId)));
     }
 }
