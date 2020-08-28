cdfe17ecce4c7d92a0919ddfde1214ba51757d86
Martin Pultz <mtpultz@gmail.com>
2020-08-28 13:46:15 -0700

    Added splice to string utils

--- prime-angular-frontend/src/app/lib/utils/string-utils.class.ts
+++ prime-angular-frontend/src/app/lib/utils/string-utils.class.ts
@@ -2,4 +2,11 @@ export class StringUtils {
   public static capitalize(value: string): string {
     return value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
   }
+
+  public static splice(text: string, insertPosition: number, insertText: string, removeCount: number = 0): string {
+    const calculatedPosition = (insertPosition < 0)
+      ? text.length + insertPosition
+      : insertPosition;
+    return `${text.substring(0, calculatedPosition)}${insertText}${text.substring(calculatedPosition + removeCount)}`;
+  }
 }
