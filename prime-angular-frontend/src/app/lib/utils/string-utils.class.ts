0715207fe2120df4d984de772d37c741987b19ee
Martin Pultz <mtpultz@gmail.com>
2020-08-28 15:40:22 -0700

    Updated string utils

--- prime-angular-frontend/src/app/lib/utils/string-utils.class.ts
+++ prime-angular-frontend/src/app/lib/utils/string-utils.class.ts
@@ -1,9 +1,14 @@
 export class StringUtils {
   public static capitalize(value: string): string {
-    return value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
+    return (value)
+      ? `${value.charAt(0).toUpperCase()}${value.slice(1).toLowerCase()}`
+      : value;
   }
 
   public static splice(text: string, insertPosition: number, insertText: string, removeCount: number = 0): string {
+    if (!text) {
+      return text;
+    }
     const calculatedPosition = (insertPosition < 0)
       ? text.length + insertPosition
       : insertPosition;
