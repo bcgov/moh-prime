a7e97f23ffb1084e9ac4305f81f8f536cd53cee9
James <jameshollinger.ee@gmail.com>
2020-08-28 20:36:27 -0700

    fix access term service

--- prime-dotnet-webapi/Models/AccessTerms/LimitsConditionsClause.cs
+++ prime-dotnet-webapi/Models/AccessTerms/LimitsConditionsClause.cs
@@ -1,7 +1,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Newtonsoft.Json;
+
 namespace Prime.Models
 {
     [Table("LimitsConditionsClause")]
@@ -13,5 +13,21 @@ namespace Prime.Models
         public string Text { get; set; }
 
         public DateTimeOffset EffectiveDate { get; set; }
+
+        public static LimitsConditionsClause FromAgreementNote(AccessAgreementNote agreementNote)
+        {
+            string text = agreementNote?.Note;
+
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return null;
+            }
+
+            return new LimitsConditionsClause
+            {
+                Text = text,
+                EffectiveDate = DateTimeOffset.Now
+            };
+        }
     }
 }
--- prime-dotnet-webapi/Services/SubmissionService.cs
+++ prime-dotnet-webapi/Services/SubmissionService.cs
@@ -131,7 +131,7 @@ namespace Prime.Services
             var newStatus = enrollee.AddEnrolmentStatus(StatusType.RequiresToa);
             newStatus.AddStatusReason(StatusReasonType.Manual);
 
-            await _accessTermService.CreateEnrolleeAccessTermAsync(enrollee);
+            await _accessTermService.CreateEnrolleeAccessTermAsync(enrolleeId);
 
             await _businessEventService.CreateStatusChangeEventAsync(enrollee.Id, "Manually Approved");
             await _context.SaveChangesAsync();
@@ -250,8 +250,8 @@ namespace Prime.Services
                 var newStatus = enrollee.AddEnrolmentStatus(StatusType.RequiresToa);
                 newStatus.AddStatusReason(StatusReasonType.Automatic);
 
-                await _accessTermService.CreateEnrolleeAccessTermAsync(enrollee);
-                await _businessEventService.CreateStatusChangeEventAsync(enrollee.Id, "Automatically Approved");
+                await _accessTermService.CreateEnrolleeAccessTermAsync(enrolleeId);
+                await _businessEventService.CreateStatusChangeEventAsync(enrolleeId, "Automatically Approved");
             }
         }
 
