d7db41d5f7f9332fdc66ab442f1cc56632f69b67
Nathan <Hogman>
2020-09-09 16:29:30 -0700

    PRIME-843 MERGE CONFLICT Duplicate junk

--- prime-angular-frontend/src/app/modules/adjudication/shared/components/site-registration-container/site-registration-container.component.ts
+++ prime-angular-frontend/src/app/modules/adjudication/shared/components/site-registration-container/site-registration-container.component.ts
@@ -16,7 +16,6 @@ import { DialogDefaultOptions } from '@shared/components/dialogs/dialog-default-
 import { ConfirmDialogComponent } from '@shared/components/dialogs/confirm-dialog/confirm-dialog.component';
 import { ClaimSiteComponent, ClaimSiteAction } from '@shared/components/dialogs/content/claim-site/claim-site.component';
 import { ClaimActionEnum } from '@shared/components/dialogs/content/claim-enrollee/claim-enrollee.component';
-import { NoteComponent } from '@shared/components/dialogs/content/note/note.component';
 
 import { AuthService } from '@auth/shared/services/auth.service';
 import { AdjudicationRoutes } from '@adjudication/adjudication.routes';
