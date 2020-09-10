534e20f30ac7823c308341ee4c4d797f1e98ea04
Martin Pultz <mtpultz@gmail.com>
2020-09-10 16:04:59 -0700

    Missed interfaces

--- prime-dotnet-webapi/Services/interfaces/IEnrolleeService.cs
+++ prime-dotnet-webapi/Services/interfaces/IEnrolleeService.cs
@@ -44,7 +44,7 @@ namespace Prime.Services
 
         Task<int> GetEnrolleeCountAsync();
 
-        Task<Enrollee> UpdateEnrolleeAdjudicator(int enrolleeId, Nullable<int> adminId = null);
+        Task<Enrollee> UpdateEnrolleeAdjudicator(int enrolleeId, int? adminId = null);
 
         Task<IEnumerable<BusinessEvent>> GetEnrolleeBusinessEvents(int enrolleeId);
 
