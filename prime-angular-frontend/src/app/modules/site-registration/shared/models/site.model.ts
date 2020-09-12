bfc2f22497a02432d7ca1a02ec3d9b463132cfd2
Martin Pultz <mtpultz@users.noreply.github.com>
2020-09-11 18:01:11 -0600

    PRIME-1003 Remote Practitioners on Site Registration admin (#843)
    * Added remote user page, Added remove user review, Added remote user routing, Added remote user menu action
    
    * Pass back remote user count through automapping and using view model
    
    * Apply remote user count to the table and toggle values based on view

--- prime-angular-frontend/src/app/modules/site-registration/shared/models/site.model.ts
+++ prime-angular-frontend/src/app/modules/site-registration/shared/models/site.model.ts
@@ -46,4 +46,5 @@ export interface Site {
 export interface SiteListViewModel extends
   Pick<Site, 'id' | 'physicalAddress' | 'doingBusinessAs' | 'submittedDate' | 'careSettingCode' | 'siteVendors' | 'completed' | 'pec' | 'status'> {
   adjudicatorIdir: string;
+  remoteUserCount: number;
 }
