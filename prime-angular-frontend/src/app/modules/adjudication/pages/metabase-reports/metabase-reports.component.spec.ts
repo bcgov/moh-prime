c3bc278a725a2e56e66c5beefa99432c4770ed6a
Nathan <Hogman>
2020-09-23 13:03:28 -0700

    PRIME-574 Fix tests

--- prime-angular-frontend/src/app/modules/adjudication/pages/metabase-reports/metabase-reports.component.spec.ts
+++ prime-angular-frontend/src/app/modules/adjudication/pages/metabase-reports/metabase-reports.component.spec.ts
@@ -1,4 +1,17 @@
+import { AdjudicationModule } from '@adjudication/adjudication.module';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthService } from '@auth/shared/services/auth.service';
+import { ConfigService } from '@config/config.service';
+import { NgxBusyModule } from '@lib/modules/ngx-busy/ngx-busy.module';
+import { NgxContextualHelpModule } from '@lib/modules/ngx-contextual-help/ngx-contextual-help.module';
+import { NgxMaterialModule } from '@lib/modules/ngx-material/ngx-material.module';
+import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
+import { MockAuthService } from 'test/mocks/mock-auth.service';
+import { MockConfigService } from 'test/mocks/mock-config.service';
 
 import { MetabaseReportsComponent } from './metabase-reports.component';
 
@@ -8,9 +21,32 @@ describe('MetabaseReportsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MetabaseReportsComponent ]
+      declarations: [
+        BrowserAnimationsModule,
+        HttpClientTestingModule,
+        NgxBusyModule,
+        NgxContextualHelpModule,
+        NgxMaterialModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+        AdjudicationModule
+      ],
+      providers: [
+        {
+          provide: APP_CONFIG,
+          useValue: APP_DI_CONFIG
+        },
+        {
+          provide: ConfigService,
+          useValue: MockConfigService
+        },
+        {
+          provide: AuthService,
+          useClass: MockAuthService
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
--- prime-angular-frontend/src/app/modules/adjudication/pages/metabase-reports/metabase-reports.component.ts
+++ prime-angular-frontend/src/app/modules/adjudication/pages/metabase-reports/metabase-reports.component.ts
@@ -15,7 +15,6 @@ export class MetabaseReportsComponent implements OnInit {
 
   public ngOnInit(): void {
     this.getMetabaseToken();
-    console.log(this.metabaseUrl);
   }
 
   private getMetabaseToken(): void {
