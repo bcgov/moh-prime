ae20bfd5bed8b5f972a15b438821f6d82bd3d91b
Martin Pultz <mtpultz@gmail.com>
2020-09-24 16:03:52 -0700

    Updated to match mockups

--- prime-angular-frontend/src/app/modules/auth/pages/bceid/bceid.component.ts
+++ prime-angular-frontend/src/app/modules/auth/pages/bceid/bceid.component.ts
@@ -1,20 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Inject } from '@angular/core';
 
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
 import { APP_CONFIG, AppConfig } from 'app/app-config.module';
+import { ViewportService } from '@core/services/viewport.service';
 import { AuthProvider } from '@auth/shared/enum/auth-provider.enum';
 import { AuthService } from '@auth/shared/services/auth.service';
 
 @Component({
   selector: 'app-bceid',
   templateUrl: './bceid.component.html',
-  styleUrls: ['./bceid.component.scss']
+  styleUrls: ['./bceid.component.scss', '../../shared/styles/landing-page.scss']
 })
-export class BceidComponent implements OnInit {
+export class BceidComponent implements OnInit, OnDestroy {
+  private unsubscribe$: Subject<void>;
+
   constructor(
     @Inject(APP_CONFIG) private config: AppConfig,
-    private authService: AuthService
-  ) { }
+    private authService: AuthService,
+    private viewportService: ViewportService
+  ) {
+    this.unsubscribe$ = new Subject<void>();
+  }
+
+  public get isMobile(): boolean {
+    return this.viewportService.isMobile;
+  }
 
   public loginUsingBCeID() {
     this.authService.login({
@@ -23,5 +36,14 @@ export class BceidComponent implements OnInit {
     });
   }
 
-  public ngOnInit() { }
+  public ngOnInit() {
+    this.viewportService.onResize()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe();
+  }
+
+  public ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 }
