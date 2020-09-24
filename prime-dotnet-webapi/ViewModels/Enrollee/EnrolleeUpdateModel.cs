67b14e46f27a8a842b76c96455ccb78d50c5e3b3
James <jameshollinger.ee@gmail.com>
2020-09-24 05:59:25 -0700

    revert update model

--- prime-dotnet-webapi/ViewModels/Enrollee/EnrolleeUpdateModel.cs
+++ prime-dotnet-webapi/ViewModels/Enrollee/EnrolleeUpdateModel.cs
@@ -34,7 +34,7 @@ namespace Prime.ViewModels
 
         public bool? IsInsulinPumpProvider { get; set; }
 
-        public ICollection<SelfDeclarationViewModel> SelfDeclarations { get; set; }
+        public ICollection<SelfDeclaration> SelfDeclarations { get; set; }
 
         [JsonIgnore]
         // This property is set by the backend from the JWT token; we cannot trust this property from the frontend
