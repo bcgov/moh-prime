cd944daabad0d2bd52d1052af8961422e834aaa5
Martin Pultz <mtpultz@users.noreply.github.com>
2020-09-24 18:45:25 -0600

    PRIME-1026 BCeID Landing page  (#866)
    * Added BCeID login
    
    * Updated to match mockups

---
+++ prime-angular-frontend/src/app/modules/auth/pages/bceid/bceid.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Inject } from '@angular/core';
+
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import { APP_CONFIG, AppConfig } from 'app/app-config.module';
+import { ViewportService } from '@core/services/viewport.service';
+import { AuthProvider } from '@auth/shared/enum/auth-provider.enum';
+import { AuthService } from '@auth/shared/services/auth.service';
+
+@Component({
+  selector: 'app-bceid',
+  templateUrl: './bceid.component.html',
+  styleUrls: ['./bceid.component.scss', '../../shared/styles/landing-page.scss']
+})
+export class BceidComponent implements OnInit, OnDestroy {
+  private unsubscribe$: Subject<void>;
+
+  constructor(
+    @Inject(APP_CONFIG) private config: AppConfig,
+    private authService: AuthService,
+    private viewportService: ViewportService
+  ) {
+    this.unsubscribe$ = new Subject<void>();
+  }
+
+  public get isMobile(): boolean {
+    return this.viewportService.isMobile;
+  }
+
+  public loginUsingBCeID() {
+    this.authService.login({
+      idpHint: AuthProvider.BCEID,
+      redirectUri: this.config.loginRedirectUrl
+    });
+  }
+
+  public ngOnInit() {
+    this.viewportService.onResize()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe();
+  }
+
+  public ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+}
