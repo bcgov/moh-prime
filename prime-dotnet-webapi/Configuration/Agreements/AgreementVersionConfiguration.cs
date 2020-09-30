8f6c8363d3f87a21d6e1c616ad1e795c6df53992
Anais <anais.hebert@nttdata.com>
2020-09-30 13:22:06 -0700

    add AgreementVersionType to configuration

--- prime-dotnet-webapi/Configuration/Agreements/AgreementVersionConfiguration.cs
+++ prime-dotnet-webapi/Configuration/Agreements/AgreementVersionConfiguration.cs
@@ -16,16 +16,16 @@ namespace Prime.Configuration.Agreements
             get
             {
                 return new AgreementVersion[]{
-                    new OboAgreement                 { Id = 1, Text = "obo-access-terms-v1.html", EffectiveDate = SEEDING_DATE, CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new RegulatedUserAgreement       { Id = 2, Text =  "ru-access-terms-v1.html", EffectiveDate = SEEDING_DATE, CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new OboAgreement                 { Id = 3, Text = "obo-access-terms-v2.html", EffectiveDate = DateTimeOffset.Parse("2020-03-05 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new RegulatedUserAgreement       { Id = 4, Text =  "ru-access-terms-v2.html", EffectiveDate = DateTimeOffset.Parse("2020-03-05 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new OboAgreement                 { Id = 5, Text = "obo-access-terms-v3.html", EffectiveDate = DateTimeOffset.Parse("2020-03-10 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new RegulatedUserAgreement       { Id = 6, Text =  "ru-access-terms-v3.html", EffectiveDate = DateTimeOffset.Parse("2020-05-07 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new OboAgreement                 { Id = 7, Text = "obo-access-terms-v4.html", EffectiveDate = DateTimeOffset.Parse("2020-05-07 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new RegulatedUserAgreement       { Id = 8, Text =  "ru-access-terms-v4.html", EffectiveDate = DateTimeOffset.Parse("2020-06-03 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new CommunityPharmacistAgreement { Id = 9, Text =  "com-pharm-terms-v1.html", EffectiveDate = SEEDING_DATE, CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new CommunityPharmacistAgreement { Id = 10, Text = "com-pharm-terms-v2.html", EffectiveDate = DateTimeOffset.Parse("2020-08-28 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE }
+                    new OboAgreement                 { Id = 1, Text = "obo-access-terms-v1.html", AgreementVersionType = AgreementVersionType.OboTOA, EffectiveDate = SEEDING_DATE, CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new RegulatedUserAgreement       { Id = 2, Text =  "ru-access-terms-v1.html", AgreementVersionType = AgreementVersionType.RegulatedUserTOA, EffectiveDate = SEEDING_DATE, CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new OboAgreement                 { Id = 3, Text = "obo-access-terms-v2.html", AgreementVersionType = AgreementVersionType.OboTOA, EffectiveDate = DateTimeOffset.Parse("2020-03-05 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new RegulatedUserAgreement       { Id = 4, Text =  "ru-access-terms-v2.html", AgreementVersionType = AgreementVersionType.RegulatedUserTOA, EffectiveDate = DateTimeOffset.Parse("2020-03-05 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new OboAgreement                 { Id = 5, Text = "obo-access-terms-v3.html", AgreementVersionType = AgreementVersionType.OboTOA, EffectiveDate = DateTimeOffset.Parse("2020-03-10 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new RegulatedUserAgreement       { Id = 6, Text =  "ru-access-terms-v3.html", AgreementVersionType = AgreementVersionType.RegulatedUserTOA, EffectiveDate = DateTimeOffset.Parse("2020-05-07 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new OboAgreement                 { Id = 7, Text = "obo-access-terms-v4.html", AgreementVersionType = AgreementVersionType.OboTOA, EffectiveDate = DateTimeOffset.Parse("2020-05-07 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new RegulatedUserAgreement       { Id = 8, Text =  "ru-access-terms-v4.html", AgreementVersionType = AgreementVersionType.RegulatedUserTOA, EffectiveDate = DateTimeOffset.Parse("2020-06-03 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new CommunityPharmacistAgreement { Id = 9, Text =  "com-pharm-terms-v1.html", AgreementVersionType = AgreementVersionType.CommunityPharmacistTOA, EffectiveDate = SEEDING_DATE, CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new CommunityPharmacistAgreement { Id = 10, Text = "com-pharm-terms-v2.html", AgreementVersionType = AgreementVersionType.CommunityPharmacistTOA, EffectiveDate = DateTimeOffset.Parse("2020-08-28 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE }
                 };
             }
         }
--- prime-dotnet-webapi/Models/Agreements/AgreementVersionType.cs
+++ prime-dotnet-webapi/Models/Agreements/AgreementVersionType.cs
@@ -5,10 +5,7 @@ namespace Prime.Models
         CommunityPharmacistTOA = 1,
         RegulatedUserTOA = 2,
         OboTOA = 3,
-
         CommunityPracticeOrganizationAgreement = 4,
-
         CommunityPharmacistOrganizationAgreement = 5
-
     }
 }
