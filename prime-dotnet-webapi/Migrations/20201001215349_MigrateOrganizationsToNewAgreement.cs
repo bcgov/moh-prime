db0b82ae3c65cffbc19bbd0b423f5abdd46bc5fa
Anais <anais.hebert@nttdata.com>
2020-10-02 14:33:19 -0700

    migrate org agreements to agreement table

--- prime-dotnet-webapi/Migrations/20201001215349_MigrateOrganizationsToNewAgreement.cs
+++ prime-dotnet-webapi/Migrations/20201001215349_MigrateOrganizationsToNewAgreement.cs
@@ -34,28 +34,33 @@ namespace Prime.Migrations
 
             // Uploaded Signed Org Agreement Documents
             migrationBuilder.Sql(@"
-                INSERT INTO ""Agreement""
-                SELECT
-                    nextval('""AccessTerm_Id_seq""') as ""Id"",
-                    o.""CreatedUserId"",
-                    o.""CreatedTimeStamp"",
-                    o.""UpdatedUserId"",
-                    current_timestamp as ""UpdatedTimeStamp"",
-                    null as ""EnrolleeId"",
-                    11 as ""AgreementVersionId"",
-                    null as ""LimitsConditionsCaluseId"",
-                    o.""AcceptedAgreementDate"" as ""CreatedDate"",
-                    o.""AcceptedAgreementDate"" as ""AcceptedDate"",
-                    null as ""ExpiryDate"",
-                    o.""Id"" as ""OrganizationId"",
-                    null as ""PartyId""
-                FROM ""Organization"" as o
-                WHERE o.""AcceptedAgreementDate"" IS NOT null
-                    AND (
-                        SELECT Count(*)
-                        FROM ""SignedAgreementDocument"" sad
-                        WHERE sad.""OrganizationId"" = o.""Id"")
-                    != 0;
+                WITH inserted AS(
+                    INSERT INTO ""Agreement""
+                    SELECT
+                        nextval('""AccessTerm_Id_seq""') as ""Id"",
+                        o.""CreatedUserId"",
+                        o.""CreatedTimeStamp"",
+                        o.""UpdatedUserId"",
+                        current_timestamp as ""UpdatedTimeStamp"",
+                        null as ""EnrolleeId"",
+                        11 as ""AgreementVersionId"",
+                        null as ""LimitsConditionsCaluseId"",
+                        o.""AcceptedAgreementDate"" as ""CreatedDate"",
+                        o.""AcceptedAgreementDate"" as ""AcceptedDate"",
+                        null as ""ExpiryDate"",
+                        o.""Id"" as ""OrganizationId"",
+                        null as ""PartyId""
+                    FROM ""Organization"" as o
+                    WHERE o.""AcceptedAgreementDate"" IS NOT null
+                        AND (
+                            SELECT Count(*)
+                            FROM ""SignedAgreementDocument"" sad
+                            WHERE sad.""OrganizationId"" = o.""Id"")
+                        != 0
+                    RETURNING ""Id"", ""OrganizationId""
+                )
+                UPDATE ""SignedAgreementDocument"" sad
+                SET ""AgreementId"" = (SELECT inserted.""Id"" FROM inserted WHERE inserted.""OrganizationId"" = sad.""OrganizationId"");
             ");
         }
 
