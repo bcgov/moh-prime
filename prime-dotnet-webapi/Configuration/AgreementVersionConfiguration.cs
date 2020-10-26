33b1d3844bc3c650054cdb56ed7e3168aafa50f6
Martin Pultz <mtpultz@gmail.com>
2020-10-26 12:42:05 -0700

    Whoops!

--- prime-dotnet-webapi/Configuration/AgreementVersionConfiguration.cs
+++ prime-dotnet-webapi/Configuration/AgreementVersionConfiguration.cs
@@ -28,7 +28,7 @@ namespace Prime.Configuration.Agreements
                     new AgreementVersion { Id = 11, AgreementType = AgreementType.CommunityPracticeOrgAgreement, Text = "com-practice-org-v1.html",    EffectiveDate = SEEDING_DATE,                                CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
                     new AgreementVersion { Id = 12, AgreementType = AgreementType.CommunityPharmacyOrgAgreement, Text = "com-pharmacy-org-v1.html",    EffectiveDate = SEEDING_DATE,                                CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
                     new AgreementVersion { Id = 13, AgreementType = AgreementType.CommunityPharmacistTOA,        Text =  "com-pharm-terms-v3.html",    EffectiveDate = DateTimeOffset.Parse("2020-10-22 00:00:00"), CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
-                    new AgreementVersion { Id = 12, AgreementType = AgreementType.PharmacyOboTOA,                Text = "pharm-obo-toa-v1.html",       EffectiveDate = SEEDING_DATE,                                CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
+                    new AgreementVersion { Id = 12, AgreementType = AgreementType.PharmacyOboTOA,                Text = "pharmacy-obo-toa-v1.html",       EffectiveDate = SEEDING_DATE,                                CreatedTimeStamp = SEEDING_DATE, UpdatedTimeStamp = SEEDING_DATE },
                 };
             }
         }
