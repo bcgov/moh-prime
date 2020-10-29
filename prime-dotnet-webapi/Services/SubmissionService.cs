5ab0eb0936bc288d905cef5e0045cc0fdac7203f
James <jameshollinger.ee@gmail.com>
2020-10-29 16:47:01 -0700

    include submission

--- prime-dotnet-webapi/Services/SubmissionService.cs
+++ prime-dotnet-webapi/Services/SubmissionService.cs
@@ -63,6 +63,7 @@ namespace Prime.Services
                 .Include(e => e.EnrolleeCareSettings)
                 .Include(e => e.Agreements)
                 .Include(e => e.SelfDeclarations)
+                .Include(e => e.Submissions)
                 .SingleOrDefaultAsync(e => e.Id == enrolleeId);
 
             bool minorUpdate = await _submissionRulesService.QualifiesAsMinorUpdateAsync(enrollee, updatedProfile);
@@ -89,7 +90,7 @@ namespace Prime.Services
                 }
             }
 
-            await this.ProcessEnrolleeApplicationRules(enrolleeId);
+            await ProcessEnrolleeApplicationRules(enrolleeId);
             await _context.SaveChangesAsync();
         }
 
