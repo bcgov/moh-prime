9025211446b1e2f04091865d549b17e080e1240b
Martin Pultz <mtpultz@gmail.com>
2020-11-03 14:34:57 -0800

    Remove unused mapping

--- prime-dotnet-webapi/ViewModels/AutoMapping.cs
+++ prime-dotnet-webapi/ViewModels/AutoMapping.cs
@@ -28,7 +28,6 @@ public class AutoMapping : Profile
         CreateMap<Enrollee, EnrolleeViewModel>()
             .ForMember(dest => dest.CurrentStatusCode, opt => opt.MapFrom(src => src.CurrentStatus.StatusCode))
             .ForMember(dest => dest.AdjudicatorIdir, opt => opt.MapFrom(src => src.Adjudicator.IDIR))
-            .ForMember(dest => dest.HasNewestAgreement, opt => opt.MapFrom(src => newestAgreementIds.Any(n => n == src.CurrentAgreementId)))
             .ForMember(dest => dest.IsRegulatedUser, opt => opt.MapFrom(src => src.IsRegulatedUser()));
 
         CreateMap<Agreement, AgreementViewModel>()
