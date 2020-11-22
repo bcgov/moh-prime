385320d1b9db0a5a31205e754198cc8abe314848
Alan Leung <177092@nttdata.com>
2020-11-22 08:11:48 -0700

    Post-ng generate

---
+++ prime-angular-frontend/src/app/modules/auth/pages/phsa/phsa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-phsa',
+  templateUrl: './phsa.component.html',
+  styleUrls: ['./phsa.component.scss']
+})
+export class PhsaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
