e3f9364c0ee26cb4c1c0a6933f0b23c1c0e656f6
Martin Pultz <mtpultz@gmail.com>
2020-12-03 13:04:32 -0800

    Updated atLeast form array validator

--- prime-angular-frontend/src/app/lib/validators/form-array.validators.ts
+++ prime-angular-frontend/src/app/lib/validators/form-array.validators.ts
@@ -1,15 +1,15 @@
-import { AbstractControl, FormArray, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 export class FormArrayValidators {
   /**
    * @description
-   * Checks that at least # of abstract control(s) in a form array is valid
+   * Checks that at least # of abstract control(s) in a form array exists
    * based on a predicate. For example, useful for a list of checkbox form
    * controls, but can be extended using a custom predicate.
    */
   public static atLeast(
     minLength: number,
-    predicate: (control: AbstractControl) => boolean = (control: AbstractControl) => !Validators.requiredTrue(control)
+    predicate: (control: AbstractControl) => boolean = (control: AbstractControl) => !!control
   ): ValidatorFn {
     return (array: FormArray): ValidationErrors | null => {
       const atLeast = array &&
