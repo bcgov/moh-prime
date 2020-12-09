6dc2f6311dcfbbefb4c5244085bd002fd97ae7b6
Martin Pultz <mtpultz@users.noreply.github.com>
2020-12-09 13:24:02 -0800

    Explicit value

--- prime-dotnet-webapi/Models/HealthAuthorityCode.cs
+++ prime-dotnet-webapi/Models/HealthAuthorityCode.cs
@@ -3,10 +3,10 @@ namespace Prime.Models
     public enum HealthAuthorityCode
     {
         NorthernHealth = 1,
-        InteriorHealth,
-        VancouverCoastalHealth,
-        IslandHealth,
-        FraserHealth,
-        ProvincialHealthServicesAuthority
+        InteriorHealth = 2,
+        VancouverCoastalHealth = 3,
+        IslandHealth = 4,
+        FraserHealth = 5,
+        ProvincialHealthServicesAuthority = 6
     }
 }
