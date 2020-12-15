b742f948a02012cf47ee8560f7f4b311baeb17b5
James <jameshollinger.ee@gmail.com>
2020-12-15 02:04:46 -0800

    add JsonIgnore

--- prime-dotnet-webapi/Models/PartyEnrolments.cs
+++ prime-dotnet-webapi/Models/PartyEnrolments.cs
@@ -1,14 +1,16 @@
 using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
 
 namespace Prime.Models
 {
     public class PartyEnrolment
     {
         [Key]
-        public int Id { get; set;  }
+        public int Id { get; set; }
 
         public int PartyId { get; set; }
 
+        [JsonIgnore]
         public Party Party { get; set; }
 
         public PartyType PartyType { get; set; }
