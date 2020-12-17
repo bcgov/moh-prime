af923af0e1514bdbcda7593ae5a13fc260573ec4
Nathan <Hogman>
2020-12-16 16:54:53 -0800

    Revert "Merge branch 'feature/PRIME-1017-revoke-vc' into feature/verifiable-credential-development"
    This reverts commit b82af783023c415443fb9e0e6f32324786363478.

--- prime-dotnet-webapi/Services/VerifiableCredentialService.cs
+++ prime-dotnet-webapi/Services/VerifiableCredentialService.cs
@@ -214,15 +214,14 @@ namespace Prime.Services
                     { "schema_version", schema.Value<string>("version") },
                     { "cred_def_id", credentialDefinitionId },
                     { "comment", "PharmaNet GPID" },
-                    { "auto_remove", false },
+                    { "auto_remove", true },
                     { "trace", false },
                     { "credential_proposal", new JObject
                         {
                             { "@type", "did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/issue-credential/1.0/credential-preview" },
                             { "attributes", attributes }
                         }
-                    },
-                    { "revoc_registry_id", "Cc6hpJk61DjGcztPiAwJyK:4:Cc6hpJk61DjGcztPiAwJyK:3:CL:166842:prime_enrollee_credential:CL_ACCUM:3a3ef3ba-146d-4945-8022-a71dc282d379" }
+                    }
                 };
 
             _logger.LogInformation("Credential offer for connection ID \"{connectionId}\" for {@JObject}", connectionId, JsonConvert.SerializeObject(credentialOffer));
