c730813d2c573b9fe68c56dada2437a07f1264ec
Nathan <Hogman>
2020-12-22 11:15:47 -0800

    PRIME-1309 db table and migration

---
+++ prime-dotnet-webapi/Models/PHSA/PreApprovedRegistration.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
+
+namespace Prime.Models
+{
+    [Table("PreApprovedRegistration")]
+    public class PreApprovedRegistration : BaseAuditable
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        [Required]
+        public PartyType PartyType { get; set; }
+    }
+}
