3f0d3d96f031b9efb063b865a9cfe7b9dd6fc9a3
Martin Pultz <mtpultz@gmail.com>
2021-01-07 19:42:50 -0800

    Clean up

--- prime-angular-frontend/src/app/modules/phsa-labtech/shared/models/phsa-lab-tech.model.ts
+++ prime-angular-frontend/src/app/modules/phsa-labtech/shared/models/phsa-lab-tech.model.ts
@@ -1,5 +1,5 @@
-import { PartyTypeEnum } from '@phsa/shared/enums/party-type.enum';
 import { Address } from '@shared/models/address.model';
+import { PartyTypeEnum } from '@phsa/shared/enums/party-type.enum';
 
 export interface PhsaEnrollee {
   firstName: string;
