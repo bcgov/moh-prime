f78ffa5c72a6143e96da43d10016a216ed73b440
James <jameshollinger.ee@gmail.com>
2021-01-29 17:14:52 -0800

    New test

--- prime-dotnet-webapi-tests/Unit Tests/EmailService.Tests.cs
+++ prime-dotnet-webapi-tests/Unit Tests/EmailService.Tests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Http;
@@ -11,6 +12,7 @@ using Prime.Services.EmailInternal;
 using PrimeTests.Utils;
 using Prime.HttpClients;
 using Prime.HttpClients.Mail;
+using Prime.ViewModels.Emails;
 
 namespace PrimeTests.UnitTests
 {
@@ -32,6 +34,75 @@ namespace PrimeTests.UnitTests
                 chesClient ?? A.Fake<IChesClient>(),
                 smtpEmailClient ?? A.Fake<ISmtpEmailClient>()
             );
+        }
+
+        [Theory]
+        [MemberData(nameof(RenewalScheduleTestCases))]
+        public async void TestSendEnrolleeRenewalEmails(int daysUntilExpiry, ExpectedEmail expected)
+        {
+            // Arrange
+            var requiredEmail = new Email("f@f", "t@t", "Required", "");
+            var passedEmail = new Email("f@f", "t@t", "Passed", "");
+
+            var smtpEmailClient = A.Fake<ISmtpEmailClient>();
+            var emailRenderingService = A.Fake<IEmailRenderingService>();
+            A.CallTo(() => emailRenderingService.RenderRenewalRequiredEmailAsync(A<string>._, A<EnrolleeRenewalEmailViewModel>._)).Returns(requiredEmail);
+            A.CallTo(() => emailRenderingService.RenderRenewalPassedEmailAsync(A<string>._, A<EnrolleeRenewalEmailViewModel>._)).Returns(passedEmail);
+
+            var service = CreateService(emailRenderingService: emailRenderingService, smtpEmailClient: smtpEmailClient);
+
+            var enrollee = TestDb.HasAnEnrollee();
+            enrollee.Agreements = new[]
+            {
+                new Agreement
+                {
+                    AcceptedDate = DateTimeOffset.Now,
+                    ExpiryDate = DateTimeOffset.Now.AddDays(daysUntilExpiry)
+                }
+            };
+            TestDb.SaveChanges();
+
+            // Act
+            await service.SendEnrolleeRenewalEmails();
+
+            // Assert
+            if (expected == ExpectedEmail.Passed)
+            {
+                A.CallTo(() => smtpEmailClient.SendAsync(passedEmail)).MustHaveHappened();
+            }
+            else if (expected == ExpectedEmail.Required)
+            {
+                A.CallTo(() => smtpEmailClient.SendAsync(requiredEmail)).MustHaveHappened();
+            }
+            else
+            {
+                A.CallTo(() => smtpEmailClient.SendAsync(A<Email>._)).MustNotHaveHappened();
+            }
+        }
+
+        public enum ExpectedEmail
+        {
+            None,
+            Required,
+            Passed
+        }
+        public static IEnumerable<object[]> RenewalScheduleTestCases()
+        {
+            foreach (int day in Enumerable.Range(-2, 15))
+            {
+                if (day == -1)
+                {
+                    yield return new object[] { day, ExpectedEmail.Passed };
+                }
+                else if (new[] { 0, 1, 2, 3, 7, 14 }.Contains(day))
+                {
+                    yield return new object[] { day, ExpectedEmail.Required };
+                }
+                else
+                {
+                    yield return new object[] { day, ExpectedEmail.None };
+                }
+            }
         }
     }
 }
