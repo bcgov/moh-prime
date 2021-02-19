878571d5d97467c17f4ad3e08ac8a39068d10c4b
James <jameshollinger.ee@gmail.com>
2021-02-19 10:24:23 -0800

    minor update

--- prime-dotnet-webapi/Controllers/EmailsController.cs
+++ prime-dotnet-webapi/Controllers/EmailsController.cs
@@ -11,6 +11,7 @@ namespace Prime.Controllers
     [Produces("application/json")]
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(Roles = Roles.PrimeApiServiceAccount)]
     public class EmailsController : ControllerBase
     {
         private readonly IEmailService _emailService;
@@ -25,7 +26,6 @@ namespace Prime.Controllers
         /// Update all logged email statuses sent using the CHES email service
         /// </summary>
         [HttpPost("management/statuses", Name = nameof(UpdateEmailLogStatuses))]
-        [Authorize(Roles = Roles.PrimeApiServiceAccount)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
@@ -41,7 +41,6 @@ namespace Prime.Controllers
         /// Send enrollee renewal reminder emails
         /// </summary>
         [HttpPost("management/enrollees/renewal", Name = nameof(SendEnrolleeRenewalEmails))]
-        [Authorize(Roles = Roles.PrimeApiServiceAccount)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
