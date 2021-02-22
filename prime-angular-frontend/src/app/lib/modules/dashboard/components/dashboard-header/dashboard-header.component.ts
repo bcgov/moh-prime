e933847c07f25978780607e8792687b90e9da6c7
Martin Pultz <mtpultz@gmail.com>
2021-02-21 20:14:58 -0800

    Updated GIS dashboard, dashboard, and dashboard header inputs

--- prime-angular-frontend/src/app/lib/modules/dashboard/components/dashboard-header/dashboard-header.component.ts
+++ prime-angular-frontend/src/app/lib/modules/dashboard/components/dashboard-header/dashboard-header.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, ChangeDetectionStrategy, Output, EventEmitter, Input } from '@angular/core';
 
+export interface DashboardHeaderConfig {
+  theme: 'blue' | 'white';
+  // TODO change name to reflect a default that will show the toggle
+  showMobileToggle: boolean;
+}
+
 @Component({
   selector: 'app-dashboard-header',
   templateUrl: './dashboard-header.component.html',
@@ -7,14 +13,37 @@ import { Component, OnInit, ChangeDetectionStrategy, Output, EventEmitter, Input
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DashboardHeaderComponent implements OnInit {
+  /**
+   * @description
+   * Dashboard header configuration for theming.
+   */
+  @Input() public headerConfig: DashboardHeaderConfig;
+  /**
+   * @description
+   * Username displayed in the dashboard header.
+   */
   @Input() public username: string;
+  /**
+   * @description
+   * Indicator that viewport dimensions match a
+   * mobile device.
+   */
   @Input() public isMobile: boolean;
-  @Input() public hasMobileSidemenu: boolean;
+  /**
+   * @description
+   * Event emission of toggling the dashboard sidenav
+   * drawer action.
+   */
   @Output() public toggle: EventEmitter<void>;
+  /**
+   * @description
+   * Event emission of logout action.
+   */
   @Output() public logout: EventEmitter<void>;
 
+  public brandImgSrc: string;
+
   constructor() {
-    this.hasMobileSidemenu = true;
     this.toggle = new EventEmitter<void>();
     this.logout = new EventEmitter<void>();
   }
@@ -27,5 +56,9 @@ export class DashboardHeaderComponent implements OnInit {
     this.logout.emit();
   }
 
-  public ngOnInit(): void { }
+  public ngOnInit(): void {
+    this.brandImgSrc = (this.headerConfig.theme === 'white')
+      ? '/assets/gov_bc_logo_white.jpeg'
+      : '/assets/gov_bc_logo_blue.png';
+  }
 }
--- prime-angular-frontend/src/app/modules/gis-enrolment/shared/components/gis-dashboard/gis-dashboard.component.ts
+++ prime-angular-frontend/src/app/modules/gis-enrolment/shared/components/gis-dashboard/gis-dashboard.component.ts
@@ -4,6 +4,7 @@ import { Observable, of } from 'rxjs';
 
 import { APP_CONFIG, AppConfig } from 'app/app-config.module';
 import { DashboardMenuItem } from '@lib/modules/dashboard/models/dashboard-menu-item.model';
+import { DashboardHeaderConfig } from '@lib/modules/dashboard/components/dashboard-header/dashboard-header.component';
 import { GisEnrolmentRoutes } from '@gis/gis-enrolment.routes';
 
 @Component({
@@ -12,16 +13,28 @@ import { GisEnrolmentRoutes } from '@gis/gis-enrolment.routes';
   styleUrls: ['./gis-dashboard.component.scss']
 })
 export class GisDashboardComponent implements OnInit {
+  public dashboardHeaderConfig: DashboardHeaderConfig;
+  public dashboardSideNavConfig: { imgSrc: string, imgAlt: string };
   public dashboardMenuItems: Observable<DashboardMenuItem[]>;
+  public dashboardResponsiveMenuItems: boolean;
   public logoutRedirectUrl: string;
 
   constructor(
     @Inject(APP_CONFIG) protected config: AppConfig
   ) {
+    this.dashboardResponsiveMenuItems = false;
     this.logoutRedirectUrl = `${this.config.loginRedirectUrl}/${GisEnrolmentRoutes.LOGIN_PAGE}`;
   }
 
   public ngOnInit(): void {
+    this.dashboardHeaderConfig = {
+      theme: 'white',
+      showMobileToggle: false
+    };
+    this.dashboardSideNavConfig = {
+      imgSrc: '/assets/gis_brand.jpeg',
+      imgAlt: 'GIS Logo'
+    };
     this.dashboardMenuItems = this.getDashboardMenuItems();
   }
 
