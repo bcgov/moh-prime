1efafde8d89ccc34001903b0e0cf127177c616c4
James <jameshollinger.ee@gmail.com>
2021-02-24 18:30:50 -0800

    prune integration tests

--- prime-dotnet-webapi-tests/Integration/EnrolleeIntegration.Tests.cs
+++ prime-dotnet-webapi-tests/Integration/EnrolleeIntegration.Tests.cs
@@ -1,17 +1,12 @@
-using System;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Newtonsoft.Json;
 using Xunit;
 
 using Prime;
 using Prime.Models;
-using PrimeTests.Utils;
 using Prime.Models.Api;
-using Prime.ViewModels;
+using PrimeTests.Utils;
 
 namespace PrimeTests.Integration
 {
@@ -32,35 +27,8 @@ namespace PrimeTests.Integration
             return enrollee;
         }
 
-        private Enrollee GetEnrollee(IServiceScope scope, int enrolleeId)
-        {
-            var _dbContext = scope.ServiceProvider.GetRequiredService<ApiDbContext>();
-            return _dbContext.Enrollees
-                .Include(e => e.PhysicalAddress)
-                .Include(e => e.MailingAddress)
-                .Include(e => e.Certifications)
-                .Include(e => e.Jobs)
-                .Include(e => e.EnrolleeCareSettings)
-                .Include(e => e.EnrolmentStatuses)
-                .AsNoTracking().Single(e => e.Id == enrolleeId);
-        }
-
         [Fact]
-        public async void TestGetEnrollees()
-        {
-            using (var scope = _factory.Server.Host.Services.CreateScope())
-            {
-                var request = TestUtils.CreateRequest(HttpMethod.Get, "/api/enrollees");
-                TestUtils.AddAdminAuth(request);
-
-                var response = await _client.SendAsync(request);
-
-                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            }
-        }
-
-        [Fact]
-        public async void TestGetEnrollees2()
+        public async void TestGetEnrollees_AsEnrollee()
         {
             using (var scope = _factory.Server.Host.Services.CreateScope())
             {
@@ -75,28 +43,7 @@ namespace PrimeTests.Integration
         }
 
         [Fact]
-        public async void TestCreateBcscEnrollee()
-        {
-            using (var scope = _factory.Server.Host.Services.CreateScope())
-            {
-                var testEnrollee = TestUtils.EnrolleeFaker.Generate();
-                var payload = new
-                {
-                    Enrollee = testEnrollee
-                };
-                var request = TestUtils.CreateRequest(HttpMethod.Post, "/api/enrollees", payload);
-                TestUtils.AddEnrolleeAuth(request, testEnrollee);
-
-                var response = await _client.SendAsync(request);
-
-                Assert.Equal(HttpStatusCode.Created, response.StatusCode);
-                var createdEnrollee = (await response.Content.ReadAsAsync<ApiResultResponse<Enrollee>>()).Result;
-                Assert.Equal(testEnrollee.UserId, createdEnrollee.UserId);
-            }
-        }
-
-        [Fact]
-        public async void TestGetSingleEnrollee()
+        public async void TestGetEnrolleeById()
         {
             using (var scope = _factory.Server.Host.Services.CreateScope())
             {
@@ -113,41 +60,20 @@ namespace PrimeTests.Integration
             }
         }
 
-        [Fact(Skip = "Test fails with an error specific to the In-Memory databse we are using when projecting to the Enrollee List ViewModel")]
-        public async void TestGetAllEnrollees()
-        {
-            using (var scope = _factory.Server.Host.Services.CreateScope())
-            {
-                CreateEnrollee(scope);
-                CreateEnrollee(scope);
-                CreateEnrollee(scope);
-                var request = TestUtils.CreateRequest(HttpMethod.Get, $"/api/enrollees");
-                TestUtils.AddAdminAuth(request);
-
-                var response = await _client.SendAsync(request);
-
-                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-                Enrollee[] responseEnrollees = (await response.Content.ReadAsAsync<ApiResultResponse<Enrollee[]>>()).Result;
-                Assert.Equal(3, responseEnrollees.Count());
-            }
-        }
-
         [Fact]
-        public async void TestUpdateSingleEnrollee()
+        public async void TestCreateEnrollee()
         {
             using (var scope = _factory.Server.Host.Services.CreateScope())
             {
-                Enrollee enrollee = CreateEnrollee(scope);
-                var updateModel = enrollee.CopyToUpdateModel();
-                updateModel.Email = enrollee.Email.Bump();
-                var request = TestUtils.CreateRequest(HttpMethod.Put, $"/api/enrollees/{enrollee.Id}", updateModel);
-                TestUtils.AddEnrolleeAuth(request, enrollee);
+                var testEnrollee = TestUtils.EnrolleeFaker.Generate();
+                var request = TestUtils.CreateRequest(HttpMethod.Post, "/api/enrollees", new { Enrollee = testEnrollee });
+                TestUtils.AddEnrolleeAuth(request, testEnrollee);
 
                 var response = await _client.SendAsync(request);
 
-                Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
-                Enrollee updatedEnrollee = GetEnrollee(scope, enrollee.Id);
-                Assert.Equal(updateModel.Email, updatedEnrollee.Email);
+                Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+                var createdEnrollee = (await response.Content.ReadAsAsync<ApiResultResponse<Enrollee>>()).Result;
+                Assert.Equal(testEnrollee.UserId, createdEnrollee.UserId);
             }
         }
     }
