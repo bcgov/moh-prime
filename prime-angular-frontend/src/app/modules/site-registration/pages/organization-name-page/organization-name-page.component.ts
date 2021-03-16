a0567f300f16255cb570348ce85d07f2b207a0b6
Martin Pultz <mtpultz@gmail.com>
2021-03-16 00:17:43 -0700

    Updated org name to use AbstractEnrolmentPage

--- prime-angular-frontend/src/app/modules/site-registration/pages/organization-name-page/organization-name-page.component.ts
+++ prime-angular-frontend/src/app/modules/site-registration/pages/organization-name-page/organization-name-page.component.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { MatDialog } from '@angular/material/dialog';
 
-import { Subscription, Observable, of, pipe } from 'rxjs';
-import { debounceTime, switchMap, tap, exhaustMap, take } from 'rxjs/operators';
+import { Observable, of, pipe } from 'rxjs';
+import { debounceTime, switchMap, tap, exhaustMap, take, map } from 'rxjs/operators';
 
 import { RouteUtils } from '@lib/utils/route-utils.class';
+import { AbstractEnrolmentPage } from '@lib/classes/abstract-enrolment-page.class';
 import { FormUtilsService } from '@core/services/form-utils.service';
 import { OrganizationResource } from '@core/resources/organization-resource.service';
 import { SiteResource } from '@core/resources/site-resource.service';
-import { ConfirmDialogComponent } from '@shared/components/dialogs/confirm-dialog/confirm-dialog.component';
 
 import { SiteRoutes } from '@registration/site-registration.routes';
 import { Site } from '@registration/shared/models/site.model';
@@ -19,15 +19,15 @@ import { OrgBookAutocompleteHttpResponse } from '@registration/shared/models/org
 import { OrganizationService } from '@registration/shared/services/organization.service';
 import { OrganizationFormStateService } from '@registration/shared/services/organization-form-state.service';
 import { OrgBookResource } from '@registration/shared/services/org-book-resource.service';
+import { OrganizationNamePageFormState } from './organization-name-page-form-state.class';
 
 @Component({
   selector: 'app-organization-name-page',
   templateUrl: './organization-name-page.component.html',
   styleUrls: ['./organization-name-page.component.scss']
 })
-export class OrganizationNamePageComponent implements OnInit {
-  public busy: Subscription;
-  public form: FormGroup;
+export class OrganizationNamePageComponent extends AbstractEnrolmentPage implements OnInit {
+  public formState: OrganizationNamePageFormState;
   public title: string;
   public routeUtils: RouteUtils;
   public organizations: string[];
@@ -38,16 +38,18 @@ export class OrganizationNamePageComponent implements OnInit {
   public SiteRoutes = SiteRoutes;
 
   constructor(
-    private route: ActivatedRoute,
-    private router: Router,
+    protected dialog: MatDialog,
+    protected formUtilsService: FormUtilsService,
     private organizationService: OrganizationService,
     private organizationResource: OrganizationResource,
     private organizationFormStateService: OrganizationFormStateService,
     private orgBookResource: OrgBookResource,
     private siteResource: SiteResource,
-    private formUtilsService: FormUtilsService,
-    private dialog: MatDialog
+    private route: ActivatedRoute,
+    router: Router
   ) {
+    super(dialog, formUtilsService);
+
     this.title = this.route.snapshot.data.title;
     this.routeUtils = new RouteUtils(route, router, SiteRoutes.MODULE_PATH);
   }
@@ -62,31 +64,6 @@ export class OrganizationNamePageComponent implements OnInit {
 
   public get doingBusinessAs(): FormControl {
     return this.form.get('doingBusinessAs') as FormControl;
-  }
-
-  public onSubmit() {
-    if (this.formUtilsService.checkValidity(this.form)) {
-      const organizationId = this.route.snapshot.params.oid;
-      const payload = this.organizationFormStateService.json;
-      this.organizationResource
-        .updateOrganization(payload)
-        .pipe(
-          exhaustMap(() =>
-            this.organizationResource.updateCompleted(organizationId)
-          ),
-          exhaustMap(
-            // Check the organization wasn't completed before the update, and
-            // if not then this is the initial registration wizard
-            () => (!this.isCompleted)
-              ? this.siteResource.createSite(organizationId)
-              : of(null)
-          )
-        )
-        .subscribe((site: Site) => {
-          this.form.markAsPristine();
-          this.nextRoute(site?.id);
-        });
-    }
   }
 
   public getOrgBookLink(orgId: string, display: boolean = false) {
@@ -117,36 +94,18 @@ export class OrganizationNamePageComponent implements OnInit {
     this.routeUtils.routeRelativeTo(SiteRoutes.ORGANIZATION_SIGNING_AUTHORITY);
   }
 
-  public nextRoute(siteId: number) {
-    const redirectPath = this.route.snapshot.queryParams.redirect;
-    if (redirectPath) {
-      this.routeUtils.routeRelativeTo([redirectPath, SiteRoutes.SITE_REVIEW]);
-    } else {
-      if (this.isCompleted) {
-        this.routeUtils.routeRelativeTo(SiteRoutes.ORGANIZATION_REVIEW);
-      } else {
-        this.routeUtils.routeRelativeTo([SiteRoutes.SITES, siteId, SiteRoutes.CARE_SETTING]);
-      }
-    }
-  }
-
-  public canDeactivate(): Observable<boolean> | boolean {
-    const data = 'unsaved';
-    return (this.form.dirty)
-      ? this.dialog.open(ConfirmDialogComponent, { data }).afterClosed()
-      : true;
-  }
-
   public ngOnInit() {
     this.createFormInstance();
+    this.patchForm();
     this.initForm();
   }
 
-  private createFormInstance() {
-    this.form = this.organizationFormStateService.organizationNameFormState.form;
+  protected createFormInstance() {
+    this.formState = this.organizationFormStateService.organizationNameFormState;
+    this.form = this.formState.form;
   }
 
-  private initForm() {
+  protected patchForm(): void {
     const organization = this.organizationService.organization;
     this.isCompleted = organization?.completed;
     this.organizationFormStateService.setForm(organization, true);
@@ -159,7 +118,9 @@ export class OrganizationNamePageComponent implements OnInit {
         .pipe(take(1), this.getDoingBusinessAs())
         .subscribe();
     }
+  }
 
+  protected initForm() {
     this.name.valueChanges
       .pipe(
         debounceTime(400),
@@ -172,13 +133,45 @@ export class OrganizationNamePageComponent implements OnInit {
       });
   }
 
+  protected performSubmission(): Observable<number | null> {
+    const organizationId = this.route.snapshot.params.oid;
+    const payload = this.organizationFormStateService.json;
+    return this.organizationResource
+      .updateOrganization(payload)
+      .pipe(
+        exhaustMap(() =>
+          this.organizationResource.updateCompleted(organizationId)
+        ),
+        exhaustMap(
+          // Check the organization wasn't completed before the update, and
+          // if not then this is the initial registration wizard
+          () => (!this.isCompleted)
+            ? this.siteResource.createSite(organizationId)
+            : of(null)
+        ),
+        map((site: Site) => site?.id)
+      );
+  }
+
+  protected afterSubmitIsSuccessful(siteId?: number): void {
+    const redirectPath = this.route.snapshot.queryParams.redirect;
+    let routePath: string | string[];
+    if (redirectPath) {
+      routePath = [redirectPath, SiteRoutes.SITE_REVIEW];
+    } else {
+      routePath = (!this.isCompleted)
+        ? [SiteRoutes.SITES, `${ siteId }`, SiteRoutes.CARE_SETTING]
+        : SiteRoutes.ORGANIZATION_REVIEW;
+    }
+
+    this.routeUtils.routeRelativeTo(routePath);
+  }
+
   private getDoingBusinessAs() {
     return pipe(
       // Expects an organization registrationId
       this.orgBookResource.doingBusinessAsMap(),
-      tap((doingBusinessAsNames: string[]) =>
-        this.doingBusinessAsNames = doingBusinessAsNames
-      )
+      tap((doingBusinessAsNames: string[]) => this.doingBusinessAsNames = doingBusinessAsNames)
     );
   }
 }
