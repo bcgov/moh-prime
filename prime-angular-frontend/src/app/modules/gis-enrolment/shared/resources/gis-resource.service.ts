2b91eae5b8077de526f01f55127f60cd6bc4ab34
Martin Pultz <mtpultz@gmail.com>
2021-03-16 03:08:48 -0700

    Added resource methods, Added GisEnrolment model

--- prime-angular-frontend/src/app/modules/gis-enrolment/shared/resources/gis-resource.service.ts
+++ prime-angular-frontend/src/app/modules/gis-enrolment/shared/resources/gis-resource.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { ApiResourceUtilsService } from '@core/resources/api-resource-utils.service';
 import { ApiResource } from '@core/resources/api-resource.service';
+import { ApiHttpResponse } from '@core/models/api-http-response.model';
+import { NoContent, NoContentResponse } from '@core/resources/abstract-resource';
 import { LoggerService } from '@core/services/logger.service';
 import { ToastService } from '@core/services/toast.service';
-import { NoContent, NoContentResponse } from '@core/resources/abstract-resource';
 
+import { Party } from '@registration/shared/models/party.model';
 import { LdapCredential } from '../models/ldap-credential.model';
+import { GisEnrolment } from '../models/gis-enrolment.model';
 
 @Injectable({
   providedIn: 'root'
@@ -28,7 +31,48 @@ export class GisResource {
         NoContentResponse,
         catchError((error: any) => {
           this.toastService.openErrorToast('You could not be authenticated.');
-          this.logger.error('[GisResource] ldapLogin error has occurred: ', error);
+          this.logger.error('[GisModule] GisResource::ldapLogin error has occurred: ', error);
+          throw error;
+        })
+      );
+  }
+
+  public getEnrolment(gisId: number): Observable<GisEnrolment> {
+    return this.apiResource.get<GisEnrolment>(`gis/${ gisId }`)
+      .pipe(
+        map((response: ApiHttpResponse<GisEnrolment>) => response.result),
+        catchError((error: any) => {
+          this.toastService.openErrorToast('');
+          this.logger.error('[GisModule] GisResource::getEnrolment error has occurred: ', error);
+          throw error;
+        })
+      );
+  }
+
+  public createEnrolment(party: Party): Observable<GisEnrolment> {
+    return this.apiResource.post<GisEnrolment>('gis', party)
+      .pipe(
+        map((response: ApiHttpResponse<GisEnrolment>) => response.result),
+        tap((newEnrolment: GisEnrolment) => {
+          this.toastService.openSuccessToast('Enrolment has been created');
+          this.logger.info('NEW_ENROLMENT', newEnrolment);
+        }),
+        catchError((error: any) => {
+          this.toastService.openErrorToast('Enrolment could not be created');
+          this.logger.error('[GisModule] GisResource::createEnrolment error has occurred: ', error);
+          throw error;
+        })
+      );
+  }
+
+  public updateEnrolment(enrolment: GisEnrolment): NoContent {
+    return this.apiResource.put<NoContent>(`gis/${ enrolment.id }`, enrolment)
+      .pipe(
+        NoContentResponse,
+        tap(() => this.toastService.openSuccessToast('Enrolment has been updated')),
+        catchError((error: any) => {
+          this.toastService.openErrorToast('Enrolment could not be updated.');
+          this.logger.error('[GisModule] GisResource::updateEnrolment error has occurred: ', error);
           throw error;
         })
       );
