8d39c10a3645c8bb87d31035576554f8d9a004bf
Johnny L <johnny.liu@nttdata.com>
2021-03-17 09:16:03 -0700

    fixed unit test

--- prime-angular-frontend/src/app/shared/components/site/site-name/site-name.component.spec.ts
+++ prime-angular-frontend/src/app/shared/components/site/site-name/site-name.component.spec.ts
@@ -1,5 +1,13 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, inject, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
+import { NgxMaterialModule } from '@lib/modules/ngx-material/ngx-material.module';
+import { SiteFormStateService } from '@registration/shared/services/site-form-state.service';
 import { SiteNameComponent } from './site-name.component';
 
 describe('SiteNameComponent', () => {
@@ -8,16 +16,32 @@ describe('SiteNameComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SiteNameComponent ]
+      declarations: [SiteNameComponent],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        ReactiveFormsModule,
+        NgxMaterialModule,
+        BrowserAnimationsModule,
+      ],
+      providers: [
+        {
+          provide: APP_CONFIG,
+          useValue: APP_DI_CONFIG
+        },
+        SiteFormStateService,
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
-  beforeEach(() => {
+  beforeEach(inject([SiteFormStateService], (siteFormStateService: SiteFormStateService) => {
     fixture = TestBed.createComponent(SiteNameComponent);
     component = fixture.componentInstance;
+    component.form = siteFormStateService.businessLicencePageFormState.form;
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
