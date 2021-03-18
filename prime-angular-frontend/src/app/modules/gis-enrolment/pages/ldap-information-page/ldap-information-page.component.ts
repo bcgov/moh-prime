f3dcfc680e918cf6a5f7ad815b3b670466d41205
Martin Pultz <mtpultz@gmail.com>
2021-03-17 21:36:47 -0700

    Revert "Temporary update no LDAP auth due to issues upstream"
    This reverts commit 1bf8f57674c7a19161b80d81d602459c0e091ca7.

--- prime-angular-frontend/src/app/modules/gis-enrolment/pages/ldap-information-page/ldap-information-page.component.ts
+++ prime-angular-frontend/src/app/modules/gis-enrolment/pages/ldap-information-page/ldap-information-page.component.ts
@@ -70,12 +70,10 @@ export class LdapInformationPageComponent extends AbstractEnrolmentPage implemen
   protected initForm(): void { } // NOOP
 
   protected performSubmission(): NoContent {
-    // TODO uncomment LDAP authentication is fixed on PHSA-side
-    // return this.gisEnrolmentResource.ldapLogin(this.gisEnrolmentService.enrolment.id, this.formState.credentials)
-    //   .pipe(
-    //     exhaustMap(() => this.gisEnrolmentResource.updateEnrolment(this.formStateService.json))
-    //   );
-    return this.gisEnrolmentResource.updateEnrolment(this.formStateService.json);
+    return this.gisEnrolmentResource.ldapLogin(this.gisEnrolmentService.enrolment.id, this.formState.credentials)
+      .pipe(
+        exhaustMap(() => this.gisEnrolmentResource.updateEnrolment(this.formStateService.json))
+      );
   }
 
   protected afterSubmitIsSuccessful(): void {
