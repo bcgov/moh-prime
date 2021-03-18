429d90f066f359245213a1f83999aa9856976dad
Martin Pultz <mtpultz@gmail.com>
2021-03-18 16:35:59 -0700

    Updated busincess licence and associated form state

--- prime-angular-frontend/src/app/modules/site-registration/pages/business-licence-page/business-licence-page-form-state.class.ts
+++ prime-angular-frontend/src/app/modules/site-registration/pages/business-licence-page/business-licence-page-form-state.class.ts
@@ -1,9 +1,9 @@
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 import { AbstractFormState } from '@lib/classes/abstract-form-state.class';
 import { Site } from '@registration/shared/models/site.model';
 
-interface BusinessLicencePageDataModel extends Pick<Site, 'businessLicenceGuid' | 'deferredLicenceReason' | 'doingBusinessAs' | 'pec'> { }
+interface BusinessLicencePageDataModel extends Pick<Site, 'doingBusinessAs' | 'pec'> { }
 
 export class BusinessLicencePageFormState extends AbstractFormState<BusinessLicencePageDataModel> {
   public constructor(
@@ -14,6 +14,27 @@ export class BusinessLicencePageFormState extends AbstractFormState<BusinessLice
     this.buildForm();
   }
 
+  public get businessLicenceGuid(): FormControl {
+    return this.form.get('businessLicenceGuid') as FormControl;
+  }
+
+  public get deferredLicenceReason(): FormControl {
+    return this.form.get('deferredLicenceReason') as FormControl;
+  }
+
+  public get doingBusinessAs(): FormControl {
+    return this.form.get('doingBusinessAs') as FormControl;
+  }
+
+  /**
+   * @description
+   * Access to doingBusinessAs and pec, but prevents transmission
+   * of the deferredLicenceReason and businessLicenceGuid.
+   *
+   * NOTE: deferredLicenceReason and businessLicenceGuid are not
+   * updated using the site update endpoint, and are only used
+   * within the business licence page.
+   */
   public get json(): BusinessLicencePageDataModel {
     if (!this.formInstance) {
       return;
