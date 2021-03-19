ea70d21d6b761f541dea143b3fbf3b681ef83353
Martin Pultz <mtpultz@gmail.com>
2021-03-19 14:24:02 -0700

    Updated GIS test suites

--- prime-angular-frontend/src/app/modules/gis-enrolment/pages/collection-notice-page/collection-notice-page.component.spec.ts
+++ prime-angular-frontend/src/app/modules/gis-enrolment/pages/collection-notice-page/collection-notice-page.component.spec.ts
@@ -1,48 +1,24 @@
-import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
-import { MockConfigService } from 'test/mocks/mock-config.service';
 import { MockAuthService } from 'test/mocks/mock-auth.service';
-import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
-import { ConfigService } from '@config/config.service';
+
 import { AuthService } from '@auth/shared/services/auth.service';
 
 import { CollectionNoticePageComponent } from './collection-notice-page.component';
-
-// import { NgxMaterialModule } from '@lib/modules/ngx-material/ngx-material.module';
-// import { NgxBusyModule } from '@lib/modules/ngx-busy/ngx-busy.module';
-// import { GisEnrolmentRoutes } from '@gis/gis-enrolment.routes';
-// import { LdapUserPageComponent } from '../ldap-user-page/ldap-user-page.component';
 
 describe('CollectionNoticePageComponent', () => {
   let component: CollectionNoticePageComponent;
   let fixture: ComponentFixture<CollectionNoticePageComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule(
+  beforeEach(async () => {
+    await TestBed.configureTestingModule(
       {
         imports: [
-          HttpClientTestingModule,
-          // RouterTestingModule.withRoutes([
-          //   {
-          //     path: GisEnrolmentRoutes.LDAP_USER_PAGE,
-          //     component: LdapUserPageComponent
-          //   }
-          // ]),
-          // NgxMaterialModule,
-          // NgxBusyModule
+          RouterTestingModule
         ],
         providers: [
-          {
-            provide: APP_CONFIG,
-            useValue: APP_DI_CONFIG
-          },
-          {
-            provide: ConfigService,
-            useClass: MockConfigService
-          },
           {
             provide: AuthService,
             useClass: MockAuthService
@@ -51,7 +27,7 @@ describe('CollectionNoticePageComponent', () => {
         schemas: [NO_ERRORS_SCHEMA]
       }
     ).compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CollectionNoticePageComponent);
