180237b0b1553a00d71daffe9c637f3d3af1f9f9
Martin Pultz <mtpultz@gmail.com>
2021-03-22 14:13:36 -0700

    Added collection notice

--- prime-angular-frontend/src/app/modules/health-auth-site-reg/pages/collection-notice-page/collection-notice-page.component.ts
+++ prime-angular-frontend/src/app/modules/health-auth-site-reg/pages/collection-notice-page/collection-notice-page.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { RouteUtils } from '@lib/utils/route-utils.class';
+import { AuthService } from '@auth/shared/services/auth.service';
+
+import { HealthAuthSiteRegRoutes } from '@health-auth/health-auth-site-reg.routes';
 
 @Component({
   selector: 'app-collection-notice-page',
@@ -6,7 +12,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collection-notice-page.component.scss']
 })
 export class CollectionNoticePageComponent implements OnInit {
-  constructor() { }
+  public isFull: boolean;
+  public routeUtils: RouteUtils;
 
-  public ngOnInit(): void { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private authService: AuthService
+  ) {
+    this.isFull = true;
+    this.routeUtils = new RouteUtils(route, router, HealthAuthSiteRegRoutes.MODULE_PATH);
+  }
+
+  public onAccept() {
+    this.authService.hasJustLoggedIn = false;
+    this.nextRoute();
+  }
+
+  public ngOnInit(): void {
+    // TODO temporarily commented out until a collection notice is provided
+    // this.authService.hasJustLoggedIn = true;
+    // TODO added until a collection notice is provided
+    this.nextRoute();
+  }
+
+  private nextRoute() {
+    this.router.navigate([HealthAuthSiteRegRoutes.SITE_MANAGEMENT], { relativeTo: this.route.parent });
+  }
 }
