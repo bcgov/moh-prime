82f596efbf87d6df698a05b77d137cea9d8d5f05
Martin Pultz <mtpultz@gmail.com>
2021-03-22 15:20:02 -0700

    Added temporary auth to health authority

--- prime-angular-frontend/src/app/modules/health-auth-site-reg/shared/modules/health-auth-site-reg-login-page/health-auth-site-reg-login-page.component.ts
+++ prime-angular-frontend/src/app/modules/health-auth-site-reg/shared/modules/health-auth-site-reg-login-page/health-auth-site-reg-login-page.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+
+import { APP_CONFIG, AppConfig } from 'app/app-config.module';
+import { IdentityProviderEnum } from '@auth/shared/enum/identity-provider.enum';
+import { AuthService } from '@auth/shared/services/auth.service';
 
 import { HealthAuthSiteRegRoutes } from '@health-auth/health-auth-site-reg.routes';
 
@@ -13,13 +17,22 @@ export class HealthAuthSiteRegLoginPageComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    @Inject(APP_CONFIG) private config: AppConfig,
+    private authService: AuthService
   ) {
     this.title = route.snapshot.data.title;
   }
 
   public onLogin() {
-    this.router.navigate([HealthAuthSiteRegRoutes.MODULE_PATH]);
+    // Route to COLLECTION_NOTICE which determines the direction of routing
+    const redirectRoute = HealthAuthSiteRegRoutes.routePath(HealthAuthSiteRegRoutes.COLLECTION_NOTICE);
+    const redirectUri = `${ this.config.loginRedirectUrl }${ redirectRoute }`;
+
+    this.authService.login({
+      idpHint: IdentityProviderEnum.BCSC,
+      redirectUri
+    });
   }
 
   public ngOnInit(): void { }
