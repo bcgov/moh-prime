7800ebad56888354220428b4d0f36aabac3cd0e1
Martin Pultz <mtpultz@users.noreply.github.com>
2021-03-26 13:04:46 -0700

    nl on subscribes

--- prime-angular-frontend/src/app/modules/adjudication/pages/site-banner-page/site-banner-page.component.ts
+++ prime-angular-frontend/src/app/modules/adjudication/pages/site-banner-page/site-banner-page.component.ts
@@ -24,11 +24,13 @@ export class SiteBannerPageComponent implements OnInit {
   }
 
   public onSave(banner: Banner): void {
-    this.busy = this.bannerResource.createOrUpdateSiteLandingBanner(banner).subscribe((banner: Banner) => this.banner = banner);
+    this.busy = this.bannerResource.createOrUpdateSiteLandingBanner(banner)
+      .subscribe((banner: Banner) => this.banner = banner);
   }
 
   public onDelete(): void {
-    this.busy = this.bannerResource.deleteSiteLandingBanner().subscribe(() => this.banner = null);
+    this.busy = this.bannerResource.deleteSiteLandingBanner()
+      .subscribe(() => this.banner = null);
   }
 
   public ngOnInit(): void {
@@ -36,6 +38,7 @@ export class SiteBannerPageComponent implements OnInit {
   }
 
   private getBanner(): void {
-    this.busy = this.bannerResource.getSiteLandingBanner().subscribe((banner: Banner) => this.banner = banner);
+    this.busy = this.bannerResource.getSiteLandingBanner()
+      .subscribe((banner: Banner) => this.banner = banner);
   }
 }
