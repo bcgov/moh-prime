cf3ba671a49ad8c81d2726c7be740e437eb67687
Anais <anais.hebert@nttdata.com>
2021-03-31 13:52:58 -0700

    fix frontend test

--- prime-angular-frontend/src/app/shared/components/dialogs/content/send-bulk-email/send-bulk-email.component.spec.ts
+++ prime-angular-frontend/src/app/shared/components/dialogs/content/send-bulk-email/send-bulk-email.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SendBulkEmailComponent } from './send-bulk-email.component';
+import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {NgxMaterialModule} from '@lib/modules/ngx-material/ngx-material.module';
+import {SendEmailComponent} from '@shared/components/dialogs/content/send-email/send-email.component';
+import {APP_CONFIG, APP_DI_CONFIG} from '../../../../../app-config.module';
 
 describe('SendBulkEmailComponent', () => {
   let component: SendBulkEmailComponent;
@@ -8,7 +13,28 @@ describe('SendBulkEmailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SendBulkEmailComponent ]
+      imports: [
+        MatDialogModule,
+        HttpClientTestingModule,
+        NgxMaterialModule
+      ],
+      declarations: [SendEmailComponent],
+      providers: [
+        {
+          provide: APP_CONFIG,
+          useValue: APP_DI_CONFIG
+        },
+        {
+          provide: MatDialogRef,
+          useValue: {}
+        },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {
+            title: 'Send Bulk Email'
+          }
+        },
+      ]
     })
     .compileComponents();
   });
