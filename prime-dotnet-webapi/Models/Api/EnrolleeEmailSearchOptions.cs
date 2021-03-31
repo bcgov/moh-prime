24f9e483031a7790ce4f3a71b2ade12abef93059
AnaisHebert <53796823+AnaisHebert@users.noreply.github.com>
2021-03-31 12:38:36 -0700

    Delete EnrolleeEmailSearchOptions.cs

--- prime-dotnet-webapi/Models/Api/EnrolleeEmailSearchOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace Prime.Models.Api
-{
-    public class EnrolleeEmailSearchOptions
-    {
-        public int? StatusCode { get; set; }
-        public string TextSearch { get; set; }
-    }
-}
