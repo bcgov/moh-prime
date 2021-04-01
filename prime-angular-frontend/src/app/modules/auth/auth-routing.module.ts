e5c8316044bd44e62e0ed93743695a85c43d23c4
Martin Pultz <mtpultz@gmail.com>
2021-03-31 17:01:16 -0700

    Fix merge conflicts

--- prime-angular-frontend/src/app/modules/auth/auth-routing.module.ts
+++ prime-angular-frontend/src/app/modules/auth/auth-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AuthorizationRedirectGuard } from './shared/guards/authorization-redirect.guard';
+import { BannerLocationCode } from '@shared/enums/banner-location-code.enum';
 
 import { AuthRoutes } from './auth.routes';
+import { AuthorizationRedirectGuard } from './shared/guards/authorization-redirect.guard';
 import { EnrolmentRoutes } from '@enrolment/enrolment.routes';
 import { SiteRoutes } from '@registration/site-registration.routes';
 import { AdjudicationRoutes } from '@adjudication/adjudication.routes';
 import { PhsaEformsRoutes } from '@phsa/phsa-eforms.routes';
 import { GisEnrolmentRoutes } from '@gis/gis-enrolment.routes';
-import { BannerLocationCode } from '@shared/enums/banner-location-code.enum';
+import { HealthAuthSiteRegRoutes } from '@health-auth/health-auth-site-reg.routes';
 
 const routes: Routes = [
   {
@@ -19,7 +20,8 @@ const routes: Routes = [
         path: EnrolmentRoutes.BCSC_LOGIN,
         canLoad: [AuthorizationRedirectGuard],
         data: { locationCode: BannerLocationCode.ENROLMENT_LANDING_PAGE },
-        loadChildren: () => import('@enrolment/shared/modules/bcsc-enrolment-login-page/bcsc-enrolment-login-page.module').then(m => m.BcscEnrolmentLoginPageModule)
+        loadChildren: () => import('@enrolment/shared/modules/bcsc-enrolment-login-page/bcsc-enrolment-login-page.module')
+          .then(m => m.BcscEnrolmentLoginPageModule)
       },
       {
         path: EnrolmentRoutes.BCEID_LOGIN,
@@ -31,7 +33,8 @@ const routes: Routes = [
         path: SiteRoutes.LOGIN_PAGE,
         canLoad: [AuthorizationRedirectGuard],
         data: { locationCode: BannerLocationCode.SITE_REGISTRATION_LANDING_PAGE },
-        loadChildren: () => import('@registration/shared/modules/site-registration-login-page/site-registration-login-page.module').then(m => m.SiteRegistrationLoginPageModule)
+        loadChildren: () => import('@registration/shared/modules/site-registration-login-page/site-registration-login-page.module')
+          .then(m => m.SiteRegistrationLoginPageModule)
       },
       {
         path: AdjudicationRoutes.LOGIN_PAGE,
@@ -50,11 +53,12 @@ const routes: Routes = [
         // TODO uncomment when redirection is possible based on the token
         // canLoad: [AuthorizationRedirectGuard],
         loadChildren: () => import('@gis/shared/modules/gis-login/gis-login.module').then(m => m.GisLoginModule)
-      }, 
+      },
       {
         path: HealthAuthSiteRegRoutes.LOGIN_PAGE,
         canLoad: [AuthorizationRedirectGuard],
-        loadChildren: () => import('@health-auth/shared/modules/health-auth-site-reg-login-page/health-auth-site-reg-login-page.module').then(m => m.HealthAuthSiteRegLoginPageModule)
+        loadChildren: () => import('@health-auth/shared/modules/health-auth-site-reg-login-page/health-auth-site-reg-login-page.module')
+          .then(m => m.HealthAuthSiteRegLoginPageModule)
       },
       {
         path: '', // Equivalent to `/` and alias for `info`
@@ -69,4 +73,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {}
