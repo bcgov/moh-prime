9ddc8ccc4fb84a2ee9836417864cf854b7a267d0
Martin Pultz <mtpultz@gmail.com>
2021-04-30 14:54:48 -0700

    Updated login for HA

--- prime-angular-frontend/src/app/modules/health-auth-site-reg/shared/modules/health-auth-site-reg-login-page/health-auth-site-reg-login-page.component.ts
+++ prime-angular-frontend/src/app/modules/health-auth-site-reg/shared/modules/health-auth-site-reg-login-page/health-auth-site-reg-login-page.component.ts
@@ -6,6 +6,7 @@ import { IdentityProviderEnum } from '@auth/shared/enum/identity-provider.enum';
 import { AuthService } from '@auth/shared/services/auth.service';
 
 import { HealthAuthSiteRegRoutes } from '@health-auth/health-auth-site-reg.routes';
+import { SiteRegistrationTypeEnum } from '@health-auth/shared/enums/site-registration-type.enum';
 
 @Component({
   selector: 'app-health-auth-site-reg-login-page',
@@ -24,7 +25,8 @@ export class HealthAuthSiteRegLoginPageComponent implements OnInit {
     this.title = route.snapshot.data.title;
   }
 
-  public onLogin() {
+  public onLogin(type: SiteRegistrationTypeEnum) {
+    // TODO choose authentication based on the site registration type
     // Route to COLLECTION_NOTICE which determines the direction of routing
     const redirectRoute = HealthAuthSiteRegRoutes.routePath(HealthAuthSiteRegRoutes.COLLECTION_NOTICE);
     const redirectUri = `${ this.config.loginRedirectUrl }${ redirectRoute }`;
