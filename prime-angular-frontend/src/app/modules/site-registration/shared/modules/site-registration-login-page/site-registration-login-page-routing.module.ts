36c904bce2f3693dabab523438ef03a16ffa885d
Martin Pultz <mtpultz@gmail.com>
2021-05-07 02:40:08 -0700

    Move location code for banner into logins

--- prime-angular-frontend/src/app/modules/site-registration/shared/modules/site-registration-login-page/site-registration-login-page-routing.module.ts
+++ prime-angular-frontend/src/app/modules/site-registration/shared/modules/site-registration-login-page/site-registration-login-page-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { BannerLocationCode } from '@shared/enums/banner-location-code.enum';
+
 import { SiteRegistrationLoginPageComponent } from './site-registration-login-page.component';
 
 const routes: Routes = [
   {
     path: '',
     component: SiteRegistrationLoginPageComponent,
-    data: { title: 'Site Registration for PharmaNet Access' }
+    data: {
+      title: 'Site Registration for PharmaNet Access',
+      locationCode: BannerLocationCode.SITE_REGISTRATION_LANDING_PAGE
+    }
   }
 ];
 
