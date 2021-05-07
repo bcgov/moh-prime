da81eee56d8d7112f4de9133b4bbd9e75c5c8070
Johnny L <johnny.liu@nttdata.com>
2021-05-06 17:47:05 -0700

    Use plain ActionResult

--- prime-dotnet-webapi/Controllers/SitesController.cs
+++ prime-dotnet-webapi/Controllers/SitesController.cs
@@ -814,7 +814,7 @@ namespace Prime.Controllers
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(typeof(ApiMessageResponse), StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<ActionResult<Site>> EnableEditingSite(int siteId)
+        public async Task<ActionResult> EnableEditingSite(int siteId)
         {
             var site = await _siteService.GetSiteAsync(siteId);
             if (site == null)
