d547f4503f642e494bac7192d4677c057ccd1d87
Martin Pultz <mtpultz@gmail.com>
2021-05-12 13:13:27 -0700

    Clean up ununsed guard

--- prime-angular-frontend/src/app/modules/health-auth-site-reg/shared/guards/health-auth-site-reg.guard.ts
+++ prime-angular-frontend/src/app/modules/health-auth-site-reg/shared/guards/health-auth-site-reg.guard.ts
@@ -1,33 +1,8 @@
 import { Injectable } from '@angular/core';
-import {
-  CanActivate, CanActivateChild, CanDeactivate, CanLoad, Route,
-  UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree
-} from '@angular/router';
 
 import { BaseGuard } from '@core/guards/base.guard';
 
 @Injectable({
   providedIn: 'root'
 })
-export class HealthAuthSiteRegGuard extends BaseGuard {
-
-  public canActivateChild(
-    childRoute: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
-
-public canDeactivate(
-    component: unknown,
-    currentRoute: ActivatedRouteSnapshot,
-    currentState: RouterStateSnapshot,
-    nextState?: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
-
-  public canLoad(
-    route: Route,
-    segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
-}
+export class HealthAuthSiteRegGuard extends BaseGuard { }
