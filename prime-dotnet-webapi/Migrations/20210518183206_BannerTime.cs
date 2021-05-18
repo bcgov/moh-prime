a04fd1084c2ad17d75fd36f997b958d27e97864c
James <jameshollinger.ee@gmail.com>
2021-05-18 11:55:07 -0700

    Migration

---
+++ prime-dotnet-webapi/Migrations/20210518183206_BannerTime.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Prime.Migrations
+{
+    public partial class BannerTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartDate",
+                table: "Banner",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndDate",
+                table: "Banner",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "StartDate",
+                table: "Banner",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "EndDate",
+                table: "Banner",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime));
+        }
+    }
+}
