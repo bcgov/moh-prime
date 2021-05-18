b61e5e3097fc32dbd632ae1305c4be760a4d30af
Nathan <Hogman>
2021-05-18 16:45:49 -0700

    Add toolbar to page component for Back navigation

--- prime-angular-frontend/src/app/shared/components/pages/page/page.component.ts
+++ prime-angular-frontend/src/app/shared/components/pages/page/page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 import { Subscription } from 'rxjs';
 
@@ -10,9 +10,18 @@ import { Subscription } from 'rxjs';
 export class PageComponent implements OnInit {
   @Input() public busy: Subscription;
   @Input() public mode: 'default' | 'full';
+  @Input() public showToolbar: boolean;
+
+  @Output() public back: EventEmitter<void>;
 
   constructor() {
     this.mode = 'default';
+    this.showToolbar = false;
+    this.back = new EventEmitter<void>();
+  }
+
+  public onBack() {
+    this.back.emit();
   }
 
   public ngOnInit() { }
