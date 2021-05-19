4ba0423b20b8db88cd42ebffc73d2fe16b8b2e2e
Martin Pultz <mtpultz@users.noreply.github.com>
2021-05-19 15:58:51 -0700

    public access and imports

--- prime-angular-frontend/src/app/modules/adjudication/shared/components/maintenance-container/maintenance-container.component.ts
+++ prime-angular-frontend/src/app/modules/adjudication/shared/components/maintenance-container/maintenance-container.component.ts
@@ -1,7 +1,8 @@
-import { AdjudicationRoutes } from '@adjudication/adjudication.routes';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+
 import { RouteUtils } from '@lib/utils/route-utils.class';
+import { AdjudicationRoutes } from '@adjudication/adjudication.routes';
 
 @Component({
   selector: 'app-maintenance-container',
@@ -34,7 +35,5 @@ export class MaintenanceContainerComponent implements OnInit {
     ];
   }
 
-  ngOnInit(): void {
-  }
-
+  public ngOnInit(): void { }
 }
