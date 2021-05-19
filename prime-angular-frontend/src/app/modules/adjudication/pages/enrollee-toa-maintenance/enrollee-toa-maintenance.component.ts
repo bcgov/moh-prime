cb7a4b97506778c9645864da03e0dcc3d5451b62
Johnny L <johnny.liu@nttdata.com>
2021-05-18 17:14:36 -0700

    Added back button from app-page

--- prime-angular-frontend/src/app/modules/adjudication/pages/enrollee-toa-maintenance/enrollee-toa-maintenance.component.ts
+++ prime-angular-frontend/src/app/modules/adjudication/pages/enrollee-toa-maintenance/enrollee-toa-maintenance.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
-import { Observable, Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
+
+import { RouteUtils } from '@lib/utils/route-utils.class';
 
 import { AgreementVersion } from '@shared/models/agreement-version.model';
 import { AgreementTypeNameMap } from '@shared/enums/agreement-type.enum';
+import { AdjudicationRoutes } from '@adjudication/adjudication.routes';
 
 import { EnrolmentResource } from '@enrolment/shared/services/enrolment-resource.service';
+
 
 @Component({
   selector: 'app-enrollee-toa-maintenance',
@@ -16,18 +21,27 @@ export class EnrolleeToaMaintenanceComponent implements OnInit {
   public enrolleeAgreementVersions$: Observable<AgreementVersion[]>;
   public AgreementTypeNameMap = AgreementTypeNameMap;
   public previewingToa: AgreementVersion;
-  public showPreview: boolean;
+
+  private routeUtils: RouteUtils;
+
 
   constructor(
+    protected route: ActivatedRoute,
+    private router: Router,
     private enrolmentResource: EnrolmentResource
-  ) { }
+  ) {
+    this.routeUtils = new RouteUtils(route, router, AdjudicationRoutes.routePath(AdjudicationRoutes.ENROLLEES));
+  }
 
   public previewToa(agreementVersion: AgreementVersion): void {
     this.previewingToa = agreementVersion;
   }
 
   public back(): void {
-    this.previewingToa = null;
+    this.previewingToa ?
+      this.previewingToa = null
+      :
+      this.routeUtils.routeRelativeTo(['./']);
   }
 
   public ngOnInit(): void {
