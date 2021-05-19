9320e27eaf016112b208d41715e74f7de76360d4
Martin Pultz <mtpultz@users.noreply.github.com>
2021-05-19 13:27:14 -0700

    nl, public accessor, and imports

--- prime-angular-frontend/src/app/modules/adjudication/shared/components/notification-email-view/notification-email-view.component.ts
+++ prime-angular-frontend/src/app/modules/adjudication/shared/components/notification-email-view/notification-email-view.component.ts
@@ -1,6 +1,7 @@
+import { Component, Input, OnInit } from '@angular/core';
+
 import { EmailTemplateTypeEnum } from '@adjudication/shared/models/email-template-type.model';
 import { EmailTemplate } from '@adjudication/shared/models/email-template.model';
-import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-notification-email-view',
@@ -12,10 +13,7 @@ export class NotificationEmailViewComponent implements OnInit {
 
   public EmailTemplateTypeEnum = EmailTemplateTypeEnum;
 
-  constructor(
-  ) { }
+  constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  public ngOnInit(): void { }
 }
