46258a9407ae3e99e3f02257b94728099406494c
James <jameshollinger.ee@gmail.com>
2021-05-25 17:11:14 -0700

    Fix Jobs Migration

---
+++ prime-dotnet-webapi/Migrations/20210525235342_FixJobs.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Prime.Migrations
+{
+    public partial class FixJobs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                update ""OboSite""
+                set ""JobTitle"" = (rec.""job_list"")
+                from (
+                    select j.""EnrolleeId"", string_agg(j.""Title"" , ', ') AS job_list
+                    FROM   ""Job"" j
+                    GROUP  BY j.""EnrolleeId""
+                ) rec
+                where ""EnrolleeId"" = rec.""EnrolleeId"";
+            ");
+
+            migrationBuilder.Sql(@"
+                update ""OboSite""
+                set ""HealthAuthorityCode"" = rec.""HealthAuthorityCode""
+                from (
+                    select eha.""EnrolleeId"", eha.""HealthAuthorityCode""
+                    FROM   ""EnrolleeHealthAuthority"" eha
+                    order by eha.""EnrolleeId"" asc, eha.""HealthAuthorityCode"" asc
+                ) rec
+                where ""CareSettingCode"" = 1 and ""EnrolleeId"" = rec.""EnrolleeId"";
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
