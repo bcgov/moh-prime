c75bc53cd87c7103e045e07ea3e576fd70ad01d1
Martin Pultz <mtpultz@gmail.com>
2021-05-27 17:40:11 -0700

    Added GIS to authentication guard

--- prime-angular-frontend/src/app/modules/auth/shared/guards/authentication.guard.ts
+++ prime-angular-frontend/src/app/modules/auth/shared/guards/authentication.guard.ts
@@ -37,7 +37,8 @@ export class AuthenticationGuard extends BaseGuard {
 
       const routes = this.config.routes;
       const adminRoutes = [routes.adjudication];
-      const moduleRoutes = [routes.enrolment, ...adminRoutes];
+      const gisRoutes = [routes.gis];
+      const moduleRoutes = [routes.enrolment, ...adminRoutes, ...gisRoutes];
       const targetModule = routePath.slice(1).split('/').shift();
 
       // Attempt to directly redirect the user to authenticate
@@ -47,10 +48,13 @@ export class AuthenticationGuard extends BaseGuard {
         // Capture the user's current location, and provide it to
         // Keycloak to redirect the user to where they originated
         // once authenticated
-        const redirectUri = `${environment.loginRedirectUrl}${routePath}`;
+        const redirectUri = `${ environment.loginRedirectUrl }${ routePath }`;
         const idpHint = (adminRoutes.includes(targetModule))
           ? IdentityProviderEnum.IDIR
-          : IdentityProviderEnum.BCSC;
+          : (gisRoutes.includes(targetModule))
+            ? IdentityProviderEnum.PHSA
+            : IdentityProviderEnum.BCSC;
+
         const options = {
           redirectUri,
           idpHint
@@ -65,6 +69,7 @@ export class AuthenticationGuard extends BaseGuard {
       } else {
         this.router.navigate([routes.auth]);
       }
+
       return reject(false);
     });
   }
