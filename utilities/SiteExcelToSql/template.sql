9dbc9b3510745ea2f79ec8b7af542505df75c521
Johnny Liu <johnny.liu@nttdata.com>
2021-05-28 16:30:16 -0700

    Fix
    - template sql change
    - bug fix

--- utilities/SiteExcelToSql/template.sql
+++ utilities/SiteExcelToSql/template.sql
@@ -3,23 +3,26 @@ DECLARE
 	DECLARE vSAPartyId public."Party"."Id"%TYPE;
 	DECLARE vSiteAddressID public."Address"."Id"%type;
 	DECLARE vFakeAddressID public."Address"."Id"%type;
-	DECLARE vRemoteUserID public."RemoteUser"."Id"%type;
 	DECLARE vOrganizationID public."Organization"."Id"%type;
 	DECLARE vSiteId public."Site"."Id"%type;
-	DECLARE vCreatorUUID uuid := '00000000-0000-0000-0000-000000000000';
+	DECLARE vCreatorUUID uuid := '00000000-0000-0000-0000-000000000001';
 BEGIN
 	--Create Fake Signing Authority Address
 	INSERT INTO public."Address"
 	("CreatedUserId", "CreatedTimeStamp", "UpdatedUserId", "UpdatedTimeStamp", "CountryCode", "ProvinceCode", "Street", "Street2", "City", "Postal", "AddressType")
-	VALUES(vCreatorUUID, now(), vCreatorUUID, now(), 'CA', 'BC', '1515 Blanshard', null, 'Victoria', 'V8W 3C8', 1)
+	VALUES(vCreatorUUID, now(), vCreatorUUID, now(), 'CA', 'BC', '350 Columbia St', null, 'Vancouver', 'V6A 4J1', 1)
 	RETURNING "Id" INTO vFakeAddressID;	
-    --Create Fake Signing Authority
+  --Create Fake Signing Authority
 	INSERT INTO public."Party"
-	("CreatedUserId", "CreatedTimeStamp", "UpdatedUserId", "UpdatedTimeStamp", "UserId", "HPDID", "FirstName", "LastName", "DateOfBirth", "JobRoleTitle", "Email", "Phone", "Fax", "SMSPhone", "PhysicalAddressId", "MailingAddressId", "MiddleName", "PreferredFirstName", "PreferredLastName", "PreferredMiddleName")
-	VALUES(vCreatorUUID, now(), vCreatorUUID, now(), vCreatorUUID, null, 'Schedule B', 'Schedule B', '0001-01-01 00:00:00', 'Schedule B Fake User', 'PrimeSupport@gov.bc.ca', '5551231234', null, null, vFakeAddressID, null, null, null, null, null)
+	("CreatedUserId", "CreatedTimeStamp", "UpdatedUserId", "UpdatedTimeStamp", "UserId", "HPDID", "FirstName", "LastName", "DateOfBirth", "JobRoleTitle", "Email", "Phone")
+	VALUES(vCreatorUUID, now(), vCreatorUUID, now(), vCreatorUUID, null, 'Norma', 'Mackenzie', '0001-01-01 00:00:00', 'Administrator of PharmaNet Onboarding', 'PrimeSupport@gov.bc.ca', '5551231234')
 	RETURNING "Id" INTO vSAPartyId;
+	--Party Address Relationship
+	INSERT INTO public."PartyAddress"
+	("PartyId", "AddressId", "CreatedTimeStamp", "CreatedUserId", "UpdatedTimeStamp", "UpdatedUserId")
+	VALUES(vSAPartyId, vFakeAddressID, now(), vCreatorUUID, now(), vCreatorUUID);
 	--Create Fake Organization
 	INSERT INTO public."Organization"
 	("CreatedUserId", "CreatedTimeStamp", "UpdatedUserId", "UpdatedTimeStamp", "Name", "DoingBusinessAs", "SigningAuthorityId", "RegistrationId", "Completed", "SubmittedDate")
-	VALUES(vCreatorUUID, now(), vCreatorUUID, now(), 'Schedule B Organization', 'Schedule B Organization', vSAPartyId, 'UNKNOWN', true, now())
+	VALUES(vCreatorUUID, now(), vCreatorUUID, now(), 'Portland Hotel Society', 'Portland Hotel Society', vSAPartyId, 'UNKNOWN', true, now())
 	RETURNING "Id" INTO vOrganizationID;
