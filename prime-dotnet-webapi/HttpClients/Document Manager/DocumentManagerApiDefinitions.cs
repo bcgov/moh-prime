9e4ffccf4139088e7c68af49e32fac4586ece9d1
James <jameshollinger.ee@gmail.com>
2021-06-02 15:23:48 -0700

    aphabetize

--- prime-dotnet-webapi/HttpClients/Document Manager/DocumentManagerApiDefinitions.cs
+++ prime-dotnet-webapi/HttpClients/Document Manager/DocumentManagerApiDefinitions.cs
@@ -4,13 +4,13 @@ namespace Prime.HttpClients.DocumentManagerApiDefinitions
 {
     public static class DestinationFolders
     {
-        public const string SignedAgreements = "signed_agreements";
-        public const string SelfDeclarations = "self_declarations";
-        public const string EnrolleeAdjudicationDocuments = "enrollee_adjudication_document";
-        public const string SignedOrgAgreements = "signed_org_agreements";
         public const string BusinessLicences = "business_licences";
-        public const string SiteAdjudicationDocuments = "site_adjudication_document";
+        public const string EnrolleeAdjudicationDocuments = "enrollee_adjudication_document";
         public const string IdentificationDocuments = "identification_document";
+        public const string SelfDeclarations = "self_declarations";
+        public const string SignedAgreements = "signed_agreements";
+        public const string SignedOrgAgreements = "signed_org_agreements";
+        public const string SiteAdjudicationDocuments = "site_adjudication_document";
     }
 
     public class DownloadToken
