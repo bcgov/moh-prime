0f36981b7669c2e7ab5e59329565555a39ba8f0a
Alan Leung <Alan.Leung@NTTDATA.COM>
2021-06-05 15:25:24 -0700

    Uploads a directory of business licence files

--- utilities/business-licence-upload/README.md
+++ utilities/business-licence-upload/README.md
@@ -1,6 +1,8 @@
+To enable a consistent execution environment, we use a Docker container for this purpose.
+
 Execute like this:
 
-`docker run -it --rm --name PRIME-1583 -e KEYCLOAK_URL=<KEYCLOAK_URL> -e KEYCLOAK_REALM=<KEYCLOAK_REALM> -e KEYCLOAK_CLIENT_ID=<KEYCLOAK_CLIENT_ID> -e KEYCLOAK_CLIENT_SECRET=<KEYCLOAK_CLIENT_SECRET> -e DOCMAN_DOCUMENTS_URL=https://pr-1293.pharmanetenrolment.gov.bc.ca/api/docman/documents -v "<HOST_SRC_DIR>:/usr/src/myapp" -w /usr/src/myapp python:3.9.5 bash upload-business-licences.sh <FILE_TO_UPLOAD>`
+`docker run -it --rm --name PRIME-1583 -e KEYCLOAK_URL=<KEYCLOAK_URL> -e KEYCLOAK_REALM=<KEYCLOAK_REALM> -e KEYCLOAK_CLIENT_ID=<KEYCLOAK_CLIENT_ID> -e KEYCLOAK_CLIENT_SECRET=<KEYCLOAK_CLIENT_SECRET> -e DOCMAN_DOCUMENTS_URL=<DOCMAN_DOCUMENTS_URL> -v "<HOST_SRC_DIR>:/usr/src/myapp" -v "<DATA_DIR>:/usr/data" -w /usr/src/myapp python:3.9.5 bash upload-business-licences.sh /usr/data`
 
 Where:
 
@@ -12,4 +14,6 @@ Where:
 | KEYCLOAK_CLIENT_SECRET | See James H if necessary
 | DOCMAN_DOCUMENTS_URL   | URL of Document Manager's `documents` end-point, e.g. `https://pr-1293.pharmanetenrolment.gov.bc.ca/api/docman/documents` 
 | HOST_SRC_DIR           | Where this README is located on host machine, e.g. `C:\Users\177092\Source\Repos\moh-prime\utilities\business-licence-upload` 
-| FILE_TO_UPLOAD         | Name of file to upload, relative to this directory 
+| DATA_DIR               | On host machine, where the directory of scanned business licence files is located, e.g. `C:\Users\177092\Downloads\prime-dummy-biz-licences`
+
+The output file is created in the same directory as this README.
--- utilities/business-licence-upload/upload-business-licences.sh
+++ utilities/business-licence-upload/upload-business-licences.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
-echo -e "-------- Getting access_token --------\n"
-TOKEN=$(curl -X POST "${KEYCLOAK_URL}/realms/${KEYCLOAK_REALM}/protocol/openid-connect/token" \
+echo -e "-------- Getting access_token --------"
+token=$(curl -X POST "${KEYCLOAK_URL}/realms/${KEYCLOAK_REALM}/protocol/openid-connect/token" \
   -H "Content-Type: application/x-www-form-urlencoded" \
   -d "grant_type=client_credentials" \
   -d "client_id=${KEYCLOAK_CLIENT_ID}" \
-  -d "client_secret=${KEYCLOAK_CLIENT_SECRET}" | python -c "import sys, json; print(json.load(sys.stdin)['access_token'])")
-echo -e ${TOKEN}
+  -d "client_secret=${KEYCLOAK_CLIENT_SECRET}" \
+  -s | python -c "import sys, json; print(json.load(sys.stdin)['access_token'])")
+echo -e ${token}
 
-FILE=$1
+# Create new file (or replace) in working directory
+printf 'PEC,Filename,Document GUID\n' > output.csv
 
-echo -e "-------- Uploading ${FILE} --------\n"
-DOC_GUID=$(curl -X POST --data-binary "@${FILE}" -H "Authorization: Bearer ${TOKEN}" "${DOCMAN_DOCUMENTS_URL}?folder=licences&filename=${FILE}" | python -c "import sys, json; print(json.load(sys.stdin)['document_guid'])")
-echo -e ${DOC_GUID}
+data_dir=$1
+for file in $data_dir/*
+do
+  echo -e "-------- Uploading ${file} and obtaining Document GUID --------"
+
+  filename_only="$(basename "${file}")"
+
+  url_encoded_filename=$(python -c "import urllib.parse, sys; print(urllib.parse.quote(sys.argv[1]))" "${filename_only}")
+
+  doc_guid=$(curl -X POST --data-binary "@${file}" -H "Authorization: Bearer ${token}" -s "${DOCMAN_DOCUMENTS_URL}?folder=paper_form_business_licences&filename=${url_encoded_filename}" | python -c "import sys, json; print(json.load(sys.stdin)['document_guid'])")
+  if [ $? -eq 0 ]; then
+    echo -e ${doc_guid}
+    # Expect files to be named like "PEC - Business License.pdf"
+    pec=$(python -c "import sys, re; match = re.search('^[A-Z]+', sys.argv[1]); print(match.group() if match is not None else 'N/A')" "${filename_only}")
+    # Append to output file
+    printf "${pec},${filename_only},${doc_guid}\n" >> output.csv
+  else
+    echo -e "WARNING:  ${file} failed to upload to Document Manager."
+  fi
+done
+
