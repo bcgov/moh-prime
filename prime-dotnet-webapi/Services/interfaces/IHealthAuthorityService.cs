39d082207f018ddf7490cdcea39808be8cfc1ea8
James <jameshollinger.ee@gmail.com>
2021-06-07 19:09:39 -0700

    HA service update

--- prime-dotnet-webapi/Services/interfaces/IHealthAuthorityService.cs
+++ prime-dotnet-webapi/Services/interfaces/IHealthAuthorityService.cs
@@ -2,11 +2,14 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Prime.Models;
 using Prime.ViewModels.Parties;
+using Prime.ViewModels.HealthAuthorities;
 
 namespace Prime.Services
 {
     public interface IHealthAuthorityService
     {
+        Task<IEnumerable<HealthAuthorityListViewModel>> GetHealthAuthoritiesAsync();
+        Task<HealthAuthorityViewModel> GetHealthAuthorityAsync(int id);
         Task<IEnumerable<AuthorizedUserViewModel>> GetAuthorizedUsersByHealthAuthorityAsync(HealthAuthorityCode code);
         Task<IEnumerable<HealthAuthorityCode>> GetHealthAuthorityCodesWithUnderReviewAuthorizedUsersAsync();
     }
