7d9dfa8d0a9c81fa5dea6bde467db1d7df91ceb1
Johnny Liu <johnny.liu@nttdata.com>
2021-06-08 16:59:06 -0700

    added app-contact-profile-form component to HA pages
    - administrators
    - technical support
    - privacy officers

--- prime-angular-frontend/src/app/modules/adjudication/pages/health-authorities/administrators-page/administrators-page.component.ts
+++ prime-angular-frontend/src/app/modules/adjudication/pages/health-authorities/administrators-page/administrators-page.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+import { Subject, Subscription } from 'rxjs';
+
+import { Contact } from '@lib/models/contact.model';
+import { RouteUtils } from '@lib/utils/route-utils.class';
+import { ContactFormState } from '@lib/classes/contact-form-state.class';
+
+import { HealthAuthorityResource } from '@core/resources/health-authority-resource.service';
+import { FormUtilsService } from '@core/services/form-utils.service';
+
+import { AdjudicationRoutes } from '@adjudication/adjudication.routes';
 
 @Component({
   selector: 'app-administrators-page',
@@ -6,10 +19,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./administrators-page.component.scss']
 })
 export class AdministratorsPageComponent implements OnInit {
+  public busy: Subscription;
+  public title: string;
+  public form: FormGroup;
+  public routeUtils: RouteUtils;
+  public contacts: Contact[];
+  public formSubmittingEvent: Subject<void>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private haResource: HealthAuthorityResource,
+    private formUtilsService: FormUtilsService,
+    route: ActivatedRoute,
+    router: Router
+  ) {
+    this.title = route.snapshot.data.title;
+    this.routeUtils = new RouteUtils(route, router, AdjudicationRoutes.MODULE_PATH);
+    this.formSubmittingEvent = new Subject<void>();
   }
 
+  public onBack(): void {
+  }
+
+  public onSubmit(): void {
+  }
+
+  public ngOnInit(): void {
+    this.createFormInstance();
+  }
+
+  private createFormInstance() {
+    this.form = new ContactFormState(this.fb, this.formUtilsService).form;
+  }
 }
