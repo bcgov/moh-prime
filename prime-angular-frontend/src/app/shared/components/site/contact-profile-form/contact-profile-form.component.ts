6639cb5ce7f7d0e495e850427f4c78e20aa149eb
Martin Pultz <mtpultz@gmail.com>
2021-06-17 10:09:17 -0700

    Updated pharma admin

--- prime-angular-frontend/src/app/shared/components/site/contact-profile-form/contact-profile-form.component.ts
+++ prime-angular-frontend/src/app/shared/components/site/contact-profile-form/contact-profile-form.component.ts
@@ -1,42 +1,71 @@
-import { Component, OnInit, Input, ContentChildren, QueryList, ViewChild } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ContentChildren,
+  QueryList,
+  ContentChild,
+  AfterContentInit,
+  ViewChild
+} from '@angular/core';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
 import { MatSlideToggle, MatSlideToggleChange } from '@angular/material/slide-toggle';
 
-import { Observable, Subject } from 'rxjs';
-import { distinctUntilChanged } from 'rxjs/operators';
-import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-
 import { FormUtilsService } from '@core/services/form-utils.service';
 import { PageSubheader2MoreInfoDirective } from '@shared/components/pages/page-subheader2/page-subheader2-more-info.directive';
-import { Contact } from '@lib/models/contact.model';
+import { SameAsComponent } from '@shared/components/site/same-as/same-as.component';
+import { Address } from '@shared/models/address.model';
 
-@UntilDestroy()
 @Component({
   selector: 'app-contact-profile-form',
   templateUrl: './contact-profile-form.component.html',
   styleUrls: ['./contact-profile-form.component.scss']
 })
-export class ContactProfileFormComponent implements OnInit {
+export class ContactProfileFormComponent implements OnInit, AfterContentInit {
+  /**
+   * @description
+   * Title of the contact profile section.
+   */
   @Input() public title: string;
+  /**
+   * @description
+   * Instance of the contact profile form.
+   */
   @Input() public form: FormGroup;
+  /**
+   * @description
+   * Show the optional fax field.
+   */
   @Input() public showFax: boolean;
-  @Input() public showAddButton: boolean;
-  @Input() public contacts: Contact[];
-  @Input() public formSubmitting: Observable<void> = new Observable<void>();
-  public showFormFields: boolean;
-  public hasPhysicalAddress: boolean;
-  public showAddressFields: boolean;
+  /**
+   * @description
+   * Pass control of the address line fields being
+   * displayed to the parent component.
+   */
+  @Input() public showAddressFields: boolean;
+  @ViewChild(MatSlideToggle) public toggle: MatSlideToggle;
+  /**
+   * @description
+   * Optional projection of the SameAsComponent for
+   * auto-filling in the contact profile form.
+   */
+  @ContentChild(SameAsComponent) public sameAs: SameAsComponent;
+  /**
+   * @description
+   * Contact specific profile information.
+   */
   @ContentChildren(PageSubheader2MoreInfoDirective, { descendants: true })
   public pageSubheaderMoreInfoChildren: QueryList<PageSubheader2MoreInfoDirective>;
-  @ViewChild('sameAddressSlideToggle')
-  public sameAddressSlideToggle: MatSlideToggle;
+  /**
+   * @description
+   * Whether the toggle is checked, which displays
+   * the address component.
+   */
+  public toggleAddress: boolean;
 
   constructor(
     private formUtilsService: FormUtilsService
-  ) {
-    this.showFax = true;
-    this.showAddButton = false;
-  }
+  ) { }
 
   public get hasPageSubheaderMoreInfo(): boolean {
     return !!this.pageSubheaderMoreInfoChildren.length;
@@ -63,63 +92,44 @@ export class ContactProfileFormComponent implements OnInit {
   }
 
   public onPhysicalAddressChange({ checked }: MatSlideToggleChange) {
-    if (checked) {
+    if (!checked) {
       this.physicalAddress.reset();
     }
 
-    this.hasPhysicalAddress = !this.hasPhysicalAddress;
+    this.toggleAddress = !this.toggleAddress;
     this.togglePhysicalAddressValidators(this.physicalAddress, ['id', 'street2']);
   }
 
-  public updateContact(contact: Contact): void {
-    this.form.patchValue(contact);
-    this.showFormFields = true;
-  }
-
-  public addContact(): void {
-    this.showFormFields = true;
-  }
-
-  public ngOnInit() {
-    // When the street is populated ensure the address is shown
-    this.physicalAddress.get('street')
-      .valueChanges
-      .pipe(distinctUntilChanged())
-      .subscribe((value: string) => (value) ? this.togglePhysicalAddress() : null);
-
-    // If first time loading, force address slider to be 'off' by default
-    if (!this.form.get('firstName').value) {
-      this.togglePhysicalAddress(true);
+  public togglePhysicalAddress() {
+    const isNotEmpty = Address.isNotEmpty(this.physicalAddress.value);
+    if (isNotEmpty) {
+      // Always show when the address is not empty
+      // regardless of the input binding value
+      this.toggleAddress = isNotEmpty;
     }
-    else {
-      this.togglePhysicalAddress();
-    }
-
-    this.formSubmitting.pipe(untilDestroyed(this))
-      .subscribe(() => {
-        // force show address fields if address form is invalid
-        this.showAddressFields = !this.sameAddressSlideToggle.checked && this.physicalAddress.invalid;
-      });
-
-    this.showFormFields = !this.contacts?.length || !this.hasPhysicalAddress;
-  }
-
-  private togglePhysicalAddress(forceDefault?: boolean) {
-    this.hasPhysicalAddress = !!(
-      this.physicalAddress.get('countryCode').value ||
-      this.physicalAddress.get('provinceCode').value ||
-      this.physicalAddress.get('street').value ||
-      this.physicalAddress.get('street2').value ||
-      this.physicalAddress.get('city').value ||
-      this.physicalAddress.get('postal').value
-    ) || forceDefault;
-
     this.togglePhysicalAddressValidators(this.physicalAddress, ['id', 'street2']);
+  }
+
+  public ngOnInit(): void {
+    this.togglePhysicalAddress();
+  }
+
+  public ngAfterContentInit(): void {
+    if (this.sameAs) {
+      // Ensure the address line fields are displayed after
+      // being patched by same as
+      this.sameAs.selected
+        .subscribe(() => {
+          this.togglePhysicalAddress();
+          this.showAddressFields = true;
+        });
+    }
   }
 
   private togglePhysicalAddressValidators(separateAddress: FormGroup, blacklist: string[] = []) {
-    (!this.hasPhysicalAddress)
+    (!this.toggleAddress)
       ? this.formUtilsService.resetAndClearValidators(separateAddress)
       : this.formUtilsService.setValidators(separateAddress, [Validators.required], blacklist);
   }
 }
+
