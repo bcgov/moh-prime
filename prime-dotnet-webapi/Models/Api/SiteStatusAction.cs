ac60a202536a8749de1747aabfb4ed83d9ef4da8
Johnny Liu <johnny.liu@nttdata.com>
2021-06-17 14:26:24 -0700

    site status action

---
+++ prime-dotnet-webapi/Models/Api/SiteStatusAction.cs
@@ -0,0 +1,12 @@
+namespace Prime.Models.Api
+{
+    public enum SiteStatusAction
+    {
+        Submit,
+        Approve,
+        Decline,
+        Unapprove,
+        Undecline,
+        RequestChange
+    }
+}
