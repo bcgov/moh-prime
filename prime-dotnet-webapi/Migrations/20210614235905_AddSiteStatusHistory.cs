51249a5f0877ba628d043fc393f2f5070cce2ae1
Alan Leung <Alan.Leung@NTTDATA.COM>
2021-06-16 17:34:05 -0700

    Database changes to add status history to sites

---
+++ prime-dotnet-webapi/Migrations/20210614235905_AddSiteStatusHistory.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Prime.Migrations
+{
+    public partial class AddSiteStatusHistory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Site");
+
+            migrationBuilder.CreateTable(
+                name: "SiteStatus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CreatedUserId = table.Column<Guid>(nullable: false),
+                    CreatedTimeStamp = table.Column<DateTimeOffset>(nullable: false),
+                    UpdatedUserId = table.Column<Guid>(nullable: false),
+                    UpdatedTimeStamp = table.Column<DateTimeOffset>(nullable: false),
+                    SiteId = table.Column<int>(nullable: false),
+                    StatusType = table.Column<int>(nullable: false),
+                    StatusDate = table.Column<DateTimeOffset>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SiteStatus", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SiteStatus_Site_SiteId",
+                        column: x => x.SiteId,
+                        principalTable: "Site",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SiteStatus_SiteId",
+                table: "SiteStatus",
+                column: "SiteId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SiteStatus");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Site",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
