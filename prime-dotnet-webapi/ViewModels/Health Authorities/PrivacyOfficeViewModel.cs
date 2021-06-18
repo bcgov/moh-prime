8be31ec84e547c8ddcd231095465d54c8dedd10a
James <jameshollinger.ee@gmail.com>
2021-06-18 14:53:00 -0700

    fluent validator

--- prime-dotnet-webapi/ViewModels/Health Authorities/PrivacyOfficeViewModel.cs
+++ prime-dotnet-webapi/ViewModels/Health Authorities/PrivacyOfficeViewModel.cs
@@ -1,3 +1,5 @@
+using FluentValidation;
+
 namespace Prime.ViewModels.HealthAuthorities
 {
     public class PrivacyOfficeViewModel
@@ -6,5 +8,14 @@ namespace Prime.ViewModels.HealthAuthorities
         public string Email { get; set; }
         public PrivacyOfficerViewModel PrivacyOfficer { get; set; }
         public AddressViewModel PhysicalAddress { get; set; }
+    }
+
+    public class PrivacyOfficeValidator : AbstractValidator<PrivacyOfficeViewModel>
+    {
+        public PrivacyOfficeValidator()
+        {
+            RuleFor(x => x.PhysicalAddress).NotNull();
+            RuleFor(x => x.PrivacyOfficer).NotNull();
+        }
     }
 }
