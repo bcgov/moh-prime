55c670bb53f5d486a91b9e40d664e341e5ecff87
Martin Pultz <mtpultz@gmail.com>
2021-06-19 19:48:55 -0700

    Clean up and stubbed in administrator selector for HA module

--- prime-angular-frontend/src/app/modules/health-auth-site-reg/pages/administrator-page/administrator-page-form-state.class.ts
+++ prime-angular-frontend/src/app/modules/health-auth-site-reg/pages/administrator-page/administrator-page-form-state.class.ts
@@ -1,3 +1,39 @@
-import { ContactFormState } from '@lib/classes/contact-form-state.class';
+import { AbstractFormState } from '@lib/classes/abstract-form-state.class';
+import { Contact } from '@lib/models/contact.model';
+import { FormBuilder, Validators } from '@angular/forms';
+import { FormUtilsService } from '@core/services/form-utils.service';
+import { FormControlValidators } from '@lib/validators/form-control.validators';
+import { FormArrayValidators } from '@lib/validators/form-array.validators';
 
-export class AdministratorPageFormState extends ContactFormState { }
+export class AdministratorPageFormState extends AbstractFormState<Contact[]> {
+  public constructor(
+    private fb: FormBuilder,
+    private formUtilsService: FormUtilsService
+  ) {
+    super();
+
+    this.buildForm();
+  }
+
+  public get json(): Contact[] {
+    if (!this.formInstance) {
+      return;
+    }
+
+    // return this.formUtilsService.toPersonJson(this.formInstance.getRawValue());
+  }
+
+  public patchValue(model: Contact[]): void {
+    if (!model) {
+      return;
+    }
+
+    // this.formUtilsService.toPersonFormModel([this.formInstance, model]);
+  }
+
+  public buildForm(disabled: boolean = false): void {
+    this.formInstance = this.fb.group({
+      administrators: this.fb.array([], FormArrayValidators.atLeast(1))
+    });
+  }
+}
