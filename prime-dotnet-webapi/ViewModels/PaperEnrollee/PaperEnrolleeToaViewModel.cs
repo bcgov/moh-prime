50fc0b59f9c09f481bbf5efc4a140ce234a75f89
Nathan <Hogman>
2021-06-21 16:57:57 -0700

    adjust name of documents

--- prime-dotnet-webapi/ViewModels/PaperEnrollee/PaperEnrolleeToaViewModel.cs
+++ prime-dotnet-webapi/ViewModels/PaperEnrollee/PaperEnrolleeToaViewModel.cs
@@ -1,10 +1,11 @@
+using System.Collections.Generic;
 using Prime.Models;
 
 namespace Prime.ViewModels
 {
     public class PaperEnrolleeToaViewModel
     {
-        // public ICollection<SupportingDocument> SupportingDocuments { get; set; }
+        public ICollection<EnrolleeAdjudicationDocument> EnrolleeAdjudicationDocuments { get; set; }
 
         // Not 100% sure how our TOA's work and if we will need another way to identify what tos is signed.
         public AgreementType? AssignedTOAType { get; set; }
