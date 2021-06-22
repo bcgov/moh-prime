90c2693a25d572828954f37f04bfd2f14ab7ddcf
James <jameshollinger.ee@gmail.com>
2021-06-21 17:06:54 -0700

    add automapper profile

--- prime-dotnet-webapi/ViewModels/Paper Enrollees/PaperEnrolleeCertificationViewModel.cs
+++ prime-dotnet-webapi/ViewModels/Paper Enrollees/PaperEnrolleeCertificationViewModel.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using Prime.Models;
 
-namespace Prime.ViewModels.PaperEnrollee
+namespace Prime.ViewModels.PaperEnrollees
 {
     public class PaperEnrolleeCertificationViewModel
     {
