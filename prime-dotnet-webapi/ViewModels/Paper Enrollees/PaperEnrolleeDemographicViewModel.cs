3a22a8f4171be968c3de3cd7b00ecc31f98cd317
James <jameshollinger.ee@gmail.com>
2021-06-22 18:39:28 -0700

    demographic validation

--- prime-dotnet-webapi/ViewModels/Paper Enrollees/PaperEnrolleeDemographicViewModel.cs
+++ prime-dotnet-webapi/ViewModels/Paper Enrollees/PaperEnrolleeDemographicViewModel.cs
@@ -1,4 +1,5 @@
 using System;
+using FluentValidation;
 
 namespace Prime.ViewModels.PaperEnrollees
 {
@@ -21,5 +22,19 @@ namespace Prime.ViewModels.PaperEnrollees
         public string PhoneExtension { get; set; }
 
         public string SmsPhone { get; set; }
+    }
+
+    public class PaperEnrolleeDemographicValidator : AbstractValidator<PaperEnrolleeDemographicViewModel>
+    {
+        public PaperEnrolleeDemographicValidator()
+        {
+            RuleFor(x => x.FirstName).NotEmpty();
+            RuleFor(x => x.LastName).NotEmpty();
+            RuleFor(x => x.GivenNames).NotEmpty();
+            RuleFor(x => x.DateOfBirth).NotEmpty();
+            RuleFor(x => x.PhysicalAddress).NotNull();
+            RuleFor(x => x.Email).NotEmpty();
+            RuleFor(x => x.Phone).NotEmpty();
+        }
     }
 }
