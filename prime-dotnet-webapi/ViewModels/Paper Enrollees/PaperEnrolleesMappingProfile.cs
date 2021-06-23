8060ab6c5261f137c0d8e005dccc9b1a2c62bd1b
James <jameshollinger.ee@gmail.com>
2021-06-22 18:47:14 -0700

    middle name mapping

--- prime-dotnet-webapi/ViewModels/Paper Enrollees/PaperEnrolleesMappingProfile.cs
+++ prime-dotnet-webapi/ViewModels/Paper Enrollees/PaperEnrolleesMappingProfile.cs
@@ -7,7 +7,9 @@ public class PaperEnrolleesMappingProfile : Profile
 {
     public PaperEnrolleesMappingProfile()
     {
-        CreateMap<PaperEnrolleeDemographicViewModel, Enrollee>();
+        CreateMap<PaperEnrolleeDemographicViewModel, Enrollee>()
+            .ForMember(dest => dest.GivenNames, opt => opt.MapFrom(src => string.Join(" ", src.FirstName, src.MiddleName)));
+
         CreateMap<PaperEnrolleeCertificationViewModel, Certification>();
     }
 }
