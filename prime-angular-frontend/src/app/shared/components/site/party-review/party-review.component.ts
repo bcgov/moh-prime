c3387ef13f2b1c1292bd4475482273edc2c8768c
Martin Pultz <mtpultz@gmail.com>
2021-06-24 16:02:50 -0700

    Update overview container

--- prime-angular-frontend/src/app/shared/components/site/party-review/party-review.component.ts
+++ prime-angular-frontend/src/app/shared/components/site/party-review/party-review.component.ts
@@ -2,7 +2,6 @@ import { Component, Input } from '@angular/core';
 
 import { Party } from '@lib/models/party.model';
 
-// TODO refactor this component it doesn't match a consistent pattern with enrolments for reuse
 @Component({
   selector: 'app-party-review',
   templateUrl: './party-review.component.html',
@@ -14,7 +13,7 @@ export class PartyReviewComponent {
    * @description
    * List of fields that should be excluded.
    */
-  @Input() public excludeList: ('jobRoleTitle' | 'fax' | 'smsPhone' | 'mailingAddress')[];
+  @Input() public excludeList: ('name' | 'jobRoleTitle' | 'fax' | 'smsPhone' | 'mailingAddress')[];
 
   constructor() {
     this.excludeList = [];
