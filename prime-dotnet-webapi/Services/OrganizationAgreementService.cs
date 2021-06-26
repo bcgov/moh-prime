7d93f3fc622324c682b1285a3570c703e7ca71ba
James <jameshollinger.ee@gmail.com>
2021-06-25 17:39:47 -0700

    square up org agreement service

--- prime-dotnet-webapi/Services/OrganizationAgreementService.cs
+++ prime-dotnet-webapi/Services/OrganizationAgreementService.cs
@@ -8,11 +8,8 @@ using AutoMapper;
 using AutoMapper.QueryableExtensions;
 
 using Prime.Models;
-using Prime.Models.Api;
 using Prime.ViewModels;
 using Prime.ViewModels.Agreements;
-using Prime.HttpClients;
-using Prime.HttpClients.DocumentManagerApiDefinitions;
 using Prime.Services.Razor;
 
 namespace Prime.Services
@@ -20,26 +17,108 @@ namespace Prime.Services
     public class OrganizationAgreementService : BaseService, IOrganizationAgreementService
     {
         private readonly IMapper _mapper;
-        private readonly IAgreementService _agreementService;
         private readonly IPdfService _pdfService;
         private readonly IRazorConverterService _razorConverterService;
-        private readonly IDocumentManagerClient _documentClient;
 
         public OrganizationAgreementService(
             ApiDbContext context,
             IHttpContextAccessor httpContext,
             IMapper mapper,
-            IAgreementService agreementService,
             IPdfService pdfService,
-            IRazorConverterService razorConverterService,
-            IDocumentManagerClient documentClient)
+            IRazorConverterService razorConverterService)
             : base(context, httpContext)
         {
             _mapper = mapper;
-            _agreementService = agreementService;
             _pdfService = pdfService;
             _razorConverterService = razorConverterService;
-            _documentClient = documentClient;
+        }
+
+
+        public async Task<IEnumerable<AgreementViewModel>> GetOrgAgreementsAsync(int organizationId)
+        {
+            return await _context.Agreements
+                .AsNoTracking()
+                .OrderByDescending(a => a.CreatedDate)
+                .Where(a => a.OrganizationId == organizationId)
+                .ProjectTo<AgreementViewModel>(_mapper.ConfigurationProvider)
+                .ToListAsync();
+        }
+
+        /// <summary>
+        /// Gets the Agreement + text of a given Org Agreement, optionally with the text in the form of a Base 64 encoded PDF.
+        /// Returns null if the Agreement does not exist on the given organization.
+        /// </summary>
+        /// <param name="organizationId"></param>
+        /// <param name="agreementId"></param>
+        /// <param name="asEncodedPdf"></param>
+        /// <returns></returns>
+        public async Task<AgreementViewModel> GetOrgAgreementAsync(int organizationId, int agreementId, bool asEncodedPdf = false)
+        {
+            var agreementVm = await _context.Agreements
+                .AsNoTracking()
+                .Where(a => a.Id == agreementId && a.OrganizationId == organizationId)
+                .ProjectTo<AgreementViewModel>(_mapper.ConfigurationProvider)
+                .SingleOrDefaultAsync();
+
+            if (agreementVm == null)
+            {
+                return null;
+            }
+
+            var orgName = await GetOrganizationName(organizationId);
+            var html = await RenderOrgAgreementHtmlAsync(agreementVm.AgreementType, orgName, agreementVm.AcceptedDate, asEncodedPdf, false);
+
+            if (asEncodedPdf)
+            {
+                var pdf = _pdfService.Generate(html);
+                agreementVm.AgreementContent = Convert.ToBase64String(pdf);
+            }
+            else
+            {
+                agreementVm.AgreementContent = html;
+            }
+
+            return agreementVm;
+        }
+
+        public async Task<string> RenderOrgAgreementHtmlAsync(AgreementType type, string orgName, DateTimeOffset? acceptedDate, bool forPdf, bool withSignature)
+        {
+            var template = (type, forPdf) switch
+            {
+                (AgreementType.CommunityPharmacyOrgAgreement, false) => RazorTemplates.OrgAgreements.CommunityPharmacy,
+                (AgreementType.CommunityPharmacyOrgAgreement, true) => RazorTemplates.OrgAgreements.CommunityPharmacyPdf,
+                (AgreementType.CommunityPracticeOrgAgreement, false) => RazorTemplates.OrgAgreements.CommunityPractice,
+                (AgreementType.CommunityPracticeOrgAgreement, true) => RazorTemplates.OrgAgreements.CommunityPracticePdf,
+                _ => throw new ArgumentException($"Invalid AgreementType {type} in {nameof(RenderOrgAgreementHtmlAsync)}")
+            };
+
+            var displayDate = acceptedDate ?? DateTimeOffset.Now;
+            // Converting to BC time here since we aren't localizing this time in the web client
+            displayDate = displayDate.ToOffset(new TimeSpan(-7, 0, 0));
+
+            return await _razorConverterService.RenderTemplateToStringAsync(template, new OrgAgreementRazorViewModel(orgName, displayDate, withSignature));
+        }
+
+        /// <summary>
+        /// Returns a Base64 encoded PDF of a given org agreement for signing.
+        /// </summary>
+        /// <param name="organizationId"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public async Task<string> GetSignableOrgAgreementAsync(int organizationId, AgreementType type)
+        {
+            var orgName = await GetOrganizationName(organizationId);
+            var html = await RenderOrgAgreementHtmlAsync(type, orgName, DateTimeOffset.Now, true, true);
+            var pdf = _pdfService.Generate(html);
+            return Convert.ToBase64String(pdf);
+        }
+
+        private async Task<string> GetOrganizationName(int organizationId)
+        {
+            return await _context.Organizations
+                .Where(o => o.Id == organizationId)
+                .Select(o => o.Name)
+                .SingleAsync();
         }
     }
 }
