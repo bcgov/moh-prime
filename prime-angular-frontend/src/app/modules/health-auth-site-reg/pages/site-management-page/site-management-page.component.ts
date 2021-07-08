699431f2c7d373ad1104500aa1a39fb752039a5b
Martin Pultz <mtpultz@gmail.com>
2021-07-08 15:56:01 -0700

    Typo and route update

--- prime-angular-frontend/src/app/modules/health-auth-site-reg/pages/site-management-page/site-management-page.component.ts
+++ prime-angular-frontend/src/app/modules/health-auth-site-reg/pages/site-management-page/site-management-page.component.ts
@@ -102,7 +102,7 @@ export class SiteManagementPageComponent implements OnInit {
     this.routeUtils.routeRelativeTo([
       HealthAuthSiteRegRoutes.HEALTH_AUTHORITIES,
       healthAuthorityId,
-      ...ArrayUtils.insertIf(healthAuthoritySiteId, [
+      ...ArrayUtils.insertIf(healthAuthoritySiteId > 0, [
         HealthAuthSiteRegRoutes.SITES,
         healthAuthoritySiteId
       ]),
