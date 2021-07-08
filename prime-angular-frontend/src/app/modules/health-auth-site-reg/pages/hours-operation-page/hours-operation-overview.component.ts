cbad2c40632c86ceb808bbd44fa02d24eb2f63f8
Martin Pultz <mtpultz@gmail.com>
2021-07-08 01:45:01 -0700

    Infinite updates

--- prime-angular-frontend/src/app/modules/health-auth-site-reg/pages/hours-operation-page/hours-operation-overview.component.ts
+++ prime-angular-frontend/src/app/modules/health-auth-site-reg/pages/hours-operation-page/hours-operation-overview.component.ts
@@ -14,7 +14,7 @@ import { HoursOperationForm } from './hours-operation-form.model';
                           [showEditRedirect]="showEditRedirect"
                           [editRoute]="HealthAuthSiteRegRoutes.HOURS_OPERATION"
                           (route)="onRoute($event)">
-      <ng-container *ngFor="let businessDay of hoursOperation?.businessHours">
+      <ng-container *ngFor="let businessDay of demo?.businessHours">
         <app-enrollee-property [title]="businessDay.day | weekday">
           <ng-container *ngIf="businessDay.startTime && businessDay.endTime">
             {{ businessDay.startTime }} to {{ businessDay.endTime }}
@@ -30,6 +30,8 @@ export class HoursOperationOverviewComponent extends AbstractOverview implements
   @Input() hoursOperation: HoursOperationForm;
   public HealthAuthSiteRegRoutes = HealthAuthSiteRegRoutes;
 
+  public demo: HoursOperationForm;
+
   constructor(
     route: ActivatedRoute,
     router: Router
@@ -37,5 +39,17 @@ export class HoursOperationOverviewComponent extends AbstractOverview implements
     super(route, router, HealthAuthSiteRegRoutes.MODULE_PATH);
   }
 
-  public ngOnInit(): void { }
+  public ngOnInit(): void {
+    // TODO need to convert times when displayed using a pipe
+    // TODO split out and reuse the methods in form state
+    this.demo = {
+      businessHours: [
+        { startTime: '9:00 am', endTime: '5:00 pm', day: 1 },
+        { startTime: '9:00 am', endTime: '5:00 pm', day: 2 },
+        { startTime: '9:00 am', endTime: '5:00 pm', day: 3 },
+        { startTime: '9:00 am', endTime: '5:00 pm', day: 4 },
+        { startTime: '9:00 am', endTime: '5:00 pm', day: 5 }
+      ]
+    };
+  }
 }
--- prime-angular-frontend/src/app/modules/health-auth-site-reg/pages/remote-users-page/remote-users-overview.component.ts
+++ prime-angular-frontend/src/app/modules/health-auth-site-reg/pages/remote-users-page/remote-users-overview.component.ts
@@ -6,30 +6,15 @@ import { HealthAuthSiteRegRoutes } from '@health-auth/health-auth-site-reg.route
 
 import { RemoteUsersForm } from './remote-users-form.model';
 
+// TODO drop specific routes out of remote-user-review and always pass in
+// TODO should be a message of no remote users added if none
 @Component({
   selector: 'app-remote-users-overview',
   template: `
-    <!--    <div *ngIf="remoteUsers?.length"-->
-    <!--         class="row mt-4">-->
-    <!--      <div class="col-6">-->
-    <!--        <app-page-subheader>-->
-    <!--          <ng-container appPageSubheaderTitle>-->
-    <!--            <mat-icon class="mr-2">account_circle</mat-icon>-->
-    <!--            Remote Practitioners-->
-    <!--          </ng-container>-->
-    <!--          <button mat-icon-button-->
-    <!--                  matTooltip="View Remote Practitioners"-->
-    <!--                  (click)="onRoute([organization.id, AdjudicationRoutes.SITE_REGISTRATION, site.id, AdjudicationRoutes.SITE_REMOTE_USERS])">-->
-    <!--            <mat-icon>navigate_next icon</mat-icon>-->
-    <!--          </button>-->
-    <!--        </app-page-subheader>-->
-    <!--        <mat-card class="enrolmentCard">-->
-    <!--          <mat-card-content>-->
-    <!--            <app-remote-user-review [remoteUsers]="site?.remoteUsers"></app-remote-user-review>-->
-    <!--          </mat-card-content>-->
-    <!--        </mat-card>-->
-    <!--      </div>-->
-    <!--    </div>-->
+    <app-remote-user-review [remoteUsers]="remoteUsers?.remoteUsers"
+                            [showEditRedirect]="showEditRedirect"
+                            [editRoute]="HealthAuthSiteRegRoutes.REMOTE_USERS"
+                            (route)="onRoute($event)"></app-remote-user-review>
   `,
   styles: [],
   encapsulation: ViewEncapsulation.None
