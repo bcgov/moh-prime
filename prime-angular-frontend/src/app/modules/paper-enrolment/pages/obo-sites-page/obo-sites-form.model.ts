108efd450bc7a76253e58f670b61ec63d357366b
Panos <paahndev@gmail.com>
2021-07-09 16:33:12 -0700

    restructured interface to include the properties needed for our obosites overview

--- prime-angular-frontend/src/app/modules/paper-enrolment/pages/obo-sites-page/obo-sites-form.model.ts
+++ prime-angular-frontend/src/app/modules/paper-enrolment/pages/obo-sites-page/obo-sites-form.model.ts
@@ -1,3 +1,9 @@
-import { HttpEnrollee } from '@shared/models/enrolment.model';
+import { OboSite } from '@enrolment/shared/models/obo-site.model';
+import { CareSetting } from '@enrolment/shared/models/care-setting.model';
+import { EnrolleeHealthAuthority } from '@shared/models/enrollee-health-authority.model';
 
-export interface OboSitesFormModel extends Pick<HttpEnrollee, 'oboSites'> { }
+export interface OboSitesFormModel {
+  oboSites: OboSite[];
+  enrolleeCareSettings: CareSetting[];
+  enrolleeHealthAuthorities: EnrolleeHealthAuthority[];
+}
