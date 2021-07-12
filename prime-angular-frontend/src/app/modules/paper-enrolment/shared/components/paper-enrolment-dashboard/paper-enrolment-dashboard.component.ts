29bf189db12343518d0cad24ac115f611ab0bf47
Panos <paahndev@gmail.com>
2021-07-12 15:52:29 -0700

    Revert WIP commit

--- prime-angular-frontend/src/app/modules/paper-enrolment/shared/components/paper-enrolment-dashboard/paper-enrolment-dashboard.component.ts
+++ prime-angular-frontend/src/app/modules/paper-enrolment/shared/components/paper-enrolment-dashboard/paper-enrolment-dashboard.component.ts
@@ -3,13 +3,11 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { AppConfig, APP_CONFIG } from 'app/app-config.module';
 import { IDashboard } from '@lib/modules/dashboard/interfaces/dashboard.interface';
 import { DashboardMenuItem } from '@lib/modules/dashboard/models/dashboard-menu-item.model';
-import { HttpEnrollee } from '@shared/models/enrolment.model';
-import { AdjudicationRoutes } from '@adjudication/adjudication.routes';
 
-import { PaperEnrolmentResource } from '@paper-enrolment/services/paper-enrolment-resource.service';
+import { AppConfig, APP_CONFIG } from 'app/app-config.module';
+import { AdjudicationRoutes } from '@adjudication/adjudication.routes';
 
 @Component({
   selector: 'app-paper-enrolment-dashboard',
@@ -21,25 +19,16 @@ export class PaperEnrolmentDashboardComponent implements OnInit, IDashboard {
   public logoutRedirectUrl: string;
 
   constructor(
-    @Inject(APP_CONFIG) protected config: AppConfig,
-    private paperEnrolmentResource: PaperEnrolmentResource
+    @Inject(APP_CONFIG) protected config: AppConfig
   ) {
     this.logoutRedirectUrl = `${this.config.loginRedirectUrl}/${AdjudicationRoutes.LOGIN_PAGE}`;
   }
 
   public ngOnInit(): void {
     this.dashboardMenuItems = this.getDashboardMenuItems();
-    this.getMeSomeData();
   }
 
   private getDashboardMenuItems(): Observable<DashboardMenuItem[]> {
     return of([]);
-  }
-
-  public getMeSomeData() {
-    this.paperEnrolmentResource.getEnrolleeById(6).subscribe(f => {
-      console.log(f);
-      console.log("Approved date is", f.approvedDate);
-    });
   }
 }
