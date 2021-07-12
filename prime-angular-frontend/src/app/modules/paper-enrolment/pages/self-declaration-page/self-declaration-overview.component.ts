3c4e29c3145e9922ff49e79abca8fe638566b8ef
Panos <paahndev@gmail.com>
2021-07-12 11:40:33 -0700

    build selfDeclaration overview comp and inserted into overview template

--- prime-angular-frontend/src/app/modules/paper-enrolment/pages/self-declaration-page/self-declaration-overview.component.ts
+++ prime-angular-frontend/src/app/modules/paper-enrolment/pages/self-declaration-page/self-declaration-overview.component.ts
@@ -1,20 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { RouteUtils } from '@lib/utils/route-utils.class';
+
+import { PaperEnrolmentRoutes } from '@paper-enrolment/paper-enrolment.routes';
+import { SelfDeclarationForm } from './self-declaration-form.model';
 
 @Component({
   selector: 'app-self-declaration-overview',
   template: `
-    <p>
-      self-declaration-overview works!
-    </p>
+      <app-enrollee-self-declarations *ngIf="selfDeclarations?.selfDeclarations"
+                                      [enrolment]="selfDeclarations"
+                                      [showAllSelfDeclarationsQuestions]="true">
+        <button *ngIf="true"
+                mat-icon-button
+                matTooltip="Edit Self-declaration"
+                (click)="onRoute(PaperEnrolmentRoutes.SELF_DECLARATION)">
+          <mat-icon>edit</mat-icon>
+        </button>
+      </app-enrollee-self-declarations>
   `,
-  styles: [
-  ]
+  styles: [],
+  encapsulation: ViewEncapsulation.None
 })
 export class SelfDeclarationOverviewComponent implements OnInit {
+  @Input() public selfDeclarations: SelfDeclarationForm;
+  public PaperEnrolmentRoutes = PaperEnrolmentRoutes;
+  public routeUtils: RouteUtils;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.routeUtils = new RouteUtils(route, router, PaperEnrolmentRoutes.MODULE_PATH);
+  }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+  }
+
+  public onRoute(routePath: string | string[]) {
+    routePath = (Array.isArray(routePath)) ? routePath : [routePath];
+    this.routeUtils.routeRelativeTo(routePath);
   }
 
 }
