6421e68cb3d84a64983bc986e83fa5a33cefe4e2
Anais <hebert.anais@gmail.com>
2021-07-12 17:49:14 -0700

    uodates

--- prime-angular-frontend/src/app/modules/adjudication/pages/license-classes-maintenance-page/license-classes-maintenance-page.component.spec.ts
+++ prime-angular-frontend/src/app/modules/adjudication/pages/license-classes-maintenance-page/license-classes-maintenance-page.component.spec.ts
@@ -1,4 +1,10 @@
+import { AdjudicationModule } from '@adjudication/adjudication.module';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ConfigService } from '@config/config.service';
+import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
+import { MockConfigService } from 'test/mocks/mock-config.service';
 
 import { LicenseClassesMaintenancePageComponent } from './license-classes-maintenance-page.component';
 
@@ -8,9 +14,24 @@ describe('LicenseClassesMaintenancePageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LicenseClassesMaintenancePageComponent ]
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        AdjudicationModule
+      ],
+      declarations: [],
+      providers: [
+        {
+          provide: APP_CONFIG,
+          useValue: APP_DI_CONFIG
+        },
+        {
+          provide: ConfigService,
+          useValue: MockConfigService
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
--- prime-angular-frontend/src/app/modules/adjudication/pages/license-classes-maintenance-page/license-classes-maintenance-page.component.ts
+++ prime-angular-frontend/src/app/modules/adjudication/pages/license-classes-maintenance-page/license-classes-maintenance-page.component.ts
@@ -1,8 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-import { LicenseConfig } from '@config/config.model';
+import { CollegeConfig, CollegeLicenseConfig } from '@config/config.model';
 import { ConfigService } from '@config/config.service';
-
+import { PrescriberIdTypeEnum } from '@shared/enums/prescriber-id-type.enum';
+export class LicenseMaintenanceConfig {
+  collegeName: string;
+  licenseCode: number;
+  prefix: string;
+  namedInImReg?: boolean;
+  licensedToProvideCare?: boolean;
+  manual?: boolean;
+  validate?: boolean;
+  prescriberIdType?: PrescriberIdTypeEnum;
+};
 @Component({
   selector: 'app-license-classes-maintenance-page',
   templateUrl: './license-classes-maintenance-page.component.html',
@@ -10,29 +20,42 @@ import { ConfigService } from '@config/config.service';
 })
 export class LicenseClassesMaintenancePageComponent implements OnInit {
 
-  public dataSource: MatTableDataSource<LicenseConfig>;
+  public dataSource: MatTableDataSource<LicenseMaintenanceConfig>;
   public columns: string[];
+
+  PrescriberIdTypeEnum = PrescriberIdTypeEnum;
 
   constructor(
     private configService: ConfigService
   ) {
     this.columns = [
-      '',
-      'code',
-      // 'licenseClass',
-      // 'pracRefId',
-      // 'nimr',
-      // 'lpc',
-      // 'forceManual',
-      // 'validate',
-      // 'practiceId'
+      'collegeName',
+      'licenseCode',
+      'prefix',
+      'namedInImReg',
+      'licensedToProvideCare',
+      'manual',
+      'validate',
+      'prescriberIdType'
     ];
-    this.dataSource = new MatTableDataSource<LicenseConfig>([]);
+    this.dataSource = new MatTableDataSource<LicenseMaintenanceConfig>([]);
   }
 
   ngOnInit(): void {
-    console.log(this.configService.licenses);
-    this.dataSource.data = this.configService.licenses;
+    const licenceMaintenanceConfig = this.configService.colleges?.flatMap((college: CollegeConfig) => {
+      return college.collegeLicenses.length
+        ? college.collegeLicenses.map((collegeLicense: CollegeLicenseConfig) => {
+          const license = this.configService.licenses.find(l => l.code === collegeLicense.licenseCode);
+          return {
+            collegeName: college.name,
+            licenseCode: collegeLicense.licenseCode,
+            ...license
+          } as LicenseMaintenanceConfig;
+        })
+        : { collegeName: college.name } as LicenseMaintenanceConfig;
+    });
+
+    this.dataSource.data = licenceMaintenanceConfig;
   }
 
 }
