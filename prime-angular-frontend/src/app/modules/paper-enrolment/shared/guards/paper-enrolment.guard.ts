1a17c86fa206a73532c8b31dada91abcb50d9615
Martin Pultz <mtpultz@gmail.com>
2021-07-17 16:16:58 -0700

    Fix merge conflict with paper guard

--- prime-angular-frontend/src/app/modules/paper-enrolment/shared/guards/paper-enrolment.guard.ts
+++ prime-angular-frontend/src/app/modules/paper-enrolment/shared/guards/paper-enrolment.guard.ts
@@ -9,9 +9,8 @@ import { BaseGuard } from '@core/guards/base.guard';
 import { LoggerService } from '@core/services/logger.service';
 import { HttpEnrollee } from '@shared/models/enrolment.model';
 import { AuthService } from '@auth/shared/services/auth.service';
-import { PaperEnrolmentResource } from '@paper-enrolment/services/paper-enrolment-resource.service';
-import { PaperEnrolmentRoutes } from '@paper-enrolment/paper-enrolment.routes';
 
+import { PaperEnrolmentRoutes } from '@paper-enrolment/paper-enrolment.routes';
 import { PaperEnrolmentResource } from '@paper-enrolment/shared/services/paper-enrolment-resource.service';
 
 @Injectable({
