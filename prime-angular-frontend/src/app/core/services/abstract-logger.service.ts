628dff967464195d8849c5822a15033c0fa7d019
Nathan <Hogman>
2021-07-20 17:05:33 -0700

    remove unused code.

--- prime-angular-frontend/src/app/core/services/abstract-logger.service.ts
+++ prime-angular-frontend/src/app/core/services/abstract-logger.service.ts
@@ -9,14 +9,6 @@ export abstract class AbstractLoggerService {
 
   /**
    * @description
-   * General output of logging information.
-   */
-  public log(msg: string, ...data: any[]) {
-    this.send('log', { msg, data });
-  }
-
-  /**
-   * @description
    * Informative output of logging information.
    */
   public info(msg: string, ...data: any[]) {
@@ -36,14 +28,6 @@ export abstract class AbstractLoggerService {
    * Outputs an error message.
    */
   public error(msg: string, ...data: any[]) {
-    this.send('error', { msg, data });
-  }
-
-  /**
-   * @description
-   * Outputs a stack trace.
-   */
-  public trace(msg: string, ...data: any[]) {
     this.send('error', { msg, data });
   }
 
