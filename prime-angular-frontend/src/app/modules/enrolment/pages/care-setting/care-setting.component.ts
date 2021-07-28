ead5a6ac9123d81e49c90268225acaebc173d1be
Martin Pultz <mtpultz@gmail.com>
2021-07-28 02:59:43 -0700

    Updates for overview validation, and clean up

--- prime-angular-frontend/src/app/modules/enrolment/pages/care-setting/care-setting.component.ts
+++ prime-angular-frontend/src/app/modules/enrolment/pages/care-setting/care-setting.component.ts
@@ -211,7 +211,7 @@ export class CareSettingComponent extends BaseEnrolmentProfilePage implements On
   }
 
   protected nextRouteAfterSubmit() {
-    const oboSites = this.enrolmentFormStateService.jobsForm.get('oboSites').value as OboSite[];
+    const oboSites = this.enrolmentFormStateService.oboSitesForm.get('oboSites').value as OboSite[];
     const certifications = this.enrolmentFormStateService.regulatoryFormState.certifications;
 
     let nextRoutePath: string;
@@ -251,7 +251,7 @@ export class CareSettingComponent extends BaseEnrolmentProfilePage implements On
    * Remove obo sites by care setting if a care setting was removed from the enrolment
    */
   private removeOboSites(careSettingCode: number): void {
-    const form = this.enrolmentFormStateService.jobsForm;
+    const form = this.enrolmentFormStateService.oboSitesForm;
     const oboSites = form.get('oboSites') as FormArray;
 
     oboSites.value?.forEach((site: OboSite, index: number) => {
