282e9d1c2f01b02cb2d234c4cafd4c1b78354666
AnaisHebert <53796823+AnaisHebert@users.noreply.github.com>
2021-08-04 11:03:13 -0700

    PRIME-1756 Cleanup/Update Verifiable Credential Code (#1476)
    * Remove Enrollee Credential collection, get qrcode on button click
    
    * webhook data model
    
    * schema request/repsonse models, update docs
    
    * Moooar cleanup
    
    * Guids
    
    * Revert "Guids"
    
    This reverts commit 2fb5c7906581bdd83b0227f8d6bf210add451ff6.
    
    * remove String.Empty

--- prime-dotnet-webapi/Models/VerifiableCredentials/Credential.cs
+++ prime-dotnet-webapi/Models/VerifiableCredentials/Credential.cs
@@ -1,20 +1,36 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-namespace Prime.Models
+using Newtonsoft.Json;
+
+namespace Prime.Models.VerifiableCredentials
 {
     [Table("Credential")]
     public class Credential : BaseAuditable
     {
         [Key]
         public int Id { get; set; }
+
+        [JsonIgnore]
+        public int EnrolleeId { get; set; }
+
+        [JsonIgnore]
+        public Enrollee Enrollee { get; set; }
+
         public string ConnectionId { get; set; }
+
         public string SchemaId { get; set; }
+
         public string CredentialExchangeId { get; set; }
+
         public string CredentialDefinitionId { get; set; }
+
         public string Alias { get; set; }
+
         public string Base64QRCode { get; set; }
+
         public DateTimeOffset? AcceptedCredentialDate { get; set; }
+
         public DateTimeOffset? RevokedCredentialDate { get; set; }
     }
 }
