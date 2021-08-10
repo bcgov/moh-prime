7093c22932e21d584be708b782f60bbb6262708e
Martin Pultz <mtpultz@gmail.com>
2021-08-10 14:26:43 -0700

    Not even close to 3... 4 more to go... maybe

--- prime-angular-frontend/src/app/shared/components/paginator/paginator.component.spec.ts
+++ prime-angular-frontend/src/app/shared/components/paginator/paginator.component.spec.ts
@@ -1,10 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
 import { NgxMaterialModule } from '@lib/modules/ngx-material/ngx-material.module';
 import { PaginatorComponent } from './paginator.component';
+import { FormControlValidators } from '@lib/validators/form-control.validators';
 
 describe('PaginatorComponent', () => {
   let component: PaginatorComponent;
@@ -14,7 +16,8 @@ describe('PaginatorComponent', () => {
     await TestBed.configureTestingModule({
       imports: [
         ReactiveFormsModule,
-        NgxMaterialModule
+        NgxMaterialModule,
+        BrowserAnimationsModule
       ],
       declarations: [
         PaginatorComponent
@@ -32,6 +35,12 @@ describe('PaginatorComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PaginatorComponent);
     component = fixture.componentInstance;
+    component.form = new FormControl(1, [
+      Validators.required,
+      FormControlValidators.numeric,
+      Validators.min(1),
+      () => null
+    ]);
     fixture.detectChanges();
   });
 
