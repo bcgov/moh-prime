2ae6f1b9266f7cbe88e91233d785e425a6da7630
Martin Pultz <mtpultz@gmail.com>
2021-08-10 10:05:05 -0700

    Update event more ^ 2 tests

--- prime-angular-frontend/src/app/lib/modules/root-routes/components/page-simple/page-simple.component.spec.ts
+++ prime-angular-frontend/src/app/lib/modules/root-routes/components/page-simple/page-simple.component.spec.ts
@@ -1,5 +1,7 @@
 import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
+import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
 import { RootRoutesModule } from '../../root-routes.module';
 import { PageSimpleComponent } from './page-simple.component';
 
@@ -13,7 +15,14 @@ describe('PageSimpleComponent', () => {
         imports: [
           RootRoutesModule
         ],
-        declarations: []
+        declarations: [],
+        providers: [
+          {
+            provide: APP_CONFIG,
+            useValue: APP_DI_CONFIG
+          }
+        ],
+        schemas: [NO_ERRORS_SCHEMA]
       }
     ).compileComponents();
   }));
--- prime-angular-frontend/src/app/modules/paper-enrolment/pages/demographic-page/demographic-overview.component.spec.ts
+++ prime-angular-frontend/src/app/modules/paper-enrolment/pages/demographic-page/demographic-overview.component.spec.ts
@@ -3,6 +3,9 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { DefaultPipe } from '@shared/pipes/default.pipe';
+import { ReplacePipe } from '@shared/pipes/replace.pipe';
+import { FormatDatePipe } from '@shared/pipes/format-date.pipe';
+import { PhonePipe } from '@shared/pipes/phone.pipe';
 import { DemographicOverviewComponent } from './demographic-overview.component';
 
 describe('DemographicOverviewComponent', () => {
@@ -15,14 +18,15 @@ describe('DemographicOverviewComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        DemographicOverviewComponent
+        DemographicOverviewComponent,
+        DefaultPipe,
+        ReplacePipe,
+        FormatDatePipe,
+        PhonePipe
       ],
-      providers: [
-        DefaultPipe
-      ],
+      providers: [],
       schemas: [NO_ERRORS_SCHEMA]
-    })
-    .compileComponents();
+    }).compileComponents();
   });
 
   beforeEach(() => {
