4b76815df955e466f4bcf7fa686d0057800ec987
Alan Leung <Alan.Leung@NTTDATA.COM>
2021-08-13 15:40:43 -0700

    Added typing, comments

--- prime-angular-frontend/src/app/modules/adjudication/shared/components/health-authority-table/health-authority-table.component.ts
+++ prime-angular-frontend/src/app/modules/adjudication/shared/components/health-authority-table/health-authority-table.component.ts
@@ -3,12 +3,11 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute, Router } from '@angular/router';
 
-import { Config } from '@config/config.model';
 import { ConfigService } from '@config/config.service';
 import { ArrayUtils } from '@lib/utils/array-utils.class';
 import { HealthAuthorityResource } from '@core/resources/health-authority-resource.service';
 import { HealthAuthorityEnum } from '@shared/enums/health-authority.enum';
-import { HealthAuthorityList } from '@shared/models/health-authority-list.model';
+import { HealthAuthorityListItem } from '@shared/models/health-authority-list.model';
 import { Role } from '@auth/shared/enum/role.enum';
 import { exhaustMap, mergeMap } from 'rxjs/operators';
 import { from } from 'rxjs';
@@ -24,17 +23,16 @@ export class HealthAuthorityTableComponent implements OnInit {
   @Output() public route: EventEmitter<string | (string | number)[]>;
 
   public siteColumns: string[];
-  public dataSource: MatTableDataSource<HealthAuthorityList | HealthAuthoritySite>;
+  public dataSource: MatTableDataSource<HealthAuthorityListItem | HealthAuthoritySite>;
   public healthAuthorityCode: HealthAuthorityEnum;
   public flaggedHealthAuthorities: HealthAuthorityEnum[];
   public Role = Role;
   public AdjudicationRoutes = AdjudicationRoutes;
   public SiteStatusType = SiteStatusType;
+  public idOfExpandedHA: number;
 
 
   constructor(
-    private configService: ConfigService,
-    private router: Router,
     private activatedRoute: ActivatedRoute,
     private healthAuthorityResource: HealthAuthorityResource
   ) {
@@ -50,19 +48,15 @@ export class HealthAuthorityTableComponent implements OnInit {
       'siteActions'
     ];
     this.route = new EventEmitter<string | (string | number)[]>();
-    this.dataSource = new MatTableDataSource<HealthAuthorityList | HealthAuthoritySite>([]);
+    this.dataSource = new MatTableDataSource<HealthAuthorityListItem | HealthAuthoritySite>([]);
     this.healthAuthorityCode = this.activatedRoute.snapshot.params.haid;
+    // No HA headers should be expanded initially
+    this.idOfExpandedHA = -1;
   }
 
   public onRoute(routePath: string | (string | number)[]) {
     this.route.emit(routePath);
   }
-
-
-  // public showSites(healthAuthorityId: number) {
-  //   this.sitesDataSource.data = this.healthAuthorityIdToSites.get(healthAuthorityId);
-  // }
-
 
   // TODO: Eliminate duplication as this method copied from SiteRegistrationTableComponent
   public displayStatus(status: SiteStatusType) {
@@ -87,80 +81,72 @@ export class HealthAuthorityTableComponent implements OnInit {
       : count;
   }
 
-
-  idOfExpandedHA = -1;
-
-
-  static isHA(obj): boolean {
-    return obj.hasUnderReviewUsers !== undefined;
+  public isHealthAuthorityObject(rowData): boolean {
+    return rowData.hasUnderReviewUsers !== undefined;
   }
 
-  isHealthAuthorityObject(obj): boolean {
-    return obj.hasUnderReviewUsers !== undefined;
+  public isGroup(index: number, rowData: HealthAuthorityListItem | HealthAuthoritySite): boolean {
+    return HealthAuthorityTableComponent.isHA(rowData);
   }
 
-
-  isGroup(index, item): boolean {
-    // TODO: always work?
-    //    return !item.siteId;
-    return HealthAuthorityTableComponent.isHA(item);
-  }
-
-  expand(item: HealthAuthorityList) {
+  public expandHeader(item: HealthAuthorityListItem) {
     this.idOfExpandedHA =
       this.idOfExpandedHA !== item.id ? item.id : -1;
   }
 
-
-  sortData(a: HealthAuthorityList | HealthAuthoritySite, b: HealthAuthorityList | HealthAuthoritySite): number {
-    if (HealthAuthorityTableComponent.isHA(a) && HealthAuthorityTableComponent.isHA(b)) {
-      return a.id - b.id;
-    }
-    else if (HealthAuthorityTableComponent.isHA(a)) {
-      if ((a as HealthAuthorityList).id === (b as HealthAuthoritySite).healthAuthorityOrganizationId) {
-        return -1;
-      } else {
-        return (a as HealthAuthorityList).id - (b as HealthAuthoritySite).healthAuthorityOrganizationId;
-      }
-    }
-    else if (HealthAuthorityTableComponent.isHA(b)) {
-      if ((b as HealthAuthorityList).id === (a as HealthAuthoritySite).healthAuthorityOrganizationId) {
-        return 1;
-      } else {
-        return (a as HealthAuthoritySite).healthAuthorityOrganizationId - (b as HealthAuthorityList).id;
-      }
-    }
-    else {
-      return (a as HealthAuthoritySite).healthAuthorityOrganizationId - (b as HealthAuthoritySite).healthAuthorityOrganizationId;
-    }
-  }
-
-
-
   public ngOnInit(): void {
     this.healthAuthorityResource.getHealthAuthorities()
       .pipe(
-        exhaustMap((healthAuthorities: HealthAuthorityList[]) => {
-          this.flaggedHealthAuthorities = healthAuthorities.reduce((fhas: number[], ha: HealthAuthorityList) =>
+        exhaustMap((healthAuthorities: HealthAuthorityListItem[]) => {
+          this.flaggedHealthAuthorities = healthAuthorities.reduce((fhas: number[], ha: HealthAuthorityListItem) =>
             [...fhas, ...ArrayUtils.insertIf(ha.hasUnderReviewUsers, ha.id)], []
           );
 
           this.dataSource.data = (this.healthAuthorityCode)
             ? healthAuthorities.filter(ha => ha.id === +this.healthAuthorityCode)
-            //            : healthAuthorities.sort((a, b) => a.id - b.id);
             : healthAuthorities.sort(this.sortData);
           return from(healthAuthorities);
         }),
-        // TODO: Rename type HealthAuthorityList to HealthAuthorityListItem?
         // As each healthAuthority is emitted, get the sites registered with that healthAuthority
-        mergeMap((healthAuthority: HealthAuthorityList) => this.healthAuthorityResource.getHealthAuthoritySites(healthAuthority.id))
+        mergeMap((healthAuthority: HealthAuthorityListItem) => this.healthAuthorityResource.getHealthAuthoritySites(healthAuthority.id))
       )
       .subscribe((sites: HealthAuthoritySite[]) => {
         // For each list of sites in stream, store in map if non-empty list
-        // TODO: Problem with simultaneous set to `data` field?
         if (sites && sites.length > 0) {
-          this.dataSource.data = [...sites, ...this.dataSource.data].sort(this.sortData);
+          this.dataSource.data = [...this.dataSource.data, ...sites].sort(this.sortData);
         }
       });
+  }
+
+  private static isHA(obj): boolean {
+    return obj.hasUnderReviewUsers !== undefined;
+  }
+
+  /**
+   * @description
+   * Compare function for sorting that intends to sort Health Authorities in ascending order by their ID,
+   * and group Site Registrations immediately following each related Health Authority.
+   */
+  private sortData(a: HealthAuthorityListItem | HealthAuthoritySite, b: HealthAuthorityListItem | HealthAuthoritySite): number {
+    if (HealthAuthorityTableComponent.isHA(a) && HealthAuthorityTableComponent.isHA(b)) {
+      return a.id - b.id;
+    }
+    else if (HealthAuthorityTableComponent.isHA(a)) {
+      if ((a as HealthAuthorityListItem).id === (b as HealthAuthoritySite).healthAuthorityOrganizationId) {
+        return -1;
+      } else {
+        return (a as HealthAuthorityListItem).id - (b as HealthAuthoritySite).healthAuthorityOrganizationId;
+      }
+    }
+    else if (HealthAuthorityTableComponent.isHA(b)) {
+      if ((b as HealthAuthorityListItem).id === (a as HealthAuthoritySite).healthAuthorityOrganizationId) {
+        return 1;
+      } else {
+        return (a as HealthAuthoritySite).healthAuthorityOrganizationId - (b as HealthAuthorityListItem).id;
+      }
+    }
+    else {
+      return (a as HealthAuthoritySite).healthAuthorityOrganizationId - (b as HealthAuthoritySite).healthAuthorityOrganizationId;
+    }
   }
 }
