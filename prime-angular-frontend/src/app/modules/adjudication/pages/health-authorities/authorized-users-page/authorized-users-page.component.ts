8ecc1c60b0392e521abb64c271e1058833349959
Nathan <Hogman>
2021-08-16 13:33:03 -0700

    Adjust AU page

--- prime-angular-frontend/src/app/modules/adjudication/pages/health-authorities/authorized-users-page/authorized-users-page.component.ts
+++ prime-angular-frontend/src/app/modules/adjudication/pages/health-authorities/authorized-users-page/authorized-users-page.component.ts
@@ -1,6 +1,7 @@
 import { AdjudicationRoutes } from '@adjudication/adjudication.routes';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ConfigService } from '@config/config.service';
 import { RouteUtils } from '@lib/utils/route-utils.class';
 import { Subscription } from 'rxjs';
 
@@ -11,13 +12,16 @@ import { Subscription } from 'rxjs';
 })
 export class AuthorizedUsersPageComponent implements OnInit {
   public busy: Subscription;
+  public haName: string;
 
   private routeUtils: RouteUtils;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
+    private configService: ConfigService,
   ) {
+    this.haName = this.configService.healthAuthorities.find(ha => ha.code === this.route.snapshot.params.haid)?.name;
     this.routeUtils = new RouteUtils(route, router, AdjudicationRoutes.routePath(AdjudicationRoutes.SITE_REGISTRATIONS));
   }
 
