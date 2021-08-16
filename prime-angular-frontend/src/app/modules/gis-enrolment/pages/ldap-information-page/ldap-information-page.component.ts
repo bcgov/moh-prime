464eb135dfbcba9e1f19c7b5581d97e0e2f35f24
Panos <paahndev@gmail.com>
2021-08-16 13:38:13 -0700

    hard coded implementation of wrong attempts countdown

--- prime-angular-frontend/src/app/modules/gis-enrolment/pages/ldap-information-page/ldap-information-page.component.ts
+++ prime-angular-frontend/src/app/modules/gis-enrolment/pages/ldap-information-page/ldap-information-page.component.ts
@@ -3,7 +3,7 @@ import { FormControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 
-import { exhaustMap } from 'rxjs/operators';
+import { exhaustMap, catchError } from 'rxjs/operators';
 
 import { RouteUtils } from '@lib/utils/route-utils.class';
 import { AbstractEnrolmentPage } from '@lib/classes/abstract-enrolment-page.class';
@@ -23,6 +23,8 @@ import { LdapInformationPageFormState } from './ldap-information-page-form-state
 export class LdapInformationPageComponent extends AbstractEnrolmentPage implements OnInit {
   public title: string;
   public formState: LdapInformationPageFormState;
+  public remainingAttempts: number;
+  public lockoutTimeInHours: number;
 
   private routeUtils: RouteUtils;
 
@@ -36,13 +38,14 @@ export class LdapInformationPageComponent extends AbstractEnrolmentPage implemen
     router: Router,
   ) {
     super(dialog, formUtilsService);
-
+    this.remainingAttempts = 4;
+    this.lockoutTimeInHours = 1;
     this.title = route.snapshot.data.title;
     this.routeUtils = new RouteUtils(route, router, GisEnrolmentRoutes.routePath(GisEnrolmentRoutes.MODULE_PATH));
   }
 
   public onBack() {
-    this.routeUtils.routeRelativeTo([`./${ GisEnrolmentRoutes.LDAP_USER_PAGE }`]);
+    this.routeUtils.routeRelativeTo([`./${GisEnrolmentRoutes.LDAP_USER_PAGE}`]);
   }
 
   public ngOnInit(): void {
@@ -63,13 +66,19 @@ export class LdapInformationPageComponent extends AbstractEnrolmentPage implemen
   protected performSubmission(): NoContent {
     return this.gisEnrolmentResource.ldapLogin(this.gisEnrolmentService.enrolment.id, this.formState.credentials)
       .pipe(
-        exhaustMap(() => this.gisEnrolmentResource.updateEnrolment(this.formStateService.json))
-      );
+        exhaustMap(() => this.gisEnrolmentResource.updateEnrolment(this.formStateService.json)),
+        catchError((error: any) => {
+          if (error.status === 401) {
+            this.remainingAttempts -= 1;
+          }
+          throw error;
+        })
+      )
   }
 
   protected afterSubmitIsSuccessful(): void {
     // Don't want the password around any longer than needed
     this.formState.clearPassword();
-    this.routeUtils.routeRelativeTo([`./${ GisEnrolmentRoutes.ORG_INFO_PAGE }`]);
+    this.routeUtils.routeRelativeTo([`./${GisEnrolmentRoutes.ORG_INFO_PAGE}`]);
   }
 }
