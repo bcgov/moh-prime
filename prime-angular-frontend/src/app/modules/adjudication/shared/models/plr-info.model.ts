e1fcb1522c328b3f2777d912b2051da44e6a8b69
Johnny Liu <58282608+johnnyl6@users.noreply.github.com>
2021-08-17 22:07:29 -0700

    PRIME-1697 PLR Information (#1493)
    * added BE method to return PLR data
    
    * added plf-info card component
    
    * added plr view model for FE
    
    * added lookup tables for PlrViewModel
    
    * fixed PlrViewModel mapping using workaround
    
    - flattened expertise in FE
    - other minor fixes
    
    * fixed typo
    
    * removed old code
    
    * fixed PlrViewModel mapping
    
    * PR feedback work
    
    - added foreignkey (PlrRoleType, PlrStatusReason) on PlrProvider model
    
    * reverted FK on PLR table
    
    * PR feedback
    
    Co-authored-by: AnaisHebert <53796823+AnaisHebert@users.noreply.github.com>

---
+++ prime-angular-frontend/src/app/modules/adjudication/shared/models/plr-info.model.ts
@@ -0,0 +1,16 @@
+export interface PlrInfo {
+  id: number;
+  identifierType: string;
+  collegeId: string;
+  providerRoleType: string;
+  namePrefix: string;
+  firstName: string;
+  secondName: string;
+  thirdName: string;
+  lastName: string;
+  statusCode: string;
+  statusReasonCode: string;
+  expertise: string;
+  updatedTimeStamp: string;
+  statusStartDate: string;
+}
