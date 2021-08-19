124301d08f3b146131433f31eddce86352c67862
Alan Leung <Alan.Leung@NTTDATA.COM>
2021-08-19 14:31:02 -0700

    Try external web service #3

--- prime-dotnet-webapi/HttpClients/PRIME-ODR API/PrimeOdrClient.cs
+++ prime-dotnet-webapi/HttpClients/PRIME-ODR API/PrimeOdrClient.cs
@@ -4,6 +4,7 @@ using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Prime.Models;
+using Flurl;
 
 namespace Prime.HttpClients
 {
@@ -22,13 +23,19 @@ namespace Prime.HttpClients
 
         public async Task<(List<PharmanetTransactionLog> Logs, bool ExistsMoreLogs)> RetrieveLatestPharmanetTxLogsAsync()
         {
-            _logger.LogDebug(PrimeEnvironment.PrimeOdrApi.Url);
-            _logger.LogDebug(PrimeEnvironment.PrimeOdrApi.Username);
-            _logger.LogDebug(PrimeEnvironment.PrimeOdrApi.SslCertFilename);
+            _logger.LogInformation(PrimeEnvironment.PrimeOdrApi.Url);
+            _logger.LogInformation(PrimeEnvironment.PrimeOdrApi.Username);
+            _logger.LogInformation(PrimeEnvironment.PrimeOdrApi.SslCertFilename);
 
             // Auth header and cert are configured to be injected in Startup.cs
-            var response = await _client.GetAsync(PrimeEnvironment.PrimeOdrApi.Url);
-            _logger.LogCritical(await response.Content.ReadAsStringAsync());
+            var response = await _client.GetAsync(PrimeEnvironment.PrimeOdrApi.Url.SetQueryParams(
+                new
+                {
+                    requestUUID = System.Guid.NewGuid().ToString(),
+                    clientName = "PRIME-dev",
+                    lastTxnId = 0,
+                }));
+            _logger.LogInformation(await response.Content.ReadAsStringAsync());
 
             return (null, false);
         }
