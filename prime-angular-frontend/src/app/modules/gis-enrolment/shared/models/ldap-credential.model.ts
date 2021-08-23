0b18f5a57d8254affdf7d3648d2b5fa43d5e6c82
Panos <paahndev@gmail.com>
2021-08-23 10:22:57 -0700

    interface to define throttling parameters

--- prime-angular-frontend/src/app/modules/gis-enrolment/shared/models/ldap-credential.model.ts
+++ prime-angular-frontend/src/app/modules/gis-enrolment/shared/models/ldap-credential.model.ts
@@ -2,3 +2,8 @@ export interface LdapCredential {
   ldapUsername: string;
   ldapPassword: string;
 }
+
+export interface LdapThrottlingParameters {
+  remainingAttempts: number;
+  lockoutTimeInHours: number;
+}
