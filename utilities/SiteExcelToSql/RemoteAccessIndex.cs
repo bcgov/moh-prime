4fc78ec69426c487a670889001d796d39b52b999
Johnny Liu <58282608+johnnyl6@users.noreply.github.com>
2021-08-26 13:42:44 -0700

    PRIME-1582 Convert Excel Document to SQL for loading into PRIME PROD DB (#1292)
    * init commit
    
    * Fix
    
    - template sql change
    - bug fix
    
    * added readme
    
    * check args
    
    * added dummy userId for Party
    
    * readme
    
    * removed .sln file
    
    * fixed typo
    
    * more notes in README file
    
    * PR review fix
    
    - fixed index offset in output log
    
    * added CareSettingCode for site data
    
    - default to private community practice 2
    
    Co-authored-by: Martin Pultz <mtpultz@users.noreply.github.com>

---
+++ utilities/SiteExcelToSql/RemoteAccessIndex.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiteExcelToSql
+{
+    public enum RemoteAccessIndex
+    {
+        LastName = 0,
+        InternetProvider,
+        StreetAddress,
+        City,
+        PostalCode,
+        MaxImage,
+        LanDrive,
+        Comments
+    }
+}
