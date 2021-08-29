c5b4c01d537c0c279ba5d7e4e0bd8e244da14bc8
Alan Leung <Alan.Leung@NTTDATA.COM>
2021-08-29 16:16:53 -0700

    Simple test

---
+++ infrastructure/openshift/retrieve-pnet-logs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+main() {
+
+  echo -e "-------- STARTING CRON --------\n"
+
+
+}
+
+main  # Ensure the whole file is downloaded before executing
