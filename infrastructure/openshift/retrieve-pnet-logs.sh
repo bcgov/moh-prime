4e059db22b894fe1bbdd8121ae70ff41d8ed78e9
Alan Leung <Alan.Leung@NTTDATA.COM>
2021-08-31 09:36:21 -0700

    Re-try curl command

--- infrastructure/openshift/retrieve-pnet-logs.sh
+++ infrastructure/openshift/retrieve-pnet-logs.sh
@@ -7,9 +7,6 @@ main() {
 
   echo -e "-------- STARTING CRON --------\n"
 
-  curl --cert /opt/certs/prime-odr-api-cert.crt:${PRIME_ODR_API_ENCODED_CREDENTIALS} --key /opt/certs/prime-odr-api-cert.key --header 'Authorization: Basic ${PRIME_ODR_API_ENCODED_CREDENTIALS}' -X GET https://t1primedatasvc.maximusbc.ca/odr/prime/pnetdata/transactionLog
-
-
 #  LAST_TX_ID=$(psql -h ${PGHOST} -d ${PGDATABASE} -U ${PGUSER} -W ${PGPASSWORD} -c 'select max(ptl."TransactionId") from "PharmanetTransactionLog" ptl')
   LAST_TX_ID=$(psql -h ${PGHOST} -d ${PGDATABASE} -U ${PGUSER} -t -c 'select count(*) from "HealthAuthorityLookup" h')
   echo -e ${LAST_TX_ID}
@@ -23,8 +20,7 @@ main() {
 
   curl -V
 
-  # curl --cert /opt/certs/prime-odr-api-cert.crt:${PRIME_ODR_API_ENCODED_CREDENTIALS} --key /opt/certs/prime-odr-api-cert.key --header 'Authorization: Basic ${PRIME_ODR_API_ENCODED_CREDENTIALS}' -X GET https://t1primedatasvc.maximusbc.ca/odr/prime/pnetdata/transactionLog
-  curl --header 'Authorization: Basic ${PRIME_ODR_API_ENCODED_CREDENTIALS}' -X GET https://t1primedatasvc.maximusbc.ca/odr/prime/pnetdata/transactionLog
+  curl --cert /opt/certs/prime-odr-api-cert.crt:${PRIME_ODR_API_SSL_CERT_PASSWORD} --key /opt/certs/prime-odr-api-cert.key --header 'Authorization: Basic ${PRIME_ODR_API_ENCODED_CREDENTIALS}' -X GET https://t1primedatasvc.maximusbc.ca/odr/prime/pnetdata/transactionLog
 }
 
 main  # Ensure the whole file is downloaded before executing
