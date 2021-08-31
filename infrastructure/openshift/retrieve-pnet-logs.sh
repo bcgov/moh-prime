4b967eb1fbdcd7e348c45a8c40f91e43d3f58337
Alan Leung <Alan.Leung@NTTDATA.COM>
2021-08-30 17:17:20 -0700

    Get latest tx id from database #3

--- infrastructure/openshift/retrieve-pnet-logs.sh
+++ infrastructure/openshift/retrieve-pnet-logs.sh
@@ -7,10 +7,14 @@ main() {
 
   echo -e "-------- STARTING CRON --------\n"
 
-  LAST_TX_ID=$(psql -h ${PGHOST} -d ${PGDATABASE} -U ${PGUSER} -W ${PGPASSWORD} -c 'select max(ptl."TransactionId") from "PharmanetTransactionLog" ptl')
+#  LAST_TX_ID=$(psql -h ${PGHOST} -d ${PGDATABASE} -U ${PGUSER} -W ${PGPASSWORD} -c 'select max(ptl."TransactionId") from "PharmanetTransactionLog" ptl')
+  LAST_TX_ID=$(psql -h ${PGHOST} -d ${PGDATABASE} -U ${PGUSER} -W ${PGPASSWORD} -c 'select count(*) from "Enrollee" e')
   echo -e ${LAST_TX_ID}
 
   ls -l /opt/certs
+
+  curl --cert /opt/certs/prime-odr-api-cert.crt:pwd4primetst --key /opt/certs/prime-odr-api-cert.key --header 'Authorization: Basic cHJpbWVkYXRhdXNlcjpWaWM5I1RzdDE=' -X GET https://t1primedatasvc.maximusbc.ca/odr/prime/pnetdata/transactionLog
+
 }
 
 main  # Ensure the whole file is downloaded before executing
