cb496f3a383eb2fefcdaada526d2d2cb85fd0698
Alan Leung <Alan.Leung@NTTDATA.COM>
2021-08-31 16:42:55 -0700

    Clean up script

--- infrastructure/openshift/retrieve-pnet-logs.sh
+++ infrastructure/openshift/retrieve-pnet-logs.sh
@@ -9,19 +9,16 @@ main() {
 
 #  LAST_TX_ID=$(psql -h ${PGHOST} -d ${PGDATABASE} -U ${PGUSER} -W ${PGPASSWORD} -c 'select max(ptl."TransactionId") from "PharmanetTransactionLog" ptl')
   LAST_TX_ID=$(psql -h ${PGHOST} -d ${PGDATABASE} -U ${PGUSER} -t -c 'select count(*) from "HealthAuthorityLookup" h')
-  echo -e ${LAST_TX_ID}
+  echo -e "Last transaction id:  ${LAST_TX_ID}\n"
 
   UUID=$(cat /proc/sys/kernel/random/uuid)
-  echo -e ${UUID}
+  echo -e "Generated request id:  ${UUID}\n"
+
+  echo -e "API client name:  ${PRIME_ODR_API_CLIENT_NAME}\n"
 
   # ls -l /etc/ssl/certs
-
-  echo ${PRIME_ODR_API_ENCODED_CREDENTIALS}
-
   curl -v --cert /opt/certs/prime-odr-api-cert.crt:${PRIME_ODR_API_SSL_CERT_PASSWORD} --key /opt/certs/prime-odr-api-cert.key --header "Authorization: Basic ${PRIME_ODR_API_ENCODED_CREDENTIALS}" \
-    -X GET https://t1primedatasvc.maximusbc.ca/odr/prime/pnetdata/transactionLog?requestUUID=
-
-  python3 --version
+    -X GET https://t1primedatasvc.maximusbc.ca/odr/prime/pnetdata/transactionLog?requestUUID=${UUID}
 }
 
 main  # Ensure the whole file is downloaded before executing
