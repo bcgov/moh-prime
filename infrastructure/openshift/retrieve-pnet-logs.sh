bb2689288a774c485b36b8da00771fe2f4a2c963
Alan Leung <Alan.Leung@NTTDATA.COM>
2021-08-31 12:52:18 -0700

    Re-try curl command #2

--- infrastructure/openshift/retrieve-pnet-logs.sh
+++ infrastructure/openshift/retrieve-pnet-logs.sh
@@ -11,14 +11,14 @@ main() {
   LAST_TX_ID=$(psql -h ${PGHOST} -d ${PGDATABASE} -U ${PGUSER} -t -c 'select count(*) from "HealthAuthorityLookup" h')
   echo -e ${LAST_TX_ID}
 
-  ls -l /etc/ssl/certs
+  # ls -l /etc/ssl/certs
 
   sha256sum /opt/certs/prime-odr-api-cert.crt
   sha256sum /opt/certs/prime-odr-api-cert.key
 
   echo ${PRIME_ODR_API_ENCODED_CREDENTIALS}
 
-  curl --cert /opt/certs/prime-odr-api-cert.crt:${PRIME_ODR_API_SSL_CERT_PASSWORD} --key /opt/certs/prime-odr-api-cert.key --header 'Authorization: Basic ${PRIME_ODR_API_ENCODED_CREDENTIALS}' -X GET https://t1primedatasvc.maximusbc.ca/odr/prime/pnetdata/transactionLog
+  curl -v --cert /opt/certs/prime-odr-api-cert.crt:${PRIME_ODR_API_SSL_CERT_PASSWORD} --key /opt/certs/prime-odr-api-cert.key --header 'Authorization: Basic ${PRIME_ODR_API_ENCODED_CREDENTIALS}' -X GET https://t1primedatasvc.maximusbc.ca/odr/prime/pnetdata/transactionLog
 
   python3 --version
 }
