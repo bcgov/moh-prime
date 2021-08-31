777ca2b7a59a2aebb213bf3a93f6817cf82817c5
Alan Leung <Alan.Leung@NTTDATA.COM>
2021-08-31 10:21:33 -0700

    Disable `set -e`

--- infrastructure/openshift/retrieve-pnet-logs.sh
+++ infrastructure/openshift/retrieve-pnet-logs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# Exit immediately if a command exits with a non-zero status.
-set -e
+# TODO: Exit immediately if a command exits with a non-zero status.
+# set -e
 
 main() {
 
@@ -16,11 +16,11 @@ main() {
   sha256sum /opt/certs/prime-odr-api-cert.crt
   sha256sum /opt/certs/prime-odr-api-cert.key
 
-  python --version
-
   curl -V
 
   curl --cert /opt/certs/prime-odr-api-cert.crt:${PRIME_ODR_API_SSL_CERT_PASSWORD} --key /opt/certs/prime-odr-api-cert.key --header 'Authorization: Basic ${PRIME_ODR_API_ENCODED_CREDENTIALS}' -X GET https://t1primedatasvc.maximusbc.ca/odr/prime/pnetdata/transactionLog
+
+  python --version
 }
 
 main  # Ensure the whole file is downloaded before executing
