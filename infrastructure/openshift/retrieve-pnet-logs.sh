592435847154b796a77ce7025267ee4877908112
Alan Leung <Alan.Leung@NTTDATA.COM>
2021-08-30 17:08:02 -0700

    Get latest tx id from database #2

--- infrastructure/openshift/retrieve-pnet-logs.sh
+++ infrastructure/openshift/retrieve-pnet-logs.sh
@@ -7,7 +7,7 @@ main() {
 
   echo -e "-------- STARTING CRON --------\n"
 
-  LAST_TX_ID=psql -h ${PGHOST} -d ${PGDATABASE} -U ${PGUSER} -W ${PGPASSWORD} -c 'select max(ptl."TransactionId") from "PharmanetTransactionLog" ptl'
+  LAST_TX_ID=$(psql -h ${PGHOST} -d ${PGDATABASE} -U ${PGUSER} -W ${PGPASSWORD} -c 'select max(ptl."TransactionId") from "PharmanetTransactionLog" ptl')
   echo -e ${LAST_TX_ID}
 
   ls -l /opt/certs
