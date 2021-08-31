cf51db5ff016b779240c4564030c78bc32f4c047
Alan Leung <Alan.Leung@NTTDATA.COM>
2021-08-31 16:48:00 -0700

    Adding querystring parameters

--- infrastructure/openshift/retrieve-pnet-logs.sh
+++ infrastructure/openshift/retrieve-pnet-logs.sh
@@ -18,7 +18,7 @@ main() {
 
   # ls -l /etc/ssl/certs
   curl -v --cert /opt/certs/prime-odr-api-cert.crt:${PRIME_ODR_API_SSL_CERT_PASSWORD} --key /opt/certs/prime-odr-api-cert.key --header "Authorization: Basic ${PRIME_ODR_API_ENCODED_CREDENTIALS}" \
-    -X GET https://t1primedatasvc.maximusbc.ca/odr/prime/pnetdata/transactionLog?requestUUID=${UUID}
+    -X GET https://t1primedatasvc.maximusbc.ca/odr/prime/pnetdata/transactionLog?requestUUID=${UUID}&clientName=${PRIME_ODR_API_CLIENT_NAME}&lastTxnId=${LAST_TX_ID}&fetchSize=${PRIME_ODR_API_FETCH_SIZE}
 }
 
 main  # Ensure the whole file is downloaded before executing
