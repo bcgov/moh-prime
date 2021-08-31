e98009b003fca2bdaf0ba36a0e859f5721431bef
Alan Leung <Alan.Leung@NTTDATA.COM>
2021-08-31 08:08:48 -0700

    Debug client cert problems #3

--- infrastructure/openshift/retrieve-pnet-logs.sh
+++ infrastructure/openshift/retrieve-pnet-logs.sh
@@ -7,6 +7,9 @@ main() {
 
   echo -e "-------- STARTING CRON --------\n"
 
+  curl --cert /opt/certs/prime-odr-api-cert.crt:${PRIME_ODR_API_ENCODED_CREDENTIALS} --key /opt/certs/prime-odr-api-cert.key --header 'Authorization: Basic ${PRIME_ODR_API_ENCODED_CREDENTIALS}' -X GET https://t1primedatasvc.maximusbc.ca/odr/prime/pnetdata/transactionLog
+
+
 #  LAST_TX_ID=$(psql -h ${PGHOST} -d ${PGDATABASE} -U ${PGUSER} -W ${PGPASSWORD} -c 'select max(ptl."TransactionId") from "PharmanetTransactionLog" ptl')
   LAST_TX_ID=$(psql -h ${PGHOST} -d ${PGDATABASE} -U ${PGUSER} -t -c 'select count(*) from "HealthAuthorityLookup" h')
   echo -e ${LAST_TX_ID}
