45e626446befcdf3dae4750101aaea8fe863d5c5
Alan Leung <Alan.Leung@NTTDATA.COM>
2021-09-01 12:29:19 -0700

    Write out meta-data

--- infrastructure/openshift/parse_api_response.py
+++ infrastructure/openshift/parse_api_response.py
@@ -3,8 +3,11 @@ import sys, json, csv;
 json_as_dict = json.load(sys.stdin);
 
 output = csv.writer(sys.stdout);
-
+# Convert JSON to CSV
 for row in json_as_dict['pnetTransactions']:
     output.writerow(row.values());
 
-
+# Let external process know whether there are more results according to JSON response
+f = open("/tmp/isThereMoreData.txt", "w")
+f.write(json_as_dict['isThereMoreData'])
+f.close()
