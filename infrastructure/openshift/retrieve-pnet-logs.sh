94a8dfd6a45bfe528c49ca001d9e36d50db7284c
Alan Leung <Alan.Leung@NTTDATA.COM>
2021-09-01 13:38:38 -0700

    Mount Python script

--- infrastructure/openshift/retrieve-pnet-logs.sh
+++ infrastructure/openshift/retrieve-pnet-logs.sh
@@ -21,15 +21,17 @@ main() {
   echo -e "API client name:  _${PRIME_ODR_API_CLIENT_NAME}_\n"
   echo -e "Fetch size:  _${PRIME_ODR_API_FETCH_SIZE}_\n"
 
-  PROCESS_JSON_SCRIPT=$(curl -s "https://raw.githubusercontent.com/bcgov/moh-prime/8553e5cec508e8454d7661f4b8a6d255afc7cea5/infrastructure/openshift/parse_api_response.py")
-  echo -e "${PROCESS_JSON_SCRIPT}\n"
+  # PROCESS_JSON_SCRIPT=$(curl -s "https://raw.githubusercontent.com/bcgov/moh-prime/8553e5cec508e8454d7661f4b8a6d255afc7cea5/infrastructure/openshift/parse_api_response.py")
+  # echo -e "${PROCESS_JSON_SCRIPT}\n"
 
   # CA certs need to be in place:  https://stackoverflow.com/questions/3160909/how-do-i-deal-with-certificates-using-curl-while-trying-to-access-an-https-url
   # ls -l /etc/ssl/certs
   # curl --cert /opt/certs/prime-odr-api-cert.crt:${PRIME_ODR_API_SSL_CERT_PASSWORD} --key /opt/certs/prime-odr-api-cert.key --header "Authorization: Basic ${PRIME_ODR_API_ENCODED_CREDENTIALS}" \
   #   "https://t1primedatasvc.maximusbc.ca/odr/prime/pnetdata/transactionLog?requestUUID=${UUID}&clientName=${PRIME_ODR_API_CLIENT_NAME}&lastTxnId=${LAST_TX_ID}&fetchSize=${PRIME_ODR_API_FETCH_SIZE}" | \
 
-  echo '{ "requestUUID": "a5dd87cf-ebc9-4061-bc6c-86814eb2135c", "numberOfTransactions": 1, "isThereMoreData": "Y", "pnetTransactions": [ { "txnId": 1, "txnDateTime": "20210726T185028.52", "userid": "923456", "sourceIpAddress": "010.085.013.001", "locationIpAddress": "010.085.013.193", "pharmacyId": "BC00000J70", "providerSoftwareId": "PE", "providerSoftwareVer": "02", "practitionerId": "700005", "collegeRef": "P1", "txnType": "TPN", "txnSubtype": null, "pnetTxnOutcome": "0" } ] }' | python3 -c ${PROCESS_JSON_SCRIPT}
+  # echo '{ "requestUUID": "a5dd87cf-ebc9-4061-bc6c-86814eb2135c", "numberOfTransactions": 1, "isThereMoreData": "Y", "pnetTransactions": [ { "txnId": 1, "txnDateTime": "20210726T185028.52", "userid": "923456", "sourceIpAddress": "010.085.013.001", "locationIpAddress": "010.085.013.193", "pharmacyId": "BC00000J70", "providerSoftwareId": "PE", "providerSoftwareVer": "02", "practitionerId": "700005", "collegeRef": "P1", "txnType": "TPN", "txnSubtype": null, "pnetTxnOutcome": "0" } ] }' | python3 -c ${PROCESS_JSON_SCRIPT}
+
+  ls -l /opt/scripts
 
   # echo -e "\n-------- Write to file? --------\n"
   # pwd
