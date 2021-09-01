83ced0486fb483a43ec8c8e6afee0d805e075c36
Alan Leung <Alan.Leung@NTTDATA.COM>
2021-09-01 07:53:27 -0700

    Download and execute multi-line Python script #3

--- infrastructure/openshift/retrieve-pnet-logs.sh
+++ infrastructure/openshift/retrieve-pnet-logs.sh
@@ -7,6 +7,8 @@ main() {
 
   echo -e "-------- STARTING CRON --------\n"
 
+  echo -e "Connecting to database host:  _${PGHOST}_\n"
+
 #  LAST_TX_ID=$(psql -h ${PGHOST} -d ${PGDATABASE} -U ${PGUSER} -W ${PGPASSWORD} -c 'select max(ptl."TransactionId") from "PharmanetTransactionLog" ptl')
   LAST_TX_ID=$(psql -h ${PGHOST} -d ${PGDATABASE} -U ${PGUSER} -t -c 'select count(*) from "HealthAuthorityLookup" h')
   # Trim whitespace
@@ -20,6 +22,7 @@ main() {
   echo -e "Fetch size:  _${PRIME_ODR_API_FETCH_SIZE}_\n"
 
   PROCESS_JSON_SCRIPT=$(curl -s "https://raw.githubusercontent.com/bcgov/moh-prime/8553e5cec508e8454d7661f4b8a6d255afc7cea5/infrastructure/openshift/parse_api_response.py")
+  echo -e "${PROCESS_JSON_SCRIPT}\n"
 
   # CA certs need to be in place:  https://stackoverflow.com/questions/3160909/how-do-i-deal-with-certificates-using-curl-while-trying-to-access-an-https-url
   # ls -l /etc/ssl/certs
