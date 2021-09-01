54a73fa3108e8e2338403394a6d48bb758bc216d
Alan Leung <Alan.Leung@NTTDATA.COM>
2021-08-31 20:41:28 -0700

    PharmanetTransactionLog table changes and defaults

---
+++ prime-dotnet-webapi/Migrations/20210901033644_PharmanetTransactionLogChangesAndDefaults.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Prime.Migrations
+{
+    public partial class PharmanetTransactionLogChangesAndDefaults : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IpAddress",
+                table: "PharmanetTransactionLog");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UpdatedUserId",
+                table: "PharmanetTransactionLog",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "UpdatedTimeStamp",
+                table: "PharmanetTransactionLog",
+                nullable: false,
+                defaultValueSql: "current_timestamp",
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CreatedUserId",
+                table: "PharmanetTransactionLog",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedTimeStamp",
+                table: "PharmanetTransactionLog",
+                nullable: false,
+                defaultValueSql: "current_timestamp",
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LocationIpAddress",
+                table: "PharmanetTransactionLog",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SourceIpAddress",
+                table: "PharmanetTransactionLog",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LocationIpAddress",
+                table: "PharmanetTransactionLog");
+
+            migrationBuilder.DropColumn(
+                name: "SourceIpAddress",
+                table: "PharmanetTransactionLog");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UpdatedUserId",
+                table: "PharmanetTransactionLog",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldDefaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "UpdatedTimeStamp",
+                table: "PharmanetTransactionLog",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValueSql: "current_timestamp");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CreatedUserId",
+                table: "PharmanetTransactionLog",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldDefaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedTimeStamp",
+                table: "PharmanetTransactionLog",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValueSql: "current_timestamp");
+
+            migrationBuilder.AddColumn<string>(
+                name: "IpAddress",
+                table: "PharmanetTransactionLog",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
--- prime-dotnet-webapi/Models/PharmanetTransactionLog.cs
+++ prime-dotnet-webapi/Models/PharmanetTransactionLog.cs
@@ -18,15 +18,19 @@ namespace Prime.Models
         [JsonProperty("txnId")]
         public long TransactionId { get; set; }
 
-        [JsonProperty("txnDate")]
+        [JsonProperty("txnDateTime")]
         public DateTime TxDateTime { get; set; }
 
         public string UserId { get; set; }
 
-        [JsonProperty("ipAddr")]
-        public string IpAddress { get; set; }
+        // IP address of the location or pharmacy that is sending the transaction.
+        public string LocationIpAddress { get; set; }
 
-        [JsonProperty("pharmId")]
+        // The Location and Source IP address information should be the same, but when pharmacy connects to PharmaNet using an intermediary like HNSecure,
+        // then the Source IP address will be that of the intermediary.
+        public string SourceIpAddress { get; set; }
+
+        [JsonProperty("pharmacyId")]
         public string PharmacyId { get; set; }
 
         [JsonProperty("txnType")]
@@ -35,18 +39,19 @@ namespace Prime.Models
         [JsonProperty("txnSubtype")]
         public string TransactionSubType { get; set; }
 
-        [JsonProperty("practId")]
+        [JsonProperty("practitionerId")]
         public string PractitionerId { get; set; }
 
+        [JsonProperty("collegeRef")]
         public string CollegePrefix { get; set; }
 
-        [JsonProperty("txnStatus")]
+        [JsonProperty("pnetTxnOutcome")]
         public string TransactionOutcome { get; set; }
 
-        [JsonProperty("prvdrSwId")]
+        [JsonProperty("providerSoftwareId")]
         public string ProviderSoftwareId { get; set; }
 
-        [JsonProperty("prvdrSwVrsn")]
+        [JsonProperty("providerSoftwareVer")]
         public string ProviderSoftwareVersion { get; set; }
     }
 }
