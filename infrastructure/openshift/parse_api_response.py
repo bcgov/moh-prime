47f652c138f41c6fc1cecbe0fd1911198466a7a5
Alan Leung <Alan.Leung@nttdata.com>
2021-09-07 10:31:39 -0700

    Added comment

--- infrastructure/openshift/parse_api_response.py
+++ infrastructure/openshift/parse_api_response.py
@@ -4,6 +4,8 @@ json_as_dict = json.load(sys.stdin);
 
 output = csv.writer(sys.stdout);
 # Convert JSON to CSV
+# Note that the columns in the CSV output need to match the invocation of the Postgres COPY command
+# Ultimately the `pnetTransactions` JSON fields need to be in the expected order 
 for row in json_as_dict['pnetTransactions']:
     output.writerow(row.values());
 
