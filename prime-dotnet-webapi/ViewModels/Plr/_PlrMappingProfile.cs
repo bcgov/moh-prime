19cdfbfa1912b32b96ad6ad2a4e47752e4d4369a
James <jameshollinger.ee@gmail.com>
2021-09-08 16:33:26 -0700

    plr

--- prime-dotnet-webapi/ViewModels/Plr/_PlrMappingProfile.cs
+++ prime-dotnet-webapi/ViewModels/Plr/_PlrMappingProfile.cs
@@ -1,6 +1,9 @@
+using System.Linq;
 using AutoMapper;
 
 using Prime.Models;
+using Prime.Models.Plr;
+using Prime.ViewModels.Plr;
 
 namespace Prime.ViewModels.Profiles
 {
@@ -8,7 +11,18 @@ namespace Prime.ViewModels.Profiles
     {
         public PlrMappingProfile()
         {
+            IQueryable<PlrRoleType> plrRoleTypes = null;
+            IQueryable<PlrStatusReason> plrStatusReasons = null;
+            CreateMap<PlrProvider, PlrViewModel>()
+                .ForMember(dest => dest.ProviderRoleType, opt => opt.MapFrom(src => plrRoleTypes.Where(r => src.ProviderRoleType == r.Code).FirstOrDefault().Name))
+                .ForMember(dest => dest.StatusReasonCode, opt => opt.MapFrom(src => plrStatusReasons.Where(s => src.StatusReasonCode == s.Code).FirstOrDefault().Name))
+                .ForMember(dest => dest.ExpertiseCode, opt => opt.MapFrom(src => src.Expertise))
+                .ForMember(dest => dest.Expertise, opt => opt.Ignore());
 
+            // Don't copy over primary keys
+            CreateMap<PlrProvider, PlrProvider>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.Ipc, opt => opt.Ignore());
         }
     }
 }
