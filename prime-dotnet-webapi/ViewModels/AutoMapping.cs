f44e8043d448ade0b66314bf54cf944b10c00230
James <jameshollinger.ee@gmail.com>
2021-09-08 16:32:11 -0700

    health authorities

--- prime-dotnet-webapi/ViewModels/AutoMapping.cs
+++ prime-dotnet-webapi/ViewModels/AutoMapping.cs
@@ -48,22 +48,7 @@ public class AutoMapping : Profile
             .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Party.Email))
             .ForMember(dest => dest.Phone, opt => opt.MapFrom(src => src.Party.Phone));
 
-        IQueryable<int> underReviewIds = null;
-        CreateMap<HealthAuthorityOrganization, HealthAuthorityListViewModel>()
-            .ForMember(dest => dest.HasUnderReviewUsers, opt => opt.MapFrom(src => underReviewIds.Contains(src.Id)));
-        CreateMap<HealthAuthorityOrganization, HealthAuthorityViewModel>()
-            .ForMember(dest => dest.CareTypes, opt => opt.MapFrom(src => src.CareTypes.Select(x => x.CareType)))
-            .ForMember(dest => dest.VendorCodes, opt => opt.MapFrom(src => src.Vendors.Select(x => x.VendorCode)))
-            .ForMember(dest => dest.TechnicalSupports, opt => opt.MapFrom(src => src.TechnicalSupports.Select(x => x.Contact)))
-            .ForMember(dest => dest.PharmanetAdministrators, opt => opt.MapFrom(src => src.PharmanetAdministrators.Select(x => x.Contact)));
-        CreateMap<PrivacyOffice, PrivacyOfficeViewModel>()
-            .ForMember(dest => dest.PrivacyOfficer, opt => opt.MapFrom(src => src.HealthAuthorityOrganization.PrivacyOfficers.Select(x => x.Contact).SingleOrDefault()))
-            .ReverseMap();
-        CreateMap<Contact, PrivacyOfficerViewModel>();
 
-        CreateMap<AuthorizedUser, AuthorizedUserViewModel>()
-            .IncludeMembers(src => src.Party);
-        CreateMap<Party, AuthorizedUserViewModel>();
 
 
 
