0901ab27f14905216da6bf7f361b517d9987ece9
James <jameshollinger.ee@gmail.com>
2021-09-08 16:26:05 -0700

    enrollees

--- prime-dotnet-webapi/ViewModels/AutoMapping.cs
+++ prime-dotnet-webapi/ViewModels/AutoMapping.cs
@@ -32,40 +32,12 @@ public class AutoMapping : Profile
             .ForMember(dest => dest.AdjudicatorIdir, opt => opt.MapFrom(src => src.Adjudicator.IDIR))
             .ForMember(dest => dest.RemoteUserCount, opt => opt.MapFrom(src => src.RemoteUsers.Count));
         CreateMap<BusinessLicence, BusinessLicence>();
-        CreateMap<EnrolleeCreateModel, Enrollee>();
-
-        IQueryable<int> newestAgreementIds = null;
-        CreateMap<Enrollee, EnrolleeListViewModel>()
-            .ForMember(dest => dest.CurrentStatusCode, opt => opt.MapFrom(src => src.CurrentStatus.StatusCode))
-            .ForMember(dest => dest.AdjudicatorIdir, opt => opt.MapFrom(src => src.Adjudicator.IDIR))
-            .ForMember(dest => dest.HasNewestAgreement, opt => opt.MapFrom(src => newestAgreementIds.Any(n => n == src.CurrentAgreementId)))
-            .ForMember(dest => dest.RemoteAccess, opt => opt.MapFrom(src => src.EnrolleeRemoteUsers.Any()))
-            .ForMember(dest => dest.CareSettingCodes, opt => opt.MapFrom(src => src.EnrolleeCareSettings.Select(ecs => ecs.CareSettingCode)))
-            .ForMember(dest => dest.RequiresConfirmation, opt => opt.MapFrom(src =>
-                !src.Submissions.OrderByDescending(s => s.CreatedDate).FirstOrDefault().Confirmed
-                && src.PreviousStatus.StatusCode == (int)StatusType.RequiresToa
-            ))
-            .ForMember(dest => dest.Confirmed, opt => opt.MapFrom(src => src.Submissions.OrderByDescending(s => s.CreatedDate).FirstOrDefault().Confirmed));
-
-        CreateMap<Enrollee, EnrolleeViewModel>()
-            .ForMember(dest => dest.CurrentStatusCode, opt => opt.MapFrom(src => src.CurrentStatus.StatusCode))
-            .ForMember(dest => dest.AdjudicatorIdir, opt => opt.MapFrom(src => src.Adjudicator.IDIR))
-            .ForMember(dest => dest.RequiresConfirmation, opt => opt.MapFrom(src =>
-                !src.Submissions.OrderByDescending(s => s.CreatedDate).FirstOrDefault().Confirmed
-                && src.PreviousStatus.StatusCode == (int)StatusType.RequiresToa
-            ))
-            .ForMember(dest => dest.Confirmed, opt => opt.MapFrom(src => src.Submissions.OrderByDescending(s => s.CreatedDate).FirstOrDefault().Confirmed))
-            .AfterMap((src, dest) => dest.IsRegulatedUser = src.IsRegulatedUser());
 
 
-
-        CreateMap<EnrolleeNote, EnrolleeNoteViewModel>();
         CreateMap<SiteRegistrationNote, SiteRegistrationNoteViewModel>();
 
         // DTOs
-        CreateMap<Enrollee, AgreementEngineDto>()
-            .ForMember(dest => dest.CareSettingCodes, opt => opt.MapFrom(src => src.EnrolleeCareSettings.Select(ecs => ecs.CareSettingCode)));
-        CreateMap<Certification, CertificationDto>();
+
 
         CreateMap<GisEnrolment, GisViewModel>()
             .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.Party.UserId))
--- prime-dotnet-webapi/ViewModels/Enrollee/_EnrolleeMappingProfile.cs
+++ prime-dotnet-webapi/ViewModels/Enrollee/_EnrolleeMappingProfile.cs
@@ -1,5 +1,6 @@
+using System.Linq;
 using AutoMapper;
-
+using Prime.DTOs.AgreementEngine;
 using Prime.Models;
 
 namespace Prime.ViewModels.Profiles
@@ -8,7 +9,36 @@ namespace Prime.ViewModels.Profiles
     {
         public EnrolleeMappingProfile()
         {
+            CreateMap<EnrolleeCreateModel, Enrollee>();
 
+            IQueryable<int> newestAgreementIds = null;
+            CreateMap<Enrollee, EnrolleeListViewModel>()
+                .ForMember(dest => dest.CurrentStatusCode, opt => opt.MapFrom(src => src.CurrentStatus.StatusCode))
+                .ForMember(dest => dest.AdjudicatorIdir, opt => opt.MapFrom(src => src.Adjudicator.IDIR))
+                .ForMember(dest => dest.HasNewestAgreement, opt => opt.MapFrom(src => newestAgreementIds.Any(n => n == src.CurrentAgreementId)))
+                .ForMember(dest => dest.RemoteAccess, opt => opt.MapFrom(src => src.EnrolleeRemoteUsers.Any()))
+                .ForMember(dest => dest.CareSettingCodes, opt => opt.MapFrom(src => src.EnrolleeCareSettings.Select(ecs => ecs.CareSettingCode)))
+                .ForMember(dest => dest.RequiresConfirmation, opt => opt.MapFrom(src =>
+                    !src.Submissions.OrderByDescending(s => s.CreatedDate).FirstOrDefault().Confirmed
+                    && src.PreviousStatus.StatusCode == (int)StatusType.RequiresToa
+                ))
+                .ForMember(dest => dest.Confirmed, opt => opt.MapFrom(src => src.Submissions.OrderByDescending(s => s.CreatedDate).FirstOrDefault().Confirmed));
+
+            CreateMap<Enrollee, EnrolleeViewModel>()
+                .ForMember(dest => dest.CurrentStatusCode, opt => opt.MapFrom(src => src.CurrentStatus.StatusCode))
+                .ForMember(dest => dest.AdjudicatorIdir, opt => opt.MapFrom(src => src.Adjudicator.IDIR))
+                .ForMember(dest => dest.RequiresConfirmation, opt => opt.MapFrom(src =>
+                    !src.Submissions.OrderByDescending(s => s.CreatedDate).FirstOrDefault().Confirmed
+                    && src.PreviousStatus.StatusCode == (int)StatusType.RequiresToa
+                ))
+                .ForMember(dest => dest.Confirmed, opt => opt.MapFrom(src => src.Submissions.OrderByDescending(s => s.CreatedDate).FirstOrDefault().Confirmed))
+                .AfterMap((src, dest) => dest.IsRegulatedUser = src.IsRegulatedUser());
+
+            CreateMap<EnrolleeNote, EnrolleeNoteViewModel>();
+
+            CreateMap<Enrollee, AgreementEngineDto>()
+                .ForMember(dest => dest.CareSettingCodes, opt => opt.MapFrom(src => src.EnrolleeCareSettings.Select(ecs => ecs.CareSettingCode)));
+            CreateMap<Certification, CertificationDto>();
         }
     }
 }
