8efee7a2904f1d51c2310d90111696ddd77ca0ca
James <jameshollinger.ee@gmail.com>
2021-09-08 16:59:53 -0700

    site mapping

--- prime-dotnet-webapi/ViewModels/SiteRegistration/_SiteMappingProfile.cs
+++ prime-dotnet-webapi/ViewModels/SiteRegistration/_SiteMappingProfile.cs
@@ -8,7 +8,18 @@ namespace Prime.ViewModels.Profiles
     {
         public SiteMappingProfile()
         {
+            int? careSettingCode = null;
+            CreateMap<Organization, OrganizationListViewModel>()
+                .ForMember(dest => dest.HasAcceptedAgreement, opt => opt.MapFrom(src => src.Agreements.Any(a => a.AcceptedDate.HasValue)))
+                .ForMember(dest => dest.HasSubmittedSite, opt => opt.MapFrom(src => src.Sites.Any(s => s.SubmittedDate.HasValue)))
+                .ForMember(dest => dest.Sites, opt => opt.MapFrom(src => src.Sites.Where(s => careSettingCode == null || s.CareSettingCode == careSettingCode)));
+            CreateMap<Site, SiteListViewModel>()
+                .ForMember(dest => dest.AdjudicatorIdir, opt => opt.MapFrom(src => src.Adjudicator.IDIR))
+                .ForMember(dest => dest.RemoteUserCount, opt => opt.MapFrom(src => src.RemoteUsers.Count));
 
+            CreateMap<BusinessLicence, BusinessLicence>();
+
+            CreateMap<SiteRegistrationNote, SiteRegistrationNoteViewModel>();
         }
     }
 }
