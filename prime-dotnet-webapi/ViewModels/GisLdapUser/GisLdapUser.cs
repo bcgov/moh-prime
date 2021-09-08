8d75fd04b48a339e071201103839374c63f07263
Martin Pultz <mtpultz@users.noreply.github.com>
2021-09-08 13:02:22 -0700

    Update GisLdapUser.cs

--- prime-dotnet-webapi/ViewModels/GisLdapUser/GisLdapUser.cs
+++ prime-dotnet-webapi/ViewModels/GisLdapUser/GisLdapUser.cs
@@ -3,7 +3,6 @@ namespace Prime.ViewModels
     public class GisLdapUser
     {
         public string RemainingAttempts { get; set; }
-        public string Unlocked { get; set; }
         public string GisUserRole { get; set; }
         public bool Success { get => GisUserRole == "GISUSER"; }
     }
