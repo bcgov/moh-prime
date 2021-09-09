79a3de8cf919cf8f0076c8fb8002892cdfcc356e
James <jameshollinger.ee@gmail.com>
2021-09-08 17:32:48 -0700

    simplify

--- prime-dotnet-webapi/ViewModels/Parties/_PartyMappingProfile.cs
+++ prime-dotnet-webapi/ViewModels/Parties/_PartyMappingProfile.cs
@@ -10,13 +10,8 @@ namespace Prime.ViewModels.Profiles
         public PartyMappingProfile()
         {
             CreateMap<GisEnrolment, GisViewModel>()
-                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.Party.UserId))
-                .ForMember(dest => dest.HPDID, opt => opt.MapFrom(src => src.Party.HPDID))
-                .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.Party.FirstName))
-                .ForMember(dest => dest.GivenNames, opt => opt.MapFrom(src => src.Party.GivenNames))
-                .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.Party.LastName))
-                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Party.Email))
-                .ForMember(dest => dest.Phone, opt => opt.MapFrom(src => src.Party.Phone));
+                .IncludeMembers(g => g.Party);
+            CreateMap<Party, GisViewModel>();
         }
     }
 }
