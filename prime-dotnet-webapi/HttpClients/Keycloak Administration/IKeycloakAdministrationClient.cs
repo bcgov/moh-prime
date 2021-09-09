fcc9ba78c5c75d98b3a41d28e6bf8bfa1237e3c3
James Hollinger <39168456+james-hollinger@users.noreply.github.com>
2021-09-08 17:22:07 -0700

    Refactor Keycloak Clients (#1584)
    * Update Keycloak Clients
    
    * prep
    
    * update clients
    
    * init
    
    * register service
    
    * simplify method
    
    * simplify
    
    * simplify
    
    * reorder
    
    Co-authored-by: Martin Pultz <mtpultz@users.noreply.github.com>

--- prime-dotnet-webapi/HttpClients/Keycloak Administration/IKeycloakAdministrationClient.cs
+++ prime-dotnet-webapi/HttpClients/Keycloak Administration/IKeycloakAdministrationClient.cs
@@ -1,15 +1,69 @@
 using System;
 using System.Threading.Tasks;
 
-using Prime.ViewModels.Parties;
 using Prime.HttpClients.KeycloakApiDefinitions;
 
 namespace Prime.HttpClients
 {
     public interface IKeycloakAdministrationClient
     {
-        Task<Role> GetRealmRole(string roleName);
+        /// <summary>
+        /// Gets the Keycloak Client representation by ClientId.
+        /// Returns null if unccessful.
+        /// </summary>
+        /// <param name="clientId"></param>
+        Task<Client> GetClient(string clientId);
+
+
+        /// <summary>
+        /// Assigns a Client Role to the user, if it exists.
+        /// Returns true if the operation was successful.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="clientId"></param>
+        /// <param name="roleName"></param>
+        Task<bool> AssignClientRole(Guid userId, string clientId, string roleName);
+
+        /// <summary>
+        /// Assigns a realm-level role to the user, if it exists.
+        /// Returns true if the operation was successful.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="roleName"></param>
         Task<bool> AssignRealmRole(Guid userId, string roleName);
-        Task<bool> UpdatePhsaUserInfo(Guid userId, PhsaChangeModel party);
+
+        /// <summary>
+        /// Gets the Keycloak Client Role representation by name.
+        /// Returns null if unccessful or if no roles of that name exist on the client.
+        /// </summary>
+        /// <param name="clientId"></param>
+        /// <param name="roleName"></param>
+        Task<Role> GetClientRole(string clientId, string roleName);
+
+        /// <summary>
+        /// Gets the Keycloak Role representation by name.
+        /// Returns null if unccessful.
+        /// </summary>
+        /// <param name="roleName"></param>
+        Task<Role> GetRealmRole(string roleName);
+
+
+        /// <summary>
+        /// Gets the Keycloak User Representation for the user.
+        /// Returns null if unccessful.
+        /// </summary>
+        /// <param name="userId"></param>
+        Task<UserRepresentation> GetUser(Guid userId);
+
+        /// <summary>
+        /// Updates the User with the given Keycloak User Representation.
+        /// Returns true if the operation was successful.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="userRep"></param>
+        Task<bool> UpdateUser(Guid userId, UserRepresentation userRep);
     }
+
+    public interface IPrimeKeycloakAdministrationClient : IKeycloakAdministrationClient { }
+    public interface IMohKeycloakAdministrationClient : IKeycloakAdministrationClient { }
 }
