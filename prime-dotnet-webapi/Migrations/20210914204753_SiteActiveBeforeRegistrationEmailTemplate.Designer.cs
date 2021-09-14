156fadd36fb46f0a205e6e1d309a197d67b3095f
Panos <paahndev@gmail.com>
2021-09-14 13:51:59 -0700

    migration and db update

--- prime-dotnet-webapi/Migrations/20210914204753_SiteActiveBeforeRegistrationEmailTemplate.Designer.cs
+++ prime-dotnet-webapi/Migrations/20210914204753_SiteActiveBeforeRegistrationEmailTemplate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Prime;
@@ -10,9 +11,10 @@ using Prime.Models;
 namespace Prime.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210914204753_SiteActiveBeforeRegistrationEmailTemplate")]
+    partial class SiteActiveBeforeRegistrationEmailTemplate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
