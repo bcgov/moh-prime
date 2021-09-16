4ecc99fae05073624407adca8dff75ac46ce08ef
Martin Pultz <mtpultz@gmail.com>
2021-09-16 14:58:14 -0700

    Updated tests

--- prime-angular-frontend/src/app/modules/health-auth-site-reg/pages/technical-support-page/technical-support-page.component.spec.ts
+++ prime-angular-frontend/src/app/modules/health-auth-site-reg/pages/technical-support-page/technical-support-page.component.spec.ts
@@ -1,6 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
+import { KeycloakService } from 'keycloak-angular';
+
+import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
 import { TechnicalSupportPageComponent } from './technical-support-page.component';
+import { CapitalizePipe } from '@shared/pipes/capitalize.pipe';
 
 describe('TechnicalSupportPageComponent', () => {
   let component: TechnicalSupportPageComponent;
@@ -8,9 +19,27 @@ describe('TechnicalSupportPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TechnicalSupportPageComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        TechnicalSupportPageComponent
+      ],
+      imports: [
+        BrowserAnimationsModule,
+        HttpClientTestingModule,
+        RouterTestingModule,
+        ReactiveFormsModule,
+        MatDialogModule,
+        MatSnackBarModule
+      ],
+      providers: [
+        KeycloakService,
+        {
+          provide: APP_CONFIG,
+          useValue: APP_DI_CONFIG
+        },
+        CapitalizePipe
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
   });
 
   beforeEach(() => {
