44158168be5b0789f34bed90737397d2f967b44f
Johnny Liu <johnny.liu@nttdata.com>
2021-09-22 09:20:11 -0700

    fixed tests

--- prime-angular-frontend/src/app/modules/adjudication/pages/health-authorities/site-information-page/site-information-page.component.spec.ts
+++ prime-angular-frontend/src/app/modules/adjudication/pages/health-authorities/site-information-page/site-information-page.component.spec.ts
@@ -1,6 +1,16 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NgxMaterialModule } from '@lib/modules/ngx-material/ngx-material.module';
 
 import { SiteInformationPageComponent } from './site-information-page.component';
+import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
+import { PermissionService } from '@auth/shared/services/permission.service';
+import { MockPermissionService } from 'test/mocks/mock-permission.service';
+import { CapitalizePipe } from '@shared/pipes/capitalize.pipe';
 
 describe('SiteInformationPageComponent', () => {
   let component: SiteInformationPageComponent;
@@ -8,9 +18,28 @@ describe('SiteInformationPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SiteInformationPageComponent ]
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        NgxMaterialModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule
+      ],
+      declarations: [SiteInformationPageComponent],
+      providers: [
+        {
+          provide: APP_CONFIG,
+          useValue: APP_DI_CONFIG
+        },
+        {
+          provide: PermissionService,
+          useClass: MockPermissionService
+        },
+        CapitalizePipe
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
--- prime-angular-frontend/src/app/modules/adjudication/pages/health-authorities/site-overview-page/site-overview-page.component.spec.ts
+++ prime-angular-frontend/src/app/modules/adjudication/pages/health-authorities/site-overview-page/site-overview-page.component.spec.ts
@@ -1,6 +1,17 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { PermissionService } from '@auth/shared/services/permission.service';
+import { NgxMaterialModule } from '@lib/modules/ngx-material/ngx-material.module';
+import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
+import { MockPermissionService } from 'test/mocks/mock-permission.service';
 
 import { SiteOverviewPageComponent } from './site-overview-page.component';
+import { CapitalizePipe } from '@shared/pipes/capitalize.pipe';
 
 describe('SiteOverviewPageComponent', () => {
   let component: SiteOverviewPageComponent;
@@ -8,9 +19,28 @@ describe('SiteOverviewPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SiteOverviewPageComponent ]
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        NgxMaterialModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule
+      ],
+      declarations: [SiteOverviewPageComponent],
+      providers: [
+        {
+          provide: APP_CONFIG,
+          useValue: APP_DI_CONFIG
+        },
+        {
+          provide: PermissionService,
+          useClass: MockPermissionService
+        },
+        CapitalizePipe
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
