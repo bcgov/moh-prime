88a75d1ab8607e09f5930d4333b37218f94799ef
Ivan P <ivan.polchenko@nttdata.com>
2021-09-29 15:07:47 -0700

    fix locale

--- document-manager/backend/app.sh
+++ document-manager/backend/app.sh
@@ -11,8 +11,8 @@
 function backend()
 {
     cd /opt/app-root/src
-    export LC_ALL=en_US.utf-8
-    export LANG=en_US.utf-8
+    export LC_ALL=C.UTF-8
+    export LANG=C.UTF-8
     python3 wait.py
     flask run ${FLASK_RUN_PARAMS} &disown 
     uwsgi uwsgi.ini
@@ -21,8 +21,8 @@ function backend()
 function migrate()
 {
     cd /opt/app-root/src
-    export LC_ALL=en_US.utf-8
-    export LANG=en_US.utf-8
+    export LC_ALL=C.UTF-8
+    export LANG=C.UTF-8
     python3 wait.py
     flask db upgrade ${FLASK_RUN_PARAMS}
 }
