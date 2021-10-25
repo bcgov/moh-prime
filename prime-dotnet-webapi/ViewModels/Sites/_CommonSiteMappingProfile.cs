1cb28c7a458fd7b603d1d21c417a4d338d73a9cf
James <jameshollinger.ee@gmail.com>
2021-10-25 12:04:16 -0700

    make it run

--- prime-dotnet-webapi/ViewModels/Sites/_CommonSiteMappingProfile.cs
+++ prime-dotnet-webapi/ViewModels/Sites/_CommonSiteMappingProfile.cs
@@ -18,12 +18,12 @@ namespace Prime.ViewModels.Profiles
                 .ReverseMap();
 
             // TODO this must be tested and adjusted
-            CreateMap<RemoteUser, RemoteAccessSearchViewModel>()
-                .IncludeMembers(user => user.Site);
-            CreateMap<CommunitySite, RemoteAccessSearchViewModel>()
-                .ForMember(dest => dest.VendorCodes, opt => opt.MapFrom(src => src.SiteVendors.Select(x => x.VendorCode)));
-            CreateMap<V2HealthAuthoritySite, RemoteAccessSearchViewModel>()
-                .ForMember(dest => dest.VendorCodes, opt => opt.MapFrom(src => new[] { src.HealthAuthorityVendor.VendorCode }));
+            // CreateMap<RemoteUser, RemoteAccessSearchViewModel>()
+            //     .IncludeMembers(user => user.Site);
+            // CreateMap<CommunitySite, RemoteAccessSearchViewModel>()
+            //     .ForMember(dest => dest.VendorCodes, opt => opt.MapFrom(src => src.SiteVendors.Select(x => x.VendorCode)));
+            // CreateMap<V2HealthAuthoritySite, RemoteAccessSearchViewModel>()
+            //     .ForMember(dest => dest.VendorCodes, opt => opt.MapFrom(src => new[] { src.HealthAuthorityVendor.VendorCode }));
         }
     }
 }
