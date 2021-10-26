23da6cb2b110b7dcd4257e00f5950b6ec522c3eb
Panos <paahndev@gmail.com>
2021-10-26 14:30:22 -0700

    rename v2 viewmodel

--- prime-dotnet-webapi/ViewModels/Health Authority Sites/HealthAuthoritySiteViewModel.cs
+++ prime-dotnet-webapi/ViewModels/Health Authority Sites/HealthAuthoritySiteViewModel.cs
@@ -1,32 +1,28 @@
 using System;
 using System.Collections.Generic;
 using Prime.Models;
-using Prime.Models.HealthAuthorities;
 
 namespace Prime.ViewModels.HealthAuthoritySites
 {
-    public class HealthAuthoritySiteViewModel
+    public class V2HealthAuthoritySiteViewModel
     {
         public int Id { get; set; }
         public int HealthAuthorityOrganizationId { get; set; }
-        // TODO should we use a relationship or direct data type?
-        // public int HealthAuthorityVendorId { get; set; }
-        public int VendorCode { get; set; }
         public string SiteName { get; set; }
-        public string SiteId { get; set; }
+        public string PEC { get; set; }
         public int SecurityGroupCode { get; set; }
-        // TODO should we use a relationship or direct data type?
-        // public int HealthAuthorityCareTypeId { get; set; }
-        public string CareType { get; set; }
-        public PhysicalAddress PhysicalAddress { get; set; }
-        public ICollection<BusinessDay> BusinessHours { get; set; }
-        public ICollection<RemoteUser> RemoteUsers { get; set; }
-        public int? HealthAuthorityPharmanetAdministratorId { get; set; }
-        public HealthAuthorityPharmanetAdministrator HealthAuthorityPharmanetAdministrator { get; set; }
-        public int? HealthAuthorityTechnicalSupportId { get; set; }
-        public HealthAuthorityTechnicalSupport HealthAuthorityTechnicalSupport { get; set; }
-        public bool Completed { get; set; }
         public DateTimeOffset? SubmittedDate { get; set; }
         public DateTimeOffset? ApprovedDate { get; set; }
+        public AddressViewModel PhysicalAddress { get; set; }
+        
+        public HealthAuthorityVendorViewModel HealthAuthorityVendor { get; set; }
+        public HealthAuthorityCareTypeViewModel CareType { get; set; }
+
+        public bool Completed { get; set; }
+
+        public ICollection<BusinessDay> BusinessHours { get; set; }
+        // public ICollection<RemoteUser> RemoteUsers { get; set; }
+        // public int? HealthAuthorityPharmanetAdministratorId { get; set; }
+        // public int? HealthAuthorityTechnicalSupportId { get; set; }
     }
 }
