444c5223f230f583404dbe291315a3e521d7f5e7
Alan Leung <Alan.Leung@nttdata.com>
2021-10-27 13:15:14 -0700

    Filling out SiteFactory

---
+++ prime-dotnet-webapi-tests/ModelFactories/BaseDocumentUploadFactory.cs
@@ -0,0 +1,32 @@
+using Bogus;
+using Prime.Models;
+
+namespace PrimeTests.ModelFactories
+{
+    public class BusinessLicenceDocumentFactory : BaseDocumentUploadFactory<BusinessLicenceDocument>
+    {
+        public BusinessLicenceDocumentFactory(BusinessLicence related)
+        {
+            this.SetBaseRules();
+
+            RuleFor(x => x.BusinessLicence, f => related);
+
+            Ignore(x => x.BusinessLicenceId);
+        }
+    }
+
+
+    public abstract class BaseDocumentUploadFactory<T> : Faker<T> where T : BaseDocumentUpload
+    {
+        protected BaseDocumentUploadFactory()
+        {
+            this.SetBaseRules();
+
+            RuleFor(x => x.Id, f => IdCounter.Id++);
+            RuleFor(x => x.DocumentGuid, f => f.Random.Guid());
+            RuleFor(x => x.Filename, f => f.Random.Word());
+            // TODO: add additional logic?
+            RuleFor(x => x.UploadedDate, f => f.Date.Past(1));
+        }
+    }
+}
--- prime-dotnet-webapi-tests/ModelFactories/Lookups/OrganizationTypeLookup.cs
+++ prime-dotnet-webapi-tests/ModelFactories/Lookups/OrganizationTypeLookup.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Collections.Generic;
 using Prime.Models;
 using Prime.Configuration.Database;
@@ -9,5 +10,8 @@ namespace PrimeTests.ModelFactories
         private static IEnumerable<CareSetting> _seedData = new CareSettingConfiguration().SeedData;
 
         public static IEnumerable<CareSetting> All { get { return _seedData; } }
+
+        // Contain only "Private Community Health Practice" and "Community Pharmacy"
+        public static IEnumerable<CareSetting> SiteCareSettings { get { return _seedData.Where(cs => cs.Code == 2 || cs.Code == 3); } }
     }
 }
