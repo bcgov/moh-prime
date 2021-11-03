a426fe887751046fb38d71d99ff9df8fe9bb0c88
Martin Pultz <mtpultz@gmail.com>
2021-11-03 00:49:34 -0700

    Update overview and submission

--- prime-angular-frontend/src/app/modules/health-auth-site-reg/pages/overview-page/overview-page.component.ts
+++ prime-angular-frontend/src/app/modules/health-auth-site-reg/pages/overview-page/overview-page.component.ts
@@ -1,21 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup } from '@angular/forms';
 
-import { Subscription, EMPTY, forkJoin } from 'rxjs';
+import { Subscription, EMPTY, of } from 'rxjs';
 import { exhaustMap } from 'rxjs/operators';
 
-import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { UntilDestroy } from '@ngneat/until-destroy';
+
+import { Moment } from 'moment';
 
 import { Contact } from '@lib/models/contact.model';
 import { RouteUtils } from '@lib/utils/route-utils.class';
+import { ToastService } from '@core/services/toast.service';
+import { FormUtilsService } from '@core/services/form-utils.service';
 import { HealthAuthorityResource } from '@core/resources/health-authority-resource.service';
-import { HealthAuthority } from '@shared/models/health-authority.model';
 import { DialogOptions } from '@shared/components/dialogs/dialog-options.model';
 import { ConfirmDialogComponent } from '@shared/components/dialogs/confirm-dialog/confirm-dialog.component';
 
 import { HealthAuthoritySite } from '@health-auth/shared/models/health-authority-site.model';
 import { HealthAuthSiteRegRoutes } from '@health-auth/health-auth-site-reg.routes';
+import { HealthAuthorityService } from '@health-auth/shared/services/health-authority.service';
+import { HealthAuthoritySiteService } from '@health-auth/shared/services/health-authority-site.service';
+import { HealthAuthoritySiteFormStateService } from '@health-auth/shared/services/health-authority-site-form-state.service';
 
 @UntilDestroy()
 @Component({
@@ -28,6 +35,11 @@ export class OverviewPageComponent implements OnInit {
   public pharmanetAdministrators: Contact[];
   public technicalSupports: Contact[];
   public healthAuthoritySite: HealthAuthoritySite;
+  public siteIsIncomplete: boolean;
+  public siteIsInReview: boolean;
+  public siteIsLocked: boolean;
+  public siteIsApproved: boolean;
+  public siteExpiryDate: string | Moment | null;
   public showEditRedirect: boolean;
   public showSubmissionAction: boolean;
   public routeUtils: RouteUtils;
@@ -37,13 +49,28 @@ export class OverviewPageComponent implements OnInit {
     protected route: ActivatedRoute,
     protected router: Router,
     private dialog: MatDialog,
-    private healthAuthorityResource: HealthAuthorityResource
+    private healthAuthorityService: HealthAuthorityService,
+    private healthAuthoritySiteService: HealthAuthoritySiteService,
+    private healthAuthoritySiteFormStateService: HealthAuthoritySiteFormStateService,
+    private healthAuthorityResource: HealthAuthorityResource,
+    private formUtilsService: FormUtilsService,
+    private toastService: ToastService
   ) {
     this.showEditRedirect = true;
     this.routeUtils = new RouteUtils(route, router, HealthAuthSiteRegRoutes.routePath(HealthAuthSiteRegRoutes.MODULE_PATH));
   }
 
   public onSubmit(): void {
+    if (this.siteIsInReview) {
+      return;
+    }
+
+    if (!this.healthAuthoritySiteFormStateService.isValidSubmission) {
+      this.healthAuthoritySiteFormStateService.forms.forEach((form: FormGroup) => this.formUtilsService.logFormErrors(form));
+      this.toastService.openErrorToast('Your site has an error that needs to be corrected before you will be able to submit');
+      return;
+    }
+
     const { haid, sid } = this.route.snapshot.params;
     const data: DialogOptions = {
       title: 'Save Site',
@@ -55,25 +82,18 @@ export class OverviewPageComponent implements OnInit {
       .pipe(
         exhaustMap((result: boolean) =>
           (result)
-            ? this.healthAuthorityResource.healthAuthoritySiteSubmit(haid, sid)
+            ? of(this.healthAuthoritySiteFormStateService.json)
             : EMPTY
+        ),
+        exhaustMap((healthAuthoritySite: HealthAuthoritySite) =>
+          this.healthAuthorityResource.healthAuthoritySiteSubmit(haid, sid, healthAuthoritySite.forUpdate())
         )
       )
       .subscribe(() => this.nextRoute());
   }
 
   public onBack(): void {
-    this.routeUtils.routeTo([
-      HealthAuthSiteRegRoutes.MODULE_PATH,
-      HealthAuthSiteRegRoutes.SITE_MANAGEMENT
-    ]);
-  }
-
-  public nextRoute(): void {
-    this.routeUtils.routeTo([
-      HealthAuthSiteRegRoutes.MODULE_PATH,
-      HealthAuthSiteRegRoutes.SITE_MANAGEMENT
-    ], { queryParams: { submitted: true } });
+    this.nextRoute();
   }
 
   public ngOnInit(): void {
@@ -83,18 +103,48 @@ export class OverviewPageComponent implements OnInit {
       return;
     }
 
-    this.busy = forkJoin({
-      healthAuthority: this.healthAuthorityResource.getHealthAuthorityById(healthAuthId),
-      healthAuthoritySite: this.healthAuthorityResource.getHealthAuthoritySiteById(healthAuthId, healthAuthSiteId)
-    })
-      .pipe(untilDestroyed(this))
-      .subscribe(({
-                    healthAuthority: { pharmanetAdministrators, technicalSupports },
-                    healthAuthoritySite
-                  }: { healthAuthority: HealthAuthority, healthAuthoritySite: HealthAuthoritySite }) => {
-        this.pharmanetAdministrators = pharmanetAdministrators;
-        this.technicalSupports = technicalSupports;
-        this.healthAuthoritySite = healthAuthoritySite;
-      });
+    const { pharmanetAdministrators, technicalSupports } = this.healthAuthorityService.healthAuthority;
+    this.pharmanetAdministrators = pharmanetAdministrators;
+    this.technicalSupports = technicalSupports;
+
+    // Initial assumption is the site has no pending updates, and
+    // the source of truth should be used
+    let healthAuthoritySite = this.healthAuthoritySiteService.site;
+
+    this.siteIsIncomplete = healthAuthoritySite.isIncomplete();
+    this.siteIsInReview = healthAuthoritySite.isInReview();
+    this.siteIsLocked = healthAuthoritySite.isLocked();
+    this.siteIsApproved = healthAuthoritySite.isApproved();
+
+    this.siteExpiryDate = (healthAuthoritySite.approvedDate)
+      ? healthAuthoritySite.getExpiryDate()
+      : null;
+
+    // Once submitted and under review no submissions are allowed
+    this.showSubmissionAction = !this.siteIsInReview;
+
+    if (this.healthAuthoritySiteFormStateService.isPatched) {
+      // Replace site with the version from the form for the user
+      // to review which maintains a subset of immutable properties
+      healthAuthoritySite = this.healthAuthoritySiteFormStateService.json;
+    }
+
+    // Store a local copy of the site for overview
+    this.healthAuthoritySite = healthAuthoritySite;
+
+    // Attempt to patch the form if not already patched so
+    // a validation check is against actual data, which will
+    // not patch if already patched during a user update
+    //
+    // NOTE: Initializes the form state service for workflow
+    // updates when not already patched and contains changes
+    this.healthAuthoritySiteFormStateService.setForm(healthAuthoritySite);
+  }
+
+  private nextRoute(): void {
+    this.routeUtils.routeTo([
+      HealthAuthSiteRegRoutes.MODULE_PATH,
+      HealthAuthSiteRegRoutes.SITE_MANAGEMENT
+    ]);
   }
 }
