d2b8f1cdeae9486ba12a837dffb682fec8ed20ac
Nathan <Hogman>
2021-11-05 14:57:13 -0700

    update test

--- prime-angular-frontend/src/app/modules/site-registration/pages/device-provider-page/device-provider-page.component.spec.ts
+++ prime-angular-frontend/src/app/modules/site-registration/pages/device-provider-page/device-provider-page.component.spec.ts
@@ -1,4 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { KeycloakService } from 'keycloak-angular';
+
+
+import { ConfigService } from '@config/config.service';
+import { NgxMaterialModule } from '@lib/modules/ngx-material/ngx-material.module';
+import { AuthService } from '@auth/shared/services/auth.service';
+import { PermissionService } from '@auth/shared/services/permission.service';
+
+import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
+import { MockAuthService } from 'test/mocks/mock-auth.service';
+import { MockPermissionService } from 'test/mocks/mock-permission.service';
+import { MockConfigService } from 'test/mocks/mock-config.service';
 
 import { DeviceProviderPageComponent } from './device-provider-page.component';
 
@@ -6,12 +23,41 @@ describe('DeviceProviderPageComponent', () => {
   let component: DeviceProviderPageComponent;
   let fixture: ComponentFixture<DeviceProviderPageComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DeviceProviderPageComponent ]
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        DeviceProviderPageComponent
+      ],
+      imports: [
+        BrowserAnimationsModule,
+        HttpClientTestingModule,
+        RouterTestingModule,
+        ReactiveFormsModule,
+        NgxMaterialModule
+      ],
+      providers: [
+        {
+          provide: APP_CONFIG,
+          useValue: APP_DI_CONFIG
+        },
+        {
+          provide: ConfigService,
+          useClass: MockConfigService
+        },
+        {
+          provide: AuthService,
+          useClass: MockAuthService
+        },
+        {
+          provide: PermissionService,
+          useClass: MockPermissionService
+        },
+        KeycloakService
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
-    .compileComponents();
-  });
+      .compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DeviceProviderPageComponent);
