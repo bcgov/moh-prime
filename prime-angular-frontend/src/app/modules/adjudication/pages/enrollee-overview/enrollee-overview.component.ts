4db01265d59a5e7b19e46d9401c075288063b808
Alan Leung <Alan.Leung@nttdata.com>
2021-11-05 14:57:32 -0700

    Improve responsiveness of Overview page

--- prime-angular-frontend/src/app/modules/adjudication/pages/enrollee-overview/enrollee-overview.component.ts
+++ prime-angular-frontend/src/app/modules/adjudication/pages/enrollee-overview/enrollee-overview.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
-import { catchError, map } from 'rxjs/operators';
+import { catchError, exhaustMap, map } from 'rxjs/operators';
 import { forkJoin, of } from 'rxjs';
 
 import { PermissionService } from '@auth/shared/services/permission.service';
@@ -93,19 +93,25 @@ export class EnrolleeOverviewComponent extends AdjudicationContainerComponent im
             }))
           ),
         enrolleeNavigation: this.adjudicationResource.getAdjacentEnrolleeId(enrolleeId),
-        plrInfo: this.adjudicationResource.getPlrInfoByEnrolleeId(enrolleeId)
-          .pipe(catchError(_ => of([])))
-      })
-        .subscribe(({ enrollee, enrolleeNavigation, plrInfo }) => {
-          this.enrollee = enrollee.enrollee;
-          this.enrollees = [enrollee.enrolleeView];
-          this.enrolment = enrollee.enrolment;
-          this.enrolleeNavigation = enrolleeNavigation;
-          this.plrInfo = plrInfo;
-          // hide the adjudication card if enrolment is editable and no 'reason for adjudication'
-          this.showAdjudication = !(enrollee.enrollee.currentStatus.statusCode === EnrolmentStatusEnum.EDITABLE
-            && !enrollee.enrollee.currentStatus.enrolmentStatusReasons?.length);
-        });
+      }).pipe(
+        map(
+          ({ enrollee, enrolleeNavigation }) => {
+            // Complete this first before attempting to get PLR info, so user can see information rendered sooner
+            this.enrollee = enrollee.enrollee;
+            this.enrollees = [enrollee.enrolleeView];
+            this.enrolment = enrollee.enrolment;
+            this.enrolleeNavigation = enrolleeNavigation;
+            // hide the adjudication card if enrolment is editable and no 'reason for adjudication'
+            this.showAdjudication = !(enrollee.enrollee.currentStatus.statusCode === EnrolmentStatusEnum.EDITABLE
+              && !enrollee.enrollee.currentStatus.enrolmentStatusReasons?.length);
+            return enrolleeId;
+          }
+        ),
+        exhaustMap((enrolleeId: number) => this.adjudicationResource.getPlrInfoByEnrolleeId(enrolleeId)
+          .pipe(
+            map((plrInfo: PlrInfo[]) => this.plrInfo = plrInfo),
+            catchError(_ => of([]))))
+      ).subscribe();
   }
 
   private enrolmentAdapter(enrollee: HttpEnrollee): Enrolment {
