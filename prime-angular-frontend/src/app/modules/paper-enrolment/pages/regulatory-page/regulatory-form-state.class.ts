8fd6747d84abc507a148212da1a6883ee030d855
Martin Pultz <mtpultz@gmail.com>
2021-11-09 18:54:04 -0800

    Fix select not showing single option by default

--- prime-angular-frontend/src/app/modules/paper-enrolment/pages/regulatory-page/regulatory-form-state.class.ts
+++ prime-angular-frontend/src/app/modules/paper-enrolment/pages/regulatory-page/regulatory-form-state.class.ts
@@ -8,7 +8,7 @@ export class RegulatoryFormState extends BaseRegulatoryPageFormState {
       .map((certification: CollegeCertification) => +certification.collegeCode);
   }
 
-  public addEmptyCollegeCertification() {
+  public addEmptyCollegeCertification(): void {
     this.addCollegeCertification();
   }
 
@@ -20,7 +20,7 @@ export class RegulatoryFormState extends BaseRegulatoryPageFormState {
    *
    * @param index to be removed
    */
-  public removeCertification(index: number) {
+  public removeCertification(index: number): void {
     this.certifications.removeAt(index);
   }
 
@@ -29,7 +29,7 @@ export class RegulatoryFormState extends BaseRegulatoryPageFormState {
    * Removes incomplete certifications from the list in preparation
    * for submission, and allows for an empty list of certifications.
    */
-  public removeIncompleteCertifications(noEmptyCert: boolean = false) {
+  public removeIncompleteCertifications(noEmptyCert: boolean = false): void {
     this.certifications.controls
       .forEach((control: FormGroup, index: number) => {
         // Remove if college code is "None" or the group is invalid
