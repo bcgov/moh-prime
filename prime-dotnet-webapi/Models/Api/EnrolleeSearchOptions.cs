c2a9982289d62083b0f63cce9615312b4f011799
James <jameshollinger.ee@gmail.com>
2021-11-17 13:08:59 -0800

    first pass of BE

--- prime-dotnet-webapi/Models/Api/EnrolleeSearchOptions.cs
+++ prime-dotnet-webapi/Models/Api/EnrolleeSearchOptions.cs
@@ -3,6 +3,7 @@ namespace Prime.Models.Api
     public class EnrolleeSearchOptions
     {
         public int? StatusCode { get; set; }
+        public bool? IsLinkedPaperEnrolment { get; set; }
         public string TextSearch { get; set; }
     }
 }
