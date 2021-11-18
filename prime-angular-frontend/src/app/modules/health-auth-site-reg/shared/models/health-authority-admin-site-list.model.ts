3f789231d92dcd4d5f05fcc384076e03bdc3d9c1
Martin Pultz <mtpultz@users.noreply.github.com>
2021-11-18 00:41:13 -0800

    Clean up

--- prime-angular-frontend/src/app/modules/health-auth-site-reg/shared/models/health-authority-admin-site-list.model.ts
+++ prime-angular-frontend/src/app/modules/health-auth-site-reg/shared/models/health-authority-admin-site-list.model.ts
@@ -14,6 +14,7 @@ export interface HealthAuthorityAdminSiteListDto extends BaseHealthAuthoritySite
   readonly authorizedUserEmail: string;
   readonly adjudicatorIdir: string;
 }
+
 export class HealthAuthoritySiteAdminList extends AbstractBaseHealthAuthoritySite implements HealthAuthorityAdminSiteListDto {
   constructor(
     public id: number,
