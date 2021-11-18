455e622e2d1c594b44a0b1e7f17a66592addc56b
Johnny Liu <johnny.liu@nttdata.com>
2021-11-17 22:33:12 -0800

    added db migration

---
+++ prime-dotnet-webapi/Migrations/20211118060758_AddEnrolmentLinkBusinessEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Prime.Migrations
+{
+    public partial class AddEnrolmentLinkBusinessEvent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "BusinessEventTypeLookup",
+                columns: new[] { "Code", "Name" },
+                values: new object[] { 10, "Paper Enrolment Link" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "BusinessEventTypeLookup",
+                keyColumn: "Code",
+                keyValue: 10);
+        }
+    }
+}
