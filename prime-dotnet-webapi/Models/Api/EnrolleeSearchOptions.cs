2e5456edf7a2ea1789c3ba659f61d4243e324e6f
James <jameshollinger.ee@gmail.com>
2021-11-18 05:03:37 -0800

    cleanup

--- prime-dotnet-webapi/Models/Api/EnrolleeSearchOptions.cs
+++ prime-dotnet-webapi/Models/Api/EnrolleeSearchOptions.cs
@@ -2,8 +2,8 @@ namespace Prime.Models.Api
 {
     public class EnrolleeSearchOptions
     {
-        public int? StatusCode { get; set; }
         public bool? IsLinkedPaperEnrolment { get; set; }
+        public int? StatusCode { get; set; }
         public string TextSearch { get; set; }
     }
 }
