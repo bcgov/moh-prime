aa40fa9585cb37f5cef14dfed7284e29de6c211f
James <jameshollinger.ee@gmail.com>
2021-11-23 21:34:42 -0800

    dont need

--- pip-dotnet-webapi/PipConfiguration.cs
+++ pip-dotnet-webapi/PipConfiguration.cs
@@ -10,9 +10,6 @@ namespace Pip
         public static bool IsDevelopment() => EnvironmentName == Environments.Development;
         private static readonly string? EnvironmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
-        public string FrontendUrl { get; set; }
-        public string BackendUrl { get; set; }
-
         public ConnectionStringConfiguration ConnectionStrings { get; set; } = new ConnectionStringConfiguration();
     }
 }
