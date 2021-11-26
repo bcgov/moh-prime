2d64c7c781a5ba1c84ea95d98f961bd42b8e9d18
James <jameshollinger.ee@gmail.com>
2021-11-25 21:37:50 -0800

    reorder

--- pip-dotnet-webapi/Models/Lookups/Province.cs
+++ pip-dotnet-webapi/Models/Lookups/Province.cs
@@ -1,7 +1,7 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Pidp.Models.Lookups
 {
