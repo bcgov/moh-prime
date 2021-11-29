73a02ade3bccf28b114b9ffdcb9f03817709b204
James <jameshollinger.ee@gmail.com>
2021-11-29 12:15:43 -0800

    update conf

--- pidp-webapi/PidpConfiguration.cs
+++ pidp-webapi/PidpConfiguration.cs
@@ -2,18 +2,18 @@ namespace Pidp
 {
     public class PidpConfiguration
     {
-        public static PidpConfiguration? Current { get; set; }
+        public static PidpConfiguration Current { get; set; } = default!;
 
         public static bool IsDevelopment() => EnvironmentName == Environments.Development;
         private static readonly string? EnvironmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
-        public ConnectionStringConfiguration ConnectionStrings { get; set; } = new ConnectionStringConfiguration();
+        public ConnectionStringConfiguration ConnectionStrings { get; set; } = default!;
 
 
         // ------- Configuration Objects -------
         public class ConnectionStringConfiguration
         {
-            public string PidpDatabase { get; set; }
+            public string PidpDatabase { get; set; } = string.Empty;
         }
     }
 }
