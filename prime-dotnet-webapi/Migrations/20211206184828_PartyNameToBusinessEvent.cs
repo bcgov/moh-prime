39ad35da4becb44957a6a80626c857c95c15582f
Panos <paahndev@gmail.com>
2021-12-06 14:43:24 -0800

    migrations

---
+++ prime-dotnet-webapi/Migrations/20211206184828_PartyNameToBusinessEvent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Prime.Migrations
+{
+    public partial class PartyNameToBusinessEvent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PartyName",
+                table: "BusinessEvent",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PartyName",
+                table: "BusinessEvent");
+        }
+    }
+}
