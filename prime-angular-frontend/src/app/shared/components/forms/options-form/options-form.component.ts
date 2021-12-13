ab58acb3fcccb0e1f6f6d14393bd6739e7857690
Nathan <Hogman>
2021-12-13 15:36:36 -0800

    revert changes to options

--- prime-angular-frontend/src/app/shared/components/forms/options-form/options-form.component.ts
+++ prime-angular-frontend/src/app/shared/components/forms/options-form/options-form.component.ts
@@ -53,12 +53,6 @@ export class OptionsFormComponent implements OnInit {
    * Whether to use select (default) or autocomplete.
    */
   @Input() public selectOrAutocomplete: 'select' | 'autocomplete';
-
-  /**
-   * @description
-   * Whether the input is readonly
-   */
-  @Input() public readonly: boolean;
   /**
    * @description
    * List of options that includes the selected option.
