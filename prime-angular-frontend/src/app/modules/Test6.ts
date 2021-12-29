aaa42c97e7b4f54b1f66b4adcf92c7420f1fd174
Amir <ameer.kubba@yahoo.com>
2021-12-29 12:04:46 -0800

    readded useless code

---
+++ prime-angular-frontend/src/app/modules/Test6.ts
@@ -0,0 +1,58 @@
+export class TestingStuff6 {
+    add(a: number, b: number): number {
+        if (a > b) {
+            return a + b;
+        }
+
+        if (a < b) {
+            return a + b;
+        }
+
+        if (a == b) {
+            return a + b;
+        }
+    }
+
+    positiveSubtract(a: number, b: number): number {
+        if (a > b) {
+            return a - b;
+        }
+
+        if (a < b) {
+            return b - a;
+        }
+
+        if (a == b) {
+            return a + b;
+        }
+    }
+
+    multiply(a: number, b: number): number {
+        if (a > b) {
+            return a * b;
+        }
+
+        if (a < b) {
+            return a * b;
+        }
+
+        if (a == b) {
+            return a * b;
+        }
+    }
+
+    
+    divide(a: number, b: number): number {
+        if (a > b) {
+            return a / b;
+        }
+
+        if (a < b) {
+            return b / b;
+        }
+
+        if (a == b) {
+            return a / b;
+        }
+    }
+}
