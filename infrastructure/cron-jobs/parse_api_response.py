bf9b2988f3a7048c69e9be95e07329b75693cff8
Alan Leung <Alan.Leung@nttdata.com>
2021-12-30 16:19:24 -0800

    Better logging for improved visibility

--- infrastructure/cron-jobs/parse_api_response.py
+++ infrastructure/cron-jobs/parse_api_response.py
@@ -7,13 +7,14 @@ json_as_dict = None
 try:
     json_as_dict = json.load(sys.stdin);
 except JSONDecodeError:
-    # Abnormal response from API.
+    print('Non-JSON response from API.', file=sys.stderr)
     # Remove in case it exists from previous loop iteration 
     os.remove(IS_THERE_MORE_DATA_FILE_LOCATION)
     sys.exit(1)
 
 output = csv.writer(sys.stdout);
-# Convert JSON to CSV
+# Using Standard Error despite following not being an error so as to not interfere with Standard Output/Input expected by downstream process 
+print('Converting JSON to CSV ...', file=sys.stderr)
 # Note that the columns in the CSV output need to match the invocation of the Postgres COPY command
 # Ultimately the `pnetTransactions` JSON fields need to be in the expected order 
 for row in json_as_dict['pnetTransactions']:
@@ -22,7 +23,10 @@ for row in json_as_dict['pnetTransactions']:
     row['providerSoftwareVer'] = row['providerSoftwareVer'].strip()
     output.writerow(row.values());
 
+isThereMoreData = json_as_dict['isThereMoreData']
+# Using Standard Error despite following not being an error so as to not interfere with Standard Output/Input expected by downstream process 
+print(f"Writing to file:  Is there more data?  {isThereMoreData}", file=sys.stderr)
 # Let external process know whether there are more results according to JSON response
 f = open(IS_THERE_MORE_DATA_FILE_LOCATION, "w")
-f.write(json_as_dict['isThereMoreData'])
+f.write(isThereMoreData)
 f.close()
