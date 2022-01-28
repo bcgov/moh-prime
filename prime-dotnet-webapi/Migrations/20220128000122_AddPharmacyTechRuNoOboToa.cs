982a64a8b938ed76ccda7690d410db873bed5f7f
Johnny Liu <johnny.liu@nttdata.com>
2022-01-27 16:11:15 -0800

    added place holder for the new toa template

---
+++ prime-dotnet-webapi/Migrations/20220128000122_AddPharmacyTechRuNoOboToa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+using Prime.Models;
+
+namespace Prime.Migrations
+{
+    public partial class AddPharmacyTechRuNoOboToa : Migration
+    {
+        private static readonly DateTimeOffset SEEDING_DATE = DateTimeOffset.Parse("2019-09-16 -7:00");
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AgreementVersion",
+                columns: new[] { "CreatedUserId", "CreatedTimeStamp", "UpdatedUserId", "UpdatedTimeStamp", "Text", "AgreementType", "EffectiveDate" },
+                values: new object[] { Guid.Empty, SEEDING_DATE, Guid.Empty, SEEDING_DATE,
+                    @"TOA TEMPLATE CONTENT",
+                    AgreementType.PharmacyTechRuNoOboTOA, SEEDING_DATE });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "BusinessEventTypeLookup",
+                keyColumn: "Id",
+                keyValue: 18);
+        }
+    }
+}
