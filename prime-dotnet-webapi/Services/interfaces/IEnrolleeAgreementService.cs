12dc6082d55c99cc80a1d102ad982e1f0cb03f5c
Alan Leung <alan.leung57@gmail.com>
2022-01-31 07:18:46 -0800

    PRIME-2080 User needs to sign a new TOA (#1963)
    * Added IsAgreementTypeIdenticalAsync service that can be called from front-end
    
    * Check for OBO to RU change
    
    * Front-end changes to implement PRIME-2080
    
    * Fix indent issues
    
    * Addressing PR feedback
    
    * OBO to RU change means new TOA to sign even if otherwise "minor update"
    
    * Update overview.component.html
    
    * PR feedback
    
    * Remove unnecessary nullable type marker
    
    * Remove logic done elsewhere
    
    * Remove unnecessary code
    
    * Changes as per PR feedback
    
    * PR feedback
    
    Co-authored-by: Alan Leung <Alan.Leung@nttdata.com>
    Co-authored-by: Nathan Hogman <59669288+hogmann@users.noreply.github.com>
    Co-authored-by: James Hollinger <39168456+james-hollinger@users.noreply.github.com>

--- prime-dotnet-webapi/Services/interfaces/IEnrolleeAgreementService.cs
+++ prime-dotnet-webapi/Services/interfaces/IEnrolleeAgreementService.cs
@@ -13,5 +13,11 @@ namespace Prime.Services
         Task CreateEnrolleeAgreementAsync(int enrolleeId);
         Task AcceptCurrentEnrolleeAgreementAsync(int enrolleeId);
         Task ExpireCurrentEnrolleeAgreementAsync(int enrolleeId);
+        /// <summary>
+        /// Returns whether the enrollee's current agreement type is OBO and changing to RU if their agreement would be
+        /// assigned as of today.  Returns <c>true</c> if such a change, <c>false</c> if not such a change OR
+        /// if cannot be automatically determined which agreement would be assigned OR enrollee does yet have an agreement
+        /// </summary>
+        Task<bool> IsOboToRuAgreementTypeChangeAsync(int enrolleeId);
     }
 }
