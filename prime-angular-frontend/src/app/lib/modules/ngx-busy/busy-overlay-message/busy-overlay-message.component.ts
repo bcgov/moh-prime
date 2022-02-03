68212207452bb1a1b2fb0e7b7b518687c5b38de4
Amir <ameer.kubba@yahoo.com>
2022-02-03 11:18:36 -0800

    removed observers, now using member variables only

--- prime-angular-frontend/src/app/lib/modules/ngx-busy/busy-overlay-message/busy-overlay-message.component.ts
+++ prime-angular-frontend/src/app/lib/modules/ngx-busy/busy-overlay-message/busy-overlay-message.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 
 import { InstanceConfigHolderService } from 'ng-busy';
 import { NgBusyService } from '../ng-busy.service';
@@ -7,21 +7,17 @@ import { NgBusyService } from '../ng-busy.service';
   templateUrl: './busy-overlay-message.component.html',
   styleUrls: ['./busy-overlay-message.component.scss']
 })
-export class BusyOverlayMessageComponent implements OnInit {
-  public message: string;
-  public isShowSpinner;
-
+export class BusyOverlayMessageComponent {
   constructor(
     @Inject('instanceConfigHolder') private instanceConfigHolder: InstanceConfigHolderService,
     private ngBusyService: NgBusyService
   ) { }
 
-  ngOnInit(): void {
-    this.ngBusyService.message$.subscribe((result: string) => {
-      this.message = result;
-    });
-    this.ngBusyService.isShowSpinner$.subscribe((result: boolean) => {
-      this.isShowSpinner = result;
-    })
+  public get message() {
+    return this.ngBusyService.message;
+  }
+
+  public get isShowSpinner() {
+    return this.ngBusyService.isShowSpinner
   }
 }
--- prime-angular-frontend/src/app/lib/modules/ngx-busy/ng-busy.service.ts
+++ prime-angular-frontend/src/app/lib/modules/ngx-busy/ng-busy.service.ts
@@ -1,35 +1,20 @@
 import { Injectable } from '@angular/core';
 
-import { Observable, Observer } from 'rxjs';
-import { share } from 'rxjs/operators';
-
-
 @Injectable({
   providedIn: 'root',
 })
 
 export class NgBusyService {
-  public message$: Observable<string>;
-  public isShowSpinner$: Observable<boolean>;
-  private message: string;
-  private isShowSpinner: boolean;
-  private messageObserver: Observer<string>;
-  private spinnerObserver: Observer<boolean>;
+  public message: string;
+  public isShowSpinner: boolean;
 
-  constructor() {
-    this.isShowSpinner = false;
-    this.message = '';
-    this.message$ = new Observable<string>(m => this.messageObserver = m).pipe(share());
-    this.isShowSpinner$ = new Observable<boolean>(m => this.spinnerObserver = m).pipe(share());
-  };
+  constructor() { };
 
   public updateMessage(message: string): void {
     this.message = message;
-    this.messageObserver.next(this.message);
   }
 
   public showSpinner(showSpinner: boolean): void {
     this.isShowSpinner = showSpinner;
-    this.spinnerObserver.next(this.isShowSpinner);
   }
 }
