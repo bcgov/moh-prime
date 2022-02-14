ca355adb1369c012ab0671b70ca29c9ca5c5531f
Johnny Liu <johnny.liu@nttdata.com>
2022-02-14 15:06:24 -0800

    added db migration

---
+++ prime-dotnet-webapi/Migrations/20220214230051_UpdateAbsenceEmailTemplate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Prime.Migrations
+{
+    public partial class UpdateAbsenceEmailTemplate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "EmailTemplate",
+                keyColumn: "Id",
+                keyValue: 19,
+                column: "Template",
+                value: "To Whom it may concern, <br> <br> @(Model.FirstName + \" \" +  Model.LastName + \" is going to be absent \") @if (Model.End.HasValue) {@(\"from \" + Model.Start.ToShortDateString() + \" to \" + Model.End.Value.ToShortDateString() + \". Please consider deactivating the user during this period. Any access during this period by the user will be considered as an unauthorized access.\")} else {@(\"indefinitely, starting \" + Model.Start.ToShortDateString() + \". Please deactivate the user on the start date. Any access during this period by the user will be considered as an unauthorized access.\")}");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "EmailTemplate",
+                keyColumn: "Id",
+                keyValue: 19,
+                column: "Template",
+                value: "To Whom it may concern, <br> <br> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione placeat necessitatibus adipisci dicta doloribus. Ratione inventore aperiam nobis consequuntur ab, cum, numquam praesentium magnam commodi quasi in voluptates enim repellat!");
+        }
+    }
+}
