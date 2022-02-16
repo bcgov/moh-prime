0d91e77c681c1563fc3384c032caf2da4300ca61
Alan Leung <Alan.Leung@nttdata.com>
2022-02-16 13:48:07 -0800

    Displaying notice from landing page

--- prime-angular-frontend/src/app/shared/components/auth/prime-enrolment-access/prime-enrolment-access.component.ts
+++ prime-angular-frontend/src/app/shared/components/auth/prime-enrolment-access/prime-enrolment-access.component.ts
@@ -1,10 +1,14 @@
 import { Component, EventEmitter, Inject, OnInit, Output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
 
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
 import { APP_CONFIG, AppConfig } from 'app/app-config.module';
 import { ViewportService } from '@core/services/viewport.service';
+import { DialogOptions } from '@shared/components/dialogs/dialog-options.model';
+import { ConfirmDialogComponent } from '@shared/components/dialogs/confirm-dialog/confirm-dialog.component';
+import { HtmlComponent } from '@shared/components/dialogs/content/html/html.component';
 import { BannerLocationCode } from '@shared/enums/banner-location-code.enum';
 
 @UntilDestroy()
@@ -27,6 +31,7 @@ export class PrimeEnrolmentAccessComponent implements OnInit {
     @Inject(APP_CONFIG) private config: AppConfig,
     private viewportService: ViewportService,
     private route: ActivatedRoute,
+    private dialog: MatDialog
   ) {
     this.login = new EventEmitter<void>();
     this.locationCode = BannerLocationCode.ENROLMENT_LANDING_PAGE;
@@ -41,7 +46,30 @@ export class PrimeEnrolmentAccessComponent implements OnInit {
   }
 
   public onLogin() {
-    this.login.emit();
+    const data: DialogOptions = {
+      title: 'Collection of Personal Information Notice',
+      //      title: 'Collection Notice',
+      component: HtmlComponent,
+      data: {
+        content: `<p>
+            The personal information you provide to the PRIME application is collected by the British Columbia Ministry
+            of Health under the authority of s. 26(a) and 26(c) of the Freedom of Information and Protection of Privacy
+            Act (FOIPPA) and s. 22(1)(b) of the Pharmaceutical Services Act for the purpose of managing your access to,
+            and use of, PharmaNet. If you have any questions about the collection or use of this information, call
+          </p>
+          <p>
+            Director, Information and PharmaNet Innovation at <a href="tel:+${this.config.phoneNumbers.director}">${this.config.phoneNumbers.director}</a>
+            or <a href="mailto:${this.config.prime.email}">${this.config.prime.email}</a>.
+          </p>`,
+      },
+      actionText: 'Next',
+    };
+
+    this.dialog.open(ConfirmDialogComponent, { data })
+      .afterClosed()
+      .subscribe((isNext: boolean) => {
+        if (isNext) this.login.emit()
+      });
   }
 
   public ngOnInit(): void {
