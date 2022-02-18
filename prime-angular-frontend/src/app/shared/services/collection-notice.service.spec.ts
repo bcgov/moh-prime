251708240b0935e6fc1a5b1f74046be3ae2b38d9
Alan Leung <Alan.Leung@nttdata.com>
2022-02-18 12:40:01 -0800

    Fix front-end tests

--- prime-angular-frontend/src/app/shared/services/collection-notice.service.spec.ts
+++ prime-angular-frontend/src/app/shared/services/collection-notice.service.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
+import { AppConfig, APP_CONFIG } from 'app/app-config.module';
+
 import { CollectionNoticeService } from './collection-notice.service';
 
 describe('CollectionNoticeService', () => {
   let service: CollectionNoticeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: APP_CONFIG,
+          useValue: AppConfig
+        }
+      ]
+    });
     service = TestBed.inject(CollectionNoticeService);
   });
 
