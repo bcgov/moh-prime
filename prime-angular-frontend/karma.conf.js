704dba66c142196e200836253a0fc15b0154283a
Amir <ameer.kubba@yahoo.com>
2022-02-23 16:04:12 -0800

    test

--- prime-angular-frontend/karma.conf.js
+++ prime-angular-frontend/karma.conf.js
@@ -1,6 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+// test
 module.exports = function (config) {
   config.set({
     basePath: '',
