893595d1697d7ab33d089789c29ae0b1707aeb9c
Amir <ameer.kubba@yahoo.com>
2022-03-04 08:37:39 -0800

    test

--- prime-angular-frontend/src/app/modules/adjudication/test-codeclimate/test-codeclimate.component.ts
+++ prime-angular-frontend/src/app/modules/adjudication/test-codeclimate/test-codeclimate.component.ts
@@ -80,4 +80,55 @@ export class TestCodeclimateComponent implements OnInit {
       ? x / y
       : throwError('can\'t divide by zero.');
   }
+
+  public add5(x, y) {
+    return x + y;
+  }
+
+  public sub5(x, y) {
+    return x - y;
+  }
+
+  public mult5(x, y) {
+    return x * y;
+  }
+  public div5(x, y) {
+    return x > 0
+      ? x / y
+      : throwError('can\'t divide by zero.');
+  }
+
+  public add6(x, y) {
+    return x + y;
+  }
+
+  public sub6(x, y) {
+    return x - y;
+  }
+
+  public mult6(x, y) {
+    return x * y;
+  }
+  public div6(x, y) {
+    return x > 0
+      ? x / y
+      : throwError('can\'t divide by zero');
+  }
+
+  public add7(x, y) {
+    return x + y;
+  }
+
+  public sub7(x, y) {
+    return x - y;
+  }
+
+  public mult7(x, y) {
+    return x * y;
+  }
+  public div7(x, y) {
+    return x > 0
+      ? x / y
+      : throwError('can\'t divide by zero.');
+  }
 }
