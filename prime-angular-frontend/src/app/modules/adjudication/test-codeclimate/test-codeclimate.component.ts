54445aef04659e6122d1f79d3fd7b52118abceee
Amir <ameer.kubba@yahoo.com>
2022-03-04 18:41:26 -0800

    reduce code

--- prime-angular-frontend/src/app/modules/adjudication/test-codeclimate/test-codeclimate.component.ts
+++ prime-angular-frontend/src/app/modules/adjudication/test-codeclimate/test-codeclimate.component.ts
@@ -13,71 +13,71 @@ export class TestCodeclimateComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  public add(x, y) {
-    return x + y;
-  }
+  // public add(x, y) {
+  //   return x + y;
+  // }
 
-  public sub(x, y) {
-    return x - y;
-  }
+  // public sub(x, y) {
+  //   return x - y;
+  // }
 
-  public mult(x, y) {
-    return x * y;
-  }
-  public div(x, y) {
-    return x > 0
-      ? x / y
-      : throwError('can\'t divide by zero.');
-  }
+  // public mult(x, y) {
+  //   return x * y;
+  // }
+  // public div(x, y) {
+  //   return x > 0
+  //     ? x / y
+  //     : throwError('can\'t divide by zero.');
+  // }
 
-  public add2(x, y) {
-    return x + y;
-  }
+  // public add2(x, y) {
+  //   return x + y;
+  // }
 
-  public sub2(x, y) {
-    return x - y;
-  }
+  // public sub2(x, y) {
+  //   return x - y;
+  // }
 
-  public mult2(x, y) {
-    return x * y;
-  }
-  public div2(x, y) {
-    return x > 0
-      ? x / y
-      : throwError('can\'t divide by zero.');
-  }
+  // public mult2(x, y) {
+  //   return x * y;
+  // }
+  // public div2(x, y) {
+  //   return x > 0
+  //     ? x / y
+  //     : throwError('can\'t divide by zero.');
+  // }
 
-  public add3(x, y) {
-    return x + y;
-  }
+  // public add3(x, y) {
+  //   return x + y;
+  // }
 
-  public sub3(x, y) {
-    return x - y;
-  }
+  // public sub3(x, y) {
+  //   return x - y;
+  // }
 
-  public mult3(x, y) {
-    return x * y;
-  }
-  public div3(x, y) {
-    return x > 0
-      ? x / y
-      : throwError('can\'t divide by zero');
-  }
+  // public mult3(x, y) {
+  //   return x * y;
+  // }
+  // public div3(x, y) {
+  //   return x > 0
+  //     ? x / y
+  //     : throwError('can\'t divide by zero');
+  // }
 
-  public add4(x, y) {
-    return x + y;
-  }
+  // public add4(x, y) {
+  //   return x + y;
+  // }
 
-  public sub4(x, y) {
-    return x - y;
-  }
+  // public sub4(x, y) {
+  //   return x - y;
+  // }
 
-  public mult4(x, y) {
-    return x * y;
-  }
-  public div4(x, y) {
-    return x > 0
-      ? x / y
-      : throwError('can\'t divide by zero.');
-  }
+  // public mult4(x, y) {
+  //   return x * y;
+  // }
+  // public div4(x, y) {
+  //   return x > 0
+  //     ? x / y
+  //     : throwError('can\'t divide by zero.');
+  // }
 }
