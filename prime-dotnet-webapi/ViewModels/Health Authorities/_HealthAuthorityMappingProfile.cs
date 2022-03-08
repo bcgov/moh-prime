ab3ff0ff799617d121392385422db0259d88b536
Alan Leung <Alan.Leung@nttdata.com>
2022-03-08 08:39:45 -0800

    Able to display Health Authority summary

--- prime-dotnet-webapi/ViewModels/Health Authorities/_HealthAuthorityMappingProfile.cs
+++ prime-dotnet-webapi/ViewModels/Health Authorities/_HealthAuthorityMappingProfile.cs
@@ -6,6 +6,7 @@ using Prime.Models.HealthAuthorities;
 using Prime.ViewModels.Parties;
 using Prime.ViewModels.HealthAuthorities;
 using Prime.ViewModels.HealthAuthoritySites;
+using System;
 
 namespace Prime.ViewModels.Profiles
 {
@@ -27,16 +28,35 @@ namespace Prime.ViewModels.Profiles
 
             CreateMap<Contact, PrivacyOfficerViewModel>();
             CreateMap<Contact, HealthAuthorityContactViewModel>();
-            // TODO:
-            CreateMap<Contact, TechnicalSupportContactViewModel>()
-                .ForMember(dest => dest.VendorsWorkedWith, opt => opt.MapFrom(o => new int[] { 1, 2, 3 }));
+
             CreateMap<HealthAuthorityContact, HealthAuthorityContactViewModel>()
+                // See https://automapperdocs.readthedocs.io/en/latest/Mapping-inheritance.html
+                .IncludeAllDerived()
                 .IncludeMembers(src => src.Contact)
                 .ForMember(dest => dest.ContactId, opt => opt.MapFrom(src => src.Contact.Id));
+            // TODO:
+            CreateMap<HealthAuthorityTechnicalSupport, TechnicalSupportContactViewModel>()
+                .AfterMap<MapVendorsWorkedWithAction>();
 
             CreateMap<AuthorizedUser, AuthorizedUserViewModel>()
                 .IncludeMembers(src => src.Party);
             CreateMap<Party, AuthorizedUserViewModel>();
+        }
+    }
+
+
+    public class MapVendorsWorkedWithAction : IMappingAction<HealthAuthorityTechnicalSupport, TechnicalSupportContactViewModel>
+    {
+        private readonly ApiDbContext _context;
+
+        public MapVendorsWorkedWithAction(ApiDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public void Process(HealthAuthorityTechnicalSupport source, TechnicalSupportContactViewModel destination, ResolutionContext context)
+        {
+            System.Diagnostics.Debug.WriteLine($"Inside {nameof(Process)}");
         }
     }
 }
