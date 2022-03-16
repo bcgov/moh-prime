e204c08ab63d52257a234a344d2ad5c9318807ed
Amir <ameer.kubba@yahoo.com>
2022-03-16 09:57:42 -0700

    wip

--- prime-angular-frontend/src/app/modules/adjudication/pages/enrollee-events/enrollee-events.component.spec.ts
+++ prime-angular-frontend/src/app/modules/adjudication/pages/enrollee-events/enrollee-events.component.spec.ts
@@ -1,3 +1,6 @@
+import faker from 'faker';
+
+import { of } from 'rxjs';
 import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
@@ -11,10 +14,15 @@ import { AdjudicationModule } from '@adjudication/adjudication.module';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { BusinessEventTypeEnum } from '@adjudication/shared/models/business-event-type.model';
+import { BusinessEvent } from '@adjudication/shared/models/business-event.model';
+import { DateContent } from '@adjudication/shared/components/dated-content-table/dated-content-table.component';
 
 describe('EnrolleeEventsComponent', () => {
   let component: EnrolleeEventsComponent;
   let fixture: ComponentFixture<EnrolleeEventsComponent>;
+
+  let mockBusinessEvents: BusinessEvent[];
+  let mockDateContent: DateContent[];
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -40,6 +48,26 @@ describe('EnrolleeEventsComponent', () => {
     fixture = TestBed.createComponent(EnrolleeEventsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    mockBusinessEvents = [
+      {
+        enrolleeId: faker.random.number(),
+        adminId: faker.random.number(),
+        adminIDIR: faker.random.word(),
+        businessEventTypeCode: BusinessEventTypeEnum.ADMIN_ACTION_CODE,
+        eventDate: null,
+        description: faker.random.words(),
+        partyName: faker.random.word()
+      },
+      {
+        enrolleeId: faker.random.number(),
+        adminId: faker.random.number(),
+        adminIDIR: faker.random.word(),
+        businessEventTypeCode: BusinessEventTypeEnum.ADMIN_ACTION_CODE,
+        eventDate: null,
+        description: faker.random.words(),
+        partyName: faker.random.word()
+      }
+    ];
   });
 
   it('should create', () => {
@@ -47,19 +75,73 @@ describe('EnrolleeEventsComponent', () => {
   });
 
   describe('testing getBusinessEvents()', () => {
-    describe('with businessEventTypes set to undefined', () => {
-      it('component businessEventsType should not change', () => { });
+    describe('passing \'undefined\' businessEventsTypes', () => {
+      it('should keep the same value as the original business events', () => {
+
+        component.getBusinessEvents(undefined);
+        expect((component as any).businessEventTypes).toEqual([]);
+      });
     });
 
+    describe('passing 1 item in the businessEventsTypes array', () => {
+      it('should change the value of the original businessEventsTypes', () => {
+        const mockBusinessEventType = [BusinessEventTypeEnum.STATUS_CHANGE_CODE];
 
-    describe('with businessEventType set properly', () => {
-      it('component businessEventsType should change to the passed-in value', () => { });
+        component.getBusinessEvents(mockBusinessEventType);
+        expect((component as any).businessEventTypes).not.toEqual([]);
+        expect((component as any).businessEventTypes).toEqual(mockBusinessEventType);
+      });
     });
 
-    describe('with getEnrolleeBusinessEvents completing', () => {
-      it('should return an array of {date, content, name} objects')
+    describe('with getEnrolleeBusinessEvents() returning no BusinessEvents', () => {
+      it('should return an empty array', () => {
+        spyOn((component as any).adjudicationResource, 'getEnrolleeBusinessEvents')
+          .and.returnValue(of([]));
+        mockDateContent = [];
+
+        component.getBusinessEvents([]);
+        component.businessEvents$.subscribe((dateContent) => {
+          expect(dateContent).toEqual(mockDateContent);
+        });
+      });
     });
 
-    describe('with getEnrolleeBusinessEvents not completing', () => { });
+    describe('with getEnrolleeBusinessEvents() returning 1 BusinessEvent', () => {
+      it('should return an array of one BusinessEvent', () => {
+        spyOn((component as any).adjudicationResource, 'getEnrolleeBusinessEvents')
+          .and.returnValue(of(mockBusinessEvents.slice(0, 1)));
+        mockDateContent = [
+          {
+            date: mockBusinessEvents[0].eventDate,
+            content: mockBusinessEvents[0].description,
+            name: mockBusinessEvents[0].adminIDIR
+          }
+        ];
+
+        component.getBusinessEvents([]);
+        component.businessEvents$.subscribe((dateContent) => {
+          expect(dateContent).toEqual(mockDateContent);
+        });
+      });
+    });
+
+    describe('with getEnrolleeBusinessEvents() returning multiple BusinessEvents', () => {
+      it('should return an array of equal size to the one returned from getEnrolleeBusinessEvents()', () => {
+        spyOn((component as any).adjudicationResource, 'getEnrolleeBusinessEvents')
+          .and.returnValue(of(mockBusinessEvents));
+        mockDateContent = mockBusinessEvents.map((businessEvent: BusinessEvent) => {
+          return {
+            date: businessEvent.eventDate,
+            content: businessEvent.description,
+            name: businessEvent.adminIDIR
+          }
+        });
+
+        component.getBusinessEvents([]);
+        component.businessEvents$.subscribe((dateContent) => {
+          expect(dateContent).toEqual(mockDateContent);
+        });
+      });
+    });
   });
 });
--- prime-angular-frontend/src/app/modules/adjudication/pages/enrollee-toa-maintenance-list-page/enrollee-toa-maintenance-list-page.component.spec.ts
+++ prime-angular-frontend/src/app/modules/adjudication/pages/enrollee-toa-maintenance-list-page/enrollee-toa-maintenance-list-page.component.spec.ts
@@ -7,10 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
 import { NgxMaterialModule } from '@lib/modules/ngx-material/ngx-material.module';
 import { EnrolleeToaMaintenanceListPageComponent } from './enrollee-toa-maintenance-list-page.component';
+import faker from 'faker';
 
-describe('EnrolleeToaMaintenanceListPageComponent', () => {
+fdescribe('EnrolleeToaMaintenanceListPageComponent', () => {
   let component: EnrolleeToaMaintenanceListPageComponent;
   let fixture: ComponentFixture<EnrolleeToaMaintenanceListPageComponent>;
+
+  let spyOnRouteRelativeTo;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -34,9 +37,36 @@ describe('EnrolleeToaMaintenanceListPageComponent', () => {
     fixture = TestBed.createComponent(EnrolleeToaMaintenanceListPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    spyOnRouteRelativeTo = spyOn((component as any).routeUtils, 'routeRelativeTo');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('testing onBack()', () => {
+    it('should call routeRelativeTo with [\'../\']', () => {
+
+      component.onBack();
+      expect(spyOnRouteRelativeTo).toHaveBeenCalledOnceWith(['../']);
+    });
+  });
+
+  describe('testing onView()', () => {
+    it('should call routeRelativeTo with the passed in Id', () => {
+      const mockId = faker.random.number();
+
+      component.onView(mockId);
+      expect(spyOnRouteRelativeTo).toHaveBeenCalledOnceWith([mockId]);
+    });
+  });
+
+  describe('testing getToaCardProperties()', () => {
+    it('should return a key/value pair of ', () => {
+      const mockId = faker.random.number();
+
+      component.onView(mockId);
+      expect(spyOnRouteRelativeTo).toHaveBeenCalledOnceWith([mockId]);
+    });
   });
 });
