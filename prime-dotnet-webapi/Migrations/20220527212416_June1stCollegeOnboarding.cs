cf72ecfebe4d3952c64eea2130e48ef1fc981cc9
Nathan <Hogman>
2022-05-27 15:00:06 -0700

    migration

---
+++ prime-dotnet-webapi/Migrations/20220527212416_June1stCollegeOnboarding.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Prime.Migrations
+{
+    public partial class June1stCollegeOnboarding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "CollegeLicense",
+                keyColumns: new[] { "CollegeCode", "LicenseCode" },
+                keyValues: new object[] { 11, 64 });
+
+            migrationBuilder.InsertData(
+                table: "LicenseDetail",
+                columns: new[] { "Id", "CreatedTimeStamp", "CreatedUserId", "EffectiveDate", "LicenseCode", "LicensedToProvideCare", "Manual", "NamedInImReg", "Prefix", "PrescriberIdType", "UpdatedTimeStamp", "UpdatedUserId", "Validate" },
+                values: new object[,]
+                {
+                    { 111, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2022, 6, 1, 8, 0, 0, 0, DateTimeKind.Utc), 59, true, false, true, "93", null, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), true },
+                    { 112, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2022, 6, 1, 8, 0, 0, 0, DateTimeKind.Utc), 65, true, false, true, "93", null, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), true },
+                    { 113, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2022, 6, 1, 8, 0, 0, 0, DateTimeKind.Utc), 66, true, false, true, "93", null, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), true },
+                    { 114, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2022, 6, 1, 8, 0, 0, 0, DateTimeKind.Utc), 67, true, false, true, "93", null, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), true },
+                    { 115, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2022, 6, 1, 8, 0, 0, 0, DateTimeKind.Utc), 70, true, false, true, "95", null, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), true }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "LicenseLookup",
+                keyColumn: "Code",
+                keyValue: 59,
+                column: "Name",
+                value: "Full - Podiatric Surgeon");
+
+            migrationBuilder.UpdateData(
+                table: "LicenseLookup",
+                keyColumn: "Code",
+                keyValue: 70,
+                column: "Name",
+                value: "Full Registrations");
+
+            migrationBuilder.InsertData(
+                table: "LicenseLookup",
+                columns: new[] { "Code", "Name", "Weight" },
+                values: new object[,]
+                {
+                    { 75, "Restricted to Specialty", 2 },
+                    { 76, "Academic", 3 },
+                    { 77, "Academic (Grand-parented)", 4 },
+                    { 78, "Full", 1 },
+                    { 79, "Non-practicing", 2 },
+                    { 80, "Temporary", 3 },
+                    { 81, "Student", 4 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "CollegeLicense",
+                columns: new[] { "CollegeCode", "LicenseCode", "CollegeLicenseGroupingCode" },
+                values: new object[,]
+                {
+                    { 7, 75, null },
+                    { 7, 76, null },
+                    { 7, 77, null },
+                    { 11, 78, null },
+                    { 11, 79, null },
+                    { 11, 80, null },
+                    { 11, 81, null }
+                });
+
+            migrationBuilder.InsertData(
+                table: "LicenseDetail",
+                columns: new[] { "Id", "CreatedTimeStamp", "CreatedUserId", "EffectiveDate", "LicenseCode", "LicensedToProvideCare", "Manual", "NamedInImReg", "Prefix", "PrescriberIdType", "UpdatedTimeStamp", "UpdatedUserId", "Validate" },
+                values: new object[,]
+                {
+                    { 116, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2022, 6, 1, 8, 0, 0, 0, DateTimeKind.Utc), 75, true, false, true, "95", null, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), true },
+                    { 117, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2022, 6, 1, 8, 0, 0, 0, DateTimeKind.Utc), 76, true, false, true, "95", null, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), true },
+                    { 118, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2022, 6, 1, 8, 0, 0, 0, DateTimeKind.Utc), 77, true, false, true, "95", null, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), true },
+                    { 119, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2022, 6, 1, 8, 0, 0, 0, DateTimeKind.Utc), 78, true, false, true, "97", null, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), true },
+                    { 120, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2022, 6, 1, 8, 0, 0, 0, DateTimeKind.Utc), 79, false, false, false, "97", null, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), true },
+                    { 121, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2022, 6, 1, 8, 0, 0, 0, DateTimeKind.Utc), 80, true, false, true, "97", null, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), true },
+                    { 122, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2022, 6, 1, 8, 0, 0, 0, DateTimeKind.Utc), 81, true, false, true, "97", null, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), true }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "CollegeLicense",
+                keyColumns: new[] { "CollegeCode", "LicenseCode" },
+                keyValues: new object[] { 7, 75 });
+
+            migrationBuilder.DeleteData(
+                table: "CollegeLicense",
+                keyColumns: new[] { "CollegeCode", "LicenseCode" },
+                keyValues: new object[] { 7, 76 });
+
+            migrationBuilder.DeleteData(
+                table: "CollegeLicense",
+                keyColumns: new[] { "CollegeCode", "LicenseCode" },
+                keyValues: new object[] { 7, 77 });
+
+            migrationBuilder.DeleteData(
+                table: "CollegeLicense",
+                keyColumns: new[] { "CollegeCode", "LicenseCode" },
+                keyValues: new object[] { 11, 78 });
+
+            migrationBuilder.DeleteData(
+                table: "CollegeLicense",
+                keyColumns: new[] { "CollegeCode", "LicenseCode" },
+                keyValues: new object[] { 11, 79 });
+
+            migrationBuilder.DeleteData(
+                table: "CollegeLicense",
+                keyColumns: new[] { "CollegeCode", "LicenseCode" },
+                keyValues: new object[] { 11, 80 });
+
+            migrationBuilder.DeleteData(
+                table: "CollegeLicense",
+                keyColumns: new[] { "CollegeCode", "LicenseCode" },
+                keyValues: new object[] { 11, 81 });
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 111);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 112);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 113);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 114);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 115);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 116);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 117);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 118);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 119);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 120);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 121);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 122);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseLookup",
+                keyColumn: "Code",
+                keyValue: 75);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseLookup",
+                keyColumn: "Code",
+                keyValue: 76);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseLookup",
+                keyColumn: "Code",
+                keyValue: 77);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseLookup",
+                keyColumn: "Code",
+                keyValue: 78);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseLookup",
+                keyColumn: "Code",
+                keyValue: 79);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseLookup",
+                keyColumn: "Code",
+                keyValue: 80);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseLookup",
+                keyColumn: "Code",
+                keyValue: 81);
+
+            migrationBuilder.InsertData(
+                table: "CollegeLicense",
+                columns: new[] { "CollegeCode", "LicenseCode", "CollegeLicenseGroupingCode" },
+                values: new object[] { 11, 64, null });
+
+            migrationBuilder.UpdateData(
+                table: "LicenseLookup",
+                keyColumn: "Code",
+                keyValue: 59,
+                column: "Name",
+                value: "Podiatric Surgeon");
+
+            migrationBuilder.UpdateData(
+                table: "LicenseLookup",
+                keyColumn: "Code",
+                keyValue: 70,
+                column: "Name",
+                value: "Practicing Dentist");
+        }
+    }
+}
