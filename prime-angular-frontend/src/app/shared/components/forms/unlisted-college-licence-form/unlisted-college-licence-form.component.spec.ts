e4c0dc7252ce0462e280f8fbdd73427a921d7760
Panos <paahndev@gmail.com>
2022-07-28 13:41:50 -0700

    tests WIP

--- prime-angular-frontend/src/app/shared/components/forms/unlisted-college-licence-form/unlisted-college-licence-form.component.spec.ts
+++ prime-angular-frontend/src/app/shared/components/forms/unlisted-college-licence-form/unlisted-college-licence-form.component.spec.ts
@@ -1,6 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { NgxMaskModule } from 'ngx-mask';
+import { KeycloakService } from 'keycloak-angular';
+
+import { MockConfigService } from 'test/mocks/mock-config.service';
+import { MockAuthService } from 'test/mocks/mock-auth.service';
 
 import { UnlistedCollegeLicenceFormComponent } from './unlisted-college-licence-form.component';
+import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
+import { ConfigService } from '@config/config.service';
+import { NgxContextualHelpModule } from '@lib/modules/ngx-contextual-help/ngx-contextual-help.module';
+import { NgxMaterialModule } from '@lib/modules/ngx-material/ngx-material.module';
+import { AuthService } from '@auth/shared/services/auth.service';
+import { RegulatoryFormState } from '@paper-enrolment/pages/regulatory-page/regulatory-form-state.class';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 describe('UnlistedCollegeLicenceFormComponent', () => {
   let component: UnlistedCollegeLicenceFormComponent;
@@ -8,16 +27,48 @@ describe('UnlistedCollegeLicenceFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UnlistedCollegeLicenceFormComponent ]
+      imports: [
+        BrowserAnimationsModule,
+        NgxContextualHelpModule,
+        HttpClientTestingModule,
+        RouterTestingModule,
+        NgxMaskModule.forRoot(),
+        NgxMaterialModule,
+        ReactiveFormsModule,
+        MatDatepickerModule
+      ],
+      declarations: [ UnlistedCollegeLicenceFormComponent ],
+      providers: [
+        {
+          provide: APP_CONFIG,
+          useValue: APP_DI_CONFIG
+        },
+        {
+          provide: ConfigService,
+          useClass: MockConfigService
+        },
+        {
+          provide: AuthService,
+          useClass: MockAuthService
+        },
+        RegulatoryFormState,
+        KeycloakService
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
     .compileComponents();
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UnlistedCollegeLicenceFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  beforeEach(inject(
+    [RegulatoryFormState],
+    (
+      regulatoryFormState: RegulatoryFormState
+    ) => {
+      fixture = TestBed.createComponent(UnlistedCollegeLicenceFormComponent);
+      component = fixture.componentInstance;
+      component.form = regulatoryFormState.buildUnlistedCollegeCertificationForm();
+      fixture.detectChanges();
+    }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
