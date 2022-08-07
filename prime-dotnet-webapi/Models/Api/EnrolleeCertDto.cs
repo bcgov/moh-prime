7aebae0032aa64ade0a9dccac6279f0c744c1645
Alan Leung <alan.leung57@gmail.com>
2022-08-06 22:09:08 -0600

    PRIME-2239 Update HpdidLookup for Provisioners (#2181)
    * Changes to implement PRIME-2237
    
    * Returning more data
    
    * Removed some data from response and made Access Type value more granular
    
    * Clean-up
    
    * More clean-up
    
    * More accurate terminology
    
    * Filter out enrollees that haven't got a signed TOA
    
    * Added API documentation
    
    * Make spelling consistent with UI
    
    Co-authored-by: Alan Leung <Alan.Leung@nttdata.com>

---
+++ prime-dotnet-webapi/Models/Api/EnrolleeCertDto.cs
@@ -0,0 +1,13 @@
+namespace Prime.Models.Api
+{
+    public class EnrolleeCertDto
+    {
+        /// <summary>
+        /// Also known as College Prefix
+        /// </summary>
+        public string PractRefId { get; set; }
+
+        public string CollegeLicenceNumber { get; set; }
+        public string PharmaNetId { get; set; }
+    }
+}
