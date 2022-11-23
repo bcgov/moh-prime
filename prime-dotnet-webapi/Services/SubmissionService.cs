c43dd0134c39bfe351e30dd0d9b891aaed200dd3
Jack Wong <108699279+bergomi02@users.noreply.github.com>
2022-11-23 08:37:28 -0800

    add comment

--- prime-dotnet-webapi/Services/SubmissionService.cs
+++ prime-dotnet-webapi/Services/SubmissionService.cs
@@ -305,6 +305,7 @@ namespace Prime.Services
                 var html = await _razorConverterService.RenderTemplateToStringAsync(RazorTemplates.Agreements.PdfNoSignature, agreement);
                 var pdfbinary = _pdfService.Generate(html);
                 var filename = "Terms-Of-Access.pdf";
+                //TODO: currently storing TOA in signed_org_agreements for now. it should be changed to TOA specific folder
                 var documentGuid = await _documentManagerClient.SendFileAsync(new System.IO.MemoryStream(pdfbinary), filename, DestinationFolders.SignedOrgAgreements);
 
                 var agreementDocument = await _agreementService.AddSignedAgreementDocumentAsync(agreement.Id, documentGuid, filename);
