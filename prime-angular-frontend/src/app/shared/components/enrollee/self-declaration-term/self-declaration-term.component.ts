b8742c2ebaaaab37f9cf259d8ab9d4389a3accd1
Jack Wong <108699279+bergomi02@users.noreply.github.com>
2022-12-13 14:40:11 -0800

    self declaration term typo fix and move content to a single comonent

---
+++ prime-angular-frontend/src/app/shared/components/enrollee/self-declaration-term/self-declaration-term.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-self-declaration-term',
+  templateUrl: './self-declaration-term.component.html',
+  styleUrls: ['./self-declaration-term.component.scss']
+})
+export class SelfDeclarationTermComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
