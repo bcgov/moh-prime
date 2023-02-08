59838a89b26f5a9f218bea32c6cc1af0b5534a86
Celina <yousiefc@pm.me>
2023-02-08 15:43:05 -0800

    AllNursesHaveParmanetId migration

---
+++ prime-dotnet-webapi/Migrations/20230208233813_AllNursesHavePharmanetId.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Prime.Migrations
+{
+    public partial class AllNursesHavePharmanetId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CHK_Agreement_EitherPartyOrEnrollee",
+                table: "Agreement");
+
+            migrationBuilder.InsertData(
+                table: "LicenseDetail",
+                columns: new[] { "Id", "AllowRequestRemoteAccess", "CreatedTimeStamp", "CreatedUserId", "EffectiveDate", "LicenseCode", "LicensedToProvideCare", "Manual", "NamedInImReg", "NonPrescribingPrefix", "Prefix", "PrescriberIdType", "UpdatedTimeStamp", "UpdatedUserId", "Validate" },
+                values: new object[,]
+                {
+                    { 203, false, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2023, 2, 2, 8, 0, 0, 0, DateTimeKind.Utc), 32, true, false, true, "RX", "R9", 2, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), true },
+                    { 204, false, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2023, 2, 2, 8, 0, 0, 0, DateTimeKind.Utc), 33, true, false, false, "RX", "R9", 2, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), false },
+                    { 205, false, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2023, 2, 2, 8, 0, 0, 0, DateTimeKind.Utc), 34, false, true, true, "RX", "R9", 2, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), false },
+                    { 206, false, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2023, 2, 2, 8, 0, 0, 0, DateTimeKind.Utc), 35, true, true, true, "RX", "R9", 2, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), false },
+                    { 207, false, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2023, 2, 2, 8, 0, 0, 0, DateTimeKind.Utc), 36, true, true, false, "RX", "R9", 2, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), false },
+                    { 208, false, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2023, 2, 2, 8, 0, 0, 0, DateTimeKind.Utc), 37, false, false, true, "RX", "R9", 2, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), false },
+                    { 209, false, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2023, 2, 2, 8, 0, 0, 0, DateTimeKind.Utc), 39, true, false, true, "RX", "R9", 2, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), true },
+                    { 210, false, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2023, 2, 2, 8, 0, 0, 0, DateTimeKind.Utc), 40, true, false, false, "RX", "R9", 2, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), false },
+                    { 211, false, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2023, 2, 2, 8, 0, 0, 0, DateTimeKind.Utc), 43, false, true, true, "YX", "Y9", 2, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), false },
+                    { 212, false, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2023, 2, 2, 8, 0, 0, 0, DateTimeKind.Utc), 46, true, false, false, "YX", "Y9", 2, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), false },
+                    { 213, false, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2023, 2, 2, 8, 0, 0, 0, DateTimeKind.Utc), 49, false, true, true, "NX", "96", 2, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), false },
+                    { 214, false, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2023, 2, 2, 8, 0, 0, 0, DateTimeKind.Utc), 52, true, false, true, null, "L9", 2, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), false },
+                    { 215, false, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2023, 2, 2, 8, 0, 0, 0, DateTimeKind.Utc), 53, true, false, false, null, "L9", 2, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), false },
+                    { 216, false, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2023, 2, 2, 8, 0, 0, 0, DateTimeKind.Utc), 54, false, true, true, null, "L9", 2, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), false },
+                    { 217, false, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2023, 2, 2, 8, 0, 0, 0, DateTimeKind.Utc), 55, true, false, true, null, "L9", 2, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), false },
+                    { 218, false, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), new DateTime(2023, 2, 2, 8, 0, 0, 0, DateTimeKind.Utc), 63, false, true, true, null, "98", 2, new DateTimeOffset(new DateTime(2019, 9, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -7, 0, 0, 0)), new Guid("00000000-0000-0000-0000-000000000000"), false }
+                });
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CHK_Agreement_EitherPartyOrEnrollee",
+                table: "Agreement",
+                sql: "( CASE WHEN \"EnrolleeId\" IS NULL THEN 0 ELSE 1 END\n                     + CASE WHEN \"PartyId\" IS NULL THEN 0 ELSE 1 END) = 1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CHK_Agreement_EitherPartyOrEnrollee",
+                table: "Agreement");
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 203);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 204);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 205);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 206);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 207);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 208);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 209);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 210);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 211);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 212);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 213);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 214);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 215);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 216);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 217);
+
+            migrationBuilder.DeleteData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 218);
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CHK_Agreement_EitherPartyOrEnrollee",
+                table: "Agreement",
+                sql: "( CASE WHEN \"EnrolleeId\" IS NULL THEN 0 ELSE 1 END\r\n                     + CASE WHEN \"PartyId\" IS NULL THEN 0 ELSE 1 END) = 1");
+        }
+    }
+}
