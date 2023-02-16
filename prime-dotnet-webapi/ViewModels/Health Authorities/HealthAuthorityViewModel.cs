1af0689be70277e49964cbd000d88ac4002f4953
Alan Leung <alan.leung57@gmail.com>
2023-02-15 22:02:32 -0700

    Reinstate property so admin pages aren't broken

--- prime-dotnet-webapi/ViewModels/Health Authorities/HealthAuthorityViewModel.cs
+++ prime-dotnet-webapi/ViewModels/Health Authorities/HealthAuthorityViewModel.cs
@@ -9,6 +9,10 @@ namespace Prime.ViewModels.HealthAuthorities
         public int Id { get; set; }
         public string Name { get; set; }
         public IEnumerable<HealthAuthorityCareTypeViewModel> CareTypes { get; set; }
+        /// <summary>
+        /// All vendors used at a Health Authority, not filtered by a Care Type
+        /// </summary>
+        public IEnumerable<HealthAuthorityVendorViewModel> Vendors { get; set; }
         public PrivacyOfficeViewModel PrivacyOffice { get; set; }
         public IEnumerable<TechnicalSupportContactViewModel> TechnicalSupports { get; set; }
         public IEnumerable<HealthAuthorityContactViewModel> PharmanetAdministrators { get; set; }
