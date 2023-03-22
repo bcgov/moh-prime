5e923f8e6a72d20edfc930bdd3caf668f1d00758
Alan Leung <alan.leung57@gmail.com>
2023-03-21 20:07:54 -0600

    Improve comments

--- prime-angular-frontend/src/app/modules/auth/shared/models/bcsc-user.model.ts
+++ prime-angular-frontend/src/app/modules/auth/shared/models/bcsc-user.model.ts
@@ -2,8 +2,8 @@ import { Address } from '@lib/models/address.model';
 import { User } from './user.model';
 
 export interface BcscUser extends User {
-  hpdid: string; // BCSC ID
-  userId: string; // Keycloak identifier
+  hpdid: string; // BCSC GUID, e.g. gtcochh2vajdtodkby27kspv554dn4is
+  userId: string; // Keycloak identifier, e.g. 0edecf21-1ec1-4e2a-9878-2fddd7c536f1
   username: string; // e.g. gtcochh2vajdtodkby27kspv554dn4is@bcsc
   givenNames: string;
   dateOfBirth: string;
