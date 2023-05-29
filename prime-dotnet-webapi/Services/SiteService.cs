a251a947cc96be2089f594bbb36e45098db1274f
Jack Wong <108699279+bergomi02@users.noreply.github.com>
2023-05-29 10:45:33 -0700

    Update logic
    matching license number to practitioner ID for BCCNM

--- prime-dotnet-webapi/Services/SiteService.cs
+++ prime-dotnet-webapi/Services/SiteService.cs
@@ -250,8 +250,10 @@ namespace Prime.Services
             var matchesAnyCert = PredicateBuilder.New<RemoteUserCertification>();
             foreach (var searchedCert in certs)
             {
+                // For BCCNM (college code = 3), matching license number to practitioner ID.
                 matchesAnyCert.Or(ruc => ruc.CollegeCode == searchedCert.CollegeCode &&
-                    (ruc.LicenseNumber == searchedCert.LicenceNumber || ruc.LicenseNumber == searchedCert.PractitionerId));
+                    ((ruc.LicenseNumber == searchedCert.LicenceNumber && searchedCert.CollegeCode != 3) ||
+                    (ruc.LicenseNumber == searchedCert.PractitionerId && searchedCert.CollegeCode == 3)));
             }
 
             IEnumerable<RemoteAccessSearchDto> searchResults = await _context.RemoteUserCertifications
