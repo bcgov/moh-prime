0d043d2ccd972647a541f6b87b62acffb66f9f14
Alan Leung <alan.leung57@gmail.com>
2023-07-28 18:29:51 -0700

    Implement #2 in PRIME-2531

---
+++ prime-dotnet-webapi/Migrations/20230729011646_ChangePharmacyVendors.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Prime.Migrations
+{
+    public partial class ChangePharmacyVendors : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "VendorLookup",
+                keyColumn: "Code",
+                keyValue: 11);
+
+            migrationBuilder.UpdateData(
+                table: "VendorLookup",
+                keyColumn: "Code",
+                keyValue: 6,
+                column: "Name",
+                value: "TELUS Health - Kroll");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "VendorLookup",
+                keyColumn: "Code",
+                keyValue: 6,
+                column: "Name",
+                value: "TELUS Health");
+
+            migrationBuilder.InsertData(
+                table: "VendorLookup",
+                columns: new[] { "Code", "CareSettingCode", "Email", "Name" },
+                values: new object[] { 11, 3, "", "BDM (Hospital pharmacy only)" });
+        }
+    }
+}
