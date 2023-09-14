3e6da6f392547f387e488de63c3f3b443132a53e
Alan Leung <alan.leung57@gmail.com>
2023-09-14 14:32:23 -0600

    Highlight to end user

--- prime-angular-frontend/src/app/shared/services/collection-notice.service.ts
+++ prime-angular-frontend/src/app/shared/services/collection-notice.service.ts
@@ -19,11 +19,15 @@ export class CollectionNoticeService {
     return `<p>
         The personal information you provide to the PRIME application is collected by the British Columbia Ministry of
         Health under the authority of s. 26(a) and 26(c) of the Freedom of Information and Protection of Privacy Act
-        (FOIPPA) and s. 22(1)(b) of the Pharmaceutical Services Act for the purpose of managing your access to, and use of,
-        PharmaNet. If you have any questions about the collection or use of this information, contact
+        (FOIPPA) and s. 22(1)(b) of the Pharmaceutical Services Act <b>for the purpose of managing your access to, and use of,
+        PharmaNet.*</b>&nbsp;&nbsp;If you have any questions about the collection or use of this information, contact
       </p>
       <p>
         Director, Information and PharmaNet Innovation at <a href="mailto:${this.config.prime.supportEmail}">${this.config.prime.supportEmail}</a>.
-      </p>`;
+      </p>
+      <p>
+        <small>* Added 14 Sept 2023</small>
+      </p>
+      `;
   }
 }
