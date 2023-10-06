62f5a19f343d67d340e1af90939e7c5fdf15ad81
Jack Wong <108699279+bergomi02@users.noreply.github.com>
2023-10-06 09:56:42 -0700

    fix ng test

--- prime-angular-frontend/src/app/shared/components/dialogs/content/change-vendor-note/change-vendor-note.component.spec.ts
+++ prime-angular-frontend/src/app/shared/components/dialogs/content/change-vendor-note/change-vendor-note.component.spec.ts
@@ -3,13 +3,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { KeycloakService } from 'keycloak-angular';
 
 import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
 import { NgxMaterialModule } from '@lib/modules/ngx-material/ngx-material.module';
 import { ChangeVendorNoteComponent } from './change-vendor-note.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-describe('NoteComponent', () => {
+describe('ChangeVendorNoteComponent', () => {
   let component: ChangeVendorNoteComponent;
   let fixture: ComponentFixture<ChangeVendorNoteComponent>;
 
@@ -22,6 +23,7 @@ describe('NoteComponent', () => {
         BrowserAnimationsModule
       ],
       providers: [
+        KeycloakService,
         {
           provide: APP_CONFIG,
           useValue: APP_DI_CONFIG
@@ -34,7 +36,14 @@ describe('NoteComponent', () => {
         },
         {
           provide: MAT_DIALOG_DATA,
-          useValue: {}
+          useValue: {
+            data: {
+              siteId: 1,
+              title: "testing",
+              vendorChangeText: "from a to b",
+              vendorCode: 1,
+            }
+          }
         },
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
