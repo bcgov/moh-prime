f35751787d9e8e7d6cc76aa25118680a36d4c529
Jack Wong <108699279+bergomi02@users.noreply.github.com>
2024-02-22 16:37:39 -0800

    fix unit test

--- prime-angular-frontend/src/app/shared/components/dialogs/content/transfer-ha-site/transfer-ha-site.component.spec.ts
+++ prime-angular-frontend/src/app/shared/components/dialogs/content/transfer-ha-site/transfer-ha-site.component.spec.ts
@@ -9,6 +9,7 @@ import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
 import { NgxMaterialModule } from '@lib/modules/ngx-material/ngx-material.module';
 import { TransferHASiteComponent } from './transfer-ha-site.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CapitalizePipe } from '@shared/pipes/capitalize.pipe';
 
 describe('TransferHASiteComponent', () => {
   let component: TransferHASiteComponent;
@@ -23,6 +24,7 @@ describe('TransferHASiteComponent', () => {
         BrowserAnimationsModule
       ],
       providers: [
+        CapitalizePipe,
         KeycloakService,
         {
           provide: APP_CONFIG,
