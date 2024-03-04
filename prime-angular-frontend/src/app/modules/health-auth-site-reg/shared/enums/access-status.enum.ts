0f94cd417782720d63826646f8fab1223d94a8fb
Jack Wong <108699279+bergomi02@users.noreply.github.com>
2024-03-04 08:43:55 -0800

    add comment as requested

--- prime-angular-frontend/src/app/modules/health-auth-site-reg/shared/enums/access-status.enum.ts
+++ prime-angular-frontend/src/app/modules/health-auth-site-reg/shared/enums/access-status.enum.ts
@@ -2,6 +2,7 @@ export enum AccessStatusEnum {
   UNDER_REVIEW = 1,
   APPROVED = 2,
   ACTIVE = 3,
+  //adding LOCKED to reserve the number value, it is not used in Site Registration
   LOCKED = 4,
   DECLINED = 5,
   DISABLED = 6,
