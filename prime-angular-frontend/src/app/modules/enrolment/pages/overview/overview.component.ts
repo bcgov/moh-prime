bc60819f17c272920f0e4b7d81d0fa11de70f237
Jack Wong <108699279+bergomi02@users.noreply.github.com>
2024-04-04 13:41:36 -0700

    PRIME-2685 can't enter new obo job site for new HA (#2467)
    * initial commit
    
    * more bug fix
    
    * fix more bug and add new warning text
    
    * fix unit tests
    
    * fix PR issue
    
    * more bug fix
    
    * add comment
    
    * fix pr issue
    
    * fix PR issues
    
    * fix PR issue

--- prime-angular-frontend/src/app/modules/enrolment/pages/overview/overview.component.ts
+++ prime-angular-frontend/src/app/modules/enrolment/pages/overview/overview.component.ts
@@ -231,6 +231,7 @@ export class OverviewComponent extends BaseEnrolmentPage implements OnInit {
       deviceProvider: enrolment.careSettings.some((careSetting) => careSetting.careSettingCode === CareSettingEnum.DEVICE_PROVIDER)
         && (!enrolment.enrolleeDeviceProviders || enrolment.enrolleeDeviceProviders.length === 0),
       missingHAOboSite: enrolment.oboSites?.length && enrolment.oboSites.some(s => s.careSettingCode == CareSettingEnum.HEALTH_AUTHORITY && s.healthAuthorityCode === null),
+      missingOboSite: this.isMissingOboSite(enrolment),
       missingPharmaNetId: this.isMissingPharmaNetId(enrolment.certifications),
       missingHealthAuthorityCareSetting: enrolment.careSettings.some(cs => cs.careSettingCode === CareSettingEnum.HEALTH_AUTHORITY)
         && !enrolment.enrolleeHealthAuthorities?.some(ha => ha.healthAuthorityCode),
@@ -249,5 +250,24 @@ export class OverviewComponent extends BaseEnrolmentPage implements OnInit {
       }
       else { return false; }
     });
+  }
+
+  private isMissingOboSite(enrolment: Enrolment): boolean {
+    if (!enrolment.careSettings.some((careSetting) => careSetting.careSettingCode === CareSettingEnum.DEVICE_PROVIDER)) {
+      if (!enrolment.certifications?.length) {
+        let missingOboJob = false;
+        enrolment.careSettings.forEach(cs => {
+          if (cs.careSettingCode === CareSettingEnum.HEALTH_AUTHORITY) {
+            enrolment.enrolleeHealthAuthorities.forEach(ha => {
+              missingOboJob = missingOboJob || !enrolment.oboSites.some(s => ha.healthAuthorityCode === s.healthAuthorityCode);
+            });
+          } else {
+            missingOboJob = missingOboJob || !enrolment.oboSites.some(s => s.careSettingCode === cs.careSettingCode);
+          }
+        })
+        if (missingOboJob) return true;
+      }
+    }
+    return false;
   }
 }
