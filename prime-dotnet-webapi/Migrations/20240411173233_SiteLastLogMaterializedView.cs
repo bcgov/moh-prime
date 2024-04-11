a012928b84b833f4b3e78572d579eb29bba0876e
Jack Wong <108699279+bergomi02@users.noreply.github.com>
2024-04-11 10:47:47 -0700

    add materialized view

---
+++ prime-dotnet-webapi/Migrations/20240411173233_SiteLastLogMaterializedView.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Prime.Migrations
+{
+    public partial class SiteLastLogMaterializedView : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sql = new StringBuilder();
+            sql.AppendLine(" CREATE MATERIALIZED VIEW SiteLastLog AS");
+            sql.AppendLine(" select \"PharmacyId\", max(\"TxDateTime\") as \"LastLogDate\"");
+            sql.AppendLine(" from \"PharmanetTransactionLog\" ptl");
+            sql.AppendLine(" group by \"PharmacyId\"");
+            migrationBuilder.Sql(sql.ToString());
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP MATERIALIZED VIEW SiteLastLog;");
+        }
+    }
+}
