48c073118ee64c28fb74cecf0646d453170750e0
Alan Leung <alan.leung57@gmail.com>
2024-06-12 10:28:35 -0600

    Roles logged

--- prime-dotnet-webapi/Configuration/Auth/AuthenticationSetup.cs
+++ prime-dotnet-webapi/Configuration/Auth/AuthenticationSetup.cs
@@ -64,11 +64,11 @@ namespace Prime.Configuration.Auth
 
                 FlattenRealmAccessRoles(identity);
 
-                // if (context.Request.Path.ToString().Contains("gpid-detail"))
-                // {
-                JwtPayload payload = ((JwtSecurityToken)context.SecurityToken).Payload;
-                Log.Logger.Debug($"Token for gpid-detail:  Issuer: {payload.Iss}, Authorized Party: {payload.Azp}, Audiences: [{string.Join(",", payload.Aud.ToArray())}], Expires at: {payload.Expiration}, resource_access: {identity.Claims.SingleOrDefault(claim => claim.Type == Claims.ResourceAccess)?.Value}");
-                // }
+                if (context.Request.Path.ToString().Contains("gpid-detail"))
+                {
+                    JwtPayload payload = ((JwtSecurityToken)context.SecurityToken).Payload;
+                    Log.Logger.Debug($"Token for gpid-detail:  Issuer: {payload.Iss}, Authorized Party: {payload.Azp}, Audiences: [{string.Join(",", payload.Aud.ToArray())}], Expires at: {payload.Expiration}, resource_access: {identity.Claims.SingleOrDefault(claim => claim.Type == Claims.ResourceAccess)?.Value}");
+                }
             }
             return Task.CompletedTask;
         }
