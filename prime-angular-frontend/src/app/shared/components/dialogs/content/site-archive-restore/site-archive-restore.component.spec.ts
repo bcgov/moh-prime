9bedb3d36870cb709ba63883f36b92bbfc1a82ed
Jack Wong <108699279+bergomi02@users.noreply.github.com>
2024-10-22 14:52:52 -0700

    fix issue

--- prime-angular-frontend/src/app/shared/components/dialogs/content/site-archive-restore/site-archive-restore.component.spec.ts
+++ prime-angular-frontend/src/app/shared/components/dialogs/content/site-archive-restore/site-archive-restore.component.spec.ts
@@ -1,6 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { RequiredNoteComponent } from './required-note.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
