f05203faf386046a171de72b29fba80f6f70e9dd
Alan Leung <alan.leung57@gmail.com>
2025-02-13 19:17:41 -0700

    PRIME-2867 Use "promote" API to address CHES unreliability (#2675)
    * Initial commit
    
    * Needed for OpenShift environment
    
    * Change order to avoid HTTP 409 from CHES

--- prime-dotnet-webapi/Configuration/PrimeConfigurationObjects.cs
+++ prime-dotnet-webapi/Configuration/PrimeConfigurationObjects.cs
@@ -59,6 +59,7 @@ namespace Prime.Configuration.Internal
         public string ClientId { get; set; }
         public string ClientSecret { get; set; }
         public string TokenUrl { get; set; }
+        public int RetryWithinHours { get; set; }
     }
 
     /// <summary>
