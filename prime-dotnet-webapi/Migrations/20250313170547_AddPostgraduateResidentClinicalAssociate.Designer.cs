8fa43236dbdd3ae40c800ee68243dfd2a17d3326
Jack Wong <108699279+bergomi02@users.noreply.github.com>
2025-03-13 10:29:13 -0700

    initial commit

--- prime-dotnet-webapi/Migrations/20250313170547_AddPostgraduateResidentClinicalAssociate.Designer.cs
+++ prime-dotnet-webapi/Migrations/20250313170547_AddPostgraduateResidentClinicalAssociate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Prime;
@@ -11,9 +12,11 @@ using Prime;
 namespace Prime.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250313170547_AddPostgraduateResidentClinicalAssociate")]
+    partial class AddPostgraduateResidentClinicalAssociate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
