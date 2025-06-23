4d1dbbb2d15f5dfb36a92d9f8c2d7eee777a8bb9
Jack Wong <108699279+bergomi02@users.noreply.github.com>
2025-06-23 15:34:13 -0700

    add new table PredecessorSiteToSuccessorSite (#2731)

--- prime-dotnet-webapi/Migrations/20250623210328_PredecessorSiteToSuccessorSite.Designer.cs
+++ prime-dotnet-webapi/Migrations/20250623210328_PredecessorSiteToSuccessorSite.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Prime;
@@ -11,9 +12,11 @@ using Prime;
 namespace Prime.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250623210328_PredecessorSiteToSuccessorSite")]
+    partial class PredecessorSiteToSuccessorSite
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
