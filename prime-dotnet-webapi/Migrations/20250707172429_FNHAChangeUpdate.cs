b3c07eedb894139ba78fde24474b5cf492f0959b
Jack Wong <108699279+bergomi02@users.noreply.github.com>
2025-07-07 10:25:30 -0700

    fix effective date

---
+++ prime-dotnet-webapi/Migrations/20250707172429_FNHAChangeUpdate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Prime.Migrations
+{
+    /// <inheritdoc />
+    public partial class FNHAChangeUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 377,
+                column: "EffectiveDate",
+                value: new DateTime(2025, 7, 1, 8, 0, 0, 0, DateTimeKind.Utc));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "LicenseDetail",
+                keyColumn: "Id",
+                keyValue: 377,
+                column: "EffectiveDate",
+                value: new DateTime(2023, 8, 28, 8, 0, 0, 0, DateTimeKind.Utc));
+        }
+    }
+}
