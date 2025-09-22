7f6cebab55c4d06e6bd3147beb8d82a362ddbd8d
Jack Wong <108699279+bergomi02@users.noreply.github.com>
2025-09-22 16:54:57 -0700

    fix unit test

--- prime-angular-frontend/src/app/shared/components/dialogs/content/link-site/link-site.component.spec.ts
+++ prime-angular-frontend/src/app/shared/components/dialogs/content/link-site/link-site.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LinkSiteComponent } from './link-site.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { APP_CONFIG, APP_DI_CONFIG } from 'app/app-config.module';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HttpClientModule } from '@angular/common/http';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('LinkSiteComponent', () => {
   let component: LinkSiteComponent;
@@ -8,8 +15,35 @@ describe('LinkSiteComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [LinkSiteComponent]
-    });
+      imports: [
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        MatSnackBarModule,
+        HttpClientModule
+      ],
+      declarations: [LinkSiteComponent],
+      providers: [
+        {
+          provide: APP_CONFIG,
+          useValue: APP_DI_CONFIG
+        },
+        {
+          provide: MatDialogRef,
+          useValue: {
+            close: (dialogResult: any) => { },
+            updateSize: () => { }
+          }
+        },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {
+            data: {
+            }
+          }
+        },
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    }).compileComponents();
     fixture = TestBed.createComponent(LinkSiteComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
