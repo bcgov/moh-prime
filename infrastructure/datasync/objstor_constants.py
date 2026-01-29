626607c59c75c97c1377409547c44b04de6381bb
Roya Shourouni <roya.shourouni@gov.bc.ca>
2026-01-29 12:11:32 -0800

    remove security hotspots

--- infrastructure/datasync/objstor_constants.py
+++ infrastructure/datasync/objstor_constants.py
@@ -1,9 +1,10 @@
 import os
 import dotenv
+import logging
 
 envPath = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(envPath):
-    print("loading dot env...")
+    logging.info("loading dot env...")
     dotenv.load_dotenv()
 
 ACCESS_KEY = os.environ["ACCESS_KEY"]
@@ -13,10 +14,3 @@ OBJSTOR_ENDPOINT = os.environ["OBJSTOR_ENDPOINT"]
 
 ADMIN_EMAIL = os.environ["ADMIN_EMAIL"]
 SMTP_SERVER = os.environ["SMTP_SERVER"]
-
-
-def print_constants():
-    print(f"ACCESS_KEY: {ACCESS_KEY}")
-    print(f"SECRET_KEY: {SECRET_KEY}")
-    print(f"S3_BUCKET_NAME: {S3_BUCKET_NAME}")
-    print(f"OBJSTOR_ENDPOINT: {OBJSTOR_ENDPOINT}")
