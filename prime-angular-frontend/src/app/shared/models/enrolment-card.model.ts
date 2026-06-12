6b9b26c27e9337a420f0ba711e7c8216ccfedd64
Jack Wong <108699279+bergomi02@users.noreply.github.com>
2026-06-12 10:02:05 -0700

    OHID-491 Update Enrolments Page to Show Enrolment Submission without TOA (#2859)
    * initial commit
    
    * remove unused code
    
    * add comment

--- prime-angular-frontend/src/app/shared/models/enrolment-card.model.ts
+++ prime-angular-frontend/src/app/shared/models/enrolment-card.model.ts
@@ -1,6 +1,9 @@
 export interface EnrolmentCard {
   agreementId: number;
+  submissionId: number;
+  agreementType: string;
   agreementAcceptedDate: string;
+  enrolmentApprovedDate: string;
   requestedRemoteAccess: boolean;
   submissionCreatedDate: string;
   isCurrent: boolean;
